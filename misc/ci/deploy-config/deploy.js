ce33ac5b806253bdfa2b054d0bb855f6fa9a447f
yuting <yuting@6121f079-4a18-41d3-8c30-8870a322f2c2>
2016-11-14 05:30:27 +0000

    回滚上一条提交
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.6@25651 6121f079-4a18-41d3-8c30-8870a322f2c2

--- misc/ci/deploy-config/deploy.js
+++ misc/ci/deploy-config/deploy.js
@@ -170,8 +170,7 @@ function deployClster( mode )
                         sharingbreak:30000,
                         diagnum:30,
                         optimeout:60000,
-                        fap:fapValue,
-                        maxpool:50
+                        fap:fapValue
                       };
          dataRG.createNode( hostList[i], dataPort, 
                             databaseDir+"/data/"+dataPort,
