463779d2ec5389382683ab50b49ac06d6667e6ec
huangansheng <huangansheng@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-05-03 07:26:13 +0000

    1 fix bug-2403: 添加追踪信息ossSocket::recv 2 fix bug-2404: 修改PD_TRACE_EXIT的流程
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.6@28855 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/rtn/rtnCoordCommands.cpp
+++ SequoiaDB/engine/rtn/rtnCoordCommands.cpp
@@ -645,6 +645,7 @@ namespace engine
       {
          contextID = pContext->contextID() ;
       }
+	  PD_TRACE_EXITRC ( SDB_RTNCOCOM_QUERYONCATALOG, rc );
       return rc ;
    error :
       // make sure to clear context whenever error happened
@@ -654,7 +655,7 @@ namespace engine
          pmdGetKRCB()->getRTNCB()->contextDelete( contextID, cb ) ;
          pContext = NULL ;
       }
-      PD_TRACE_EXIT ( SDB_RTNCOCOM_QUERYONCATALOG ) ;
+      //PD_TRACE_EXIT ( SDB_RTNCOCOM_QUERYONCATALOG ) ;
       goto done ;
    }
 
