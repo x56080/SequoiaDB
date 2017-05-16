0041503ee1667283ec8de94d4d3af28b73b1d56b
chensiqin <chensiqin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-05-16 07:06:39 +0000

    同步公共js
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@29087 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/sdv/js/lib/func.js
+++ testcase_new/sdv/js/lib/func.js
@@ -15,9 +15,10 @@ if ( typeof(CATASVCNAME) == "undefined" ) { CATASVCNAME = "11800"; }
 if ( typeof(COORDHOSTNAME) == "undefined" ) { COORDHOSTNAME = 'localhost'; }
 if ( typeof(UUID) == "undefined" ) { UUID = 1 ; }
 if ( typeof(UUNAME) == "undefined" ) { UUNAME = "ID"+UUID+"NAME" ; }
-if ( typeof(SPAREPORTSTART) == "undefined" ) 	{ SPAREPORTSTART = '26000'; }
-if ( typeof(SPAREPORTSTOP)   == "undefined" ) 	{ SPAREPORTSTOP   = '27000'; }
+if ( typeof(RSRVPORTBEGIN) == "undefined" ) 	{ RSRVPORTBEGIN = '26000'; }
+if ( typeof(RSRVPORTEND)   == "undefined" ) 	{ RSRVPORTEND   = '27000'; }
 if ( typeof(SPAREPORTPATH)   == "undefined" ) 	{ SPAREPORTPATH = "/opt/sequoiadb/database/"; }
+if ( typeof(WORKDIR)   == "undefined" ) 	{ WORKDIR   = "/tmp/jstest"; }
 
 var COMMCSNAME = CHANGEDPREFIX + "_cs" ;
 var COMMCLNAME = CHANGEDPREFIX + "_cl" ;
