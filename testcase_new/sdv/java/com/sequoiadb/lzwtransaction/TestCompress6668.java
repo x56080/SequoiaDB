d1ea0d81ffb505f018098d09f7c757239bf1b648
chensiqin <chensiqin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-05-16 01:24:49 +0000

    添加配置工程用例
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@29068 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/sdv/java/com/sequoiadb/lzwtransaction/TestCompress6668.java
+++ testcase_new/sdv/java/com/sequoiadb/lzwtransaction/TestCompress6668.java
@@ -20,7 +20,7 @@ import com.sequoiadb.base.DBCursor;
 import com.sequoiadb.base.Sequoiadb;
 import com.sequoiadb.exception.BaseException;
 import com.sequoiadb.testcommon.SdbTestBase;
-
+import com.sequoiadb.testcommon.SdbConfTestBase;
 
 /**
  * 1、CL压缩类型为lzw，开启事务，对CL做增删改查数据 
@@ -29,11 +29,17 @@ import com.sequoiadb.testcommon.SdbTestBase;
  * @author chensiqin
  * @Date 2016-12-16
  */
-public class TestCompress6668 extends SdbTestBase {
+public class TestCompress6668 extends SdbConfTestBase {
     private Sequoiadb sdb;
     private CollectionSpace cs;
     private DBCollection cl;
     private String clName = "cl6668";
+    
+    @Override
+    protected void setNodeConf(){
+        dataConf.put("transactionon", true);
+        stdalnConf.put("transactionon", true);
+    }
     
     @BeforeClass
     public void setUp() {
--- testcase_new/sdv/java/com/sequoiadb/lzwtransaction/TestCompress6669.java
+++ testcase_new/sdv/java/com/sequoiadb/lzwtransaction/TestCompress6669.java
@@ -20,6 +20,7 @@ import com.sequoiadb.base.DBCursor;
 import com.sequoiadb.base.Sequoiadb;
 import com.sequoiadb.exception.BaseException;
 import com.sequoiadb.testcommon.SdbTestBase;
+import com.sequoiadb.testcommon.SdbConfTestBase;
 
 
 /**
@@ -29,11 +30,17 @@ import com.sequoiadb.testcommon.SdbTestBase;
  * @author chensiqin
  * @Date 2016-12-16
  */
-public class TestCompress6669 extends SdbTestBase {
+public class TestCompress6669 extends SdbConfTestBase {
     private Sequoiadb sdb;
     private CollectionSpace cs;
     private DBCollection cl;
     private String clName = "cl6669";
+    
+    @Override
+    protected void setNodeConf(){
+        dataConf.put("transactionon", true);
+        stdalnConf.put("transactionon", true);
+    }
     
     @BeforeClass
     public void setUp() {
