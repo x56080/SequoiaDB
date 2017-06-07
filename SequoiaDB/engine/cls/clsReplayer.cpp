92da06ceae87ca20822586e2873c0a8b9f073efe
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-06-07 05:06:07 +0000

    fix bug:SEQUOIADBMAINSTREAM-2496 sdbrestore when log=LOG_TYPE_INVALIDATE_CATA will coredump
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.6@29434 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/cls/clsReplayer.cpp
+++ SequoiaDB/engine/cls/clsReplayer.cpp
@@ -544,16 +544,20 @@ namespace engine
             {
                goto error ;
             }
-            pCatAgent->lock_w() ;
-            if ( ossStrchr( name, '.' ) )
+            /// when sdbrestore, the cata agent is NULL
+            if ( pCatAgent )
             {
-               pCatAgent->clear( name ) ;
+               pCatAgent->lock_w() ;
+               if ( ossStrchr( name, '.' ) )
+               {
+                  pCatAgent->clear( name ) ;
+               }
+               else
+               {
+                  pCatAgent->clearBySpaceName( name ) ;
+               }
+               pCatAgent->release_w() ;
             }
-            else
-            {
-               pCatAgent->clearBySpaceName( name ) ;
-            }
-            pCatAgent->release_w() ;
 
             rc = SDB_OK ;
             break ;
