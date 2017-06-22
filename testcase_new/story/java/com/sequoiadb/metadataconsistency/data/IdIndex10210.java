ca497cd94428ab2c0ff94997d9330894801f889b
huangxiaoni <huangxiaoni@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-06-22 09:14:51 +0000

    增加对cl不存在getCL返回null的判断，该问题在主干2.9版本已解决，跟开发确认分支不合入
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@29799 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/java/com/sequoiadb/metadataconsistency/data/IdIndex10210.java
+++ testcase_new/story/java/com/sequoiadb/metadataconsistency/data/IdIndex10210.java
@@ -93,7 +93,8 @@ public class IdIndex10210 extends SdbTestBase {
 				clDB.dropIdIndex();
 			}catch(BaseException e){
 				int eCode = e.getErrorCode();
-				if( eCode != -248 && eCode != -23){ //-248: Dropping the collection space is in progress
+				if( eCode != -248 && eCode != -23 && eCode != -34 && 
+						!e.getMessage().toString().contains("null")){ //-248: Dropping the collection space is in progress
 					throw e;
 				}
 			}finally{
