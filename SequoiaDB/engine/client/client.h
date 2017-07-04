8806517880d65256f701fae7ffe046ef8f9e0d4b
tanzhaobo <tanzhaobo@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-07-04 11:54:58 +0000

    为listBackup/removeBackup添加options选项的说明。jira2593
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@29934 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/client/client.h
+++ SequoiaDB/engine/client/client.h
@@ -1912,9 +1912,13 @@ SDB_EXPORT INT32 sdbBackupOffline ( sdbConnectionHandle cHandle,
     \param [in] cHandle The connection handle
     \param [in] options Contains configuration infomations for remove backups, list all the backups in the default backup path if null. The "options" contains 3 options as below. All the elements in options are optional. eg: {"GroupName":["RGName1", "RGName2"], "Path":"/opt/sequoiadb/backup", "Name":"backupName"}
 
-        GroupName   : Assign the backups of specifed replica groups to be list
-        Path        : Assign the backups in specifed path to be list, if not assign, use the backup path asigned in the configuration file
-        Name        : Assign the backups with specifed name to be list
+        GroupID     : Specified the group id of the backups, default to list all the backups of all the groups.
+        GroupName   : Specified the group name of the backups, default to list all the backups of all the groups.
+        Path        : Specified the path of the backups, default to use the backup path asigned in the configuration file.
+        Name        : Specified the name of backup, default to list all the backups.
+        IsSubDir    : Specified the "Path" is a subdirectory of the backup path assigned in the configuration file or not, default to be false.
+        Prefix      : Specified the prefix name of the backups, support for using wildcards("%g","%G","%h","%H","%s","%s"),such as: Prefix:"%g_bk_", default to not using wildcards.
+        Detail      : Display the detail of the backups or not, default to be false.
     \param [in] condition The matching rule, return all the documents if null
     \param [in] selector The selective rule, return the whole document if null
     \param [in] orderBy The ordered rule, never sort if null
@@ -1935,9 +1939,13 @@ SDB_EXPORT INT32 sdbListBackup ( sdbConnectionHandle cHandle,
     \param [in] cHandle The connection handle
     \param [in] options Contains configuration infomations for remove backups, remove all the backups in the default backup path if null. The "options" contains 3 options as below. All the elements in options are optional. eg: {"GroupName":["RGName1", "RGName2"], "Path":"/opt/sequoiadb/backup", "Name":"backupName"}
 
-        GroupName   : Assign the backups of specifed replica groups to be remove
-        Path        : Assign the backups in specifed path to be remove, if not assign, use the backup path asigned in the configuration file
-        Name        : Assign the backups with specifed name to be remove
+        GroupID     : Specified the group id of the backups, default to list all the backups of all the groups.
+        GroupName   : Specified the group name of the backups, default to list all the backups of all the groups.
+        Path        : Specified the path of the backups, default to use the backup path asigned in the configuration file.
+        Name        : Specified the name of backup, default to list all the backups.
+        IsSubDir    : Specified the "Path" is a subdirectory of the backup path assigned in the configuration file or not, default to be false.
+        Prefix      : Specified the prefix name of the backups, support for using wildcards("%g","%G","%h","%H","%s","%s"),such as: Prefix:"%g_bk_", default to not using wildcards.
+        Detail      : Display the detail of the backups or not, default to be false.
     \retval SDB_OK Operation Success
     \retval Others Operation Fail
 */
