cff3cc492190fc4bf2b4730a5bd39d505d9d603f
huangansheng <huangansheng@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-07-06 06:20:19 +0000

    SEQUOIADBMAINSTREAM-2404: 修复trace记录标志使用错误问题
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.6@29968 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/oss/ossIO.cpp
+++ SequoiaDB/engine/oss/ossIO.cpp
@@ -2299,6 +2299,7 @@ INT32 ossLockFile ( OSSFILE *pFile, OSS_FILE_LOCK lockType )
    rc = SDB_OK ;
 #endif
 done :
+   PD_TRACE_EXITRC ( SDB_OSSLOCKFILE, rc ) ;
    return rc ;
 error :
    goto done ;
