d5441248b6a41dc387b51faa2a8b87b12c4a1694
jwhe <jwhe@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-07-27 12:23:20 +0000

    SEQUOIADBMAINSTREAM-683 sac在standalone的配置项屏蔽preferedinstance、syncstrategy、weight、maxreplsync
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@30323 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/web/app/controller/Deploy/InstallModule/Sdb/Mod.js
+++ SequoiaDB/web/app/controller/Deploy/InstallModule/Sdb/Mod.js
@@ -85,7 +85,6 @@
          $location.path( '/Deploy/Index' ).search( { 'r': new Date().getTime() } ) ;
          return ;
       }
-
       $scope.stepList = _Deploy.BuildSdbStep( $scope, $location, deployType, $scope['Url']['Action'], 'sequoiadb' ) ;
       if( $scope.stepList['info'].length == 0 )
       {
@@ -1223,11 +1222,29 @@
          SdbRest.OmOperation( data, {
             'success': function( configure ){
                $scope.installConfig = configure[0] ;
-               $scope.Template = configure[0]['Property'] ;
                $scope.NodeList = configure[0]['Config'] ;
                $.each( $scope.NodeList, function( index ){
                   $scope.NodeList[index]['i'] = index ;
                } ) ;
+               //删除单机版不需要的配置项
+               if( $scope.Configure['DeployMod'] == 'standalone' )
+               {
+                  $scope.Template = [] ;
+                  $.each( configure[0]['Property'], function( index, info ){
+                     if( info['Name'] == 'preferedinstance' ||
+                         info['Name'] == 'syncstrategy' ||
+                         info['Name'] == 'weight' ||
+                         info['Name'] == 'maxreplsync' )
+                     {
+                        return true ;
+                     }
+                     $scope.Template.push( info ) ;
+                  } ) ;
+               }
+               else
+               {
+                  $scope.Template = configure[0]['Property'] ;
+               }
                $scope.NodeTable['body'] = $scope.NodeList ;
                if( $scope.Configure['DeployMod'] == 'standalone' )
                {
