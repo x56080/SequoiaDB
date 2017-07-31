f7ff191769b2b6db83f2f6eff372a73e35883927
tanzhaobo <tanzhaobo@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-07-31 04:47:11 +0000

    修改因事务没开启，而导致测试用例错误。
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@30400 6121f079-4a18-41d3-8c30-8870a322f2c2

--- driver/test/java/com/sequoiadb/test/db/SdbSnapshotList.java
+++ driver/test/java/com/sequoiadb/test/db/SdbSnapshotList.java
@@ -95,8 +95,8 @@ public class SdbSnapshotList {
         }
 
         // 9
-        sdb.beginTransaction();
         try {
+            sdb.beginTransaction();
             cl.insert(new BasicBSONObject());
             cursor = sdb.getSnapshot(Sequoiadb.SDB_SNAP_TRANSACTIONS_CURRENT, "", "", "");
             System.out.println("result of SDB_SNAP_TRANSACTIONS_CURRENT is: ");
@@ -109,6 +109,8 @@ public class SdbSnapshotList {
             while(cursor.hasNext()){
                 System.out.println(cursor.getNext());
             }
+        } catch(BaseException e) {
+            Assert.assertTrue(e.getErrorType().equals("SDB_DPS_TRANS_DIABLED"));
         } finally {
             sdb.commit();
         }
@@ -182,8 +184,8 @@ public class SdbSnapshotList {
         }
 
         // 11
-        sdb.beginTransaction();
         try {
+            sdb.beginTransaction();
             BSONObject dump = new BasicBSONObject();
             BSONObject obj = new BasicBSONObject();
             cl.insert(obj);
@@ -198,6 +200,8 @@ public class SdbSnapshotList {
             while(cursor.hasNext()){
                 System.out.println(cursor.getNext());
             }
+        } catch(BaseException e) {
+            Assert.assertTrue(e.getErrorType().equals("SDB_DPS_TRANS_DIABLED"));
         } finally {
             sdb.commit();
         }
