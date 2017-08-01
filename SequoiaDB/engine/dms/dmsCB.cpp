d13f60398a0a2cf251d58de62c24241b16672526
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-08-01 02:28:38 +0000

    opt: 读写锁为写优先抢占，当try_get()元数据失败马上重试会导致抢占写锁的机率很大，因此在try_get()后作短暂sleep
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.6@30451 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/dms/dmsCB.cpp
+++ SequoiaDB/engine/dms/dmsCB.cpp
@@ -379,8 +379,8 @@ namespace engine
       }
       if ( !_mutex.try_get() )
       {
-         ossSleep( 100 ) ;
          _latchVec[suID]->release_w () ;
+         ossSleep( 100 ) ;
          goto retry ;
       }
       isLocked = TRUE ;
