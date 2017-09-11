070adb1efdaaa15c029e6610cba0468b56fd0d08
zhaoyu <zhaoyu@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-09-11 02:53:39 +0000

    调整cl的ReplSize为0，保证切分前的数据主备一致,分支用例同步修改
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@31150 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/reliability/src/com/sequoiadb/split/killnode/KillNodeSplit2768.java
+++ testcase_new/reliability/src/com/sequoiadb/split/killnode/KillNodeSplit2768.java
@@ -63,7 +63,7 @@ public class KillNodeSplit2768 extends SdbTestBase {
             commSdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
             CollectionSpace commCS = commSdb.getCollectionSpace(csName);
             DBCollection cl = commCS.createCollection(clName, (BSONObject) JSON.parse(
-                    "{ShardingKey:{'sk':1},ShardingType:'range',Group:'" + srcGroupName + "'}"));
+                    "{ShardingKey:{'sk':1},ShardingType:'range',Group:'" + srcGroupName + "',ReplSize:0}"));
             // 准备切分的数据
             insertData(cl, 0, 5000);
         }
