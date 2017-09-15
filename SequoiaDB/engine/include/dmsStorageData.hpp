7683cbfd6d168d68ae25ebe3b0a0f59226b41eb7
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-09-15 05:51:54 +0000

    code review: getMBContext will release memory when lock failed
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@31291 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/include/dmsStorageData.hpp
+++ SequoiaDB/engine/include/dmsStorageData.hpp
@@ -1159,7 +1159,12 @@ namespace engine
       (*pContext)->_latch = &_mblock[mbID] ;
       if ( SHARED == lockType || EXCLUSIVE == lockType )
       {
-         return (*pContext)->mbLock( lockType ) ;
+         INT32 rc = (*pContext)->mbLock( lockType ) ;
+         if ( rc )
+         {
+            releaseMBContext( *pContext ) ;
+            return rc ;
+         }
       }
       return SDB_OK ;
    }
