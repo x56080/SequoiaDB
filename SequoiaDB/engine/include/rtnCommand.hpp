1ec29e1ac662575912ae0cdfafd12955df6b71f5
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-09-15 08:26:57 +0000

    fix bug: SEQUOIADBMAINSTREAM-2771 调整 getQueryMeta 采用 Hint : { Collection : xxx, Hint : { xxx } } 的方式
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.6@31301 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/include/rtnCommand.hpp
+++ SequoiaDB/engine/include/rtnCommand.hpp
@@ -673,6 +673,7 @@ namespace engine
          const CHAR           *_selectBuff ;
          const CHAR           *_orderByBuff ;
          BSONObj              _hintObj ;
+         BOOLEAN              _hintExist ;
          INT32                _flags ;
 
    } ;
--- SequoiaDB/engine/rtn/rtnCommand.cpp
+++ SequoiaDB/engine/rtn/rtnCommand.cpp
@@ -1124,6 +1124,7 @@ namespace engine
       _flags = 0 ;
       _numToReturn = -1 ;
       _numToSkip = 0 ;
+      _hintExist = FALSE ;
    }
 
    _rtnGet::~_rtnGet ()
@@ -1159,6 +1160,10 @@ namespace engine
       if ( SDB_FIELD_NOT_EXIST == rc )
       {
          rc = SDB_OK ;
+      }
+      else
+      {
+         _hintExist = TRUE ;
       }
       PD_RC_CHECK( rc, PDERROR, "Failed to get field[%s], rc: %d",
                    FIELD_NAME_HINT, rc ) ;
@@ -1293,8 +1298,9 @@ namespace engine
       BSONObj matcher ( _matcherBuff ) ;
       BSONObj orderBy ( _orderByBuff ) ;
 
-      if ( _hintObj.isEmpty() )
+      if ( !_hintExist )
       {
+         /// compatiable with old version. Old version use selector for hint
          _hintObj = BSONObj( _selectBuff ) ;
       }
 
