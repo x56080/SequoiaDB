5e9ae627d5b524a398a8ce820d463429dbc2f66c
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-09-25 16:04:01 +0000

    fix bug: SEQUOIADBMAINSTREAM-2860 1、当 repl bucket 状态为 CLS_BUCKET_WAIT_ROLLBACK 时， repl sync 线程不允许超时退出； 2、在并发同步状态下不能并发的操作调用接口由 waitEmpty 改为 waitEmptyWithCheck 3、修正在并发同步发生错误时，回滚后导致的日志信息不正确行为（需要回滚事务信息）
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@31542 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/cls/clsReplSession.cpp
+++ SequoiaDB/engine/cls/clsReplSession.cpp
@@ -758,6 +758,8 @@ namespace engine
    {
       INT32 rc = SDB_OK ;
       PD_TRACE_ENTRY ( SDB__CLSDSTREPSN__RLBCK );
+      dpsTransCB *pTransCB = pmdGetKRCB()->getTransCB() ;
+
       const dpsLogRecordHeader *header = (const dpsLogRecordHeader *)log;
       PD_LOG( PDDEBUG, "Session[%s]: Begin to rollback lsn:[%lld, %d]",
               sessionName(), header->_lsn, header->_version ) ;
@@ -770,13 +772,14 @@ namespace engine
       }
 
       // rollback trans info
-      if ( !sdbGetTransCB()->isNeedSyncTrans() )
+      if ( pTransCB && pTransCB->isTransOn() &&
+           !pTransCB->isNeedSyncTrans() )
       {
          dpsLogRecord record ;
          record.load( log ) ;
-         if ( !sdbGetTransCB()->rollbackTransInfoFromLog( record ) )
+         if ( !pTransCB->rollbackTransInfoFromLog( record ) )
          {
-            sdbGetTransCB()->setIsNeedSyncTrans( TRUE ) ;
+            pTransCB->setIsNeedSyncTrans( TRUE ) ;
          }
       }
 
@@ -905,6 +908,7 @@ namespace engine
                                          UINT32 &num )
    {
       INT32 rc = SDB_OK ;
+      INT32 rcTmp = SDB_OK ;
       PD_TRACE_ENTRY ( SDB__CLSDSTREPSN__REPLG ) ;
 
       dpsLogRecordHeader *recordHeader = NULL ;
@@ -1024,12 +1028,13 @@ namespace engine
       if ( _pReplBucket->waitEmptyAndRollback() )
       {
          DPS_LSN expectLSN = _pReplBucket->completeLSN() ;
-         rc = _logger->move( expectLSN.offset, expectLSN.version ) ;
-         if ( rc )
+         rcTmp = _logger->move( expectLSN.offset, expectLSN.version ) ;
+         if ( rcTmp )
          {
             PD_LOG( PDERROR, "Session[%s]: Failed to move lsn to "
                     "[%u, %llu], rc: %d, need to synchronize full data",
-                    sessionName(), expectLSN.version, expectLSN.offset ) ;
+                    sessionName(), expectLSN.version, expectLSN.offset,
+                    rcTmp ) ;
             _fullSync() ;
          }
          else
@@ -1040,13 +1045,13 @@ namespace engine
       }
       else if ( needRollback )
       {
-         rc = _rollback( log ) ;
-         if ( SDB_OK != rc )
+         rcTmp = _rollback( log ) ;
+         if ( rcTmp )
          {
             PD_LOG( PDERROR, "Session[%s]: Failed to rollback[%lld, "
                     "type: %d], rc: %d, need to synchronize full data",
                     sessionName(), recordHeader->_lsn, recordHeader->_type,
-                    rc ) ;
+                    rcTmp ) ;
             _fullSync() ;
          }
       }
--- SequoiaDB/engine/dps/dpsReplicaLogMgr.cpp
+++ SequoiaDB/engine/dps/dpsReplicaLogMgr.cpp
@@ -433,7 +433,7 @@ namespace engine
       _mergeLogs( info.getMergeBlock(), info.getMergeBlock().pageMeta() );
       SHARED_UNLOCK_NODES( info.getMergeBlock().pageMeta() );
 
-      if ( _transCB && !_restoreFlag )
+      if ( _transCB && _transCB->isTransOn() && !_restoreFlag )
       {
          if ( info.getMergeBlock().isRow() )
          {
