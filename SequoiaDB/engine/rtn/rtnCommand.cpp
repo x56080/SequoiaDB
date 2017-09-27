a12259b1d4dac132223c95049e1ecac3412e0e17
yuting <yuting@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-09-27 06:11:04 +0000

    SEQUOIADBMAINSTREAM-2781 独立模式下创建分区表成功
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.6@31595 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/rtn/rtnCommand.cpp
+++ SequoiaDB/engine/rtn/rtnCommand.cpp
@@ -528,6 +528,7 @@ namespace engine
       PD_TRACE_ENTRY ( SDB__RTNCREATECL_INIT ) ;
       BSONObj matcher ( pMatcherBuff ) ;
       BSONElement ele ;
+
       rc = rtnGetStringElement ( matcher, FIELD_NAME_NAME,
                                  &_collectionName ) ;
       if ( rc )
@@ -536,6 +537,24 @@ namespace engine
                   "creation, rc = %d", FIELD_NAME_NAME, rc ) ;
          goto error ;
       }
+
+      // creating partition cl in standalone is meaningless
+      if ( SDB_ROLE_STANDALONE == pmdGetDBRole() )
+      {
+         if ( matcher.hasField( FIELD_NAME_SHARDINGKEY )     ||
+              matcher.hasField( FIELD_NAME_SHARDTYPE )       ||
+              matcher.hasField( FIELD_NAME_ENSURE_SHDINDEX ) ||
+              matcher.hasField( FIELD_NAME_ISMAINCL )        ||
+              matcher.hasField( FIELD_NAME_PARTITION )       ||
+              matcher.hasField( FIELD_NAME_DOMAIN_AUTO_SPLIT ) )
+         {
+            rc = SDB_INVALIDARG ;
+            PD_LOG( PDERROR, "Can't create partition cl in standalone, rc: %d",
+                    rc ) ;
+            goto error ;
+         }
+      }
+
       // ensure sharding key
       rc = rtnGetBooleanElement( matcher, FIELD_NAME_ENSURE_SHDINDEX,
                                  enSureIndex ) ;
@@ -546,6 +565,7 @@ namespace engine
       }
       PD_RC_CHECK( rc, PDERROR, "Field[%s] value is error in obj[%s]",
                    FIELD_NAME_ENSURE_SHDINDEX, matcher.toString().c_str() ) ;
+
       // if we want to create sharding key index, let's do it
       if ( enSureIndex )
       {
@@ -559,6 +579,7 @@ namespace engine
                       FIELD_NAME_SHARDINGKEY,
                       matcher.toString().c_str() ) ;
       }
+
       // check the attribute, we don't care the return code
       rtnGetBooleanElement ( matcher, FIELD_NAME_COMPRESSED,
                              isCompressed ) ;
