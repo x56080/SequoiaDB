01022aac96a8feca9033c913dd18cb706c22ca27
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-10-09 05:14:33 +0000

    fix: dps dump lob remove/update 值错误
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@31704 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/dps/dpsLogRecord.cpp
+++ SequoiaDB/engine/dps/dpsLogRecord.cpp
@@ -1026,7 +1026,7 @@ namespace engine
          {
             len += ossSnprintf( outBuf + len, outSize - len,
                                 " Type   : %s(%d)"OSS_NEWLINE,
-                                 "LOB_REMOVE", LOG_TYPE_LOB_WRITE ) ;
+                                 "LOB_REMOVE", LOG_TYPE_LOB_REMOVE ) ;
 
             dpsLogRecord::iterator itr ;
             itr = this->find( DPS_LOG_PUBLIC_FULLNAME ) ;
@@ -1116,7 +1116,7 @@ namespace engine
          {
             len += ossSnprintf( outBuf + len, outSize - len,
                                 " Type   : %s(%d)"OSS_NEWLINE,
-                                 "LOB_U", LOG_TYPE_LOB_WRITE ) ;
+                                 "LOB_U", LOG_TYPE_LOB_UPDATE ) ;
 
             dpsLogRecord::iterator itr ;
             itr = this->find( DPS_LOG_PUBLIC_FULLNAME ) ;
