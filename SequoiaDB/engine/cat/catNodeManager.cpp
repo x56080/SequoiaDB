5d085b6ba9104cba8e21f049eaca81c5be18ffea
heguoming <heguoming@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-10-11 08:36:59 +0000

    Code review: Fix wrong messages in catNodeManager::getNodeInfo()
    Backport r31768 to sequoiadb_2.6
    
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.6@31770 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/cat/catNodeManager.cpp
+++ SequoiaDB/engine/cat/catNodeManager.cpp
@@ -1690,7 +1690,7 @@ namespace engine
          {
             if ( SDB_DMS_EOC != rc )
             {
-               PD_LOG ( PDERROR, "Failed to get more from colleciton %s, "
+               PD_LOG ( PDERROR, "Failed to get more from collection %s, "
                         "rc = %d", CAT_NODE_INFO_COLLECTION, rc ) ;
             }
             else
@@ -1778,7 +1778,7 @@ namespace engine
                      {
                         rc = SDB_CAT_CORRUPTION ;
                         PD_LOG( PDERROR, "Failed to get the field: %s",
-                                CAT_HOST_FIELD_NAME );
+                                CAT_SERVICE_FIELD_NAME );
                         goto error ;
                      }
                      strShardServiceNameTmp = getShardServiceName(beServiceTmp);
