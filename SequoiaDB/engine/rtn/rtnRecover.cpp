e4968cc112783d50b3f8bd4c060f68b877707949
yangshangde <yangshangde@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-10-17 07:13:57 +0000

    同步问题修改：SEQUOIADBMAINSTREAM-2903 集群掉电extent中出现了记录环境导致 rebuild 无.法退出，重组文件无限扩大
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@31908 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/rtn/rtnRecover.cpp
+++ SequoiaDB/engine/rtn/rtnRecover.cpp
@@ -506,6 +506,8 @@ namespace engine
       dmsRecordRW recordRW ;
       dmsRecordData recordData ;
       dmsCompressorEntry *pEntry = NULL ;
+      std::set<dmsOffset> offsetSet ;
+      std::pair<std::set<dmsOffset>::iterator, BOOLEAN> result ;
 
       pEntry = _pSU->data()->getCompressorEntry( mbContext->mbID() ) ;
       extRW = _pSU->data()->extent2RW( extentID, -1 ) ;
@@ -532,6 +534,18 @@ namespace engine
             break ;
          }
          rid._offset = nextOffset ;
+         // We use a set to find the possible record link list circle in case
+         // of data corruption. If found, ignore the remainning ones in the
+         // list.
+         result = offsetSet.insert( rid._offset ) ;
+         if ( !result.second )
+         {
+            PD_LOG( PDERROR, "Record list circle found at record[%d.%d]. "
+                    "Stop scanning the current extent", rid._extent,
+                    rid._offset ) ;
+            break ;
+         }
+
          recordRW = _pSU->data()->record2RW( rid, mbContext->mbID() ) ;
          recordRW.setNothrow( TRUE ) ;
          pRecord = recordRW.readPtr() ;
