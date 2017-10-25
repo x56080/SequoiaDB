80c9c7753ba6e62693d2c4cf370844898ff90ff3
liweichao <liweichao@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-10-25 05:35:14 +0000

    optimize py3include.py
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@32082 6121f079-4a18-41d3-8c30-8870a322f2c2

--- driver/python/py3include.py
+++ driver/python/py3include.py
@@ -1,34 +1,28 @@
 # find python3 C API include directory
-import sys
-import platform
 import os
+import platform
+import sys
 
-def GuessOS():
-    id = platform.system()
-    if id == 'Linux':
-        return 'linux'
-    elif id == 'Windows' or id == 'Microsoft':
-        return 'win32'
-    else:
-        return None
-
-guess_os = GuessOS()
 
 if sys.version_info[0] != 3:
     raise Exception("not python3")
 
-if guess_os == 'linux':
-        ver = sys.version_info[0]
-        sub = sys.version_info[1]
-        python_include = sys.prefix + '/include/' + ("python%d.%d" % (ver, sub))
+python_include = ""
+python_lib = ""
+
+_os = platform.system()
+if _os == 'Linux':
+    ver = sys.version_info[0]
+    sub = sys.version_info[1]
+    python_include = sys.prefix + '/include/' + ("python%d.%d" % (ver, sub))
+    if not os.path.exists(python_include):
+        python_include += "m"
         if not os.path.exists(python_include):
-            python_include += "m"
-            if not os.path.exists(python_include):
-                raise Exception("can't find python3 include directory")
-        python_lib = sys.prefix + '/lib/' + ("python%d.%d" % (ver, sub))
-elif guess_os == 'win32':
-        python_include = sys.prefix + "/include/"
-        python_lib = sys.prefix + '/libs/'
+            raise Exception("can't find python3 include directory")
+    python_lib = sys.prefix + '/lib/' + ("python%d.%d" % (ver, sub))
+elif _os == 'Windows':
+    python_include = sys.prefix + "/include/"
+    python_lib = sys.prefix + '/libs/'
 
 print(python_include)
 print(python_lib)
