f9c97e2d21ee1a66cde0296f9db120296b58c7ec
liweichao <liweichao@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-10-25 01:06:18 +0000

    fix bug: SEQUOIADBMAINSTREAM-2813 Python API文档接口描述错误
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@32068 6121f079-4a18-41d3-8c30-8870a322f2c2

--- driver/python/pysequoiadb/client.py
+++ driver/python/pysequoiadb/client.py
@@ -30,7 +30,6 @@ from pysequoiadb.collectionspace import collectionspace
 from pysequoiadb.collection import collection
 from pysequoiadb.cursor import cursor
 from pysequoiadb.replicagroup import replicagroup
-from pysequoiadb.datacenter import datacenter
 from pysequoiadb.common import const
 from pysequoiadb.error import (SDBBaseError, SDBTypeError)
 
@@ -1446,22 +1445,3 @@ class client(object):
          raise
 
       return valid
-
-   def get_datacenter(self):
-      """get data center
-
-      Return values:
-         an object of data center 
-      Exceptions:
-         pysequoiadb.error.SDBBaseError
-      """
-      try:
-         dc = datacenter()
-         rc = sdb.sdb_get_datacenter(self._client, cs._dc)
-         pysequoiadb._raise_if_error("Failed to get data center", rc)
-      except SDBBaseError:
-         del dc;
-         dc = None
-         raise
-
-      return dc
