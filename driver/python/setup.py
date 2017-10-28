c777bfb23756616695bf1a37718660657750b666
liweichao <liweichao@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-10-28 04:43:06 +0000

    move decimal.py from pysequoiadb to bson
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@32140 6121f079-4a18-41d3-8c30-8870a322f2c2

--- driver/python/setup.py
+++ driver/python/setup.py
@@ -12,59 +12,60 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-from distutils.core import Extension, setup
-import sys
-import os
 import glob
+import os
 import shutil
+import sys
+from distutils.core import setup
+
 from version import version
 
 if 'win32' == sys.platform:
-   dlls = './pysequoiadb/*.dll'
-   bson = './bson/*.dll'
-   for file in glob.glob(dlls):
-      if file.startswith('lib'):
-         newname = file[3:]
-      newname = file[:-3] + 'pyd'
-      shutil.copy(file, newname)
+    dlls = './pysequoiadb/*.dll'
+    bson = './bson/*.dll'
+    for file in glob.glob(dlls):
+        if file.startswith('lib'):
+            newname = file[3:]
+        newname = file[:-3] + 'pyd'
+        shutil.copy(file, newname)
 
-   for file in glob.glob(bson):
-      if file.startswith('lib'):
-         newname = file[3:]
-      newname = file[:-3] + 'pyd'
-      shutil.copy(file, newname)
+    for file in glob.glob(bson):
+        if file.startswith('lib'):
+            newname = file[3:]
+        newname = file[:-3] + 'pyd'
+        shutil.copy(file, newname)
 
-   libsdb = 'sdb.pyd'
-   libdecimal = 'bsondecimal.pyd'
+    libsdb = 'sdb.pyd'
+    libdecimal = 'bsondecimal.pyd'
 else:
-   libsdb = 'sdb.so'
-   libdecimal = 'bsondecimal.so'
+    libsdb = 'sdb.so'
+    libdecimal = 'bsondecimal.so'
 
 extra_opts = {}
-extra_opts['packages'] = [ 'bson', 'pysequoiadb']
-extra_opts['package_dir']={ 'pysequoiadb':'pysequoiadb', 'bson':'bson'}
-extra_opts['package_data'] = { 'pysequoiadb':['err.prop', libsdb],
-                               'bson':[ 'buffer.h',
-                                        'buffer.c',
-                                        '_cbsonmodule.h',
-                                        '_cbsonmodule.c',
-                                        'encoding_helpers.h',
-                                        'encoding_helpers.c',
-                                        'time64.h',
-                                        'time64.c',
-                                        'time64_config.h',
-                                        'time64_limits.h',
-                                        libdecimal],}
-#extra_opts['ext_modules'] = ext_modules
-setup(name = 'pysequoiadb',
-      version = version,
-      author = 'SequoiaDB Inc.',
-      license = 'Apache License 2',
-      description = 'This is a sequoiadb python driver use adapter package',
-      url = 'http://www.sequoiadb.com',
+extra_opts['packages'] = ['bson', 'pysequoiadb']
+extra_opts['package_dir'] = {'pysequoiadb': 'pysequoiadb', 'bson': 'bson'}
+extra_opts['package_data'] = {'pysequoiadb': [libsdb],
+                              'bson': ['buffer.h',
+                                       'buffer.c',
+                                       '_cbsonmodule.h',
+                                       '_cbsonmodule.c',
+                                       'encoding_helpers.h',
+                                       'encoding_helpers.c',
+                                       'time64.h',
+                                       'time64.c',
+                                       'time64_config.h',
+                                       'time64_limits.h',
+                                       libdecimal],}
+# extra_opts['ext_modules'] = ext_modules
+setup(name='pysequoiadb',
+      version=version,
+      author='SequoiaDB Inc.',
+      license='Apache License 2',
+      description='This is a sequoiadb python driver use adapter package',
+      url='http://www.sequoiadb.com',
       **extra_opts)
 
 if 'win32' == sys.platform:
-   pyds = './pysequoiadb/*.pyd'
-   for file in glob.glob(pyds):
-      os.remove(file)
+    pyds = './pysequoiadb/*.pyd'
+    for file in glob.glob(pyds):
+        os.remove(file)
