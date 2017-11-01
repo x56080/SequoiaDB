f53cdf60b88289fd76fd35c1927bc3b65c2aa9d1
liweichao <liweichao@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-11-01 06:36:05 +0000

    fix bug: SEQUOIADBMAINSTREAM-2756 集合不存在时CollectionSpace.getCollection()返回null
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.6@32175 6121f079-4a18-41d3-8c30-8870a322f2c2

--- driver/java/src/main/java/com/sequoiadb/base/CollectionSpace.java
+++ driver/java/src/main/java/com/sequoiadb/base/CollectionSpace.java
@@ -94,9 +94,7 @@ public class CollectionSpace {
 		if (isCollectionExist(collectionName)) {
 			return new DBCollection(sequoiadb, this, collectionName);
 		} else {
-			// TODO: 
-			//throw new BaseException("SDB_DMS_NOTEXIST", collectionName);
-			return null;
+			throw new BaseException("SDB_DMS_NOTEXIST", collectionName);
 		}
 	}
 
