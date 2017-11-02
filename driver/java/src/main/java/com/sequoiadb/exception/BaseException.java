5199a83a95eaf3f22ac1146f6b737c940c77df42
liweichao <liweichao@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-11-02 01:47:52 +0000

    fix bug: SEQUOIADBMAINSTREAM-2190 错误码不存在时，BaseException.getErrorCode()返回为0
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@32218 6121f079-4a18-41d3-8c30-8870a322f2c2

--- driver/java/src/main/java/com/sequoiadb/exception/BaseException.java
+++ driver/java/src/main/java/com/sequoiadb/exception/BaseException.java
@@ -1,31 +1,32 @@
-/**
- * Copyright (C) 2012 SequoiaDB Inc.
- * <p>
+/*
+ * Copyright 2017 SequoiaDB Inc.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
-package com.sequoiadb.exception;
+*/
 
+package com.sequoiadb.exception;
 
 import java.util.Arrays;
 
 /**
- * @author tanzhaobo
- *
+ * @class BaseException
+ * @brief Base exception of SequoiaDB.
  */
 public class BaseException extends RuntimeException {
 
     private static final long serialVersionUID = -6115487863398926195L;
 
+    private int errcode;
     private SDBError error;
     private String detail;
 
@@ -34,10 +35,11 @@ public class BaseException extends RuntimeException {
      * @param error  The enumeration object of sequoiadb error.
      * @param detail The error detail.
      * @param e      The exception used to build exception chain.
-     * @since v2.8
+     * @since 2.8
      */
     public BaseException(SDBError error, String detail, Throwable e) {
         super(e);
+        this.errcode = error.getErrorCode();
         this.error = error;
         this.detail = detail;
     }
@@ -45,104 +47,101 @@ public class BaseException extends RuntimeException {
     /**
      * @param error  The enumeration object of sequoiadb error.
      * @param detail The error detail.
-     * @since v2.8
+     * @since 2.8
      */
     public BaseException(SDBError error, String detail) {
-        this(error, detail, null);
+        this.errcode = error.getErrorCode();
+        this.error = error;
+        this.detail = detail;
     }
 
     /**
-     * @param error  The enumeration object of sequoiadb error.
-     * @param e      The exception used to build exception chain.
-     * @since v2.8
+     * @param error The enumeration object of sequoiadb error.
+     * @param e     The exception used to build exception chain.
+     * @since 2.8
      */
     public BaseException(SDBError error, Throwable e) {
         this(error, null, e);
     }
 
     /**
-     * @param error  The enumeration object of sequoiadb error.
-     * @since v2.8
+     * @param error The enumeration object of sequoiadb error.
+     * @since 2.8
      */
     public BaseException(SDBError error) {
-        this(error, null, null);
+        this(error, (String) null);
     }
 
 
     /**
      * @param errCode The error code return by engine.
-     * @since v2.8
+     * @since 2.8
      */
     public BaseException(int errCode) {
-        this(SDBError.getSDBError(errCode));
+        this(errCode, (String) null);
     }
 
 
     /**
      * @param errCode The error code return by engine.
      * @param detail  The error detail.
-     * @since v2.8
+     * @since 2.8
      */
     public BaseException(int errCode, String detail) {
-        this(SDBError.getSDBError(errCode), detail, null);
+        this.errcode = errCode;
+        this.error = SDBError.getSDBError(errCode);
+        this.detail = detail;
     }
 
     /**
      * @param errorType The error type.
-     * @deprecated since v2.8
+     * @deprecated
      */
     public BaseException(String errorType, Object... objs) {
-        try {
-            this.error = SDBError.valueOf(errorType);
-        } catch (Exception e) {
-            // nothing to do
-        }
-
-        this.detail = Arrays.toString(objs);
+        this(SDBError.valueOf(errorType), Arrays.toString(objs));
     }
 
     /**
-     *
      * @param errorCode The error code return by engine.
-     * @deprecated since v2.8
+     * @deprecated
      */
     public BaseException(int errorCode, Object... objs) {
-        this(SDBError.getSDBError(errorCode), Arrays.toString(objs));
+        this(errorCode, Arrays.toString(objs));
     }
 
 
     /**
-     * @brief Get the error message.
      * @return The error message.
+     * @brief Get the error message.
      */
     @Override
     public String getMessage() {
-        if (detail != null && detail != "") {
+        if (detail != null && !detail.isEmpty()) {
             if (error != null) {
                 return error.toString() + ", detail: " + detail;
             } else {
-                return detail;
+                return getErrorType() + "(" + errcode + "), detail: " + detail;
             }
         } else if (error != null) {
             return error.toString();
         } else {
-            return "Unknown Error";
+            return getErrorType() + "(" + errcode + ")";
         }
     }
 
     /**
-     * @brief Get the error type.
      * @return The error type.
+     * @brief Get the error type.
      */
     public String getErrorType() {
-        return error != null ? error.getErrorType() : "Unknown Type";
+        return error != null ? error.getErrorType() : "SDB_UNKNOWN";
     }
 
     /**
-     * @brief Get the error code.
      * @return The error code.
+     * @brief Get the error code.
      */
     public int getErrorCode() {
-        return error != null ? error.getErrorCode() : 0;
+        return errcode;
     }
 }
