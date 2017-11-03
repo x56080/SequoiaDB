d237dd4ccbfed6accfc2e9b2663a28bbfc8ef9ce
chensiqin <chensiqin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-11-03 06:16:18 +0000

    bson只有double类型，因此随机函数获取的float值为8位，而在db里面是double 16位
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@32266 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/driver/java/com/sequoiadb/basicoperation/TestQueryAndRemove7089.java
+++ testcase_new/driver/java/com/sequoiadb/basicoperation/TestQueryAndRemove7089.java
@@ -89,16 +89,16 @@ public class TestQueryAndRemove7089 extends SdbTestBase{
                     bson.put("age", random.nextDouble());
                     bson.put("bin", str.getBytes());
                     bson.put("num", random.nextInt(100));
-                    bson.put("weight", random.nextFloat());
+                    bson.put("weight", 0.12);
                 } else if (i%3 == 1) {
                     bson.put("age", random.nextDouble());
                     bson.put("bin", str.getBytes());
                     bson.put("num", random.nextInt(100));
-                    bson.put("weight", random.nextFloat());
+                    bson.put("weight", 0.34);
                     bson.put("name", i + "lisi");
                 } else if (i%3 == 2) {
                     bson.put("num", random.nextInt(100));
-                    bson.put("weight", random.nextFloat());
+                    bson.put("weight", 0.56);
                     bson.put("name", i + "lisi");
                     bson.put("bin", str.getBytes());
                     bson.put("age", random.nextDouble());
