dbb527f5d24f62c866e73899ea4a1052d6e00b87
huangxiaoni <huangxiaoni@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-11-13 07:20:44 +0000

    新增节点，保证删除的节点不是最后一个节点
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@32451 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/java/com/sequoiadb/metadataconsistency/cluster/Node10233.java
+++ testcase_new/story/java/com/sequoiadb/metadataconsistency/cluster/Node10233.java
@@ -19,10 +19,11 @@ import com.sequoiadb.testcommon.SdbTestBase;
 import com.sequoiadb.testcommon.SdbThreadBase;
 
 /**
-* TestLink: seqDB-10233:concurrency[createNode, removeNode]
-* @author xiaoni huang init
-* @Date   2016.10.25
-*/
+ * TestLink: seqDB-10233:concurrency[createNode, removeNode]
+ * 
+ * @author xiaoni huang init
+ * @Date 2016.10.25
+ */
 
 public class Node10233 extends SdbTestBase {
 	private SimpleDateFormat dateFm = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss");
@@ -30,103 +31,105 @@ public class Node10233 extends SdbTestBase {
 	private String rgName = "rg10233";
 	private Random random = new Random();
 	private int msec = 1000;
-	
+
 	@BeforeClass
-	public void setUp(){
-		//start time
-		System.out.println("Begin to run " + getClass().getName() 
-					+ ", begin in: " + dateFm.format(new Date().getTime()));
-		try{
+	public void setUp() {
+		// start time
+		System.out
+				.println("Begin to run " + getClass().getName() + ", begin in: " + dateFm.format(new Date().getTime()));
+		try {
 			sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
-			//judge the mode and group number
-			if(CommLib.isStandAlone(sdb) || CommLib.OneGroupMode(sdb)){
+			// judge the mode and group number
+			if (CommLib.isStandAlone(sdb) || CommLib.OneGroupMode(sdb)) {
 				throw new SkipException("The mode is standlone, or only one group, skip the testCase.");
 			}
 			CommLib.clearGroup(sdb, rgName);
+
 			sdb.createReplicaGroup(rgName);
-		}catch(BaseException e){
+			CommLib.createNode(sdb, rgName, SdbTestBase.reservedPortBegin, SdbTestBase.reservedPortEnd,
+					SdbTestBase.reservedDir);
+			ReplicaGroup rgDB = sdb.getReplicaGroup(rgName);
+			rgDB.start();
+		} catch (BaseException e) {
 			sdb.disconnect();
 			Assert.fail(e.getMessage());
 		}
-		
+
 	}
-	
+
 	@AfterClass
-	public void tearDown(){
-		try{
+	public void tearDown() {
+		try {
 			CommLib.clearGroup(sdb, rgName);
-		}catch(BaseException e){
+		} catch (BaseException e) {
 			Assert.fail(e.getMessage());
-		}finally{
-			System.out.println("End to run " + getClass().getName() 
-						+ ", end in: " + dateFm.format(new Date().getTime()));
+		} finally {
+			System.out
+					.println("End to run " + getClass().getName() + ", end in: " + dateFm.format(new Date().getTime()));
 			sdb.disconnect();
 		}
 	}
-	
+
 	@Test
-	public void test(){
-		
+	public void test() {
 		CreateNode createNode = new CreateNode();
 		createNode.start();
 
 		RemoveNode removeNode = new RemoveNode();
 		CommLib.sleep(random.nextInt(msec));
 		removeNode.start();
-		
-		if( !( createNode.isSuccess() && removeNode.isSuccess() ) ){
+
+		if (!(createNode.isSuccess() && removeNode.isSuccess())) {
 			Assert.fail(createNode.getErrorMsg() + removeNode.getErrorMsg());
 		}
-		
-		//check results
+
+		// check results
 		CommLib.checkRGOfCatalog(rgName);
 	}
 
-	private class CreateNode extends SdbThreadBase{
+	private class CreateNode extends SdbThreadBase {
 		@Override
-		public void exec() throws BaseException{
+		public void exec() throws BaseException {
 			Sequoiadb db = null;
-			try
-			{
+			try {
 				db = new Sequoiadb(SdbTestBase.coordUrl, "", "");
-	
-				CommLib.createNode(db, 
-								   rgName, 
-								   SdbTestBase.reservedPortBegin, 
-								   SdbTestBase.reservedPortEnd, 
-								   SdbTestBase.reservedDir);
+
+				CommLib.createNode(db, rgName, SdbTestBase.reservedPortBegin, SdbTestBase.reservedPortEnd,
+						SdbTestBase.reservedDir);
 				ReplicaGroup rgDB = db.getReplicaGroup(rgName);
 				rgDB.start();
-			}catch(BaseException e){
-				throw e;
-			}finally{
+			} catch (BaseException e) {
+				int eCode = e.getErrorCode();
+				if (eCode != -156) {
+					throw e;
+				}
+			} finally {
 				db.disconnect();
 			}
 		}
 	}
 
-	private class RemoveNode extends SdbThreadBase{
+	private class RemoveNode extends SdbThreadBase {
 		@Override
-		public void exec() throws BaseException{
+		public void exec() throws BaseException {
 			Sequoiadb db = null;
-			try
-			{
+			try {
 				db = new Sequoiadb(SdbTestBase.coordUrl, "", "");
 				ReplicaGroup rgDB = db.getReplicaGroup(rgName);
-				
+
 				Node node = rgDB.getSlave();
 				String hostName = node.getHostName();
 				int svcName = node.getPort();
 				rgDB.removeNode(hostName, svcName, null);
-			}catch(BaseException e){
+			} catch (BaseException e) {
 				int eCode = e.getErrorCode();
-				if( eCode != -155){
+				if (eCode != -155) {
 					throw e;
 				}
-			}finally{
+			} finally {
 				db.disconnect();
 			}
 		}
 	}
-	
+
 }
