5db0449016ab9680d480b11b4b94b7eb1b1c9b23
jwhe <jwhe@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-11-23 02:46:23 +0000

    SEQUOIADBMAINSTREAM-3030 修复SecureSdb默认返回字符串，而不是数组的问题
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.6@32679 6121f079-4a18-41d3-8c30-8870a322f2c2

--- driver/php5/ext/class/class_securesdb.c
+++ driver/php5/ext/class/class_securesdb.c
@@ -29,7 +29,14 @@ PHP_METHOD( SecuresDB, __construct )
    CHAR *pPassword   = NULL ;
    zval *pThisObj    = getThis() ;
    sdbConnectionHandle connection = SDB_INVALID_HANDLE ;
+
+   zend_update_property_long( Z_OBJCE_P( pThisObj ),
+                              pThisObj,
+                              ZEND_STRL( "_return_model" ),
+                              1 TSRMLS_CC ) ;
+
    PHP_SET_ERRNO_OK( TRUE, pThisObj ) ;
+
    if( PHP_GET_PARAMETERS( "|zss",
                            &pAddress,
                            &pUserName,
