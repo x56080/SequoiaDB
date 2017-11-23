25d27f767a3d7b25009acfee60d6f92ebc088924
liuxiaoxuan <liuxiaoxuan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-11-23 11:19:27 +0000

    上传python新增接口用例
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@32711 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/driver/python/clustermanager/test_node_12499_12500.py
@@ -0,0 +1,102 @@
+# @decription: attach/detach data node
+# @testlink:   seqDB-12499/seqDB-12500
+# @interface:  attach_node
+#              detach_node
+# @author:     zhaoyu 2017-9-9
+
+import unittest
+from lib import testlib
+from lib import sdbconfig
+from commlib import *
+from pysequoiadb.error import SDBBaseError
+
+class TestDataNode12499(testlib.SdbTestBase):
+   def setUp(self):
+      testlib.drop_cs(self.db, self.cs_name, ignore_not_exist=True)
+      self.data_rg_name = "data12499"
+
+   def test_data_node_12499(self):
+      # check standalone
+      if testlib.is_standalone():
+         self.skipTest('run mode is standalone')
+      
+      # create data rg
+      data_rg = self.db.create_replica_group(self.data_rg_name)
+      
+      # create SYSSpare
+      spare_rg = self.db.create_replica_group("SYSSpare")
+      
+      # create node 1
+      data_hostname = self.db.get_replica_group_by_name("SYSCatalogGroup").get_master().get_hostname()
+      service_name1 = str(sdbconfig.sdb_config.rsrv_port_begin)
+      data_dbpath1 = sdbconfig.sdb_config.rsrv_node_dir + service_name1
+      data_rg.create_node(data_hostname, service_name1, data_dbpath1)
+
+      # create node 2
+      service_name2 = str(sdbconfig.sdb_config.rsrv_port_begin + 10)
+      data_dbpath2 = sdbconfig.sdb_config.rsrv_node_dir + service_name2
+      config = {"logfilesz": 128}
+      data_rg.create_node(data_hostname, service_name2, data_dbpath2, config)
+      
+      # rg start
+      data_rg.start()
+      
+      check_rg_master( data_rg )
+      
+      rg_master = data_rg.get_master()
+      rg_slave = data_rg.get_slave()
+      master_data_connect_status = check_data_start_status(rg_master)
+      slave_data_connect_status = check_data_start_status(rg_slave)
+      self.assertTrue(master_data_connect_status)
+      self.assertTrue(slave_data_connect_status)
+      
+      #create cs cl
+      cs = self.db.create_collection_space(self.cs_name)
+      cl = cs.create_collection(self.cl_name, {"Group":self.data_rg_name})
+      
+      # detach node
+      data_rg_slave_service = data_rg.get_slave().get_servicename()
+      data_rg.detach_node(data_hostname, data_rg_slave_service, {"KeepData": True})
+      
+      # attach node with config
+      spare_rg.attach_node(data_hostname, data_rg_slave_service, {"KeepData": True})
+      spare_rg.start()
+            
+      # check data
+      cl_full_name = self.cl_name_qualified
+      spare_data = client(data_hostname, data_rg_slave_service)
+      get_full_name = spare_data.get_collection(cl_full_name).get_full_name()
+      self.assertEqual(get_full_name, cl_full_name)
+      
+      # detach node no config
+      spare_rg.detach_node(data_hostname, data_rg_slave_service)
+      
+      #attach node no config
+      data_rg.attach_node(data_hostname, data_rg_slave_service)
+      data = client(data_hostname, data_rg_slave_service)
+      try:
+         get_full_name = data.get_collection(cl_full_name)
+      except SDBBaseError as e:
+         if(-34 != e.code):
+            print(e.detail)
+            self.fail("check_data_fail")
+            
+      #dropcs from catalog
+      self.db.drop_collection_space(self.cs_name)
+      
+      # remove node
+      self.db.remove_replica_group(self.data_rg_name)
+      self.db.remove_replica_group("SYSSpare")
+   
+   def tearDown(self):
+      self.remove_rg(self.data_rg_name, 'tear_down_fail')
+      self.remove_rg('SYSSpare', 'tear_down_fail')
+      self.db.disconnect()
+      
+   def remove_rg(self, rg, msg):
+      try:
+         self.db.remove_replica_group(rg)
+      except SDBBaseError as e:
+         if -154 != e.code and -159 != e.code:
+            print(e.detail)
+            self.fail(msg)
