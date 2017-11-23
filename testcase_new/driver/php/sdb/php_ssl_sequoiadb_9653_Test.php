de6e281d550b86f76ca438d9c89de8f241a210b7
huangxiaoni <huangxiaoni@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-11-23 04:51:33 +0000

    v2.8新增ssl用例
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@32684 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/driver/php/sdb/php_ssl_sequoiadb_9653_Test.php
@@ -0,0 +1,27 @@
+/****************************************************
+@description:      ssl
+@testlink cases:   seqDB-9652
+@modify list:
+        2017-11-21 xiaoni huang init
+****************************************************/
+<?php
+define('Cur_Path', dirname(__FILE__));
+include_once Cur_Path.'/../global.php';
+class sslTest extends PHPUnit_Framework_TestCase
+{
+   protected static $db ;
+
+   public function test_ssl()
+   {
+      $address = globalParameter::getHostName().':'.globalParameter::getCoordPort();
+      self::$db = new Sequoiadb();
+      $err = self::$db -> connect($address, '', '', true);
+      if ( $err['errno'] != -15 )
+      {
+         throw new Exception("failed to connect db");
+      }
+         
+   }
+   
+};
+?>
---
+++ testcase_new/driver/php/ssl/php_ssl_securesdb_9652_Test.php
@@ -0,0 +1,52 @@
+/****************************************************
+@description:      ssl
+@testlink cases:   seqDB-9652
+@modify list:
+        2017-11-21 xiaoni huang init
+****************************************************/
+<?php
+define('Cur_Path', dirname(__FILE__));
+include_once Cur_Path.'/../global.php';
+class sslTest extends PHPUnit_Framework_TestCase
+{
+   protected static $db ;
+   protected static $csName  = 'cs9652_sec';
+   protected static $clName  = 'cl';
+/*
+   public static function setUpBeforeClass()
+   {
+      $address = globalParameter::getHostName().':'.globalParameter::getCoordPort();
+      self::$db = new SecureSdb();
+      $err = self::$db -> connect($address, '', '');
+      if ( $err['errno'] != 0 )
+      {
+         throw new Exception("failed to connect db");
+      }
+   }
+*/
+   public function test_ssl()
+   {/*
+      // create cs
+      self::$db -> createCS( self::$csName, null );
+      $this -> assertEquals( 0, self::$db -> getError()['errno'] );
+      
+      // get cs
+      $csDB = self::$db -> getCS( self::$csName );
+      $this -> assertEquals( 0, self::$db -> getError()['errno'] );
+      
+      // create cl
+      $csDB -> createCL( self::$clName, null );
+      $this -> assertEquals( 0, self::$db -> getError()['errno'] );
+      
+      // drop cs
+      self::$db -> dropCS( self::$csName );
+      $this -> assertEquals( 0, self::$db -> getError()['errno'] );    */  
+   }
+   /*
+   public static function tearDownAfterClass()
+   {
+      $err = self::$db->close();
+   }
+   */
+};
+?>
