ee755b5977010b0e67322da6f42f0673aa83b9d4
liweichao <liweichao@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-11-25 08:34:36 +0000

    fix: SEQUOIADBMAINSTREAM-3037 shard平面会话信息访问时没有确保是在同一线程，导致 map 被破坏
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@32802 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/cls/clsMgr.cpp
+++ SequoiaDB/engine/cls/clsMgr.cpp
@@ -227,7 +227,7 @@ namespace engine
       }
       else if ( _sessionTimerID == timerID )
       {
-         if ( _mapSession.size() <= MAX_SHD_SESSION_CATCH_DEQ_SIZE / 10 )
+         if ( _mapSession.size() <= MAX_SHD_SESSION_CATCH_DEQ_SIZE / 2 )
          {
             goto done ;
          }
