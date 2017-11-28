029628b689afe3e917748d0e7a49aed5cdc55988
tanzhaobo <tanzhaobo@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-11-28 07:17:05 +0000

    SEQUOIADBMAINSTREAM-3048：同步主干接口。
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.6@32851 6121f079-4a18-41d3-8c30-8870a322f2c2

--- driver/test/java/com/sequoiadb/test/db/SdbTest.java
+++ driver/test/java/com/sequoiadb/test/db/SdbTest.java
@@ -65,6 +65,15 @@ public class SdbTest {
 		sdb.dropCollectionSpace(Constants.TEST_CS_NAME_1);
 	}
 
+    @Test
+    public void getHostNameAndPortTest() {
+        String hostName = sdb.getHost();
+        int port = sdb.getPort();
+        assertTrue(hostName != null && !hostName.isEmpty());
+        assertTrue(port != 0);
+//        System.out.println(String.format("%s:%d", hostName, port));
+    }
+	
 	@Test
 	public void sdbisValid(){}
 //	@Test
