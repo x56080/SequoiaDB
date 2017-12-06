98145acf6cde15b5ee9fac26b982e6cb8396b0b3
heguoming <heguoming@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-12-06 00:50:53 +0000

    Fix SEQUOIADBMAINSTREAM-3063 : memory leak in clsCatalogSet::updateCatSet()
    Backport 32932 to sequoiadb_2.6
    
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.6@33000 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/cls/clsCatalogAgent.cpp
+++ SequoiaDB/engine/cls/clsCatalogAgent.cpp
@@ -2094,13 +2094,17 @@ namespace engine
 
       if ( canMergerLeft )
       {
-         pNewItem->_lowBound = iterLeft->second->getLowBound();
+         clsCatalogItem *item = iterLeft->second ;
+         pNewItem->_lowBound = item->getLowBound();
          _mapItems.erase( iterLeft );
+         SAFE_OSS_DELETE( item ) ;
       }
       if ( canMergerRight )
       {
-         pNewItem->_upBound = iterRight->second->getUpBound();
+         clsCatalogItem *item = iterRight->second ;
+         pNewItem->_upBound = item->getUpBound();
          _mapItems.erase( iterRight );
+         SAFE_OSS_DELETE( item ) ;
       }
       rc = _addItem( pNewItem );
       PD_RC_CHECK( rc, PDERROR,
@@ -2124,7 +2128,9 @@ namespace engine
          std::string strSubClName = it->second->getSubClName();
          if ( 0 == strSubClName.compare( subCLName ) )
          {
+            clsCatalogItem *item = it->second ;
             _mapItems.erase( it++ ) ;
+            SAFE_OSS_DELETE( item ) ;
          }
          else
          {
