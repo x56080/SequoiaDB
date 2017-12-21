33c31e8763f1e0bb77c5cef2b0d0ced064bf4b62
liangxuewang <liangxuewang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-12-21 02:19:39 +0000

    同步主干修改
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@33253 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/js/forceSession/commlib.js
+++ testcase_new/story/js/forceSession/commlib.js
@@ -77,22 +77,15 @@ function isSystemEDU( context )
 {
    var nodeName = context.NodeName ;
    var sessionId = context.SessionID ;
-   var cursor = db.snapshot( SDB_SNAP_CONTEXTS, { NodeName: nodeName, SessionID: sessionId } ) ;
-   var contextArr = cursor.next().toObj().Contexts ;
+   var cursor = db.snapshot( SDB_SNAP_SESSIONS, { NodeName: nodeName, SessionID: sessionId } ) ;
+   var eduType = cursor.next().toObj().Type ;
    var systemEduTypes = [ "TCPListener", "RestListener", "ReplReader", "LogWriter",
                           "LogArchiveMgr", "DpsRollback", "ShardReader", "Cluster",
                           "ClusterShard", "ClusterLogNotify", "CatalogMgr",
                           "CatalogNetwork", "CoordNetwork", "CoordMgr", "OMManager",
                           "OMNet", "SyncClockWorker", "PipeListener", "FAPListener",
-                          "DBMonitor", "RtnNetwork", "SignalTest" ] ;
-   for( var i = 0;i < contextArr.length;i++ )
-   {
-      var eduType = contextArr[i].Type ;
-      if( systemEduTypes.indexOf( eduType ) != -1 )
-      {
-         println( "contexts i is system edu type: " + eduType ) ;
-         return true ;
-      }
-   }
-   return false ;
+                          "DBMonitor", "RtnNetwork", "SignalTest", "SeAdapter", "SeIndexerReader",
+                          "SeService" ] ;
+   println( "session: " + nodeName + ":" + sessionId + ", edu type is " + eduType ) ;
+   return systemEduTypes.indexOf( eduType ) !== -1 ;
 }
