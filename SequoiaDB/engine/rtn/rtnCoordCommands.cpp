3c4d869fc4aaa030c26ef7cf6746859e3ff119fc
heguoming <heguoming@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-12-24 14:22:34 +0000

    Revert r33247
    "SEQUOIADBMAINSTREAM-3105 : check command name in rtnCoordCMDOperateOnNode::execute()"
    
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.6@33315 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/rtn/rtnCoordCommands.cpp
+++ SequoiaDB/engine/rtn/rtnCoordCommands.cpp
@@ -4106,11 +4106,7 @@ namespace engine
    done:
       if ( strHostName && svcname )
       {
-         const CHAR * commandName = queryOption._fullName ;
-         PD_AUDIT_COMMAND( AUDIT_SYSTEM,
-                           ( NULL == commandName ?
-                             "" : ( '$' == commandName[0] ?
-                                    commandName + 1 : commandName ) ),
+         PD_AUDIT_COMMAND( AUDIT_SYSTEM, queryOption._fullName + 1,
                            AUDIT_OBJ_NODE, "", rc,
                            "HostName:%s, ServiceName:%s", strHostName,
                            svcname ) ;
