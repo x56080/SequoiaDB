81b17a957ca20cf7c11cb333867f915abce43f4f
luweikang <luweikang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-12-26 05:48:34 +0000

    回归问题单3118,补充自动化用例
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@33366 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/java/com/sequoiadb/split/SplitUtils.java
+++ testcase_new/story/java/com/sequoiadb/split/SplitUtils.java
@@ -1,10 +1,13 @@
 package com.sequoiadb.split;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import org.bson.BSONObject;
+import org.bson.BasicBSONObject;
 import org.bson.types.BasicBSONList;
 import org.bson.util.JSON;
+import org.testng.Assert;
 
 import com.sequoiadb.base.CollectionSpace;
 import com.sequoiadb.base.DBCollection;
@@ -25,6 +28,39 @@ import com.sequoiadb.testcommon.CommLib;
 public class SplitUtils {
 
 	public static final int FLG_INSERT_CONTONDUP = 0x00000001;
+	private static ArrayList<String> groupList ;
+	
+	public static boolean isStandAlone(Sequoiadb sdb){
+		try{
+			sdb.listReplicaGroups();		
+		}catch(BaseException e){
+			if( e.getErrorCode() == -159 ){
+				System.out.printf("run mode is standalone");	 
+				return true;
+			} 	
+		}	
+		return false;
+	}
+	
+	public static boolean OneGroupMode(Sequoiadb sdb){
+		 if(getDataGroups(sdb).size() < 2){
+			System.out.printf("only one group");
+			return true;
+		 }
+		 return false;
+	 }
+	
+	public static ArrayList<String> getDataGroups(Sequoiadb sdb){		 
+		 try{
+			 groupList = sdb.getReplicaGroupNames();
+			 groupList.remove("SYSCatalogGroup");
+			 groupList.remove("SYSCoord");
+			 groupList.remove("SYSSpare");
+		 }catch(BaseException e){				
+			 Assert.assertTrue(false,"getDataGroups fail " + e.getMessage());				
+		 }			
+		 return groupList;
+	 } 
 
 	public static CollectionSpace createCS(String csName, Sequoiadb db) throws BaseException {
 		CollectionSpace tmp = null;
@@ -170,6 +206,70 @@ public class SplitUtils {
 				dbc.close();
 			}
 		}
+	}
+	
+	/**
+	 * generating byte to write lob
+	 * @param length
+	 *        generating byte stream size
+	 * @return String sb
+	 */
+	public static String getRandomString(int length){
+		String base = "abcdefahijklmnopqrstuvwxyz0123456789";
+		Random random = new Random();
+		
+		int times = length / 1024;
+		if (times < 1)
+		{
+			times = 1;
+		}
+		
+		int len = length >= 1024 ? 1024 : 0;
+		int mulByteNum = length % 1024;
+		
+		
+		StringBuffer sb = new StringBuffer(); 
+		for (int i = 0; i < times; ++i){
+			if (i == times - 1){
+				len += mulByteNum;
+			}
+
+			char ch = base.charAt(random.nextInt(base.length()));
+			for (int k = 0; k < len; ++k){
+				sb.append(ch);
+			}
+
+		}	
+		return sb.toString();
+	}
+	
+	public static String getSrcGroupName(Sequoiadb sdb,String csName,String clName){		
+		String groupName = "";
+		String cond = String.format("{Name:\"%s.%s\"}", csName, clName);
+		DBCursor cr = sdb.getSnapshot(8, cond, null, null);
+		while(cr.hasNext()){
+			 BSONObject obj = cr.getNext();
+			 
+			 BasicBSONObject doc = (BasicBSONObject)obj;
+			 doc.getString("Name");
+			 BasicBSONList subdoc = (BasicBSONList)doc.get("CataInfo");
+			 BasicBSONObject elem = (BasicBSONObject)subdoc.get(0);
+			 groupName = elem.getString("GroupName");
+		}		
+		return groupName;
+	}
+	
+	public static String getSplitGroupName(String groupName){
+		String tarRgName = "";
+		for (int i = 0; i < groupList.size(); ++i){
+			String name = groupList.get(i);
+			if (!name.equals(groupName)){
+				tarRgName = name;				
+				break;
+			}
+		}
+		return tarRgName;	    
+	
 	}
 
 }
