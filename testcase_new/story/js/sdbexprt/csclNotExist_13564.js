e5177bba86dde233c0d8ac2ba4aa6093bfb907bb
liangxuewang <liangxuewang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-01-02 05:01:54 +0000

    同步主干用例
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@33432 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/js/sdbexprt/csclNotExist_13564.js
+++ testcase_new/story/js/sdbexprt/csclNotExist_13564.js
@@ -1,21 +1,21 @@
 /*******************************************************************
 * @Description : test export with --cscl multi cs
-*                seqDB-13564:--cscl指定的集合空间/集合不存在             
+*                seqDB-13564:--cscl指定的集合空间/集合不存在
+*                seqDB-8202:--clname指定集合不存在
+*                seqDB-8203:--csname指定集合空间不存在            
 * @author      : Liang XueWang 
 *
 *******************************************************************/
-var csname = COMMCSNAME + "_sdbexprt13564" ;
-
 main() ;
 
 function main()
 {
-   commCreateCS( db, csname ) ;
+   commCreateCL( db, COMMCSNAME, COMMCLNAME ) ;
   
    testExprtNotExistCs() ;
    testExprtNotExistCl() ;
    
-   commDropCS( db, csname ) ;
+   commDropCL( db, COMMCSNAME, COMMCLNAME ) ;
 }
 
 function testExprtNotExistCs()
@@ -32,6 +32,16 @@ function testExprtNotExistCs()
                  " --force true" ;
    testRunCommand( command, 8 ) ;
    
+   var command = installPath + "bin/sdbexprt" +
+                 " -s " + COORDHOSTNAME +
+                 " -p " + COORDSVCNAME + 
+                 " -c notExistCs " +
+                 " -l " + COMMCLNAME + 
+                 " --file " + csvfile +
+                 " --type csv" + 
+                 " --force true" ;
+   testRunCommand( command, 8 ) ;
+   
    cmd.run( "rm -rf " + csvfile ) ;
 }
 
@@ -45,7 +55,16 @@ function testExprtNotExistCl()
                  " -p " + COORDSVCNAME + 
                  " --file " + jsonfile +
                  " --type json" +
-                 " --cscl " + csname + ".notExistCl" ;
+                 " --cscl " + COMMCSNAME + ".notExistCl" ;
+   testRunCommand( command, 8 ) ;
+   
+   var command = installPath + "bin/sdbexprt" +
+                 " -s " + COORDHOSTNAME +
+                 " -p " + COORDSVCNAME +
+                 " -c " + COMMCSNAME +
+                 " -l notExistCl" +
+                 " --file " + jsonfile +
+                 " --type json" ;
    testRunCommand( command, 8 ) ;
    
    cmd.run( "rm -rf " + jsonfile ) ;
