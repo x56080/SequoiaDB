bfe7dcb2dd73dbababb003e27506eaf97fce5715
heguoming <heguoming@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-01-11 23:32:32 +0000

    开发改功能，测试用例更新：设置优先从1-7读时，预期的节点是根据编目上的顺序，不会排除主节点
    Backport r28211 to sequoiadb_2.8
    
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@33605 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/js/query/setSessionAttr_1to7_13771.js
+++ testcase_new/story/js/query/setSessionAttr_1to7_13771.js
@@ -5,29 +5,34 @@
 main();
 
 function main()
-{	  
-	try
-	{	
-	   if( commIsStandalone(db) )
-		{  
-			println(" Deploy mode is standalone!");
-			return;
-		}
-		
+{  
+   try
+   {
+      if( commIsStandalone(db) )
+      {  
+         println(" Deploy mode is standalone!");
+         return;
+      }
+      
       var csName = COMMCSNAME;
       var clName = COMMCLNAME;
       
+      // create
       var clObj = new Collection( csName, clName, {ReplSize:0} );
       var cl = clObj.create();
-      var groupName  = clObj.getGroups()[0];
-      
+      var groupName = clObj.getGroups()[0];
+      var nodeList = getNodeList( groupName );
+
+      // insert
       var recs = [{a:1},{a:2},{a:3},{a:4}];  
       insertAnotherSession( csName, clName, recs );               
       
-      var hasSlave = checkSlaveExist( groupName );
-      
-      var queryNodes = testSet1to7( cl, recs ); 
-      checkQueryNodes( groupName, queryNodes, hasSlave );        
+      // query
+      var instances = [ 1, 2, 3, 4, 5, 6, 7 ]; 
+      for( var i in instances )   
+      {
+         setAndcheck( cl, instances[i], nodeList );
+      }
    }
    catch( e )
    {
@@ -35,79 +40,31 @@ function main()
    }
 }
 
-function testSet1to7( cl, recs )
+function setAndcheck( cl, instance, nodeList )
 {
-   var nodeList = [];
-   var opts = [ 1, 2, 3, 4, 5, 6, 7 ]; 
-   
-   for( var i in opts )   
+   println( "---begin to set PreferedInstance: " + instance );     
+   db.setSessionAttr( { "PreferedInstance": instance } );  
+             
+   var queryNode = cl.find().explain({Run:true}).current().toObj().NodeName;
+
+   var expNode = nodeList[ ( instance - 1 ) % nodeList.length ];
+   if( queryNode !== expNode )
    {
-      var opt = opts[i];
-      
-      println( "---begin to set PreferedInstance: " + opt );     
-      db.setSessionAttr({"PreferedInstance":opt});  
-                
-      var queryNode = cl.find().explain({Run:true}).current().toObj().NodeName;
-      nodeList.push( queryNode );
-      
-      var rc = cl.find().sort({a:1});
-      checkRec( rc, recs );
+      throw buildException( "setAndcheck()", null, "explain", expNode, queryNode );
    }
-         
+}
+
+function getNodeList( groupName )
+{
+   var nodeList =[];
+   var groupInfo = db.getRG(groupName).getDetail().current().toObj().Group;
+   for( var i in groupInfo )
+   {
+      var nodeInfo = groupInfo[i].HostName + ":" + groupInfo[i].Service[0].Name;
+      nodeList.push( nodeInfo );
+   }
+   println( "---get nodelist of " + groupName + ": " + nodeList );
+   
    return nodeList;
 }
 
-function checkQueryNodes( groupName, queryNodes, hasSlave )
-{
-   println("---begin to check query nodes");
-   
-   expRole = !hasSlave;  
-   var rc = db.exec( "select NodeName from $SNAPSHOT_SYSTEM where GroupName='" + 
-                     groupName + "' and IsPrimary=" + expRole );
-   var expNodes = [];
-   while( rc.next() )
-   {
-      var node = rc.current().toObj().NodeName;
-      expNodes.push( node );
-   }
-   
-   queryNodes = arrayUnique( queryNodes );
-   if( JSON.stringify(queryNodes.sort()) !== JSON.stringify(expNodes.sort()) )
-   {
-      throw buildException( "checkQueryNodes()", null, "queryNodes",
-   							    expNodes, queryNodes );
-   }
-}
-
-function checkSlaveExist( groupName )
-{
-   println("---begin to make sure that group[" + groupName + "] has master node");
-   
-   var rc = db.exec("select IsPrimary,NodeName from $SNAPSHOT_SYSTEM where GroupName='" + groupName + "' and IsPrimary=false ");
-   var num = rc.size();
-   if( num === 0 )
-   {
-      return false;
-   }
-   else
-   {
-      return true;
-   }
-}
-
-//数组去掉重复元素
-function arrayUnique( arr )
-{
-   arr = arr.sort();
-   
-   var uniqueArr = [ arr[0] ];
-   for( var i = 1; i < arr.length; i++ )
-   {
-      if( arr[i] !== uniqueArr[ uniqueArr.length-1] )
-      {
-         uniqueArr.push( arr[i] );
-      }
-   }
-   
-   return uniqueArr;
-}
