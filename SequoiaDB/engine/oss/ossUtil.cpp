ba90cff71ae44c360c84419996abf4c19cf8bfed
wujiaming <wujiaming@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-01-12 02:40:54 +0000

    SEQUOIADBMAINSTREAM-3184 ossGetDiskInfo函数问题修改
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@33618 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/oss/ossUtil.cpp
+++ SequoiaDB/engine/oss/ossUtil.cpp
@@ -1032,6 +1032,8 @@ INT32 ossGetDiskInfo ( const CHAR *pPath, INT64 &totalBytes,
    struct statvfs vfs ;
    FILE *fp = NULL ;
    struct mntent *me = NULL ;
+   struct mntent dummy ;
+   CHAR tmpBuff[OSS_MAX_PATHSIZE] = {0} ;
    struct stat pathStat ;
    INT32 retcode = 0 ;
    BOOLEAN findOut = FALSE ;
@@ -1062,7 +1064,7 @@ INT32 ossGetDiskInfo ( const CHAR *pPath, INT64 &totalBytes,
    PD_CHECK( NULL != fp, SDB_SYS, error, PDERROR, "Failed to set mnt entry"
              ", errno: %d, rc = %d", ossGetLastError (), rc );
 
-   while ( NULL != ( me = getmntent (fp) ) )
+   while ( NULL != ( me = getmntent_r ( fp, &dummy, tmpBuff, OSS_MAX_PATHSIZE ) ) )
    {
       struct stat fsStat ;
       INT32 retcode = stat ( me->mnt_fsname, &fsStat );
@@ -1102,6 +1104,11 @@ done :
    {
       SDB_OSS_FREE ( lpszVolumePath ) ;
       lpszVolumePath = NULL ;
+   }
+#elif defined (_LINUX) || defined (_AIX)
+   if ( NULL != fp )
+   {
+      endmntent( fp ) ;
    }
 #endif
    PD_TRACE_EXITRC ( SDB_OSSGETDISKINFO, rc );
