6a0846dd38996ada32a77e37bad7a10e5d2a95e5
heguoming <heguoming@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-01-12 13:15:25 +0000

    SEQUOIADBMAINSTREAM-3183 : detachCL后更新数据节点的catalog缓存时需要清空子表的主表字符串字段
    Backport r33644 to sequoiadb_2.6
    
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.6@33646 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/cls/clsCatalogAgent.cpp
+++ SequoiaDB/engine/cls/clsCatalogAgent.cpp
@@ -1623,6 +1623,10 @@ namespace engine
       {
          _mainCLName = ele.str();
       }
+      else
+      {
+         _mainCLName.clear() ;
+      }
 
       /// get attribute, it is optional.
       ele = catSet.getField (CAT_ATTRIBUTE_NAME ) ;
