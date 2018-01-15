0c9a2032e3c258bca17f67affc803944f3c0afac
heguoming <heguoming@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-01-15 07:20:03 +0000

    SEQUOIADBMAINSTREAM-3195 : 修改切分任务重启卡住的问题
    切分任务在 CLS_TASK_STATUS_META 状态后重启， 就表示数据已经生效了，
    而且已经完成切分， 只需要源节点执行清理任务
    
    Backport r33664 to sequoiadb_2.8
    
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@33668 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/cls/clsFSDstSession.cpp
+++ SequoiaDB/engine/cls/clsFSDstSession.cpp
@@ -2066,8 +2066,10 @@ namespace engine
             ossSleep ( OSS_ONE_SEC ) ;
          }
       }
-      // the task is finished, need to notify peer to clean up data
-      else if ( CLS_TASK_STATUS_FINISH == _pTask->status() )
+      // the task is finished or catalog meta-data is changed,
+      // need to notify peer to clean up data
+      else if ( CLS_TASK_STATUS_FINISH == _pTask->status() ||
+                CLS_TASK_STATUS_META == _pTask->status() )
       {
          PD_LOG ( PDEVENT, "Session[%s]: Split task[%s] already finished,"
                   "need to notify destination node to clean up data",
@@ -2108,7 +2110,7 @@ namespace engine
       // if the session is not finished, restart query
       if ( CLS_FS_STATUS_END != _status || STEP_END != _step )
       {
-         if ( 0 != _needSyncData && _step < STEP_META )
+         if ( 0 != _needSyncData && _step <= STEP_META )
          {
             EDUID cleanupJobID = PMD_INVALID_EDUID ;
             startCleanupJob( _pTask->clFullName(), _pTask->splitKeyObj(),
