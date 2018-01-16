bb95f1f2095bbee275b493d6f6f61dbb1e0f74a3
liangxuewang <liangxuewang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-01-16 01:00:30 +0000

    回归问题单3185，增加用例
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@33683 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/js/sdb/snapshotSystem_14117.js
@@ -0,0 +1,59 @@
+/*******************************************************************
+* @Description : test snapshot system with /etc/mtab leak
+*                seqDB-14117:获取系统快照后检查/etc/mtab文件句柄泄露                 
+* @author      : Liang XueWang
+* 
+*******************************************************************/
+var cmd = new Cmd() ;
+
+main( db ) ;
+
+function main( db )
+{
+   if( commIsStandalone( db ) )
+   {
+      println( "Run mode is standalone" ) ;
+      return ;
+   }
+   
+   db.snapshot( SDB_SNAP_SYSTEM ) ;
+   var pid = getCataPid() ;
+   var fpNum = getFpNum( pid ) ;
+   if( parseInt( fpNum ) !== 0 )
+   {
+      throw buildException( "main", null, "check /etc/mtab fp num after snapshot system", 
+                            0, fpNum ) ;
+   }
+}
+
+// get local cata node pid
+function getCataPid()
+{
+   try
+   {
+      var cursor = System.listProcess( {}, { cmd: "sequoiadb(" + CATASVCNAME + ") C" } ) ;
+      var obj = cursor.next().toObj() ;
+   }
+   catch( e )
+   {
+      throw buildException( "getCataPid", e, "get cata node pid", 0, e ) ;
+   }
+   var pid = obj["pid"] ;
+   println( "cata node pid: " + pid ) ;
+   return pid ;
+}
+
+// get process pid /etc/mtab fp num
+function getFpNum( pid )
+{
+   var command = "lsof -p " + pid + " | grep /etc/mtab | wc -l" ;
+   try
+   {
+      var info = cmd.run( command ).split( "\n" ) ;
+   }
+   catch( e )
+   {
+      throw buildException( "getFpNum", e, "cmd run command: " + command, 0, e ) ;
+   }
+   return info[0] ;
+}
