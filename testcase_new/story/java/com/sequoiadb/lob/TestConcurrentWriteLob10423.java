37d4f4999c3f5fd4a6d285175ed6bb49f77c52d4
wuyan <wuyan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-01-26 05:37:29 +0000

    优化用例，并发操作只写lob
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@33988 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/java/com/sequoiadb/lob/TestConcurrentWriteLob10423.java
+++ testcase_new/story/java/com/sequoiadb/lob/TestConcurrentWriteLob10423.java
@@ -4,7 +4,9 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.bson.BSONObject;
 import org.bson.types.ObjectId;
+import org.bson.util.JSON;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -30,8 +32,10 @@ public class TestConcurrentWriteLob10423 extends SdbTestBase {
 	private String clName = "cl_lob10423";	
 	private static Sequoiadb sdb = null;
 	private CollectionSpace cs = null;	
+	private DBCollection dbcl = null;
 	private Random random = new Random();
 	private AtomicInteger sameOidWriteOKCount = new AtomicInteger(0);
+	private byte[] sameOidWriteOkBuff = null;
     	
 	@BeforeClass
 	public void setUp(){
@@ -48,14 +52,16 @@ public class TestConcurrentWriteLob10423 extends SdbTestBase {
 		WriteAndReadLobTask writeDiffLobTask = new WriteAndReadLobTask();
 		writeDiffLobTask.start(50);
 		
-		WriteSameOidLobTask writeSameLobTask = new WriteSameOidLobTask();
+		ObjectId oid = new ObjectId("5a3b6f23c5d07c3000f73a8b");
+		WriteSameOidLobTask writeSameLobTask = new WriteSameOidLobTask(oid);
 		writeSameLobTask.start(5);
-		
+			
 		Assert.assertTrue( writeDiffLobTask.isSuccess(), writeDiffLobTask.getErrorMsg());
 		Assert.assertTrue( writeSameLobTask.isSuccess(), writeSameLobTask.getErrorMsg());
 		//write lob of same oid only one success
 		int expSuccessNum = 1;
-		Assert.assertEquals(sameOidWriteOKCount.get(), expSuccessNum);	
+		Assert.assertEquals(sameOidWriteOKCount.get(), expSuccessNum);
+		checkWriteOkLobBySameOid(oid);
 	}
 
 	@AfterClass
@@ -103,13 +109,16 @@ public class TestConcurrentWriteLob10423 extends SdbTestBase {
 		}
 	}
 	
-	private class WriteSameOidLobTask extends SdbThreadBase {		
+	private class WriteSameOidLobTask extends SdbThreadBase {
+		private ObjectId oid;
+		
+		public WriteSameOidLobTask(ObjectId oid){
+			this.oid = oid;
+		}
 		@Override
 		public void exec() throws Exception {			
 			int writeLobSize = random.nextInt(1024*1024);;
-			byte[] lobBuff = LobOprUtils.getRandomBytes(writeLobSize);	
-			ObjectId oid = new ObjectId("5a3b6f23c5d07c3000f73a8b");
-			
+			byte[] lobBuff = LobOprUtils.getRandomBytes(writeLobSize);				
 			Sequoiadb sdb = null;
 		    try{	
 		    	sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
@@ -118,14 +127,6 @@ public class TestConcurrentWriteLob10423 extends SdbTestBase {
 				lob.write(lobBuff);	
 				oid = lob.getID();	
 				lob.close();
-				
-				//read and check the lob data
-				DBLob rLob = cl.openLob(oid);
-				byte[] rbuff = new byte[(int) rLob.getSize()];
-				rLob.read(rbuff);	
-				rLob.close();
-				Arrays.equals(rbuff, lobBuff);
-
 				//recorded the numbers of write lob successful 
 				sameOidWriteOKCount.getAndIncrement();
 		    }catch(BaseException e){		    	
@@ -141,11 +142,20 @@ public class TestConcurrentWriteLob10423 extends SdbTestBase {
 		}
 	}
 	
-	public void createCL(){						
+	private void checkWriteOkLobBySameOid(ObjectId oid){
+			//read and check the lob data
+			DBLob rLob = dbcl.openLob(oid);
+			byte[] rbuff = new byte[(int) rLob.getSize()];
+			rLob.read(rbuff);	
+			rLob.close();
+			Arrays.equals(rbuff, sameOidWriteOkBuff);
+	 }	
+	
+	private void createCL(){						
 	    try
 	    {
 	    	 cs = sdb.getCollectionSpace(SdbTestBase.csName);			
-		     cs.createCollection(clName);			
+	    	 dbcl = cs.createCollection(clName,(BSONObject)JSON.parse("{ ReplSize: 0 }"));			
 	    }catch(BaseException e){
 		    Assert.assertTrue(false,"create cl fail "+e.getErrorType()+":"+e.getMessage());
 	    }
