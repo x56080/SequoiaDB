4cb23ad641ba8333b452f3cb73678f019be9828b
wuyan <wuyan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-01-31 10:25:05 +0000

    修改为创建主子表
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@34105 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/js/sessionAccess/sessionAccess_14091_14104.js
+++ testcase_new/story/js/sessionAccess/sessionAccess_14091_14104.js
@@ -9,35 +9,47 @@ main();
 function main()
 {	  
 	try
-	{	  
-	   var clName = CHANGEDPREFIX + "_sessionAcess14091";      
+	{	        
       var db = new Sdb(COORDHOSTNAME, COORDSVCNAME ) ; 
-      
+      if( true == commIsStandalone( db ) )
+      {
+         println( "run mode is standalone" );
+         return;
+      } 
+       
       //get groupname
       var groups = commGetGroups( db ) ;
       var groupName = groups[0][0]["GroupName"] ;   
          
-      //create cl ,then insert data  
-      var dbcl = commCreateCLByOption( db, COMMCSNAME, clName, {ReplSize:0,Group:groupName});  
-      insertData( dbcl);
+      //create cs/maincl/subcl/attchcl ,then insert data 
+      var csName = CHANGEDPREFIX + "_cs14091";
+      var mainCLName = CHANGEDPREFIX + "_maincl14091";
+      var subclName1 = CHANGEDPREFIX + "_sessionAcess14091a";
+      var subclName2 = CHANGEDPREFIX + "_sessionAcess14091b"; 
+      commCreateCS( db, csName, false, "Failed to create CS.");
+      var mainCL = createMainCL( db, groupName, csName, mainCLName )
+      createSubCL( db, groupName, csName,subclName1);
+      createSubCL( db, groupName, csName,subclName2);
+      attachCL(  csName, mainCL, subclName1, 0, 3000 );  
+      attachCL(  csName, mainCL, subclName2, 3000, 6000 );      
+      insertData( mainCL);
       
       println("---begin to test testcase14091"); 
       //set [M/S/A] ,set the preferedInstanceMode is ordered 
       var queryInstanceidList = ["M","S","A"];            
       db.setSessionAttr( { PreferedInstance: queryInstanceidList,PreferedInstanceMode: "random"  } ) ; 
-      var actAccessNode = getAccessNode( dbcl);      
+      var actAccessNode = getAccessNode(mainCL);      
       checkAccessNodeIsPrimary( actAccessNode, groupName, true );           
       
       //set [S/M/A] ,set the preferedInstanceMode is ordered(test 14104)
       println("---begin to set the [S/M/A]"); 
       var queryInstanceidList1 = ["S","M","A"];            
       db.setSessionAttr( { PreferedInstance: queryInstanceidList1,PreferedInstanceMode: "random"  } ) ; 
-      var actAccessNode = getAccessNode( dbcl); 
+      var actAccessNode = getAccessNode( mainCL); 
       checkAccessNodeIsPrimary( actAccessNode, groupName, false ); 
       println("---end to test testcase14091 ");  
       
-      commDropCL( db, COMMCSNAME, clName, true, true,
-               "clear collection in the beginning" ) ;      
+      commDropCS( db, csName, false, "Failed to drop CS.");      
    }
    catch( e )
    {
@@ -50,6 +62,29 @@ function main()
          db.close()
       }
    }
+}
+
+function createMainCL( db, groupName, csName, mainCLName )
+{
+   var options = { ShardingKey: { no:1 }, ReplSize:0,Group:groupName,IsMainCL:true } ;
+   var mainCL = commCreateCLByOption( db, csName, mainCLName, options, false, 
+                                      true, "Failed to create mainCL." );                                  
+   return mainCL ; 
+}
+
+function createSubCL( db, groupName, csName,subCLName)
+{  
+   var options  = { ShardingKey: { a:1 }, ShardingType: "hash", 
+                    ReplSize:0, Group:groupName,Compressed:true } ;
+   var subCL = commCreateCLByOption( db, csName, subCLName, options, false, 
+                                     true, "Failed to create subCL." );
+   return subCL ;
+}
+
+function attachCL(  csName, mainCL, subCLName, beginNo, endNo )
+{    
+   var options = { LowBound: {"no":beginNo}, UpBound: {"no":endNo} } ; 
+   mainCL.attachCL( csName+"."+subCLName, options );
 }
 
 
