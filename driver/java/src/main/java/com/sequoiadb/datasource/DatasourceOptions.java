d60d155e9952b034e4b2dcf23a53b6e65e8793c2
tanzhaobo <tanzhaobo@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-02-08 04:44:32 +0000

    SEQUOIADBMAINSTREAM-3252：同步coord地址后，主动关闭查询的游标。
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@34238 6121f079-4a18-41d3-8c30-8870a322f2c2

--- driver/java/src/main/java/com/sequoiadb/datasource/DatasourceOptions.java
+++ driver/java/src/main/java/com/sequoiadb/datasource/DatasourceOptions.java
@@ -137,11 +137,11 @@ public class DatasourceOptions implements Cloneable {
     /**
      * @fn void setSyncCoordInterval(int syncCoordInterval)
      * @brief Set the interval for updating coord's addresses from catalog in milliseconds.
-     *         When the offered value is less than 60,000 milliseconds, use 60,000 milliseconds instead.
      * @param syncCoordInterval Default to be 0ms.
      * @note The updated coord addresses will cover the addresses in the pool.
-     *       When "syncCoordInterval" is 0, the pool will stop updating coord's addresses from
-     *       catalog.
+     *       The offered value can not less than 0. When it is 0, the pool will
+     *       stop updating coord's addresses from catalog, and when it is less than
+     *       60,000 milliseconds, use 60,000 milliseconds instead.
      * @since v1.12.6 and v2.2
      */
     public void setSyncCoordInterval(int syncCoordInterval) {
