e5282da01f18faeb6b5e3d973cf1cde1f52cfebf
wujiaming <wujiaming@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-03-14 06:09:06 +0000

    SEQUOIADBMAINSTREAM-3336 C/C++驱动API文档快照类型错误
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.6@34763 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/client/client.hpp
+++ SequoiaDB/engine/client/client.hpp
@@ -3367,7 +3367,7 @@ namespace sdbclient
         SDB_SNAP_COLLECTIONSPACES : Get the snapshot of all the collection spaces
         SDB_SNAP_DATABASE         : Get the snapshot of the database
         SDB_SNAP_SYSTEM           : Get the snapshot of the system
-        SDB_SNAP_CATA             : Get the snapshot of the catalog
+        SDB_SNAP_CATALOG          : Get the snapshot of the catalog
         SDB_SNAP_TRANSACTIONS     : Get snapshot of transactions in current session
         SDB_SNAP_TRANSACTIONS_CURRENT : Get snapshot of all the transactions
 
@@ -3412,7 +3412,7 @@ namespace sdbclient
         SDB_SNAP_COLLECTIONSPACES : Get the snapshot of all the collection spaces
         SDB_SNAP_DATABASE         : Get the snapshot of the database
         SDB_SNAP_SYSTEM           : Get the snapshot of the system
-        SDB_SNAP_CATA             : Get the snapshot of the catalog
+        SDB_SNAP_CATALOG          : Get the snapshot of the catalog
         SDB_SNAP_TRANSACTIONS     : Get snapshot of transactions in current session
         SDB_SNAP_TRANSACTIONS_CURRENT : Get snapshot of all the transactions
 
