fcfbcd54cd17e28c1f3fed923e284749c5461372
jwhe <jwhe@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-03-22 02:27:28 +0000

    SEQUOIADBMAINSTREAM-3387 php驱动SequoiaGroup::getNodeNum接口废弃
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.6@34942 6121f079-4a18-41d3-8c30-8870a322f2c2

--- driver/php5/ext/class/class_group.c
+++ driver/php5/ext/class/class_group.c
@@ -159,9 +159,11 @@ error:
    goto done ;
 }
 
-//node 该接口存在问题
+//废弃
 PHP_METHOD( SequoiaGroup, getNodeNum )
 {
+   RETVAL_LONG( -1 ) ;
+   /*
    INT32 rc = SDB_OK ;
    INT32 status     = 0 ;
    INT32 count      = 0 ;
@@ -183,13 +185,11 @@ PHP_METHOD( SequoiaGroup, getNodeNum )
       rc = SDB_INVALIDARG ;
       goto error ;
    }
-   /* 没有实现status
    rc = php_zval2Int( pStatus, &status TSRMLS_CC ) ;
    if( rc )
    {
       goto error ;
    }
-   */
    PHP_READ_VAR( pThisObj, "_SequoiaDB", pSequoiadb ) ;
    PHP_READ_HANDLE( pSequoiadb,
                     connection,
@@ -247,6 +247,7 @@ error:
    RETVAL_LONG( -1 ) ;
    PHP_SET_ERROR( FALSE, pThisObj, rc ) ;
    goto done ;
+*/
 }
 
 PHP_METHOD( SequoiaGroup, getDetail )
