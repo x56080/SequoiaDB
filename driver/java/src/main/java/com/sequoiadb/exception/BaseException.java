a1294877a1f0b93cef7c74144458a4a780795c3a
yuting <yuting@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-05-07 02:23:46 +0000

    修改Apache开源协议
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@35524 6121f079-4a18-41d3-8c30-8870a322f2c2

--- driver/java/src/main/java/com/sequoiadb/exception/BaseException.java
+++ driver/java/src/main/java/com/sequoiadb/exception/BaseException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 SequoiaDB Inc.
+ * Copyright (C) 2018 SequoiaDB Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
