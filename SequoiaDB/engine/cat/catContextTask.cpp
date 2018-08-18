ef43673a49844ba4a6df29234c4cee66d6161010
yuting <yuting@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-08-18 04:59:21 +0000

    SEQUOIADBMAINSTREAM-3575 创建索引时字段数量约束最多32个，报错时getLastErrObj()能返回exception信息
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@36575 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/cat/catContextTask.cpp
+++ SequoiaDB/engine/cat/catContextTask.cpp
@@ -585,9 +585,10 @@ namespace engine
       }
       catch( std::exception &e )
       {
-         PD_LOG( PDERROR, "Occur exception: %s, create index: %s, cl name: %s",
-                 e.what(), _boIdxKey.toString().c_str(), _dataName.c_str() ) ;
          rc = SDB_INVALIDARG ;
+         PD_LOG_MSG( PDERROR, "%s", e.what() ) ;
+         PD_LOG( PDERROR, "Create index: %s, cl name: %s, rc: %d.",
+                 _boIdxKey.toString().c_str(), _dataName.c_str(), rc ) ;
          goto error ;
       }
 
