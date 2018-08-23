bd9358079d29d85612303f1f6bc4b4cb7eeca1c5
jwhe <jwhe@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-08-23 10:22:24 +0000

    SEQUOIADBMAINSTREAM-3755 修复OM的query host status命令在高负载容易超时失败的问题
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@36654 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/include/omDef.hpp
+++ SequoiaDB/engine/include/omDef.hpp
@@ -340,7 +340,8 @@ namespace engine
    // milliseconds
    //two hours
    #define  OM_MSG_TIMEOUT_TWO_HOUR          (2 * 3600 * 1000)
-   #define  OM_MSG_TIMEOUT_TEN_SECS          (10 * 1000)
+   #define  OM_MSG_TIMEOUT_THIRTY_SECS       (30 * 1000)
+   #define  OM_MSG_TIMEOUT_ONE_MIN           (60 * 1000)
 
    #define  OM_BASICCHECK_INTERVAL           OM_MSG_TIMEOUT_TWO_HOUR
    #define  OM_INSTALL_AGET_INTERVAL         OM_MSG_TIMEOUT_TWO_HOUR
@@ -349,8 +350,8 @@ namespace engine
    #define  OM_WAIT_SCAN_RES_INTERVAL        OM_MSG_TIMEOUT_TWO_HOUR
 
    // wait for response's timeout
-   #define  OM_NOTIFY_TASK_TIMEOUT           OM_MSG_TIMEOUT_TEN_SECS
-   #define  OM_QUERY_HOST_STATUS_TIMEOUT     OM_MSG_TIMEOUT_TEN_SECS
+   #define  OM_NOTIFY_TASK_TIMEOUT           OM_MSG_TIMEOUT_THIRTY_SECS
+   #define  OM_QUERY_HOST_STATUS_TIMEOUT     OM_MSG_TIMEOUT_ONE_MIN
 
    #define  OM_WAIT_PROGRESS_RES_INTERVAL    (3000)
    #define  OM_WAIT_AGENT_EXIT_RES_INTERVAL  (1000)
