426523aaa55b421fe4c136697c2b36d2bd4113f9
wangkexin <wangkexin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-09-10 07:53:48 +0000

    SEQUOIADBMAINSTREAM-3437【外部需求】sdbimprt工具需要对timestamp类型的字段单独指定精度
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@36787 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/js/sdbimprt/sdbimprt_dataType_timestamp_15772.js
@@ -0,0 +1,103 @@
+/*******************************************************************************
+*@Description:   seqDB-15772:fields支持时区格式
+*@Author:        2018-9-10  wangkexin
+********************************************************************************/
+main();
+
+function main()
+{  
+   try
+   {
+      var csName = COMMCSNAME;
+      var clName = COMMCLNAME+"_15772" ;
+      var cl = readyCL( csName, clName );
+      
+      var imprtFile = tmpFileDir +"15772.csv";
+      readyData( imprtFile );
+      importData( csName, clName, imprtFile );
+   	
+      checkCLData( cl );
+      cleanCL( csName, clName );
+   }
+      catch(e)
+   {
+   	throw e;
+   }
+}
+
+function readyData( imprtFile )
+{
+   println("\n---Begin to ready data.");
+   
+   var file = fileInit( imprtFile );
+   file.write('time1,time2,time3\n"2014-01-01","2001/01/01","1990-01-01"\n"2014-01-01Z","2001/01/01Z","1990-01-01Z"\n"2014-01-01+0200","2001/01/01+0200","1990-01-01+0200"\n"2014-01-01 helloworld","2001/01/01 helloworld","1990-01-01 helloworld"');
+   var fileInfo = cmd.run( "cat "+ imprtFile );
+   println( imprtFile +"\n" + fileInfo );
+   file.close();
+}
+
+function importData( csName, clName, imprtFile )
+{
+   println("\n---Begin to import data and check exec result.");
+   
+   //remove rec file
+   var tmpRec = csName +"_"+ clName +"*.rec";
+   cmd.run( "rm -rf "+ tmpRec );
+   
+   //import operation
+   var imprtOption = installDir +'bin/sdbimprt -s '+ COORDHOSTNAME +' -p '+ COORDSVCNAME 
+                     +' -c '+ csName +' -l '+ clName 
+                     +' --type csv '
+					 +' --fields=\'time1 timestamp("YYYY-MM-DD"), time2 timestamp("YYYY/MM/DDZ"), time3 timestamp("YYYY-MM-DD+0600")\' '
+					 +' --headerline true '
+					 +' --file '+ imprtFile;
+   println( imprtOption );
+   var rc = cmd.run( imprtOption );
+   println( rc );
+   
+   //check import results
+   var rcObj = rc.split("\n");
+   var expParseRecords    = "parsed records: 4";
+   var expParseFailure    = "parse failure: 0";
+   var expImportedRecords = "imported records: 4";
+   var actParseRecords    = rcObj[0];
+   var actParseFailure    = rcObj[1];
+   var actImportedRecords = rcObj[4];
+   if( expParseRecords !== actParseRecords || expParseRecords !== actParseRecords 
+    || expImportedRecords !== actImportedRecords )
+   {
+      throw buildException( "importData", null, "[sdbimprt results]", 
+                        "["+ expParseRecords +", "+ expParseFailure +", "+ expImportedRecords +"]", 
+                        "["+ actParseRecords +", "+ actParseFailure +", "+ actImportedRecords +"]" );
+   }
+    
+   // clean tmpRec
+   cmd.run( "rm -rf " + tmpRec );
+}
+
+function checkCLData( cl )
+{
+   println("\n---Begin to check cl data.");
+   
+   var rc = cl.find({},{_id:{$include:0}});
+   var recsArray = [];
+   while( tmpRecs = rc.next() )
+   {
+      recsArray.push( tmpRecs.toObj() );
+   }
+   
+   var expCnt  = 4;  
+   var expRecs = '[{"time1":{"$timestamp":"2014-01-01-00.00.00.000000"},"time2":{"$timestamp":"2001-01-01-00.00.00.000000"},"time3":{"$timestamp":"1990-01-01-02.00.00.000000"}},'
+                 +'{"time1":{"$timestamp":"2014-01-01-00.00.00.000000"},"time2":{"$timestamp":"2001-01-01-08.00.00.000000"},"time3":{"$timestamp":"1990-01-01-08.00.00.000000"}},'
+				 +'{"time1":{"$timestamp":"2014-01-01-00.00.00.000000"},"time2":{"$timestamp":"2001-01-01-06.00.00.000000"},"time3":{"$timestamp":"1990-01-01-06.00.00.000000"}},'
+				 +'{"time1":{"$timestamp":"2014-01-01-00.00.00.000000"},"time2":{"$timestamp":"2001-01-01-00.00.00.000000"},"time3":{"$timestamp":"1990-01-01-02.00.00.000000"}}]';
+   var actCnt  = recsArray.length;
+   var actRecs = JSON.stringify( recsArray );
+   if( actCnt !== expCnt || actRecs !== expRecs )
+   {
+      throw buildException( "checkCLdata", null, "[find]", 
+                        "[cnt:"+ expCnt +", recs:"+ expRecs +"]", 
+                        "[cnt:"+ actCnt +", recs:"+ actRecs +"]" );
+   }
+   println( "cl records: "+ actRecs );
+}
