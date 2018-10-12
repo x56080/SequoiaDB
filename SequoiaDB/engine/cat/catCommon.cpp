b343c74cb8984385052f39db80976c278bfde00b
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-10-12 18:02:31 +0000

    opt: SEQUOIADBMAINSTREAM-3855 编目自动切分算法分配不均匀
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@37101 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/cat/catCommon.cpp
+++ SequoiaDB/engine/cat/catCommon.cpp
@@ -3905,7 +3905,7 @@ namespace engine
          UINT32 totalBound = (UINT32) clInfo._shardPartition ;
          UINT32 grpSize = splitLst.size() ;
          UINT32 avgBound = totalBound / grpSize ;
-         UINT32 modBound = totalBound % grpSize ;
+         UINT32 modMark = grpSize - ( totalBound % grpSize ) ;
          UINT32 beginBound = CAT_HASH_LOW_BOUND ;
          UINT32 endBound = beginBound + avgBound ;
 
@@ -3929,9 +3929,8 @@ namespace engine
             const std::string &grpName = iterGrp->first ;
             BSONObj lowBound, upBound ;
 
-            if ( modBound > 0 )
+            if ( itemID >= modMark )
             {
-               --modBound ;
                ++endBound ;
             }
 
