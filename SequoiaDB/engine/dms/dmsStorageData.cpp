6362a3366f5dfd628853baaddf3c8f44ba4aa2e1
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-10-17 16:08:54 +0000

    fix bug: truncate collection and inset dead lock. SEQUOIADBMAINSTREAM-3819
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.6@37188 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/dms/dmsStorageData.cpp
+++ SequoiaDB/engine/dms/dmsStorageData.cpp
@@ -1014,7 +1014,7 @@ namespace engine
                   rc = SDB_OK ;
                   goto done ;
                }
-               /// Try cl IX lock
+               /// Test cl IX lock
                 else if ( SDB_OK != pTransCB->transLockTestIX( cb,
                                                                _logicalCSID,
                                                                context->mbID(),
