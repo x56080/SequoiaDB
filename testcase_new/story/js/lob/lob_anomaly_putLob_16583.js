232e19452d639a568046316da9865c19d9eae4a5
wangkexin <wangkexin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-11-21 00:32:11 +0000

    修改冗余参数和代码格式
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@37860 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/js/lob/lob_anomaly_putLob_16583.js
+++ testcase_new/story/js/lob/lob_anomaly_putLob_16583.js
@@ -9,21 +9,18 @@ main(db);
 function main( db )
 {
 	var clName = CHANGEDPREFIX + "_putlob16583";
-	//clean environment before test
 	commDropCL( db, COMMCSNAME, clName, true, true, "clear collection in the beginning" ) ; 
 	
 	var testFile = CHANGEDPREFIX + "_lobTest16583.file" ;
 	lobAutoFile( testFile ) ;
-	var incorrectOid = "123456";
-	
-	// create collection
-	var cl = commCreateCL( db, COMMCSNAME, clName, -1, true, true, true,
-                          "create collection" ) ;
+
+	var cl = commCreateCL( db, COMMCSNAME, clName, -1, true, true, true, "create collection" ) ;
 	
 	println("begin to put lob with incorrect oid")
 	// put lob with incorrect oid
 	try
 	{
+		var incorrectOid = "123456";
 		cl.putLob(testFile, incorrectOid) ;
 		throw "ErrExecuteLob" ;
 	}
@@ -31,17 +28,15 @@ function main( db )
 	{
 		if( -6 != e )
 		{
-			throw buildException("check put lob with incorrect oid ", e, "check the put lob with incorrect oid",
-                           -6, e);
+			throw buildException("check put lob with incorrect oid ", e);
 		}
-	}finally
+	}
+	finally
 	{
 		var cmd = new Cmd() ;
 		// remove lobfile
 		cmd.run( "rm -rf " + testFile ) ;
-		commDropCL( db, COMMCSNAME, clName, true, true,
-		"drop collection in the end , error" ) ;
-		db.close( ) ;
-   }
+		commDropCL( db, COMMCSNAME, clName, true, true, "drop collection in the end , error" ) ;
+	}
 }
 
