bed442e39ef5aaefdc9e0621d10c2973bf7cd653
zhaoyu <zhaoyu@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-11-22 07:11:15 +0000

    切分与queryAndRemove并发，有可能数据未删除到，通过全局变量来获取删除记录数并比较
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@37906 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/sdv/java/com/sequoiadb/split/Split514.java
+++ testcase_new/sdv/java/com/sequoiadb/split/Split514.java
@@ -37,6 +37,7 @@ public class Split514 extends SdbTestBase {
 	private String srcGroupName;
 	private String destGroupName;
 	private Sequoiadb commSdb = null;
+	private int removeNum = 0;
 
 	@BeforeClass(enabled = true)
 	public void setUp() {
@@ -94,9 +95,10 @@ public class Split514 extends SdbTestBase {
 			DBCollection cl = sdb.getCollectionSpace(csName).getCollection(clName);
 			
 			// 删除a:0 - a:50 的记录
-			dbc = cl.query("{a:{$gte:0,$lt:50}}", null, null, null, 10, 50);
+			dbc = cl.queryAndRemove((BSONObject) JSON.parse("{a:{$gte:0,$lt:50}}"), null, null, null, 10, 50, 0);
 			while (dbc.hasNext()) {
-				cl.delete(dbc.getNext());
+				dbc.getNext();
+				removeNum++;
 			}
 
 			if (!split.isSuccess()) {
@@ -166,7 +168,7 @@ public class Split514 extends SdbTestBase {
 
 			long destDataCount = destDataNode.getCollectionSpace(csName).getCollection(clName)
 					.getCount("{a:{$gte:0,$lt:100}}");
-			Assert.assertEquals(destDataCount, 60);// 目标组应当含有上述范围数据
+			Assert.assertEquals(destDataCount, 100-removeNum);// 目标组应当含有上述范围数据
 			Assert.assertEquals(destDataNode.getCollectionSpace(csName).getCollection(clName).getCount(), 60);// 目标组应当仅含有上述范围数据
 
 			insertAndCheck(sdb, destDataNode, srcdataNode); // 重新插入数据，检查落入情况
