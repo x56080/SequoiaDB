e485e4891734175d16c76b2a1915913bb0679b53
tanzhaobo <tanzhaobo@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-12-11 05:54:03 +0000

    SEQUOIADBMAINSTREAM-4019：attachNode/detachNode去除options默认参数
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.6@38291 6121f079-4a18-41d3-8c30-8870a322f2c2

--- driver/java/src/main/java/com/sequoiadb/base/ReplicaGroup.java
+++ driver/java/src/main/java/com/sequoiadb/base/ReplicaGroup.java
@@ -410,84 +410,89 @@ public class ReplicaGroup {
     }
 
     /**
-     * @fn Node attachNode(String hostName, int port,
-    BSONObject configure)
+     * @fn Node attachNode(String hostName, int port, BSONObject options)
      * @brief Attach node.
-     * @param hostName
-     *          host name
-     * @param port
-     *          port
-     * @param configure
-     *          configuration for this operation
+     * @param hostName  host name
+     * @param port      port
+     * @param options configuration for this operation,
+     *                can not be null or empty, can be the follow options:
+     *                <ul>
+     *                <li>KeepData : Whether to keep the original data of the new
+     *                               node. This option has no default value. User
+     *                               should specify its value explicitly.</li>
+     *                </ul>
      * @return the attach Node object
-     * @exception com.sequoiadb.exception.BaseException
+     * @throws BaseException If error happens.
      */
     public Node attachNode(String hostName, int port,
-                           BSONObject configure) throws BaseException {
+                           BSONObject options) throws BaseException {
         BSONObject config = new BasicBSONObject();
         config.put(SequoiadbConstants.FIELD_NAME_GROUPNAME, name);
         config.put(SequoiadbConstants.FIELD_NAME_HOST, hostName);
         config.put(SequoiadbConstants.PMD_OPTION_SVCNAME,
                 Integer.toString(port));
         config.put(SequoiadbConstants.FIELD_NAME_ONLY_ATTACH, true);
-        if (configure != null) {
-            for (String key : configure.keySet()) {
+        if (options != null) {
+            for (String key : options.keySet()) {
                 if (key.equals(SequoiadbConstants.FIELD_NAME_GROUPNAME)
                         || key.equals(SequoiadbConstants.FIELD_NAME_HOST)
                         || key.equals(SequoiadbConstants.PMD_OPTION_SVCNAME)
                         || key.equals(SequoiadbConstants.FIELD_NAME_ONLY_ATTACH))
                     continue;
 
-                config.put(key, configure.get(key));
+                config.put(key, options.get(key));
             }
         }
         SDBMessage rtn = adminCommand(SequoiadbConstants.CREATE_CMD,
                 SequoiadbConstants.NODE, config);
         int flags = rtn.getFlags();
         if (flags != 0) {
-            throw new BaseException(flags, hostName, port, configure);
+            throw new BaseException(flags, hostName, port, options);
         }
 
         return getNode(hostName, port);
     }
 
     /**
-     * @fn void detachNode(String hostName, int port,
-    BSONObject configure)
+     * @fn void detachNode(String hostName, int port, BSONObject options)
      * @brief Detach node.
-     * @param hostName
-     *          host name
-     * @param port
-     *          port
-     * @param configure
-     *          configuration for this operation
-     * @return void
-     * @exception com.sequoiadb.exception.BaseException
+     * @param hostName  host name
+     * @param port      port
+     * @param options configuration for this operation,
+     *                can not be null or empty, can be the follow options:
+     *                <ul>
+     *                <li>KeepData : Whether to keep the original data of the
+     *                               detached node. This option has no default
+     *                               value. User should specify its value explicitly.</li>
+     *                <li>Enforced : Whether to detach the node forcibly, default
+     *                               to be false.</li>
+     *                </ul>
+     * @throws BaseException If error happens.
      */
     public void detachNode(String hostName, int port,
-                           BSONObject configure) throws BaseException {
+                           BSONObject options) throws BaseException {
         BSONObject config = new BasicBSONObject();
         config.put(SequoiadbConstants.FIELD_NAME_GROUPNAME, name);
         config.put(SequoiadbConstants.FIELD_NAME_HOST, hostName);
         config.put(SequoiadbConstants.PMD_OPTION_SVCNAME,
                 Integer.toString(port));
         config.put(SequoiadbConstants.FIELD_NAME_ONLY_DETACH, true);
-        if (configure != null) {
-            for (String key : configure.keySet()) {
+        if (options != null) {
+            for (String key : options.keySet()) {
                 if (key.equals(SequoiadbConstants.FIELD_NAME_GROUPNAME)
                         || key.equals(SequoiadbConstants.FIELD_NAME_HOST)
                         || key.equals(SequoiadbConstants.PMD_OPTION_SVCNAME)
                         || key.equals(SequoiadbConstants.FIELD_NAME_ONLY_DETACH))
                     continue;
 
-                config.put(key, configure.get(key));
+                config.put(key, options.get(key));
             }
         }
         SDBMessage rtn = adminCommand(SequoiadbConstants.REMOVE_CMD,
                 SequoiadbConstants.NODE, config);
         int flags = rtn.getFlags();
         if (flags != 0) {
-            throw new BaseException(flags, hostName, port, configure);
+            throw new BaseException(flags, hostName, port, options);
         }
     }
 
