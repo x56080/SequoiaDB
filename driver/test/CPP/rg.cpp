f4477a1b501ecdcf740f8e2d9cce391a05afdcb6
tanzhaobo <tanzhaobo@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-12-12 02:01:01 +0000

    SEQUOIADBMAINSTREAM-4019：attachNode/detachNode去除options默认参数
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@38319 6121f079-4a18-41d3-8c30-8870a322f2c2

--- driver/test/CPP/rg.cpp
+++ driver/test/CPP/rg.cpp
@@ -235,7 +235,7 @@ TEST_F( replicaGroupTest, detachNode )
    INT32 rc = SDB_OK ;
    sdbNode node ;
    // detach node 
-   rc = rg.detachNode( pNodeHostName2, pNodeSvcName2 ) ;
+   rc = rg.detachNode( pNodeHostName2, pNodeSvcName2, BSON("KeepData" << false) ) ;
    ASSERT_EQ( SDB_OK, rc ) << "Failed to detach data node from group " <<
       pGroupName << ", rc = " << rc ;
 
@@ -245,7 +245,7 @@ TEST_F( replicaGroupTest, detachNode )
       "SDB_CLS_NODE_NOT_EXIST, but rc = " << rc ;
 
    // attach node
-   rc = rg.attachNode( pNodeHostName2, pNodeSvcName2 ) ;
+   rc = rg.attachNode( pNodeHostName2, pNodeSvcName2, BSON("KeepData" << false) ) ;
    ASSERT_EQ( SDB_OK, rc ) << "Failed to attach data node to group " <<
       pGroupName << ", rc = " << rc ;
   
