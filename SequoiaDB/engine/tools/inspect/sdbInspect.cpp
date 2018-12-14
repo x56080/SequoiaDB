956051ac90135e837fec0a418a7d335afdf7285d
zhaowenbo <zhaowenbo@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-12-14 08:42:55 +0000

    SEQUOIADBMAINSTREAM-3913:【BUG】【new】【inspect工具】执行inspect指定输出报告到其他挂载路径下报IO错误 修改临时文件路径生成方式
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.6@38385 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/tools/inspect/sdbInspect.cpp
+++ SequoiaDB/engine/tools/inspect/sdbInspect.cpp
@@ -2594,42 +2594,14 @@ error:
 }
 
 /**
-** prepend outfile path to tmpfile
+** make tmpFile path from outFile
 ***/
-void prependOutFilePath( const CHAR *outfile, CHAR *tmpfile )
+void makeTmpFileName( const CHAR *outFile, UINT32 loopIndex, CHAR *tmpFile, UINT32 len )
 {
-   const CHAR *slashPos = NULL ;
-   UINT32 pathLength = 0 ;
+   SDB_ASSERT( NULL != outFile && NULL != tmpFile, "outFile & tmpFile can't be NULL" ) ;
 
-   if ( NULL == outfile || NULL == tmpfile )
-   {
-      goto done ;
-   }
-
-   if ( 0 == ossStrncmp( "", outfile, OSS_MAX_PATHSIZE ) )
-   {
-      goto done ;
-   }
-   else
-   {
-#if defined (_LINUX)
-      slashPos = ossStrrchr( outfile, '/') ;
-#elif defined (_WINDOWS)
-      slashPos = ossStrrchr( outfile, '\\') ;
-#endif
-
-      if ( NULL == slashPos )
-      {
-         goto done ;
-      }
-
-      pathLength = slashPos - outfile + 1 ;
-      ossMemmove( tmpfile + pathLength, tmpfile, pathLength ) ;
-      ossMemcpy( tmpfile, outfile, pathLength ) ;
-   }
-
-done:
-   return ;
+   ossMemset( tmpFile, 0, OSS_MAX_PATHSIZE ) ;
+   ossSnprintf( tmpFile, len, "%s"CI_TMP_FILE_SUFFIX, outFile, loopIndex ) ;
 }
 
 /**
@@ -3412,9 +3384,8 @@ INT32 _sdbCi::inspect()
       do
       {
          curLoop += 1 ;
-         ossMemset( tmpFile, 0, OSS_MAX_PATHSIZE ) ;
-         ossSnprintf( tmpFile, OSS_MAX_PATHSIZE, CI_TMP_FILE, curLoop ) ;
-         prependOutFilePath( _header._outfile, tmpFile ) ;
+         makeTmpFileName( _header._outfile, curLoop, tmpFile, OSS_MAX_PATHSIZE ) ;
+
          rc = inspectWithoutFile( coord, &_header, tmpFile, totalRecord ) ;
       }while ( CI_INSPECT_ERROR == rc ) ;
 
@@ -3443,9 +3414,7 @@ INT32 _sdbCi::inspect()
 
    for (INT32 idx = curLoop ; idx < _header._loop && !finish ; ++idx)
    {
-      ossMemset( tmpFile, 0, OSS_MAX_PATHSIZE ) ;
-      ossSnprintf( tmpFile, OSS_MAX_PATHSIZE, CI_TMP_FILE, idx + 1 ) ;
-      prependOutFilePath( _header._outfile, tmpFile ) ;
+      makeTmpFileName( _header._outfile, idx + 1, tmpFile, OSS_MAX_PATHSIZE ) ;
 
       rc = inspectWithFile( &_header, inFile, tmpFile, totalRecord, finish ) ;
       CHECK_VALUE( ( SDB_OK != rc ), error ) ;
@@ -3473,8 +3442,7 @@ INT32 _sdbCi::inspect()
    // delete temp file
    for ( INT32 idx = 0 ; idx < _header._loop ; ++idx )
    {
-      ossMemset( tmpFile, 0, OSS_MAX_PATHSIZE ) ;
-      ossSnprintf( tmpFile, OSS_MAX_PATHSIZE, CI_TMP_FILE, idx + 1 ) ;
+      makeTmpFileName( _header._outfile, idx + 1, tmpFile, OSS_MAX_PATHSIZE ) ;
 
       ossDelete( tmpFile ) ;
    }
