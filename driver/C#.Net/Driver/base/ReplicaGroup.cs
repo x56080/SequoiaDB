af49570fa59afb847c94ce18c98aab62eee9d7c2
tanzhaobo <tanzhaobo@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-12-19 06:29:02 +0000

    SEQUOIADBMAINSTREAM-4019：attachNode/detachNode去除options默认参数
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@38473 6121f079-4a18-41d3-8c30-8870a322f2c2

--- driver/C#.Net/Driver/base/ReplicaGroup.cs
+++ driver/C#.Net/Driver/base/ReplicaGroup.cs
@@ -612,7 +612,9 @@ namespace SequoiaDB
          *                               node. This option has no default value. User
          *                               should specify its value explicitly.</li>
          *                </ul>
-         *  \retval void
+         *  \return void
+         *  \exception SequoiaDB.BaseException
+         *  \exception System.Exception
          */
         public void AttachNode(string hostName, int port, BsonDocument options)
         {
@@ -657,7 +659,9 @@ namespace SequoiaDB
          *                <li>Enforced : Whether to detach the node forcibly, default
          *                               to be false.</li>
          *                </ul>
-         *  \retval void
+         *  \return void
+         *  \exception SequoiaDB.BaseException
+         *  \exception System.Exception
          */
         public void DetachNode(string hostName, int port, BsonDocument options)
         {
