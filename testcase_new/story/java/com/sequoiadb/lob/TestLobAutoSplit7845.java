0bd278d9063dedac6a91d0e5531bd7950b1b084a
wuyan <wuyan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-01-31 01:33:23 +0000

    更新lob在每个数据组中数量比较的误差值
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@39244 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/java/com/sequoiadb/lob/TestLobAutoSplit7845.java
+++ testcase_new/story/java/com/sequoiadb/lob/TestLobAutoSplit7845.java
@@ -45,6 +45,8 @@ public class TestLobAutoSplit7845 extends SdbTestBase {
 	public void setUp(){
 		try{
 			sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+			
+
 		}catch(BaseException e){			
 			Assert.assertTrue(false,"connect %s failed,"+SdbTestBase.coordUrl+e.getMessage());
 		}
@@ -88,7 +90,7 @@ public class TestLobAutoSplit7845 extends SdbTestBase {
 	
 	@Test( dependsOnMethods = "testAutoSplitLob")
 	public void checkSplitResult(){	
-		double expErrorValue = 0.5;	
+		double expErrorValue = 0.95;	
 		LobOprUtils.checkSplitResult(sdb, csName, clName, groupList, expErrorValue);		
 	}
 	
