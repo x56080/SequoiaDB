068101f4faa394ba7eabe6629987f9c1df66aede
wangkexin <wangkexin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-02-19 08:34:15 +0000

    重写会话隔离用例，改包readwriteseparation为sessionaccess
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@39421 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/driver/java/com/sequoiadb/readwriteseparation/Const.java
+++
@@ -1,9 +0,0 @@
-package com.sequoiadb.readwriteseparation;
-
-/**
- * Created by laojingtang on 18-1-29.
- */
-public class Const {
-    public static final String RGNAME="ReadWriteSeqpartRG";
-//    public static final String TESTGROUPNAME="ReadWriteSeqpart";
-}
