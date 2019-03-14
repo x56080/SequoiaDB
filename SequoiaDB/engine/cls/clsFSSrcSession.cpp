57385af28d6ccd556c24ebbb699e57230de6c010
yuting <yuting@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-03-14 06:36:22 +0000

    SEQUOIADBMAINSTREAM-4255 全量同步过程中有元数据操作，在全量同步结束后该操作都会到目的节点重放 修改：syncLog时修正beginLSN
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.6@39824 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/cls/clsFSSrcSession.cpp
+++ SequoiaDB/engine/cls/clsFSSrcSession.cpp
@@ -858,6 +858,14 @@ namespace engine
             _lsn.offset += header->_length ;
             _lsn.version = header->_version ;
 
+            /// We should update the begin lsn offset. Because in notifyLSN(),
+            /// when extLID is invalid, add ZERO to _beginLSNOffset for
+            /// perfermance
+            if ( header->_lsn == _beginLSNOffset )
+            {
+               _beginLSNOffset += header->_length ;
+            }
+
             // if we read something making the mb length exceed threshold, let's
             // break out from the loop
             if ( CLS_SYNC_MAX_LEN <= _mb.length() ||
