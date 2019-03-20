884e961587c45ff36916cf32e58c83d8406dc3d5
huangxiaoni <huangxiaoni@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-03-20 07:20:20 +0000

    新增，用例补充自动化
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@39939 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/sdv/js/clsMgr_sync/commlib.js
@@ -0,0 +1,89 @@
+/*******************************************************************************
+*@Description :  common function
+*@Modify list :
+*                2019/3/19  XiaoNi Huang Init
+*******************************************************************************/
+
+
+/* ****************************************************
+@Description: clean cl in the pre-condition, than create cl
+@Return: cl
+@Author: XiaoNi Huang
+**************************************************** */
+function readyCL( clName, options )
+{
+   println("\n---Begin to create CL.");
+   commDropCL( db, COMMCSNAME, clName, true, true, 
+            "Failed to drop CL in the pre-condition." );  
+            
+   if( options == "undefined" )
+   {                   
+      var cl = commCreateCL( db, COMMCSNAME, clName, -1, true, true, false,
+                          "Failed to create CL." ); 
+   }
+   else
+   {
+      var cl = commCreateCLByOption(db, COMMCSNAME, clName, options, true, true);
+   }   
+   return cl;
+}
+
+/* ****************************************************
+@Description: clean cl in the end-condition
+@Author: XiaoNi Huang
+**************************************************** */
+function cleanCL( clName )
+{
+   println("\n---Begin to drop CL.");	
+   commDropCL( db, COMMCSNAME, clName, false, false,
+               "Failed to drop CL in the end-condition" );
+}
+
+/* ****************************************************
+@Description: create data groups
+@Return: dataGroupNames
+@Author: XiaoNi Huang
+**************************************************** */
+function createDataGroups( hostName , groupNum, tmpGroupName, nodeNum )
+{
+   if( nodeNum === "undefined" ){ nodeNum = 1; }
+   var dataGroupNames = [];
+   var tmpNum = 0;
+   for(var i = 0; i < groupNum; i++)
+   {
+      var groupName = tmpGroupName + "_" + i;
+      var rg = db.createRG( groupName );
+      for( var j = 0; j < nodeNum; j++ )
+      {
+         var port = parseInt( RSRVPORTBEGIN ) + ( tmpNum * 10 );
+         rg.createNode( hostName, port, RSRVNODEDIR +"data/"+ port );
+         rg.start();
+         tmpNum++;
+      }
+      dataGroupNames.push( groupName );
+   }
+   return dataGroupNames;
+}
+
+
+/* ****************************************************
+@Description: remove data groups
+@Author: XiaoNi Huang
+**************************************************** */
+function removeDataGroups( dataGroupNames, ignoreRGNotExist )
+{
+   try
+   {
+      for(var i = 0 ; i < dataGroupNames.length; i++)
+      {
+         db.removeRG( dataGroupNames[i] );
+      }
+   }
+   catch( e )
+   {
+      if( -154 == e && !ignoreRGNotExist)
+      {
+         throw e;
+      }
+   }
+}
---
+++ testcase_new/sdv/js/clsMgr_sync/creatRG_split_447.js
@@ -0,0 +1,98 @@
+﻿/******************************************************************************
+*@Description : seqDB-447:新建分区组进行数据切分 
+*@Author      : 2019-3-19  XiaoNi Huang
+******************************************************************************/
+main();
+
+function main()
+{  
+   if( commIsStandalone(db) )
+   {
+      println(" Deploy mode is standalone!");
+      return;
+   }
+   if( commGetGroupsNum(db) < 2 )
+   {  
+      println("This testcase needs at least 2 groups to split sub cl!");
+      return;
+   }   
+   
+   var tmpGroupName = "rg_447";
+   var groupNum = 1;
+   var nodeNum = 2;
+   var newGroupNames = [];
+   var clName = "cl_447";
+   var recordsNum = 10000;
+   
+   // clean env
+   commDropCL( db, COMMCSNAME, clName, true, true, 
+            "Failed to drop CL in the pre-condition." ); 
+   removeDataGroups( [tmpGroupName + "_0"], true );  //createDataGroups, tmpGroupName+"_"+i
+   
+   println("\n---Begin to createCL and insert records");
+   var options = {ShardingKey:{a:1}, ShardingType:"range", ReplSize:0};
+   var cl = commCreateCLByOption(db, COMMCSNAME, clName, options, true, true);
+   var srcRg = commGetCLGroups( db, COMMCSNAME + "." + clName )[0];
+
+   var docs = readyDocs( recordsNum );
+   cl.insert( docs );
+   checkRecords( cl, docs );
+   
+   println("\n---Begin to createRG");
+   newGroupNames = createDataGroups( COORDHOSTNAME , groupNum, tmpGroupName, nodeNum );
+   var targetRg = newGroupNames[0];
+   
+   println("\n---Begin to split and check results");
+   cl.split( srcRg, targetRg, { a: recordsNum/2 }, { a: recordsNum } );
+   checkRecords( cl, docs );
+   checkRgRecords( srcRg, targetRg, clName, docs );
+   
+   // clean env
+   cleanCL( clName ); 
+   removeDataGroups( newGroupNames, false );
+}
+
+function readyDocs( recordsNum )
+{
+   var doc = [];
+   for( var i = 0; i < recordsNum; i++ )
+   {
+      doc.push( {a:i, b:"test" + i, c:"hello" } );
+   }   
+   return doc;
+}
+
+function checkRecords( cl, expData ) 
+{
+   var rc = cl.find( {}, {_id:{$include:0}} ).sort({a:1} );
+   var rcRecs = new Array();
+   while( tmpRecs = rc.next() )
+   {
+      rcRecs.push( tmpRecs.toObj() );
+   }   
+   
+   var expRecs = JSON.stringify( expData );
+   var actRecs = JSON.stringify( rcRecs );
+   if( expRecs !== actRecs )
+   {
+      throw buildException( "checkResult", null, "", expRecs, "  " + actRecs );
+   }
+}
+
+function checkRgRecords( srcRg, targetRg, clName, docs )
+{  
+   var docsNum = docs.length;
+   // srcRg
+   println("   Begin to checkRgRecords[" + srcRg +"]");
+   var nodeDB = db.getRG( srcRg ).getMaster().connect();
+   var cl = nodeDB.getCS( COMMCSNAME ).getCL( clName );
+   checkRecords( cl, docs.slice( 0, (docsNum/2) ) );
+   nodeDB.close();
+   
+   // targetRg
+   println("   Begin to checkRgRecords[" + targetRg +"]");
+   var nodeDB = db.getRG( targetRg ).getMaster().connect();
+   var cl = nodeDB.getCS( COMMCSNAME ).getCL( clName );
+   checkRecords( cl, docs.slice( docsNum/2, docsNum ) );
+   nodeDB.close();
+}
