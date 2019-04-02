5dff4d7b2220a3b0faa86cdf5d80070c81f6b3d5
wangkexin <wangkexin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-04-02 08:58:09 +0000

    同步主干修改，增加用例失败检测，删除创建的组，避免影响其他用例
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@40182 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/js/subCL_sync/subCL_set_partition_15669.js
+++ testcase_new/story/js/subCL_sync/subCL_set_partition_15669.js
@@ -17,7 +17,7 @@ function main()
       println( "run mode is standalone.");
       return;
     }
-    
+
     var groupsArray = commGetGroups( db, false, "", true, true, true );
     var dataRGNum = groupsArray.length;
     if(dataRGNum < 3)
@@ -25,7 +25,7 @@ function main()
       println( "at least three data groups.");
       return;
     }
-    
+
     var hostNames = [];
     for(var i = 1 ; i < groupsArray[0].length; i++)
     {
@@ -35,66 +35,80 @@ function main()
     //新创建6个组
     var newDataRGNames = createDataGroups( db, hostNames, 6 );
 
-    //将新建的6个组和环境中原有的3个（或3个以上）的组名一起存放在totalDataRGNames中
-    var totalDataRGArray = commGetGroups( db, false, "", true, true, true );
-    var totalDataRGNames = [];
-    for( var i = 0 ; i < totalDataRGArray.length ; i++ )
-    {
-        totalDataRGNames.push( totalDataRGArray[i][0].GroupName );
-    }
-    
-    db.createDomain( domainName, totalDataRGNames, {AutoSplit:true} );
-    commCreateCS( db, csName, true, "", {Domain:domainName} );
-    
-    println( "start to create main cl." );
-    var mainCLOption = { ShardingKey:{"a":1}, ShardingType:"range", IsMainCL:true };
-    var dbcl = commCreateCLByOption( db, csName, mainCL_Name, mainCLOption, true, true );
-    
-    println("start to create sub cl.");
-    var subClOption = {ShardingKey:{"b":1}, ShardingType:"hash", AutoSplit:true, Partition:8, ReplSize:0};
     try
     {
-        db.getCS( csName ).createCL( subCL_Name, subClOption );
-        throw "expect fail but success.";
-    }catch( e )
-    {
-        if( e !== -6 )
+        //将新建的6个组和环境中原有的3个（或3个以上）的组名一起存放在totalDataRGNames中
+        var totalDataRGArray = commGetGroups( db, false, "", true, true, true );
+        var totalDataRGNames = [];
+        for( var i = 0 ; i < totalDataRGArray.length ; i++ )
         {
-            throw buildException( "main()", e, "create subCL", '-6', e );
+            totalDataRGNames.push( totalDataRGArray[i][0].GroupName );
+        }
+
+        db.createDomain( domainName, totalDataRGNames, {AutoSplit:true} );
+        commCreateCS( db, csName, true, "", {Domain:domainName} );
+
+        println( "start to create main cl." );
+        var mainCLOption = { ShardingKey:{"a":1}, ShardingType:"range", IsMainCL:true };
+        var dbcl = commCreateCLByOption( db, csName, mainCL_Name, mainCLOption, true, true );
+
+        println("start to create sub cl.");
+        var subClOption = {ShardingKey:{"b":1}, ShardingType:"hash", AutoSplit:true, Partition:8, ReplSize:0};
+        try
+        {
+            db.getCS( csName ).createCL( subCL_Name, subClOption );
+            throw "expect fail but success.";
+        }catch( e )
+        {
+            if( e !== -6 )
+            {
+                throw buildException( "main()", e, "create subCL", '-6', e );
+            }
         }
     }
-    
-    //清除环境
-    commDropCS( db, csName, true, "drop CS in the end" );
-    removeDataRG( db, newDataRGNames );
-    db.dropDomain( domainName );
+    finally
+    {
+        //清除环境
+        commDropCS( db, csName, true, "drop CS in the end" );
+        removeDataRG( db, newDataRGNames );
+        db.dropDomain( domainName );
+    }
 }
 
 function createDataGroups( db, hostNames , groupNum )
 {
     var dataGroupNames = [];
-    var hostNameIndex = 0;
-    for( var i = 0; i < groupNum; i++ )
+    var rgName = "";
+    try
     {
-        var port = parseInt( RSRVPORTBEGIN )+( i*10 );
-        var rgName = "group15669_" + i;
-        var dataRG = db.createRG( rgName );
-        if( hostNameIndex == hostNames.length )
+        var hostNameIndex = 0;
+        for( var i = 0; i < groupNum; i++ )
         {
-            hostNameIndex = 0 ;
+            var port = parseInt( RSRVPORTBEGIN )+( i*10 );
+            rgName = "group15669_" + i;
+            var dataRG = db.createRG( rgName );
+            dataGroupNames.push( rgName );
+            if( hostNameIndex == hostNames.length )
+            {
+                hostNameIndex = 0 ;
+            }
+            dataRG.createNode( hostNames[hostNameIndex], port, RSRVNODEDIR+"data/"+port );
+            dataRG.start();
+            hostNameIndex++;
         }
-        dataRG.createNode( hostNames[hostNameIndex], port, RSRVNODEDIR+"data/"+port );
-        dataRG.start();
-        dataGroupNames.push( rgName );
-        hostNameIndex++;
+        return dataGroupNames;
     }
-    return dataGroupNames;
+    catch( e )
+    {
+        removeDataRG( db, dataGroupNames );
+        throw "create data RG " + rgName + " failed , errorCode = " + e;
+    } 
 }
 
-function removeDataRG( db, dataGroupNames )
+function removeDataRG( db, dataRGNames )
 {
-    for(var i = 0 ; i < dataGroupNames.length; i++)
+    for(var i = 0 ; i < dataRGNames.length; i++)
     {
-        db.removeRG( dataGroupNames[i] );
+        db.removeRG( dataRGNames[i] );
     }
 }
