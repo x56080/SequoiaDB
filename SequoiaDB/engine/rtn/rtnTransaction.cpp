1a05159ec2599b9a2cbb717362090eb7dfbb2492
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-04-04 12:39:25 +0000

    fix: pd trace not matched entry and exit
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@40247 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/rtn/rtnTransaction.cpp
+++ SequoiaDB/engine/rtn/rtnTransaction.cpp
@@ -71,9 +71,9 @@ namespace engine
       }
       PD_LOG( PDINFO, "Begin transaction operations(transID=%llu)",
               cb->getTransID() ) ;
-      PD_TRACE_EXIT ( SDB_RTNTRANSBEGIN ) ;
 
    done:
+      PD_TRACE_EXIT ( SDB_RTNTRANSBEGIN ) ;
       return rc;
    error:
       goto done ;
