7ad3f6b61df5b45bfc0bb92570c3832a8759942d
yangshangde <yangshangde@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-04-17 02:23:37 +0000

    SEQUOIADBMAINSTREAM-4299集合包含分区键字段为空数组的记录(a:[])，split切分后带条件a:[]查询，协调节点异常停止
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.6@40493 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/cls/clsCataHashMatcher.cpp
+++ SequoiaDB/engine/cls/clsCataHashMatcher.cpp
@@ -229,7 +229,10 @@ namespace engine
                eleShard = iterField->second ;
             }
 
-            bobKey.appendAs( eleShard, "" ) ;
+            if ( !eleShard.eoo() )
+            {
+               bobKey.appendAs( eleShard, "" ) ;
+            }
          }
 
          if ( includeAllKey )
