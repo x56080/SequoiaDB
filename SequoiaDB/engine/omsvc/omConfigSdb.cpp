4ce3a81850192cfdaffc07a7b56d30d5db34347d
hejiawen <hejiawen@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-07-01 07:42:17 +0000

    SEQUOIADBMAINSTREAM-4527 OM取消挂载盘限制，允许服务的数据路径是任意路径
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@41913 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/omsvc/omConfigSdb.cpp
+++ SequoiaDB/engine/omsvc/omConfigSdb.cpp
@@ -148,7 +148,7 @@ namespace engine
    {
       INT32 rc = SDB_OK ;
 
-      rc = _addPath( dbPath, host ) ;
+      rc = _addPath( dbPath, host, TRUE ) ;
       if ( SDB_OK != rc )
       {
          PD_LOG( PDERROR, "failed to add db path to node" );
