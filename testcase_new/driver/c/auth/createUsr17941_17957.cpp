aa662a5b61cd83ff4cf9742ee34ce94b7c43a05c
liuxiaoxuan <liuxiaoxuan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-07-10 05:02:12 +0000

    按照4215检视意见，修改c用例
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@42076 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/driver/c/auth/createUsr17941_17957.cpp
+++ testcase_new/driver/c/auth/createUsr17941_17957.cpp
@@ -8,184 +8,122 @@
 #include <gtest/gtest.h>
 #include <client.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "../common/testcommon.hpp"
+#include "testcommon.hpp"
 
-sdbConnectionHandle db  = SDB_INVALID_HANDLE ;
-sdbConnectionHandle cataDB = SDB_INVALID_HANDLE ;
-const char* userName = "user_17941_17957" ;
-const char* passwd = "passwd_17941_17957" ;
-
-int setUp()
+class createUsr17941_17957: public testing::Test
 {
-    INT32 rc = SDB_OK ;
-  
-    // connect sdb  
-    getConf() ;
-    rc = sdbConnect( HOSTNAME, SVCNAME, USER, PASSWD, &db ) ;
-    CHECK_RC( rc, "fail to connect sdb, rc = %d\n", rc ) ;
+protected:
+   sdbConnectionHandle db ;
+   sdbConnectionHandle cataDB ;
+   sdbReplicaGroupHandle rg ;
+   sdbNodeHandle masterNode ;
+   const char* userName ;
+   const char* passwd ;
 
-    if( isStandalone( db ) )
-    {
-        printf( "run mode is stand alone.\n" ) ;
-        goto done ;
-    }
+   void SetUp()
+   {
+      INT32 rc = SDB_OK ;
+      userName = "user_17941_17957" ;
+      passwd = "passwd_17941_17957" ;
 
-    // get catalog master node connection
-    int groupId ;
-    sdbReplicaGroupHandle rg ;
-    sdbNodeHandle masterNode ;
-    groupId = 1 ;
-    rg = SDB_INVALID_HANDLE ;
-    masterNode = SDB_INVALID_HANDLE ;
-    rc = sdbGetReplicaGroup1( db, groupId, &rg ) ;
-    CHECK_RC( rc, "fail to get SYSCatalogGroup, rc = %d\n", rc ) ;
-    rc = sdbGetNodeMaster( rg, &masterNode ) ;
-    CHECK_RC( rc, "fail to get master, rc = %d\n", rc ) ;
-   
-    const char* hostName ;
-    const char* svcName ;
-    const char* nodeName ;
-    int nodeId ;
-    hostName = NULL ;
-    svcName = NULL ;
-    nodeName = NULL ;
-    nodeId = -1 ;
-    rc = sdbGetNodeAddr( masterNode, &hostName, &svcName, &nodeName, &nodeId ) ;
-    CHECK_RC( rc, "fail to get node address, rc = %d\n", rc ) ;
-    rc = sdbConnect( hostName, svcName, USER, PASSWD, &cataDB ) ;
-    CHECK_RC( rc, "fail to connect to catalog master node: %s:%s, rc = %d\n", hostName, svcName, rc ) ;
-    
-    done:
-       return rc ;
-    error:
-       goto done ;
-}
+      getConf() ;
+      rc = sdbConnect( HOSTNAME, SVCNAME, USER, PASSWD, &db ) ;
+      ASSERT_EQ( SDB_OK, rc ) ; 
 
-int tearDown()
+      if( isStandalone( db ) )
+      {
+         printf( "Run mode is standalone\n" ) ;
+         return ;
+      }
+
+      // get catalog master node connection
+      int groupId = 1 ;
+      rc = sdbGetReplicaGroup1( db, groupId, &rg ) ;
+      ASSERT_EQ( SDB_OK, rc ) ;
+      rc = sdbGetNodeMaster( rg, &masterNode ) ;
+      ASSERT_EQ( SDB_OK, rc ) ;
+
+      const char* hostName ;
+      const char* svcName ;
+      const char* nodeName ;
+      int nodeId = -1;
+      rc = sdbGetNodeAddr( masterNode, &hostName, &svcName, &nodeName, &nodeId ) ;
+      ASSERT_EQ( SDB_OK, rc ) ;
+      rc = sdbConnect( hostName, svcName, USER, PASSWD, &cataDB ) ;
+      ASSERT_EQ( SDB_OK, rc ) ;
+   }
+
+   void TearDown()
+   {
+      INT32 rc = SDB_OK ;
+
+      if( isStandalone( db ) )
+      {
+         printf( "Run mode is standalone\n" ) ;
+         return ;
+      }
+
+      rc = sdbRemoveUsr( db, userName, passwd ) ;
+      ASSERT_EQ( SDB_OK, rc ) ;
+      sdbReleaseNode( masterNode ) ;
+      sdbReleaseReplicaGroup( rg ) ;
+      sdbDisconnect( db ) ;
+      sdbDisconnect( cataDB ) ;
+      sdbReleaseConnection( db ) ;
+      sdbReleaseConnection( cataDB ) ;
+   }
+} ;
+
+TEST_F( createUsr17941_17957, withOption )
 {
-    int rc = SDB_OK ;
-	
-    if( isStandalone( db ) )
-    {
-        printf( "run mode is stand alone.\n" ) ;
-        goto done ;
-    }
-    
-    rc = sdbRemoveUsr( db, userName, passwd ) ;
-    CHECK_RC( rc, "fail to remove usr, rc = %d\n", rc ) ;
-	   sdbDisconnect( db ) ;
-    sdbDisconnect( cataDB ) ;
-	   sdbReleaseConnection( db ) ;
-    sdbReleaseConnection( cataDB ) ;
+   int rc = SDB_OK ;
 
-    done:
-       return rc ;
-    error:
-       goto done ;
-}
+   if( isStandalone( db ) )
+   {
+      printf( "Run mode is standalone\n" ) ;
+      return ;
+   }
 
-TEST( createUsr17941_17957, withOption17941 )
-{
-    int rc = SDB_OK ;
-    // setup
-    rc = setUp() ;
-    if( isStandalone( db ) )  return ;
-    ASSERT_EQ( rc, SDB_OK ) ;
+   // invalid parameter
+   bson obj ;
+   bson_init( &obj ) ;
+   bson_append_string( &obj, "AuditMask", "test" ) ;
+   bson_finish( &obj ) ;
+   rc = sdbCreateUsr2( db, userName, passwd, &obj ) ;
+   ASSERT_EQ( SDB_INVALIDARG, rc ) ;
+   bson_destroy( &obj ) ;
 
-    const char* mask = "CLUSTER|DML|DDL|DCL|!INSERT|UPDATE" ;
-    bson obj ;
-    bson_init( &obj ) ;
-    bson_append_string( &obj, "AuditMask", mask ) ;
-    bson_finish( &obj ) ;
-    rc = sdbCreateUsr2( db, userName, passwd, &obj ) ;
-    ASSERT_EQ( SDB_OK, rc ) << "fail to create user" ; 
-    bson_destroy( &obj ) ;
-   
-    sdbCollectionHandle cl  = SDB_INVALID_HANDLE ;
-    rc = sdbGetCollection( cataDB, "SYSAUTH.SYSUSRS", &cl ) ;
-    ASSERT_EQ( rc, SDB_OK ) << "fail to get collection SYSAUTH.SYSUSRS" ;
-   
-    CHAR destMask[64] ;
-    bson subobj ;
-    bson_init( &obj ) ;
-    bson_init( &subobj ) ;
-    sdbCursorHandle cursor = SDB_INVALID_HANDLE ;
-    rc = sdbQuery( cl, NULL, NULL, NULL, NULL, 0, -1, &cursor ) ;
-    ASSERT_EQ( rc, SDB_OK ) << "fail to query" ;
-    rc = sdbNext( cursor, &obj ) ;
-    ASSERT_EQ( SDB_OK, rc ) << "fail to get next" ;
-    bson_iterator it, sub ;
-    bson_find( &it, &obj, "Options" ) ;
-    bson_iterator_subobject( &it, &subobj ) ;
-    bson_find( &sub, &subobj, "AuditMask" ) ;
-    bson_print( &subobj ) ;
-    string srcMask = bson_iterator_string( &sub ) ;
-    strcpy( destMask, srcMask.c_str() ) ;
-    ASSERT_EQ( 0, strcmp( destMask, mask ) ) << "check auditmask wrong, expect:" << mask << " actual:" << destMask ;
-    bson_destroy( &obj ) ;
-    bson_destroy( &subobj ) ;
-    sdbReleaseCursor( cursor ) ;
-    sdbReleaseCollection( cl ) ;
-    
-    // teardown
-    rc = tearDown() ;
-    ASSERT_EQ( rc, SDB_OK ) ;
-}
+   const char* mask = "CLUSTER|DML|DDL|DCL|!INSERT|UPDATE" ;
+   bson_init( &obj ) ;
+   bson_append_string( &obj, "AuditMask", mask ) ;
+   bson_finish( &obj ) ;
+   rc = sdbCreateUsr2( db, userName, passwd, &obj ) ;
+   ASSERT_EQ( SDB_OK, rc ) << "fail to create user" ;
+   bson_destroy( &obj ) ;
 
-TEST( createUsr17941_17957, paramCheck17957 )
-{
-    int rc = SDB_OK ;
-    // setup
-    rc = setUp() ;
-    if( isStandalone( db ) )  return ;
-    ASSERT_EQ( rc, SDB_OK ) ;
+   sdbCollectionHandle cl ;
+   rc = sdbGetCollection( cataDB, "SYSAUTH.SYSUSRS", &cl ) ;
+   ASSERT_EQ( rc, SDB_OK ) << "fail to get collection SYSAUTH.SYSUSRS" ;
 
-    // invalid parameter
-    bson obj ;
-    bson_init( &obj ) ;
-    bson_append_string( &obj, "AuditMask", "test" ) ;
-    bson_finish( &obj ) ;
-    rc = sdbCreateUsr2( db, userName, passwd, &obj ) ;
-    ASSERT_EQ( SDB_INVALIDARG, rc ) ; 
-    bson_destroy( &obj ) ;
-    
-    // empty string
-    const char* mask = " " ;
-    bson_init( &obj ) ;
-    bson_append_string( &obj, "AuditMask", mask ) ;
-    bson_finish( &obj ) ;
-    rc = sdbCreateUsr2( db, userName, passwd, &obj ) ;
-    ASSERT_EQ( SDB_OK, rc ) << "fail to create user" ; 
-    bson_destroy( &obj ) ;
-   
-    sdbCollectionHandle cl  = SDB_INVALID_HANDLE ;
-    rc = sdbGetCollection( cataDB, "SYSAUTH.SYSUSRS", &cl ) ;
-    ASSERT_EQ( rc, SDB_OK ) << "fail to get collection SYSAUTH.SYSUSRS" ;
-   
-    CHAR destMask[64];
-    bson subobj ;
-    bson_init( &obj ) ;
-    bson_init( &subobj ) ;
-    sdbCursorHandle cursor = SDB_INVALID_HANDLE ;
-    rc = sdbQuery( cl, NULL, NULL, NULL, NULL, 0, -1, &cursor ) ;
-    ASSERT_EQ( rc, SDB_OK ) << "fail to query" ;
-    rc = sdbNext( cursor, &obj ) ;
-    ASSERT_EQ( SDB_OK, rc ) << "fail to get next" ;
-    bson_iterator it, sub ;
-    bson_find( &it, &obj, "Options" ) ;
-    bson_iterator_subobject( &it, &subobj ) ;
-    bson_find( &sub, &subobj, "AuditMask" ) ;
-    string srcMask = bson_iterator_string( &sub ) ;
-    strcpy( destMask, srcMask.c_str() ) ;
-    ASSERT_EQ( 0, strcmp( destMask, mask ) ) << "check auditmask wrong, expect:" << mask << " actual:" << destMask ;
-    bson_destroy( &obj ) ;
-    bson_destroy( &subobj ) ;
-    sdbReleaseCursor( cursor ) ;
-    sdbReleaseCollection( cl ) ;
-    
-    // teardown
-    rc = tearDown() ;
-    ASSERT_EQ( rc, SDB_OK ) ;
+   CHAR destMask[64] ;
+   bson subobj ;
+   bson_init( &obj ) ;
+   bson_init( &subobj ) ;
+   sdbCursorHandle cursor ;
+   rc = sdbQuery( cl, NULL, NULL, NULL, NULL, 0, -1, &cursor ) ;
+   ASSERT_EQ( rc, SDB_OK ) << "fail to query" ;
+   rc = sdbNext( cursor, &obj ) ;
+   ASSERT_EQ( SDB_OK, rc ) << "fail to get next" ;
+   bson_iterator it, sub ;
+   bson_find( &it, &obj, "Options" ) ;
+   bson_iterator_subobject( &it, &subobj ) ;
+   bson_find( &sub, &subobj, "AuditMask" ) ;
+   bson_print( &subobj ) ;
+   string srcMask = bson_iterator_string( &sub ) ;
+   strcpy( destMask, srcMask.c_str() ) ;
+   ASSERT_EQ( 0, strcmp( destMask, mask ) ) << "check auditmask wrong, expect:" << mask << " actual:" << destMask ;
+   bson_destroy( &obj ) ;
+   bson_destroy( &subobj ) ;
+   sdbReleaseCursor( cursor ) ;
+   sdbReleaseCollection( cl ) ;
 }
