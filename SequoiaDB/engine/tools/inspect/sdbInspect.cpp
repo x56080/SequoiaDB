4aa2bdfbfd8436b0be6002b05d35841c7d1599ab
heguoming <heguoming@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-07-12 02:20:35 +0000

    SEQUOIADBMAINSTREAM-4683: fix ossSnprintf with collection full name
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.6@42108 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/tools/inspect/sdbInspect.cpp
+++ SequoiaDB/engine/tools/inspect/sdbInspect.cpp
@@ -2370,7 +2370,7 @@ INT32 getCiCollection( ciNode *master, const CHAR *csName,
    hasCollection = ( 0 != ossStrncmp( "", clName, CI_CL_NAME_SIZE ) ) ;
    if ( hasCollection )
    {
-      ossSnprintf( fullName, CI_CL_FULLNAME_SIZE, "%s.%s", csName, clName ) ;
+      ossSnprintf( fullName, sizeof( fullName ), "%s.%s", csName, clName ) ;
    }
 
    // get collections from master node
@@ -2659,7 +2659,7 @@ INT32 inspectWithoutFile( sdbclient::sdb *coord, ciHeader *header,
    hasGroup = ( 0 != ossStrncmp( "", header->_groupName,
                 CI_GROUPNAME_SIZE ) ) ;
    // combine collection full name
-   ossSnprintf( fullName, CI_CL_FULLNAME_SIZE, "%s.%s",
+   ossSnprintf( fullName, sizeof( fullName ), "%s.%s",
                 header->_csName, header->_clName ) ;
 
    groupList.resetCurrentNode() ;
