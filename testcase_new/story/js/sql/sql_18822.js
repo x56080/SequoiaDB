ee2a36416a47fc3494e42a5a9bd9c257d9e3cacc
yinzhen <yinzhen@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-07-15 06:32:49 +0000

    SEQUOIADBMAINSTREAM-4515 补充用例
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@42152 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/js/sql/sql_18822.js
@@ -0,0 +1,37 @@
+/************************************
+*@Description: 聚集查询where子句使用“<>”判断
+*@author:      yinzhen
+*@createdate:  2019.07.15
+*@testlinkCase: seqDB-18822
+**************************************/
+
+main();
+
+function main(){
+   var clName = COMMCLNAME + "_sql_18822";
+   commDropCL(db, COMMCSNAME, clName, true, true);
+   
+   var cl = commCreateCL(db, COMMCSNAME, clName);
+   insertData(cl);
+   var cursor = db.exec("select * from (select min(b) as min, max(b) as max from cs.cl group by a) as T where T.min <> T.max");
+   var actList = [];
+   while(cursor.next()){
+      var obj = cursor.current().toObj();
+      actList.push(obj);
+   }
+   
+   // check result
+   if(actList.length != 1){
+      throw buildException("main()", "RESULT ERROR", "check return result length", "{\"min\": \"201901\", \"max\": \"201902\"}", JSON.stringify(actList));
+   }
+   var expObj = {"min":"201901", "max":"201902"};
+   if(JSON.stringify(expObj) != JSON.stringify(actList[0])){
+      throw buildException("main()", "RESULT ERROR", "check return result", "{\"min\": \"201901\", \"max\": \"201902\"}", JSON.stringify(actList));
+   }
+   
+   commDropCL(db, COMMCSNAME, clName, true, true);
+}
+
+function insertData(cl){
+   cl.insert([{a:1, b:"201902"},{a:1, b:"201902"},{a:2, b:"201902"},{a:2, b:"201902"},{a:3, b:"201901"},{a:3, b:"201902"}]);
+}
