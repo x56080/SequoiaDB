d9f94d281021fe642bc97f1952ff105f24cb83bc
huangxiaoni <huangxiaoni@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-07-16 00:55:17 +0000

    代码优化
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@42178 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/js/sql/sql_18822.js
+++ testcase_new/story/js/sql/sql_18822.js
@@ -1,19 +1,18 @@
 /************************************
-*@Description: 聚集查询where子句使用“<>”判断
-*@author:      yinzhen
-*@createdate:  2019.07.15
-*@testlinkCase: seqDB-18822
+*@Description: seqDB-18822 聚集查询where子句使用“<>”判断
+*@Author     : 2019.07.15 yinzhen 
 **************************************/
-
 main();
 
 function main(){
    var clName = COMMCLNAME + "_sql_18822";
-   commDropCL( db, COMMCSNAME, clName, true, true );
-   
+   commDropCL( db, COMMCSNAME, clName, true, true );   
    var cl = commCreateCL( db, COMMCSNAME, clName );
-   cl.insert( [{a:1, b:"201902"}, {a:1, b:"201902"}, {a:2, b:"201902"}, {a:2, b:"201902"},
-      {a:3, b:"201901"}, {a:3, b:"201902"}]);
+   
+   cl.insert( [{a:1, b:"201902"}, {a:1, b:"201902"}, 
+               {a:2, b:"201902"}, {a:2, b:"201902"},
+               {a:3, b:"201901"}, {a:3, b:"201902"}]);
+      
    var cursor = db.exec( "select * from (select min(b) as min, max(b) as max from "
       + COMMCSNAME + "." + clName + " group by a) as T where T.min <> T.max" );
    var actList = [];
@@ -23,14 +22,16 @@ function main(){
    }
    
    // check result
-   if( actList.length != 1 ){
-      throw buildException( "main()", "RESULT ERROR", "check return result length", 
-         "{\"min\": \"201901\", \"max\": \"201902\"}", JSON.stringify( actList ) );
+   if( actList.length !== 1 ){
+      throw buildException( "main", "RESULT ERROR", "check return result length", 
+               "{\"min\": \"201901\", \"max\": \"201902\"}", 
+               JSON.stringify( actList ) );
    }
    var expObj = {"min":"201901", "max":"201902"};
-   if( JSON.stringify( expObj ) != JSON.stringify( actList[0] ) ){
-      throw buildException( "main()", "RESULT ERROR", "check return result",
-         "{\"min\": \"201901\", \"max\": \"201902\"}", JSON.stringify( actList ) );
+   if( JSON.stringify( expObj ) !== JSON.stringify( actList[0] ) ){
+      throw buildException( "main", "RESULT ERROR", "check return result",
+               "{\"min\": \"201901\", \"max\": \"201902\"}", 
+               JSON.stringify( actList ) );
    }
    
    commDropCL( db, COMMCSNAME, clName, true, true );
