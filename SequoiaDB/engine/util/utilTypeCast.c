147050b1406718d78dddb0a97fb25d95d8afabcb
hejiawen <hejiawen@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-09-02 04:20:46 +0000

    SEQUOIADBMAINSTREAM-4848 调整解析数值支持123.E格式
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@42965 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/util/utilTypeCast.c
+++ SequoiaDB/engine/util/utilTypeCast.c
@@ -157,7 +157,9 @@ SDB_EXPORT INT32 utilStrToNumber( const CHAR* data, INT32 length,
          ++pStr ;
          ++len ;
 
-         if( *pStr < '0' || *pStr > '9' )
+         if( ( *pStr < '0' || *pStr > '9' ) &&
+             ( *pStr != 'e' && *pStr != 'E' ) )
+         {
          {
             goto finish ;
          }
