0e3f5edf4f354ad37f421e7503ecdc0f31e8f717
fangjiabin <fangjiabin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-09-16 02:30:43 +0000

    SEQUOIADBMAINSTREAM-4823，修改sdb shell插入超过24字节的OID时的不报错的问题
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@43183 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/spt/sptConvertor.cpp
+++ SequoiaDB/engine/spt/sptConvertor.cpp
@@ -61,6 +61,8 @@
 #define SPT_SPEOBJ_DECIMAL "$decimal"
 #define SPT_SPEOBJ_PRESICION "$precision"
 
+#define SPT_OID_STR_LENGTH 24
+
 /*
 // check date type bounds
 #define SDB_DATE_TYPE_CHECK_BOUND(tm)                 \
@@ -800,6 +802,13 @@ INT32 sptConvertor::_addSpecialObj( JSObject *obj,
       if ( SDB_OK != rc )
       {
          _setErrorMsg( "Failed to conversion ObjectId value", FALSE ) ;
+         rc = SDB_INVALIDARG ;
+         goto error ;
+      }
+
+      if( SPT_OID_STR_LENGTH != strValue.size() )
+      {
+         _setErrorMsg( "The length of ObjectId is not equal 24", FALSE );
          rc = SDB_INVALIDARG ;
          goto error ;
       }
