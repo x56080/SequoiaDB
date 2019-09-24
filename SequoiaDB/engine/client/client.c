44f609786eb464bd130ef62f34275db96014beae
tanzhaobo <tanzhaobo@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-09-24 06:41:16 +0000

    SEQUOIADBMAINSTREAM-4939：getMore从引擎端返回的rc值不为0时，驱动应该把contextID设置为-1
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@43363 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/client/client.c
+++ SequoiaDB/engine/client/client.c
@@ -630,6 +630,10 @@ static INT32 _extract ( MsgHeader *msg, INT32 size,
       goto error ;
    }
 
+   if ( SDB_OK != replyFlag )
+   {
+      contextID = -1 ;
+   }
    rc = replyFlag ;
    _pErrorBuf = NULL ;
    _errorBufSize = 0 ;
