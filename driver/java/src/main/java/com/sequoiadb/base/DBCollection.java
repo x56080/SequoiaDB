ae19797e0a3a86590f8226ecf3c6c5dde0959584
tanzhaobo <tanzhaobo@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-10-09 12:56:53 +0000

    SEQUOIADBMAINSTREAM-4981： query接口屏蔽外部输入的query and modify 的flag
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.6@43588 6121f079-4a18-41d3-8c30-8870a322f2c2

--- driver/java/src/main/java/com/sequoiadb/base/DBCollection.java
+++ driver/java/src/main/java/com/sequoiadb/base/DBCollection.java
@@ -731,8 +731,6 @@ public class DBCollection {
     public DBCursor explain(BSONObject matcher, BSONObject selector,
                             BSONObject orderBy, BSONObject hint, long skipRows, long returnRows,
                             int flag, BSONObject options) throws BaseException {
-
-        flag |= DBQuery.FLG_QUERY_EXPLAIN;
         BSONObject innerHint = new BasicBSONObject();
         if (null != hint) {
             innerHint.put(SequoiadbConstants.FIELD_NAME_HINT, hint);
@@ -741,7 +739,10 @@ public class DBCollection {
         if (null != options) {
             innerHint.put(SequoiadbConstants.FIELD_NAME_OPTIONS, options);
         }
-
+        if (flag != 0) {
+            flag = DBQuery.eraseSingleFlag(flag, DBQuery.FLG_QUERY_MODIFY);
+        }
+        flag |= DBQuery.FLG_QUERY_EXPLAIN;
         return _query(matcher, selector, orderBy, innerHint, skipRows,
                 returnRows, flag);
     }
@@ -983,6 +984,7 @@ public class DBCollection {
                           int flag) throws BaseException {
         if (flag != 0) {
             flag = DBQuery.eraseSingleFlag(flag, DBQuery.FLG_QUERY_EXPLAIN);
+            flag = DBQuery.eraseSingleFlag(flag, DBQuery.FLG_QUERY_MODIFY);
         }
         return _query(matcher, selector, orderBy, hint, skipRows, returnRows, flag);
     }
@@ -1139,9 +1141,11 @@ public class DBCollection {
             newHint.putAll(hint);
         }
         newHint.put(SequoiadbConstants.FIELD_NAME_MODIFY, modify);
-
+        if (flag != 0) {
+            flag = DBQuery.eraseSingleFlag(flag, DBQuery.FLG_QUERY_EXPLAIN);
+        }
         flag |= DBQuery.FLG_QUERY_MODIFY;
-        return query(matcher, selector, orderBy, newHint,
+        return _query(matcher, selector, orderBy, newHint,
                 skipRows, returnRows, flag);
     }
 
