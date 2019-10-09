f2ca256d5922c2361538048bc1df585194d7ef59
wangwenjing <wangwenjing@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-10-09 09:05:41 +0000

    SEQUOIADBMAINSTREAM-4997
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@43582 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/driver/cpp/list/listtest_19946.cpp
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ * @Description:    test case for C++ driver
+ *                  
+ *                  
+ * @Modify:         wenjing wang Init
+ *                  2019-10-09
+ *******************************************************************************/
+#include <gtest/gtest.h>
+#include <client.hpp>
+#include "testcommon.hpp"
+#include "arguments.hpp"
+#include "testBase.hpp"
+#include <set>
+#include <string>
+
+using namespace sdbclient ;
+using namespace bson ;
+
+class listTest : public testBase
+{
+protected:   
+   void SetUp()
+   {
+      testBase::SetUp() ;
+      INT32 rc = SDB_OK ;
+     
+   }
+   
+   void TearDown()
+   {
+      testBase::TearDown() ;
+   }
+} ;
+
+TEST_F( listTest,  AllParameters)
+{
+   INT32 rc = SDB_OK ;
+   
+   int prevGroupId = 0 ;
+   BSONObj cond = BSON( "GroupID" << BSON( "$gt" << 2 ) ) ;
+   BSONObj sel = BSON( "GroupName" << "" << "GroupID" << "" ) ;
+   BSONObj hint = BSON( "GroupID" << 1 ) ;
+   BSONObj orderby = BSON( "GroupID" << 1 ) ;
+   
+   sdbCursor cursor ;
+   rc = db.getList( cursor, SDB_LIST_GROUPS, cond, sel, orderby, hint, 0, 2 ) ;
+   if ( rc == -159){
+      return ;
+   }
+   
+   ASSERT_EQ( SDB_OK, rc ) << "fail to getList" ;
+   
+   BSONObj obj ;
+   rc = cursor.next( obj ) ;
+   ASSERT_EQ( SDB_OK, rc ) << "fail to get next" ;
+   
+   ASSERT_EQ(obj.hasField("GroupID"), 1);
+   ASSERT_EQ(obj.hasField("GroupName"), 1);
+   prevGroupId = obj.getField("GroupID").Int();
+
+   std::set<std::string> fields ;
+   obj.getFieldNames( fields) ;
+   ASSERT_EQ(2, fields.size()) ;
+
+   rc = cursor.next( obj ) ;
+   ASSERT_EQ( SDB_OK, rc ) << "fail to get next" ;
+
+   ASSERT_EQ(obj.hasField("GroupID"), 1);
+   ASSERT_EQ(obj.hasField("GroupName"), 1);
+
+   int curGroupId = obj.getField("GroupID").Int(); 
+   ASSERT_GT(curGroupId, prevGroupId) ;
+   
+   obj.getFieldNames( fields) ;
+   ASSERT_EQ(2, fields.size()) ;
+
+   rc = cursor.next( obj ) ;
+   ASSERT_EQ( SDB_DMS_EOC, rc ) << "fail to get next" ;
+   cursor.close() ;
+}
