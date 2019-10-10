824a3144addb05707cc6ac36a3fb5eacd00da6a4
tanzhaobo <tanzhaobo@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-10-10 05:26:25 +0000

    SEQUOIADBMAINSTREAM-4981： query接口屏蔽外部输入的query and modify 的flag
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@43600 6121f079-4a18-41d3-8c30-8870a322f2c2

--- driver/C#.Net/Driver/base/DBCollection.cs
+++ driver/C#.Net/Driver/base/DBCollection.cs
@@ -660,6 +660,7 @@ namespace SequoiaDB
             if (flag != 0)
             {
                 flag = DBQuery.eraseSingleFlag(flag, DBQuery.FLG_QUERY_EXPLAIN);
+                flag = DBQuery.eraseSingleFlag(flag, DBQuery.FLG_QUERY_MODIFY);
             }
             return _Query(query, selector, orderBy, hint, skipRows, returnRows, flag);
         }
@@ -738,8 +739,12 @@ namespace SequoiaDB
             }
             newHint.Add(SequoiadbConstants.FIELD_MODIFY, modify);
 
+            if (flag != 0)
+            {
+                flag = DBQuery.eraseSingleFlag(flag, DBQuery.FLG_QUERY_EXPLAIN);
+            }
             flag |= DBQuery.FLG_QUERY_MODIFY;
-            return Query(query, selector, orderBy, newHint, skipRows, returnRows, flag);
+            return _Query(query, selector, orderBy, newHint, skipRows, returnRows, flag);
         }
 
         /** \fn DBCursor QueryAndUpdate(BsonDocument query, BsonDocument selector, BsonDocument orderBy, BsonDocument hint, 
@@ -863,7 +868,10 @@ namespace SequoiaDB
             {
                 newObj.Add(SequoiadbConstants.FIELD_OPTIONS, options);
             }
-
+            if (flag != 0)
+            {
+                flag = DBQuery.eraseSingleFlag(flag, DBQuery.FLG_QUERY_MODIFY);
+            }
             return _Query(query, selector, orderBy, newObj, skipRows, returnRows, flag | DBQuery.FLG_QUERY_EXPLAIN);
         }
 
--- driver/C#.Net/DriverTest/DBCollectionTest.cs
+++ driver/C#.Net/DriverTest/DBCollectionTest.cs
@@ -349,6 +349,9 @@ namespace DriverTest
             // case 3
             cursor = coll.Query(null, null, null, hint, 0, -1, 
                 DBQuery.FLG_QUERY_WITH_RETURNDATA | DBQuery.FLG_QUERY_FORCE_HINT | DBQuery.FLG_QUERY_PARALLED);
+            // case 4
+            cursor = coll.Query(null, null, null, hint, 0, -1,
+                DBQuery.FLG_QUERY_MODIFY | DBQuery.FLG_QUERY_EXPLAIN);
         }
 
         [TestMethod()]
