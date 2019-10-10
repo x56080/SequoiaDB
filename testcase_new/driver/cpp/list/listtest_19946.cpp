af8cde341833a348af4ecc37922cf0726505466a
wangwenjing <wangwenjing@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-10-10 04:24:38 +0000

    加入考虑集群组的数量
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@43593 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/driver/cpp/list/listtest_19946.cpp
+++ testcase_new/driver/cpp/list/listtest_19946.cpp
@@ -19,11 +19,27 @@ using namespace bson ;
 class listTest : public testBase
 {
 protected:   
+   int dataGroupNum ;
+
    void SetUp()
    {
       testBase::SetUp() ;
       INT32 rc = SDB_OK ;
+      
+      dataGroupNum = 0 ;
+
+      BSONObj cond = BSON( "GroupID" << BSON( "$gte" << 1000 ) ) ;
+      sdbCursor cursor ;
+      rc = db.getList( cursor, SDB_LIST_GROUPS, cond ) ;
+      if ( rc == -159){
+         return ;
+      }
      
+      BSONObj obj ;
+      while ( cursor.next(obj) == 0 )
+      {
+         dataGroupNum++ ;
+      }
    }
    
    void TearDown()
@@ -62,17 +78,19 @@ TEST_F( listTest,  AllParameters)
    obj.getFieldNames( fields) ;
    ASSERT_EQ(2, fields.size()) ;
 
-   rc = cursor.next( obj ) ;
-   ASSERT_EQ( SDB_OK, rc ) << "fail to get next" ;
+   if ( dataGroupNum >= 2 ){
+      rc = cursor.next( obj ) ;
+      ASSERT_EQ( SDB_OK, rc ) << "fail to get next" ;
 
-   ASSERT_EQ(obj.hasField("GroupID"), 1);
-   ASSERT_EQ(obj.hasField("GroupName"), 1);
+      ASSERT_EQ(obj.hasField("GroupID"), 1);
+      ASSERT_EQ(obj.hasField("GroupName"), 1);
 
-   int curGroupId = obj.getField("GroupID").Int(); 
-   ASSERT_GT(curGroupId, prevGroupId) ;
+      int curGroupId = obj.getField("GroupID").Int(); 
+      ASSERT_GT(curGroupId, prevGroupId) ;
    
-   obj.getFieldNames( fields) ;
-   ASSERT_EQ(2, fields.size()) ;
+      obj.getFieldNames( fields) ;
+      ASSERT_EQ(2, fields.size()) ;
+   }
 
    rc = cursor.next( obj ) ;
    ASSERT_EQ( SDB_DMS_EOC, rc ) << "fail to get next" ;
