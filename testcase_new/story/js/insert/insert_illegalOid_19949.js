b3edbefab00723001fa5283ee4df2685e73a6385
liuxiaoxuan <liuxiaoxuan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-10-10 05:34:30 +0000

    回归SEQUOIADBMAINSTREAM-4823，补充自动化用例
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@43607 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/js/insert/insert_illegalOid_19949.js
@@ -0,0 +1,43 @@
+/******************************************************************************
+*@Description: 插入记录，指定oid值非法
+*@author:      liuxiaoxuan
+*@createdate:  2019.10.10
+*@testlinkCase: seqDB-19949:插入记录，指定oid值非法
+******************************************************************************/
+
+main();
+function main()
+{
+    var clName = COMMCLNAME + "_insert19949";	
+    commDropCL( db, COMMCSNAME, clName, true, true, "drop collection in the beginning" ) ; 	
+    var dbcl = commCreateCL( db, COMMCSNAME, clName );
+   
+    //插入Oid值长度小于24字节
+    try
+    {
+        dbcl.insert( {a : {"$oid" : "123abcd00af12358902300"} } );   
+    }
+    catch ( e )
+    {
+        if( -6 !== e )
+        {
+            throw new Error( "insert fail: " + getErr(e) );
+        }			
+    }
+	
+    //插入Oid值长度等于24字节但内容不正确
+    try
+    {
+        dbcl.insert( {a : ObjectId( "123abcd00ef12358902300eg") } );   
+    }
+    catch ( e )
+    {
+        if( -6 !== e )
+        {
+            throw new Error( "insert fail: " + getErr(e) );
+        }			
+    }
+
+    commDropCL( db, COMMCSNAME, clName, true, true, "drop collection in the end" ) ;
+}
+
