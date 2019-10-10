7744c391129650584879837a45e47674cacace06
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-10-10 05:32:43 +0000

    fix bug: SEQUOIADBMAINSTREAM-4987 SIGABRT导致的coredump
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.6@43605 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/rtn/rtnContext.cpp
+++ SequoiaDB/engine/rtn/rtnContext.cpp
@@ -150,8 +150,11 @@ namespace engine
 
       /// wait prefetch complete
       _prefetchID = 0 ;
-      _prefetchLock.get() ;
-      _prefetchLock.release() ;
+      if ( _prefetchLock.get() )
+      {
+         _prefetchLock->lock_w() ;
+         _prefetchLock->release_w() ;
+      }
       _pPrefWatcher = NULL ;
 
       if ( _pResultBuffer )
@@ -432,7 +435,17 @@ namespace engine
          SDB_BPSCB *bpsCB = pmdGetKRCB()->getBPSCB() ;
          if ( bpsCB->isPrefetchEnabled() )
          {
-            _prefetchLock.get_shared() ;
+            if ( !_prefetchLock.get() )
+            {
+               ossRWMutex *pMutex = SDB_OSS_NEW ossRWMutex() ;
+               if ( !pMutex )
+               {
+                  goto done ;
+               }
+               _prefetchLock = ctxMutexPtr( pMutex ) ;
+            }
+
+            _prefetchLock->lock_r() ;
             if ( SDB_OK == bpsCB->sendPrefechReq( bpsDataPref( _prefetchID,
                                                                this ) ) )
             {
@@ -440,7 +453,7 @@ namespace engine
             }
             else
             {
-               _prefetchLock.release_shared() ;
+               _prefetchLock->release_r() ;
             }
          }
       }
@@ -533,7 +546,8 @@ namespace engine
       _waitPrefetchNum.dec() ;
       if ( FALSE == againTry )
       {
-         _prefetchLock.release_shared() ;
+         ctxMutexPtr tmpMutexPtr( _prefetchLock ) ;
+         tmpMutexPtr->release_r() ;
       }
       return rc ;
    error:
