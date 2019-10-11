835ef474fe6c1a4f0fc23ccf16aba7a6c31fe54e
tanzhaobo <tanzhaobo@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-10-11 10:09:31 +0000

    SEQUOIADBMAINSTREAM-4098：2.8驱动getSnapshot的hint不生效，将该参数声明为Reserved状态。
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.8@43670 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/client/client.hpp
+++ SequoiaDB/engine/client/client.hpp
@@ -3443,10 +3443,9 @@ namespace sdbclient
     \param [in] condition The matching rule, match all the documents if not provided.
     \param [in] select The selective rule, return the whole document if not provided.
     \param [in] orderBy The ordered rule, result set is unordered if not provided.
-    \param [in] hint The options provided for specific list type. Reserved.
-    \param [in] numToSkip Skip the first numToSkip documents.
-    \param [in] numToReturn Only return numToReturn documents. -1 means return
-                all matched results.
+    \param [in] hint Reserved.
+    \param [in] numToSkip Reserved.
+    \param [in] numToReturn Reserved.
     \param [out] cursor The return cursor object of query.
     \retval SDB_OK Operation Success
     \retval Others Operation Fail
