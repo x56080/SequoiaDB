181b29dc9e2e4225a9ca0806b16a97d990f8758c
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-10-11 04:23:24 +0000

    fix SEQUOIADBMAINSTREAM-4634 磁盘空间满，open lob文件失败导致缓存注册异常
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.6@43637 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/util/utilCache.cpp
+++ SequoiaDB/engine/util/utilCache.cpp
@@ -1778,6 +1778,11 @@ namespace engine
       INT32 rc = SDB_OK ;
       utilCacheBucket* pBucket = NULL ;
 
+      if ( _hasReg )
+      {
+         goto done ;
+      }
+
       if ( !_pMgr || !pFile || 0 == pageSize || 0 == bucketSize )
       {
          rc = SDB_INVALIDARG ;
@@ -1810,6 +1815,7 @@ namespace engine
    done:
       return rc ;
    error:
+      fini( NULL ) ;
       goto done ;
    }
 
