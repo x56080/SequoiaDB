4ad043446c80b8a862ec5a9a1560edfdf235b00b
tanzhaobo <tanzhaobo@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-10-12 08:44:52 +0000

    SEQUOIADBMAINSTREAM-5020：修复sdb shell内存泄漏问题。
    git-svn-id: http://192.168.20.11/sequoiadb/branches/sequoiadb_2.6@43695 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/spt/sptConvertor.cpp
+++ SequoiaDB/engine/spt/sptConvertor.cpp
@@ -1296,8 +1296,7 @@ INT32 sptConvertor::_appendToBson( const std::string &name,
                   {
                      bson_append_bson( bs, name.c_str(), bsobj ) ;
                   }
-
-                  bson_destroy( bsobj ) ;
+                  bson_dispose( bsobj ) ;
                }
                else if ( SDB_OK != rc )
                {
