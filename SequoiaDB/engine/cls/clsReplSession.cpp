01c9f9427b0f0e2d167b9d11e1de3158d985064a
heguoming <heguoming@sequoiadb.com>
2019-11-13 17:37:46 +0800

    SEQUOIADBMAINSTREAM-5209: fix parallel replay issue
    increase request ID after error in parallel replay moved LSN back to ignore
    results from earlier requests

--- SequoiaDB/engine/cls/clsReplSession.cpp
+++ SequoiaDB/engine/cls/clsReplSession.cpp
@@ -184,6 +184,9 @@ namespace engine
             PD_LOG( PDEVENT, "Session[%s]: Move lsn to[%u, %llu]",
                     sessionName(), expectLSN.version, expectLSN.offset ) ;
          }
+         // force to increase request ID
+         // ignore results from earlier requests
+         ++ _requestID ;
       }
 
       if ( CLS_SESSION_STATUS_SYNC == _status )
@@ -935,11 +938,6 @@ namespace engine
          recordHeader = (dpsLogRecordHeader *)log ;
          needRollback = FALSE ;
 
-         PD_LOG( PDDEBUG, "Session[%s]: Replay record [lsn offset: %lld, "
-                 "version: %d, len:%d, preLsn:%lld]", sessionName(),
-                 recordHeader->_lsn, recordHeader->_version,
-                 recordHeader->_length, recordHeader->_preLsn ) ;
-
          expectLSN = _logger->expectLsn() ;
 
          if ( expectLSN.compareOffset( recordHeader->_lsn ) > 0 )
@@ -1003,6 +1001,20 @@ namespace engine
             goto error ;
          }
 #endif
+
+         if ( expectLSN.offset != recordHeader->_lsn )
+         {
+            PD_LOG( PDERROR, "Session[%s]: Row record [%llu] failed to "\
+                    "match expect LSN [%llu]", sessionName(),
+                    recordHeader->_lsn, expectLSN.offset ) ;
+            rc = SDB_SYS ;
+            goto error ;
+         }
+
+         PD_LOG( PDDEBUG, "Session[%s]: Replay record [lsn offset: %lld, "
+                 "version: %d, len:%d, preLsn:%lld]", sessionName(),
+                 recordHeader->_lsn, recordHeader->_version,
+                 recordHeader->_length, recordHeader->_preLsn ) ;
 
          rc = _replay( recordHeader ) ;
          if ( SDB_OK != rc )
