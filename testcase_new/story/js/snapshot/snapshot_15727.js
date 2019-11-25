eedf654f193cebb642130d78b0e5cc060de1e608
zhaoxiaoni <zhaoxiaoni@sequoiadb.com>
2019-11-25 14:12:25 +0800

    CI失败用例修改

--- testcase_new/story/js/snapshot/snapshot_15727.js
+++ testcase_new/story/js/snapshot/snapshot_15727.js
@@ -6,58 +6,74 @@
 
 function main()
 {
-   if (commGetGroupsNum(db)<2)
-   {
+   var groups = commGetGroups(db);
+   if ( groups.length < 2)
+   {  
       return ;
    }
-   var groups = commGetGroups(db);
-   var groupname = groups[0][0].GroupName;
-   var groupnodenum = groups[0].length - 1 ;
-   //cond+skip+limit
-   cur = db.snapshot(SDB_SNAP_CONFIGS,new SdbSnapshotOption().cond({$and:[{GroupName:groupname}, {role:"data"}]}).sel({"archiveon":{$include:1},"role":{$include:1},"NOO":{$include:1}}).skip(groupnodenum-1).limit(groupnodenum).sort({role:1}));
-   var size=0;
-   while( cur.next() )
+   var groupName = groups[0][0].GroupName;
+   var groupNodeNum = groups[0].length - 1 ;
+
+   //cond+sel+skip+limit
+   var sel = {"role": 1};
+   var skip = groupNodeNum-1;
+   var limit = groupNodeNum;
+   var cond = {"$and": [{"GroupName":  groupName}, {"role": "data"}]};
+   var cursor = db.snapshot(SDB_SNAP_CONFIGS,new SdbSnapshotOption().cond(cond).sel(sel).skip(skip).limit(limit));
+
+   var actResult = [];
+   var expResult = [{"role": "data"}];
+   while( cursor.next() )
    {
-      size++;
-      var ret = cur.current();
+      actResult.push(cursor.current().toObj());
    }
-   if( size !== 1 )
+   checkResult(actResult, expResult);   
+  
+   //Specify all parameters
+   skip = 0;
+   limit = 1;
+   var  count = 0;
+   var options = {"expand": false};
+   sel = {"archiveon": 1,"role": 1};
+   cond = {"$and": [{"GroupName": groupName}, {"role": "data"}]};
+   cursor = db.snapshot(SDB_SNAP_CONFIGS, new SdbSnapshotOption().cond(cond).sel(sel).skip(skip).limit(limit).options(options));
+   var actResult = [];
+   expResult = [{"role": "data", "archiveon": 1}];
+   while( cursor.next() )
    {
-      throw buildException("check count", "", "snapshot(SDB_SNAP_CONFIGS,new SdbSnapshotOption().cond({$and:[{GroupName:groupname}, {role:\"data\"}]}).sel({\"archiveon\":{$include:1},\"role\":{$include:1},\"NOO\":{$include:1}}).skip(commGetGroupsNum(db)-1).limit(commGetGroupsNum(db)).sort({role:1}))", 1, size);
+      actResult.push(cursor.current().toObj());
    }
-   
-   //所有参数组合
-   cur = db.snapshot(SDB_SNAP_CONFIGS,new SdbSnapshotOption().cond({$and:[{GroupName:groupname}, {role:"data"}]}).sel({"archiveon":{$include:1},"NOO":{$include:1}}).skip(0).limit(1).sort({NOO:1}).options({"expand":false}))
-   var size=0;
-   while( cur.next() )
+   checkResult(actResult, expResult);
+
+   //cond+sel+options+limit
+   count = 0;
+   limit = 1;
+   sel = {"role": 1, "archiveon": 1};
+   var options = {"expand": false};
+   cond = {$and:[{GroupName: groupName}, {role: "data"}]};
+   cursor = db.snapshot(SDB_SNAP_CONFIGS,new SdbSnapshotOption().cond(cond).sel(sel).options(options).limit(limit))
+   var actResult = [];
+   expResult = [{"role": "data"}];
+   while( cursor.next() )
    {
-      size++;
-      var ret = cur.current();
-      if (ret.toString() !== "{}") 
-      {
-         throw buildException("check count", "", "snapshot(SDB_SNAP_CONFIGS,new SdbSnapshotOption().cond({$and:[{GroupName:groupname}, {role:\"data\"}]}).sel({\"archiveon\":{$include:1},\"NOO\":{$include:1}}).skip(0).limit(1).sort({NOO:1}).options({\"expand\":false}))", "{}", ret.toString());
-      }
+      actResult.push(cursor.current().toObj());
+      count++;
    }
-   if( size !== 1 )
+   if( count !== 1 )
    {
-      throw buildException("check count", "", "snapshot(SDB_SNAP_CONFIGS,new SdbSnapshotOption().cond({$and:[{GroupName:groupname}, {role:\"data\"}]}).sel({\"archiveon\":{$include:1},\"NOO\":{$include:1}}).skip(0).limit(1).sort({NOO:1}).options({\"expand\":false}))", 1, size);
-   }
-   //cond+options
-   cur = db.snapshot(SDB_SNAP_CONFIGS,new SdbSnapshotOption().cond({$and:[{GroupName:groupname}, {role:"data"}]}).options({"expand":false}))
-   var size=0;
-   while( cur.next() )
-   {
-      size++;
-      var ret = cur.current();
-      if( ret.toObj().archiveon !== undefined)
-      {
-         throw buildException("check count", "", "snapshot(SDB_SNAP_CONFIGS,new SdbSnapshotOption().cond({$and:[{GroupName:groupname}, {role:\"data\"}]}).options({\"expand\":false}))", undefined, ret.toObj().archiveon);
-      }
-   }
-   if( size <= 0 )
-   {
-      throw buildException("check count", "", "test snapshot", ">0", "<=0");
+      throw new Error("count: " + count);
    }
 }
 
-main(db) ;
+try
+{
+   main();
+}
+catch(e)
+{
+   if ( e.constructor === Error )
+   {
+      println(e.stack) ;  
+   }
+   throw e;
+}
