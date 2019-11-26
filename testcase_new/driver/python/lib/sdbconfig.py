695ae50f032cc5f82c0fbe004de030ef99174d0a
chensiqin <chensiqin@sequoiadb.com>
2019-11-26 10:38:54 +0800

    分支3.2增加变量注释

--- testcase_new/driver/python/lib/sdbconfig.py
+++ testcase_new/driver/python/lib/sdbconfig.py
@@ -10,13 +10,21 @@ class SdbConfig(object):
       config_file = os.path.join(os.getcwd(), "config.json")
       fp = open(config_file)
       configs = json.load(fp)
+      #协调节点主机名，默认localhost
       self.host_name = configs['HOSTNAME']
+      #协调节点端口号，默认11810
       self.service = configs['SVCNAME']
+      #公共CS
       self.changed_prefix = configs['CHANGEDPREFIX']
+      #用例创建节点预留端口号最小值
       self.rsrv_port_begin = configs['RSRVPORTBEGIN']
+      #用例创建节点预留端口号最大值
       self.rsrv_port_end = configs['RSRVPORTEND']
+      #用例创建节点存放节点数据目录
       self.rsrv_node_dir = configs['RSRVNODEDIR']
+      #用例存放临时文件的目录
       self.work_dir = configs['WORKDIR']
+      #失败是否停止用例开关
       self.break_on_failure = configs['BREAK_ON_FAILURE']
 
       fp.close()
