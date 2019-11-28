eedce63ffaca20c81b4d4071dd7f89328d1e206a
zhaoyu <zhaoyu@sequoiadb.com>
2019-11-28 13:26:14 +0800

    code测试用例同步到分支3.2

--- testcase_new/story/js/code/code_11052.js
+++ testcase_new/story/js/code/code_11052.js
@@ -1,7 +1,7 @@
 ﻿/************************************************************************
 *@Description:   seqDB-11052:支持code类型
 *@Author:  2017/2/7  huangxiaoni init
-*          2019/11/18 zhaoyu modify
+*		     2019/11/18 zhaoyu modify
 ************************************************************************/
 try
 {
@@ -33,37 +33,11 @@ function main()
    db.eval( cmd );
 
    checkResult(procedureName);
-   
-   commRemoveProcedure( db, procedureName);
-   if( commIsStandalone(db) )
-   {
-      println("\n---Deploy mode is standalone!");
-      return;
-   } 
-   //ready env
-   var procedureName = "abc11052";
-   commRemoveProcedure( db, procedureName);
-
-   //structural data
-   println("\n---Begin to createProcedure.");
-   var cmd = "db.createProcedure( function " + procedureName + "(x, y){return x+y;} )";       
-   db.eval( cmd );
-
-   checkResult(procedureName);
    commRemoveProcedure( db, procedureName);
 }
 
 function checkResult(procedureName)
 {
-   //compare the returned records
-   var rc = db.listProcedures({ name: procedureName } ).current().toObj().func;
-
-   var expRlt = '{"$code":"function ' + procedureName + '(x, y) {\\n    return x + y;\\n}"}';
-   var actRlt = JSON.stringify( rc );
-   if( expRlt !== actRlt )
-   {
-   throw new Error("expect: " + expRlt + ",actual: " + actRlt);
-   }
    //compare the returned records
    var rc = db.listProcedures({ name: procedureName } ).current().toObj().func;
 
