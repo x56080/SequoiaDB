f2b7ffe6ee004e003f4d3e4933b0af8576bf00a3
yinzhen <yinzhen@sequoiadb.com>
2019-11-28 14:04:42 +0800

    ci-#1306 修改CI失败用例

--- testcase_new/story/java/src/test/java/com/sequoiadb/fulltext/parallel/Fulltext12123.java
+++ testcase_new/story/java/src/test/java/com/sequoiadb/fulltext/parallel/Fulltext12123.java
@@ -3,7 +3,9 @@ package com.sequoiadb.fulltext.parallel;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.bson.BSONObject;
 import org.bson.util.JSON;
@@ -37,6 +39,7 @@ public class Fulltext12123 extends FullTestBase {
     private String indexName = "fulltext12123";
     private int insertNum = 20000;
     private ThreadExecutor te = new ThreadExecutor(FullTextUtils.THREAD_TIMEOUT);
+    private List<Map<String, String>> clInfos = new ArrayList<>();
 
     @Override
     protected void initTestProp() {
@@ -48,6 +51,7 @@ public class Fulltext12123 extends FullTestBase {
         for (int i = 0; i < csNum; i++) {
             String csName = csBasicName + "_" + i;
             csNames.add(csName);
+
         }
 
         for (int i = 0; i < clNum; i++) {
@@ -61,6 +65,10 @@ public class Fulltext12123 extends FullTestBase {
             }
             CollectionSpace cs = sdb.createCollectionSpace(csName);
             for (int i = 0; i < clNum; i++) {
+                Map<String, String> map = new HashMap<>();
+                map.put("csName", csName);
+                map.put("clName", clNames.get(i));
+                clInfos.add(map);
                 DBCollection cl = cs.createCollection(clNames.get(i));
                 cl.createIndex("id", "{id:1}", false, false);
                 insertRecord(cl, insertNum);
@@ -75,21 +83,21 @@ public class Fulltext12123 extends FullTestBase {
     protected void caseFini() throws Exception {
         List<String> esIndexNames = new ArrayList<String>();
         List<String> cappedCLNames = new ArrayList<String>();
-        for (String csName : csNames) {
+        for (Map<String, String> clInfo : clInfos) {
+            String csName = clInfo.get("csName");
             if (sdb.isCollectionSpaceExist(csName)) {
                 CollectionSpace cs = sdb.getCollectionSpace(csName);
-                for (String clName : clNames) {
-                    DBCollection cl = cs.getCollection(clName);
-                    if (cl.isIndexExist(indexName)) {
-                        String esIndexName = FullTextDBUtils.getESIndexName(cl, indexName);
-                        esIndexNames.add(esIndexName);
-                        String cappedCLName = FullTextDBUtils.getCappedName(cl, indexName);
-                        cappedCLNames.add(cappedCLName);
-                    }
+                String clName = clInfo.get("clName");
+                DBCollection cl = cs.getCollection(clName);
+                if (cl.isIndexExist(indexName)) {
+                    String esIndexName = FullTextDBUtils.getESIndexName(cl, indexName);
+                    esIndexNames.add(esIndexName);
+                    String cappedCLName = FullTextDBUtils.getCappedName(cl, indexName);
+                    cappedCLNames.add(cappedCLName);
                 }
             }
-
         }
+
         for (String csName : csNames) {
             FullTextDBUtils.dropCollectionSpace(sdb, csName);
         }
@@ -101,16 +109,18 @@ public class Fulltext12123 extends FullTestBase {
     @Test
     public void test() throws Exception {
         // 获取原始集合所在组及固定集合名，作为后续结果校验的输入
-        List<String> cappedCLNames = new ArrayList<>();
-        List<String> esIndexNames = new ArrayList<>();
-        for (String csName : csNames) {
-            CollectionSpace cs = sdb.getCollectionSpace(csName);
-            for (int i = 0; i < clNum / 2; i++) {
-                DBCollection cl = cs.getCollection(clNames.get(i));
-                String cappedCLName = FullTextDBUtils.getCappedName(cl, indexName);
-                cappedCLNames.add(cappedCLName);
-                List<String> esIndexName = FullTextDBUtils.getESIndexNames(cl, indexName);
-                esIndexNames.addAll(esIndexName);
+        for (Map<String, String> clInfo : clInfos) {
+            String csName = clInfo.get("csName");
+            if (sdb.isCollectionSpaceExist(csName)) {
+                CollectionSpace cs = sdb.getCollectionSpace(csName);
+                String clName = clInfo.get("clName");
+                DBCollection cl = cs.getCollection(clName);
+                if (cl.isIndexExist(indexName)) {
+                    String esIndexName = FullTextDBUtils.getESIndexName(cl, indexName);
+                    clInfo.put("esIndexName", esIndexName);
+                    String cappedCLName = FullTextDBUtils.getCappedName(cl, indexName);
+                    clInfo.put("cappedCLName", cappedCLName);
+                }
             }
         }
 
@@ -131,23 +141,21 @@ public class Fulltext12123 extends FullTestBase {
         // 由于线程同时创建/删除了多个全文索引，无法通过返回值来判断预期结果；
         // 如果集合存在，则原始集合与ES端数据一致，主备节点数据一致
         // 如果集合不存在，则对应固定集合也被删除，无固定集合残留
-        for (int i = 0; i < csNames.size(); i++) {
-            if (sdb.isCollectionSpaceExist(csNames.get(i))) {
-                CollectionSpace cs = sdb.getCollectionSpace(csNames.get(i));
-                for (int j = 0; j < clNum; j++) {
-                    DBCollection cl = cs.getCollection(clNames.get(j));
-                    if (cs.isCollectionExist(clNames.get(j))) {
-                        if (cl.isIndexExist(indexName)) {
-                            Assert.assertTrue(FullTextUtils.isIndexCreated(cl, indexName, insertNum));
+        for (Map<String, String> clInfo : clInfos) {
+            String csName = clInfo.get("csName");
+            if (sdb.isCollectionSpaceExist(csName)) {
+                CollectionSpace cs = sdb.getCollectionSpace(csName);
+                String clName = clInfo.get("clName");
+                if (cs.isCollectionExist(clName)) {
+                    DBCollection cl = cs.getCollection(clName);
+                    if (cl.isIndexExist(indexName)) {
+                        Assert.assertTrue(FullTextUtils.isIndexCreated(cl, indexName, insertNum));
+                    } else {
+                        String cappedCLName = clInfo.get("cappedCLName");
+                        String esIndexName = clInfo.get("cappedCLName");
+                        if (cappedCLName != null && esIndexName != null) {
+                            Assert.assertTrue(FullTextUtils.isIndexDeleted(sdb, esIndexName, cappedCLName));
                         }
-                    }
-                    // 只校验删除全文索引成功时，固定集合删除成功的逻辑；成功的逻辑在if分支已校验
-                    if ((i + 1) * j + 1 <= cappedCLNames.size()) {
-                        System.out.println("cappedCLNames.get((i + 1) * j):" + cappedCLNames.get((i + 1) * j));
-                        System.out.println("esIndexNames.get((i + 1) * j):" + esIndexNames.get((i + 1) * j));
-
-                        Assert.assertTrue(FullTextUtils.isIndexDeleted(sdb, esIndexNames.get((i + 1) * j),
-                                cappedCLNames.get((i + 1) * j)));
                     }
                 }
             }
