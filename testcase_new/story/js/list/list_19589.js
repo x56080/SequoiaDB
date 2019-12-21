999f402d1cef46cbf4f87995e09aec4b8100fbe3
zhaoxiaoni <42-zhaoxiaoni@users.noreply.gitlab.example.com>
2019-12-21 16:01:43 +0800

    优化list相关用例

--- testcase_new/story/js/list/list_19589.js
+++ testcase_new/story/js/list/list_19589.js
@@ -4,36 +4,17 @@
 *@author      : wangkexin
 *@Date        : 2019-09-27
 ******************************************************************************/
-function main ()
-{
-   var groups = commGetGroups( db, false, "", true, true, false );
-   var dataNodeNames = [];
-   for( var i = 0; i < groups.length; i++ )
-   {
-      dataNodeNames.push( groups[i][1].HostName + ":" + groups[i][1].svcname );
-   }
+testConf.skipStandAlone = true;
 
-   for( var index in dataNodeNames )
-   {
-      var cur = db.list( SDB_LIST_SVCTASKS, { "NodeName": dataNodeNames[index], "TaskID": 0, "TaskName": "Default" } );
-      var arr = cur.toArray();
-      if( arr.length != 1 )
-      {
-         println( "act arr :" + arr );
-         throw new Error( "check list failed, exp returned number is 1, act returned number is " + arr.length );
-      }
-   }
-}
+main( test );
 
-try
+function test()
 {
-   main();
-}
-catch( e )
-{
-   if( e.constructor === Error )
+   var groups = commGetGroups( db );
+   var nodeName =  groups[0][1].HostName + ":" +  groups[0][1].svcname;
+   var array = db.list( SDB_LIST_SVCTASKS, { "NodeName": nodeName, "TaskID": 0, "TaskName": "Default" } ).toArray();
+   if( array.length !== 1 )
    {
-      println( e.stack );
+      throw new Error( "array.length = " + array.length );
    }
-   throw e;
 }
--- testcase_new/story/js/list_sync/list_19696.js
+++ testcase_new/story/js/list_sync/list_19696.js
@@ -4,49 +4,34 @@
 *@author      : wangkexin
 *@Date        : 2019-09-27
 ******************************************************************************/
-function main ()
+testConf.skipStandAlone = true;
+
+main( test );
+
+function test()
 {
-   if( commIsStandalone( db ) )
+   var groupName = commGetGroups( db )[0][0].GroupName;
+   var backupName = "backup_19696";
+
+   db.backup( { Name: backupName, GroupName: groupName } );
+   try
    {
-      println( "------Deploy is standalone" );
-      return;
-   }
-
-   var groupsArray = commGetGroups( db, false, "", true, true, false );
-   var rgName = groupsArray[0][0].GroupName;
-   var backupName = "backup19696";
-
-   //backup group data 
-   println( "begin to backup data group: " + rgName );
-   db.backup( { Name: backupName, GroupName: rgName } );
-
-   //check list result
-   var fields = ["Version", "Name", "ID", "NodeName", "GroupName", "EnsureInc", "BeginLSNOffset", "EndLSNOffset", "TransLSNOffset", "StartTime", "LastLSN", "LastLSNCode", "HasError"];
-   var cur = db.list( SDB_LIST_BACKUPS, { "Name": backupName } );
-   while( cur.next() )
-   {
-      var ret = cur.current().toObj();
-      for( var index in fields )
+      var cursor = db.list( SDB_LIST_BACKUPS, { "Name": backupName } );
+      while( cursor.next() )
       {
-         var field = fields[index];
-         if( ret[field] == null )
+         var backup = cursor.current().toObj();
+         for( var key in backup )
          {
-            throw new Error( "compare field failed, exp field  [" + field + "] is not null, but found null : ret[field] = " + ret[field] );
+            if( backup[key] == null )
+            {
+               throw new Error( "backup[" + i + "] is null" );
+            }
          }
       }
    }
-   db.removeBackup( { Name: backupName } );
+   finally
+   {
+      db.removeBackup( { Name: backupName } );
+   }
 }
 
-try
-{
-   main();
-}
-catch( e )
-{
-   if( e.constructor === Error )
-   {
-      println( e.stack );
-   }
-   throw e;
-}
