69e0a793b164c1edcf0f95baa8cb6acd0ae55a5b
huangxiaoni <huangxiaoni@sequoiadb.com>
2019-12-23 16:59:15 +0800

    ci-sdbexprt_auth，解决远程创建目录后指定本地目录导入失败找不到目录的问题

--- testcase_new/story/js/sdbexprt_auth/cscl_c_l_13565.js
+++ testcase_new/story/js/sdbexprt_auth/cscl_c_l_13565.js
@@ -37,7 +37,7 @@ function main ()
 function testExprtNoCsCl ()
 {
    var csvDir = tmpFileDir + "13565/";
-   commMakeDir( "localhost", csvDir );
+   cmd.run( "mkdir -p " + csvDir ) ;
 
    var command = installPath + "bin/sdbexprt" +
       " -s " + COORDHOSTNAME +
