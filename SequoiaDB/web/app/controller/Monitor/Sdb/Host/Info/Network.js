ba858d87261d60be22d22ce1f0b1e237603d99fb
huangweigang <huangweigang@sequoiadb.com>
2019-12-31 16:03:28 +0800

    SEQUOIADBMAINSTREAM-5170 修正sac错别字

--- SequoiaDB/web/app/controller/Monitor/Sdb/Host/Info/Network.js
+++ SequoiaDB/web/app/controller/Monitor/Sdb/Host/Info/Network.js
@@ -68,8 +68,8 @@
                                  'RXPackets': rxPackets,
                                  'TXBytes':   sizeConvert( txBytes ),
                                  'TXPackets': txPackets,
-                                 'RXChart': { 'options': window.SdbSacManagerConf.NetwordInEchart },
-                                 'TXChart': { 'options': window.SdbSacManagerConf.NetwordOutEchart }
+                                 'RXChart': { 'options': window.SdbSacManagerConf.NetworkInEchart },
+                                 'TXChart': { 'options': window.SdbSacManagerConf.NetworkOutEchart }
                               } ) ;
                               netList[index2]['TXChart']['value'] = [ [ 0, 0, true, false ] ] ;
                               netList[index2]['RXChart']['value'] = [ [ 0, 0, true, false ] ] ;
