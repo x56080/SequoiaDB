5a7ef19b38cb92035137b467fd6e74064d3ce10b
zhaoyu <43-zhaoyu@users.noreply.gitlab.example.com>
2020-01-09 20:47:13 +0800

    Transaction add check v3.2

--- testcase_new/story/java/src/test/java/com/sequoiadb/testcommon/SdbTestBase.java
+++ testcase_new/story/java/src/test/java/com/sequoiadb/testcommon/SdbTestBase.java
@@ -7,6 +7,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -17,6 +18,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import org.bson.BSONObject;
 import org.bson.BasicBSONObject;
 import org.bson.util.JSON;
+import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
@@ -336,6 +338,19 @@ public class SdbTestBase {
             // sequoiadb.dropCollectionSpace(cappedCSName);
             // }
             // sdb.close() ;
+
+            // 检查事务快照
+            DBCursor cursor = sequoiadb
+                    .getSnapshot( Sequoiadb.SDB_SNAP_TRANSACTIONS, "", "", "" );
+            ArrayList< BSONObject > List = new ArrayList<>();
+            while ( cursor.hasNext() ) {
+                List.add( cursor.getNext() );
+            }
+            if ( !List.isEmpty() ) {
+                System.out.println( "SDB_SNAP_TRANSACTIONS in List:" + List );
+                Assert.fail( "SDB_SNAP_TRANSACTIONS is not empty！" );
+            }
+
         } catch ( BaseException e ) {
             e.printStackTrace();
         } finally {
