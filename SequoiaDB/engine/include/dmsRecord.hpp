f0c270fd8cd2700e8adb618388e5c0ade321d66e
Yuanxi Chen <chenyuanxi@sequoiadb.com>
2020-01-09 22:37:19 +0800

    fix record size during migration; getown bson object during delete so that LOGGING use proper delObject

--- SequoiaDB/engine/include/dmsRecord.hpp
+++ SequoiaDB/engine/include/dmsRecord.hpp
@@ -448,11 +448,11 @@ namespace engine
       // inflight migration from a v0 record
       void migrateFromV0( )
       {
+         UINT32 oldsize = ((dmsRecord_v0 *) this)->getSize() ;
          SDB_ASSERT( !(this->hasGlobTransID()), 
                      "This is not a V0 record" ) ;
          // Only migrate if has enough space for the extra size difference
-         if ( ((dmsRecord_v0 *) this)->getSize() - 
-              (DMS_RECORD_V1_METADATA_SZ - DMS_RECORD_V0_METADATA_SZ)  > 
+         if ( oldsize - DMS_RECORD_V1_METADATA_SZ >
               ((dmsRecord_v0 *) this)->getDataLength() )
          {
 
@@ -462,6 +462,9 @@ namespace engine
             // TODO:  generate createLSN using _oid for newly migrated record
 
             setHasGlobTransID() ;
+            // update record size
+            setSize( oldsize +
+                     (DMS_RECORD_V1_METADATA_SZ - DMS_RECORD_V0_METADATA_SZ) ) ;
          }
          
          return ;
