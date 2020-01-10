eccf281766e98f9e9fa8c369b1f1eec23fad2af0
chensiqin <chensiqin@sequoiadb.com>
2020-01-10 17:16:10 +0800

    SEQUOIADBMAINSTREAM-5357问题单屏蔽用例

--- testcase_new/story/java/src/test/java/com/sequoiadb/split/Split10536.java
+++ testcase_new/story/java/src/test/java/com/sequoiadb/split/Split10536.java
@@ -77,7 +77,8 @@ public class Split10536 extends SdbTestBase {
         cl.bulkInsert( tmp, 0 );
     }
 
-    @Test
+    //SEQUOIADBMAINSTREAM-5357
+    @Test(enabled=false)
     public void test() {
         Sequoiadb db = null;
         Split splitThread = null;
