858ff2813847bc3ab51d05ec122a4734b9b01fed
zhaoxiaoni <42-zhaoxiaoni@users.noreply.gitlab.example.com>
2020-01-15 13:46:39 +0800

    对diff部分代码进行优化

--- testcase_new/story/js/sdbreplay/sdbreplay_18537.js
+++ testcase_new/story/js/sdbreplay/sdbreplay_18537.js
@@ -93,9 +93,6 @@ function checkCsvFileLocal ( rtCmd, clName, expDataArr, groupName )
       }
    }
    var cmd = "diff -w " + expCsvFilePath + " " + actCsvFilePath;
-   var diff = rtCmd.run( cmd );
-   if( diff != "" )
-   {
-      throw diff;
-   }
+   //diff比较不一致会直接抛错
+   rtCmd.run( cmd );
 }
