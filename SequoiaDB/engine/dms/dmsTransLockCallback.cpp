b1a731f0ef1f30481a3e1b072519de59b29067d8
heguoming <heguoming@sequoiadb.com>
2020-02-02 19:16:18 +0800

    SEQUOIADBMAINSTREAM-5437: quick fix issue
    - check rbsRecordData before print to log

--- SequoiaDB/engine/dms/dmsTransLockCallback.cpp
+++ SequoiaDB/engine/dms/dmsTransLockCallback.cpp
@@ -1004,9 +1004,9 @@ namespace engine
          }
 
 #ifdef _DEBUG
-         if ( _oldVer )
+         if ( _oldVer && _rbsRecordData )
          {
-            PD_LOG( PDDEBUG, 
+            PD_LOG( PDDEBUG,
                     "Set oldVer[%x] for rid[%s] in memory, lockmod=%d,"
                     "_useOldVersion=%d,_rbsRecordData.isEmpty()=%d",
                     _oldVer, lockId.toString().c_str(), requestLockMode,
@@ -1019,7 +1019,7 @@ namespace engine
       PD_TRACE_EXIT( SDB_DMSTRANSLOCKCALLBACK_AFTERLOCKACQUIRE );
       return  rc ;
    error :
-      goto done ; 
+      goto done ;
    }
 
    // Description:
