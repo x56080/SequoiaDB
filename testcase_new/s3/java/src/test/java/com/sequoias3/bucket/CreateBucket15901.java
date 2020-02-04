a82210594689a597da2df7ade44f44b7a886c998
fanyu <fanyu@sequoiadb.com>
2020-02-04 15:37:46 +0800

    SEQUOIADBMAINSTREAM-5345 补充校验时间

--- testcase_new/s3/java/src/test/java/com/sequoias3/bucket/CreateBucket15901.java
+++ testcase_new/s3/java/src/test/java/com/sequoias3/bucket/CreateBucket15901.java
@@ -1,17 +1,19 @@
 package com.sequoias3.bucket;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.Bucket;
-import com.amazonaws.services.s3.model.CreateBucketRequest;
-import com.amazonaws.services.s3.model.Owner;
-import com.sequoias3.testcommon.CommLib;
-import com.sequoias3.testcommon.S3TestBase;
+import java.util.List;
+
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.util.List;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.CreateBucketRequest;
+import com.amazonaws.services.s3.model.Owner;
+import com.amazonaws.services.s3.model.S3Object;
+import com.sequoias3.testcommon.CommLib;
+import com.sequoias3.testcommon.S3TestBase;
 
 /**
  * test content: create bucket testlink-case: seqDB-15901
@@ -21,7 +23,8 @@ import java.util.List;
  * @version 1.00
  */
 public class CreateBucket15901 extends S3TestBase {
-    String bucketName = "bucket15901";
+    private String bucketName = "bucket15901";
+    private String objectName = "object15901";
     private boolean runSuccess = false;
     private AmazonS3 s3Client = null;
 
@@ -29,14 +32,14 @@ public class CreateBucket15901 extends S3TestBase {
     @BeforeClass
     private void setUp() {
         s3Client = CommLib.buildS3Client();
-        if ( s3Client.doesBucketExist( bucketName ) ) {
-            s3Client.deleteBucket( bucketName );
-        }
+        CommLib.clearBucket( s3Client, bucketName );
     }
 
     @Test
     public void testCreateBucket() {
         s3Client.createBucket( new CreateBucketRequest( bucketName ) );
+        //创建对象是为了校验桶的创建时间
+        s3Client.putObject( bucketName, objectName, "test" );
         checkCreateBucketResult();
         runSuccess = true;
     }
@@ -45,7 +48,7 @@ public class CreateBucket15901 extends S3TestBase {
     private void tearDown() {
         try {
             if ( runSuccess ) {
-                s3Client.deleteBucket( bucketName );
+                CommLib.clearBucket( s3Client, bucketName );
             }
         } finally {
             s3Client.shutdown();
@@ -54,7 +57,7 @@ public class CreateBucket15901 extends S3TestBase {
 
     private void checkCreateBucketResult() {
         // create one bucket,check the bucket name and owner name
-        List<Bucket> buckets = s3Client.listBuckets();
+        List< Bucket > buckets = s3Client.listBuckets();
         boolean findBucketFlag = false;
         for ( int i = 0; i < buckets.size(); i++ ) {
             String actBucketName = buckets.get( i ).getName();
@@ -63,6 +66,13 @@ public class CreateBucket15901 extends S3TestBase {
                 Owner actOwner = buckets.get( i ).getOwner();
                 Assert.assertEquals( actOwner.getDisplayName(),
                         S3TestBase.s3UserName );
+                S3Object object = s3Client.getObject( bucketName, objectName );
+                long actDate = buckets.get( i ).getCreationDate().getTime();
+                long expDate = object.getObjectMetadata().getLastModified()
+                        .getTime();
+                //桶的创建时间减去对象的创建时间的绝对值不超过30分钟
+                Assert.assertTrue( Math.abs( actDate - expDate ) < 30 * 60 *
+                        1000 );
                 findBucketFlag = true;
                 break;
             }
