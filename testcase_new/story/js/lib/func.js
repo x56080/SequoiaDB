3af8fd1c44cefae36f41de82a1f19f3296292c87
luweikang <luweikang@sequoiadb.com>
2020-02-05 15:48:37 +0800

    create RG公共方法优化，增加创建节点配置选项

--- testcase_new/story/js/lib/func.js
+++ testcase_new/story/js/lib/func.js
@@ -1707,12 +1707,16 @@ function commInspectData( db, group, csName, clName, loop )
                   [{hostname: "xxx", svcname: "xxx", logpath: "xxx"}]
 @author: luweikang
 ******************************************************************* */
-function commCreateRG( db, rgName, nodeNum, hostname )
+function commCreateRG( db, rgName, nodeNum, hostname, nodeOption )
 {
    if( hostname === undefined )
    { 
       var nodeList = commGetSnapshot( db, SDB_SNAP_SYSTEM, {Role: "coord", RawData: true} );
       hostname = nodeList[0].HostName;
+   }
+   if( nodeOption === undefined )
+   {
+      nodeOption = { diaglevel: 5 };
    }
    
    try
@@ -1750,7 +1754,7 @@ function commCreateRG( db, rgName, nodeNum, hostname )
          }
          try
          {
-            rg.createNode( hostname, svc, dbPath, { diaglevel: 5 } );
+            rg.createNode( hostname, svc, dbPath, nodeOption );
             println( "create node: " + hostname + ":" + svc + " dbpath: " + dbPath );
             var nodeInfo = { "hostname": hostname, "svcname": svc, "logpath": hostname + ":" + CMSVCNAME + "@" + dbPath + "/diaglog/sdbdiag.log" };
             nodeInfos.push( nodeInfo );
