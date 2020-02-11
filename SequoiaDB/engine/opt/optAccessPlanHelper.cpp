8c89de53421abb96e280985e1fec1efb75771437
Chen Yuanxi <chenyuanxi@sequoiadb.com>
2020-02-12 04:03:26 +0800

    5467, allow RC to use index even if it was created after the transaction started

--- SequoiaDB/engine/opt/optAccessPlanHelper.cpp
+++ SequoiaDB/engine/opt/optAccessPlanHelper.cpp
@@ -279,7 +279,8 @@ namespace engine
       //       be available for write operators from global transactions
       if ( options.isWriteOp() ||
            !_eduCB->isGlobTrans() ||
-           !sdbGetTransCB()->isRRSupported() )
+           ( _eduCB->getTransExecutor()->getTransIsolation() != 
+             TRANS_ISOLATION_RR ) )
       {
          goto done ;
       }
