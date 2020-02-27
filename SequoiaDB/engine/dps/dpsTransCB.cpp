b568ec34339296f6368190dd9c27a0880058d2b8
Yuanxi Chen <chenyuanxi@sequoiadb.com>
2020-02-28 06:53:24 +0800

    check for null pointer

--- SequoiaDB/engine/dps/dpsTransCB.cpp
+++ SequoiaDB/engine/dps/dpsTransCB.cpp
@@ -2782,11 +2782,16 @@ namespace engine
       INT32 rc = SDB_OK ;
       PD_TRACE_ENTRY( SDB_DPSTRANSCB_TRANSLOCKGETX ) ;
 
-      PD_TRACE4( SDB_DPSTRANSCB_TRANSLOCKGETX, 
+      PD_TRACE2( SDB_DPSTRANSCB_TRANSLOCKGETX, 
                  PD_PACK_UINT(logicCSID),
-                 PD_PACK_UINT(collectionID),
-                 PD_PACK_UINT(recordID->_extent),
-                 PD_PACK_UINT(recordID->_offset) ) ;
+                 PD_PACK_UINT(collectionID) ) ;
+      if ( recordID )
+      {
+         PD_TRACE2( SDB_DPSTRANSCB_TRANSLOCKGETX, 
+                    PD_PACK_UINT(recordID->_extent),
+                    PD_PACK_UINT(recordID->_offset) ) ;
+      }
+
       if ( _isOn )
       {
          dpsTransLockId lockId( logicCSID, collectionID, recordID );
@@ -2856,12 +2861,18 @@ namespace engine
       INT32 rc = SDB_OK ;
       PD_TRACE_ENTRY( SDB_DPSTRANSCB_TRANSLOCKGETS ) ;
 
-      PD_TRACE4( SDB_DPSTRANSCB_TRANSLOCKGETS, 
+      PD_TRACE2( SDB_DPSTRANSCB_TRANSLOCKGETS, 
                  PD_PACK_UINT(logicCSID),
-                 PD_PACK_UINT(collectionID),
-                 PD_PACK_UINT(recordID->_extent),
-                 PD_PACK_UINT(recordID->_offset) ) ;
-      if ( !_isOn )
+                 PD_PACK_UINT(collectionID) ) ;
+
+      if ( recordID )
+      {
+         PD_TRACE2( SDB_DPSTRANSCB_TRANSLOCKGETS, 
+                    PD_PACK_UINT(recordID->_extent),
+                    PD_PACK_UINT(recordID->_offset) ) ;
+      }
+
+      if ( _isOn )
       {
          dpsTransLockId lockId( logicCSID, collectionID, recordID );
          rc = _transLockMgr->acquire( eduCB->getTransExecutor(),
@@ -3170,11 +3181,15 @@ namespace engine
       INT32 rc = SDB_OK ;
       PD_TRACE_ENTRY( SDB_DPSTRANSCB_TRANSLOCKTRYS ) ;
 
-      PD_TRACE4( SDB_DPSTRANSCB_TRANSLOCKTRYS, 
+      PD_TRACE2( SDB_DPSTRANSCB_TRANSLOCKTRYS, 
                  PD_PACK_UINT(logicCSID),
-                 PD_PACK_UINT(collectionID),
-                 PD_PACK_UINT(recordID->_extent),
-                 PD_PACK_UINT(recordID->_offset) ) ;
+                 PD_PACK_UINT(collectionID) ) ;
+      if ( recordID )
+      {
+         PD_TRACE2( SDB_DPSTRANSCB_TRANSLOCKGETS, 
+                    PD_PACK_UINT(recordID->_extent),
+                    PD_PACK_UINT(recordID->_offset) ) ;
+      }
 
       if ( _isOn )
       {
