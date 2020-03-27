93db719709aacbade0e01a5ffba39c605641a936
zhaoyu <zhaoyu@sequoiadb.com>
2020-03-27 18:35:00 +0800

    1、适配mvcc分支；2、优化用例实现

--- testcase_new/sdv/java/src/test/java/com/sequoiadb/lzwtransaction/LzwTransUtils.java
+++ testcase_new/sdv/java/src/test/java/com/sequoiadb/lzwtransaction/LzwTransUtils.java
@@ -7,81 +7,45 @@ import java.util.Random;
 import org.bson.BSONObject;
 import org.bson.BasicBSONObject;
 import org.bson.types.BSONDecimal;
-import org.testng.Assert;
 
 import com.sequoiadb.base.CollectionSpace;
 import com.sequoiadb.base.DBCollection;
 import com.sequoiadb.base.Node;
 import com.sequoiadb.base.ReplicaGroup;
 import com.sequoiadb.base.Sequoiadb;
-import com.sequoiadb.exception.BaseException;
 
 public class LzwTransUtils {
 
-    /**
-     * Judge the mode
-     * 
-     * @param sdb
-     * @return true/false, true is standalone, false is cluster
-     */
-    public boolean isStandAlone( Sequoiadb sdb ) {
-        try {
-            sdb.listReplicaGroups();
-        } catch ( BaseException e ) {
-            if ( e.getErrorCode() == -159 ) { // -159:The operation is for coord
-                                              // node only
-                // System.out.printf("The mode is standalone.");
-                return true;
-            }
-        }
-        return false;
-    }
-
     public static List< String > getDataRgNames( Sequoiadb sdb ) {
         List< String > rgNames = new ArrayList< String >();
-        try {
-            rgNames = sdb.getReplicaGroupNames();
-            rgNames.remove( "SYSCatalogGroup" );
-            rgNames.remove( "SYSCoord" );
-            rgNames.remove( "SYSSpare" );
-        } catch ( BaseException e ) {
-            Assert.fail( e.getMessage() );
-        }
+        rgNames = sdb.getReplicaGroupNames();
+        rgNames.remove( "SYSCatalogGroup" );
+        rgNames.remove( "SYSCoord" );
+        rgNames.remove( "SYSSpare" );
         return rgNames;
     }
 
     public static DBCollection createCL( CollectionSpace cs, String clName,
             BSONObject option ) {
         DBCollection cl = null;
-        try {
-            if ( cs.isCollectionExist( clName ) ) {
-                cs.dropCollection( clName );
-            }
-            cl = cs.createCollection( clName, option );
-        } catch ( BaseException e ) {
-            Assert.fail( e.getMessage() );
+        if ( cs.isCollectionExist( clName ) ) {
+            cs.dropCollection( clName );
         }
+        cl = cs.createCollection( clName, option );
         return cl;
     }
 
     public void insertData( DBCollection cl, int start, int recSum,
             int strLength ) {
-        try {
-            // List<BSONObject> recs = new ArrayList<BSONObject>();
-            for ( int i = start; i < recSum; i++ ) {
-                BSONObject rec = new BasicBSONObject();
-                rec.put( "_id", i );
-                rec.put( "age", i );
-                rec.put( "num", i );
-                rec.put( "str", getRandomString( strLength ) );
-                rec.put( "decimal", new BSONDecimal( "123.456" ) );
-                rec.put( "decimal", new BSONDecimal( "1." + getDecimal999() ) );
-                // recs.add(rec);
-                cl.insert( rec );
-            }
-            // cl.bulkInsert(recs, 0);
-        } catch ( BaseException e ) {
-            throw e;
+        for ( int i = start; i < recSum; i++ ) {
+            BSONObject rec = new BasicBSONObject();
+            rec.put( "_id", i );
+            rec.put( "age", i );
+            rec.put( "num", i );
+            rec.put( "str", getRandomString( strLength ) );
+            rec.put( "decimal", new BSONDecimal( "123.456" ) );
+            rec.put( "decimal", new BSONDecimal( "1." + getDecimal999() ) );
+            cl.insert( rec );
         }
     }
 
@@ -107,13 +71,9 @@ public class LzwTransUtils {
     public static String getGroupIPByGroupName( Sequoiadb sdb,
             String groupName ) {
         String dataUrl = "";
-        try {
-            ReplicaGroup replicaGroup = sdb.getReplicaGroup( groupName );
-            Node master = replicaGroup.getMaster();
-            dataUrl = master.getNodeName();
-        } catch ( BaseException e ) {
-            Assert.fail( e.getMessage() );
-        }
+        ReplicaGroup replicaGroup = sdb.getReplicaGroup( groupName );
+        Node master = replicaGroup.getMaster();
+        dataUrl = master.getNodeName();
         return dataUrl;
     }
 }
--- testcase_new/sdv/java/src/test/java/com/sequoiadb/lzwtransactionsync/TestSeqDB6670.java
+++ testcase_new/sdv/java/src/test/java/com/sequoiadb/lzwtransactionsync/TestSeqDB6670.java
@@ -1,7 +1,5 @@
 package com.sequoiadb.lzwtransactionsync;
 
-import java.util.List;
-
 import org.bson.BSONObject;
 import org.bson.BasicBSONObject;
 import org.bson.types.BasicBSONList;
@@ -14,12 +12,12 @@ import org.testng.annotations.Test;
 import com.sequoiadb.base.CollectionSpace;
 import com.sequoiadb.base.DBCollection;
 import com.sequoiadb.base.DBCursor;
-import com.sequoiadb.base.Node;
 import com.sequoiadb.base.ReplicaGroup;
 import com.sequoiadb.base.Sequoiadb;
 import com.sequoiadb.exception.BaseException;
-import com.sequoiadb.testcommon.SdbTestBase;
 import com.sequoiadb.lzwtransaction.LzwTransUtils;
+import com.sequoiadb.testcommon.CommLib;
+import com.sequoiadb.testcommon.SdbTestBase;
 
 /**
  * 1、日志满导致事务回滚的场景，比如：日志文件大小配置小点，开启事务，大批量灌数据
@@ -39,148 +37,116 @@ public class TestSeqDB6670 extends SdbTestBase {
 
     @BeforeClass()
     public void setUp() {
-        try {
-            this.sdb = new Sequoiadb( SdbTestBase.coordUrl, "", "" );
-            // 跳过 standAlone 和数据组不足的环境
-            LzwTransUtils util = new LzwTransUtils();
-            if ( util.isStandAlone( this.sdb ) ) {
-                throw new SkipException( "skip StandAlone" );
-            }
-            if ( LzwTransUtils.getDataRgNames( this.sdb ).size() < 2 ) {
-                throw new SkipException(
-                        "current environment less than tow groups " );
-            }
-            this.cs = this.sdb.getCollectionSpace( SdbTestBase.csName );
-            this.port1 = SdbTestBase.reservedPortBegin + 670;
-            this.port2 = SdbTestBase.reservedPortBegin + 680;
-            this.port3 = SdbTestBase.reservedPortBegin + 690;
-        } catch ( BaseException e ) {
-            Assert.fail( e.getMessage() );
+        sdb = new Sequoiadb( SdbTestBase.coordUrl, "", "" );
+
+        // 跳过 standAlone 和数据组不足的环境
+        if ( CommLib.isStandAlone( sdb ) ) {
+            throw new SkipException( "skip standAlone." );
         }
+        if ( CommLib.OneGroupMode( sdb ) ) {
+            throw new SkipException( "skip one group mode." );
+        }
+
+        cs = sdb.getCollectionSpace( SdbTestBase.csName );
+        port1 = SdbTestBase.reservedPortBegin + 670;
+        port2 = SdbTestBase.reservedPortBegin + 680;
+        port3 = SdbTestBase.reservedPortBegin + 690;
     }
 
     /**
      * 1、CL压缩类型为lzw，开启事务，对CL做增删改查数据 2、日志爆满，自动回滚 3、检查返回结果，并检查数据压缩情况
+     * 
+     * @throws Exception
      */
     @Test()
-    public void test() {
-        try {
-            createDataGroup();
-            createCL();
-            // 插入数据，使cl存在已被压缩的记录
-            LzwTransUtils util = new LzwTransUtils();
-            util.insertData( this.cl, 0, 99, 1024 * 1024 );
-            util.insertData( this.cl, 99, 109, 1024 * 1024 );
-            BSONObject bObject = getSnapshotDetail();
-            // wait for creating dictionary
-            while ( !"true"
-                    .equals( bObject.get( "DictionaryCreated" ).toString() ) ) {
-                try {
-                    Thread.sleep( 10 * 1000 );
-                    bObject = getSnapshotDetail();
-                } catch ( InterruptedException e ) {
-                    e.printStackTrace();
-                }
-            }
-
-            util.insertData( this.cl, 109, 115, 1024 );
-            // 做事务操作之前的压缩信息
-            BSONObject before = getSnapshotDetail();
-            if ( ( double ) before.get( "CurrentCompressionRatio" ) >= 1 ) {
-                Assert.fail( "CurrentCompressionRatio >= 1 !" );
-            }
-            Assert.assertEquals( before.get( "DictionaryCreated" ).toString(),
-                    "true" );
-            // 开启事务
-            this.sdb.beginTransaction();
-            // 对cl做增删改查操作,将日志写满
-            try {
-                this.cl.delete( "{_id:{$et:114}}", "{'':'_id'}" );
-                for ( int i = 1; i <= 40000; i++ ) {
-                    util.insertData( this.cl, 115, 116, 1024 );
-                    this.cl.delete( "{_id:115}" );
-                }
-                Assert.fail();
-            } catch ( BaseException e ) {
-                Assert.assertEquals( e.getErrorCode(), -203 );
-            }
-
-            checkData();
-
-            // 回滚后记录跟开启事务前数据一致且记录压缩状态一致
-            BSONObject after = getSnapshotDetail();
-            Assert.assertEquals( before.get( "CompressionType" ).toString(),
-                    after.get( "CompressionType" ).toString() );
-            Assert.assertEquals( before.get( "DictionaryCreated" ).toString(),
-                    after.get( "DictionaryCreated" ).toString() );
-            if ( ( double ) after.get( "CurrentCompressionRatio" ) >= 1 ) {
-                Assert.fail( "CurrentCompressionRatio >= 1 !" );
-            }
-        } catch ( BaseException e ) {
-            Assert.fail( e.getMessage() );
+    public void test() throws Exception {
+        // 1. 创建新组及3个数据节点
+        if ( sdb.isRelicaGroupExist( rgName ) ) {
+            sdb.removeReplicaGroup( rgName );
         }
-    }
 
-    public void checkData() {
+        // 取coord节点的全局事务及mvcc配置，加入到节点配置上，避免mvcc分支，事务执行报-6的错误
+        BSONObject configure = new BasicBSONObject();
+        BSONObject transConfig = getTransConfig( sdb, "coord" );
+        configure.putAll( transConfig );
+        configure.put( "logfilenum", 5 );
+        configure.put( "transactionon", true );
+        configure.put( "diaglevel", 5 );
+
+        String hostName = sdb.getReplicaGroup( "SYSCatalogGroup" ).getMaster()
+                .getHostName();
+        ReplicaGroup rg = sdb.createReplicaGroup( rgName );
+        rg.createNode( hostName, port1, SdbTestBase.reservedDir + port1 + "/",
+                configure );
+        rg.createNode( hostName, port2, SdbTestBase.reservedDir + port2 + "/",
+                configure );
+        rg.createNode( hostName, port3, SdbTestBase.reservedDir + port3 + "/",
+                configure );
+        rg.start();
+
+        // 2.集合创建在新建的组上
+        BSONObject option = new BasicBSONObject();
+        option.put( "Group", rgName );
+        option.put( "Compressed", true );
+        option.put( "CompressionType", "lzw" );
+        cl = LzwTransUtils.createCL( cs, clName, option );
+
+        // 3.插入数据，使cl存在已被压缩的记录
+        LzwTransUtils util = new LzwTransUtils();
+        util.insertData( cl, 0, 99, 1024 * 1024 );
+        util.insertData( cl, 99, 109, 1024 * 1024 );
+        BSONObject bObject = getSnapshotDetail();
+        while ( !"true"
+                .equals( bObject.get( "DictionaryCreated" ).toString() ) ) {
+            Thread.sleep( 10 * 1000 );
+            bObject = getSnapshotDetail();
+        }
+
+        // 4.继续插入数据，数据会被压缩，之后压缩率会小于1
+        util.insertData( cl, 109, 115, 1024 );
+        BSONObject before = getSnapshotDetail();
+        if ( ( double ) before.get( "CurrentCompressionRatio" ) >= 1 ) {
+            Assert.fail( "CurrentCompressionRatio >= 1 !" );
+        }
+        Assert.assertEquals( before.get( "DictionaryCreated" ).toString(),
+                "true" );
+
+        // 5.开启事务，对cl做增删改查操作,将日志写满
         try {
-            // 协调节点检查数据正确性
-            Assert.assertEquals( this.cl.getCount(
+            sdb.beginTransaction();
+            cl.delete( "{_id:{$et:114}}", "{'':'_id'}" );
+            for ( int i = 1; i <= 40000; i++ ) {
+                util.insertData( cl, 115, 116, 1024 );
+                cl.delete( "{_id:115}" );
+            }
+            Assert.fail( "need throw an error." );
+        } catch ( BaseException e ) {
+            Assert.assertEquals( e.getErrorCode(), -203 );
+        }
+
+        // 6.校验集合中的数据及主备节点一致性
+        Assert.assertTrue( TransUtil.isCLConsistency( cl ) );
+        Assert.assertEquals(
+                cl.getCount( "{$and:[{_id:{$gte:0}},{_id:{$lt:115}}]}" ), 115 );
+        Sequoiadb slaveData = null;
+        try {
+            slaveData = sdb.getReplicaGroup( rgName ).getSlave().connect();
+            DBCollection slaveCL = slaveData.getCollectionSpace( csName )
+                    .getCollection( clName );
+            Assert.assertEquals( slaveCL.getCount(
                     "{$and:[{_id:{$gte:0}},{_id:{$lt:115}}]}" ), 115 );
-            // 连接主节点数据正确性
-            checkDataInMaster();
-            // 连接从节点检查数据正确性
-            checkDataInSlave();
-        } catch ( BaseException e ) {
-            Assert.fail( e.getMessage() );
+        } finally {
+            slaveData.close();
         }
 
-    }
-
-    private void checkDataInSlave() {
-        Sequoiadb dataDb = null;
-        try {
-            // 连接源组data验证数据
-            List< String > rgNames = LzwTransUtils.getDataRgNames( this.sdb );
-            ReplicaGroup replicaGroup = sdb.getReplicaGroup( this.rgName );
-            Node slave = replicaGroup.getSlave();
-            String url = slave.getNodeName();
-            dataDb = new Sequoiadb( url, "", "" );
-            CollectionSpace cs = dataDb
-                    .getCollectionSpace( SdbTestBase.csName );
-            DBCollection dbcl = cs.getCollection( this.clName );
-            while ( true ) {
-                if ( dbcl.getCount(
-                        "{$and:[{_id:{$gte:0}},{_id:{$lt:115}}]}" ) == 115 ) {
-                    break;
-                }
-            }
-            // Assert.assertEquals(dbcl.getCount("{$and:[{_id:{$gte:0}},{_id:{$lt:115}}]}"),
-            // 115);
-        } catch ( BaseException e ) {
-            Assert.fail( e.getMessage() );
-        } finally {
-            dataDb.disconnect();
-        }
-    }
-
-    private void checkDataInMaster() {
-        Sequoiadb dataDb = null;
-        try {
-            // 连接源组data验证数据
-            List< String > rgNames = LzwTransUtils.getDataRgNames( this.sdb );
-            String url = LzwTransUtils.getGroupIPByGroupName( this.sdb,
-                    this.rgName );
-            dataDb = new Sequoiadb( url, "", "" );
-            CollectionSpace cs = dataDb
-                    .getCollectionSpace( SdbTestBase.csName );
-            DBCollection dbcl = cs.getCollection( this.clName );
-            Assert.assertEquals(
-                    dbcl.getCount( "{$and:[{_id:{$gte:0}},{_id:{$lt:115}}]}" ),
-                    115 );
-        } catch ( BaseException e ) {
-            Assert.fail( e.getMessage() );
-        } finally {
-            dataDb.disconnect();
+        // 7.回滚后记录跟开启事务前数据一致且记录压缩状态一致
+        BSONObject after = getSnapshotDetail();
+        Assert.assertEquals( before.get( "CompressionType" ).toString(),
+                after.get( "CompressionType" ).toString() );
+        Assert.assertEquals( before.get( "DictionaryCreated" ).toString(),
+                after.get( "DictionaryCreated" ).toString() );
+        if ( ( double ) after.get( "CurrentCompressionRatio" ) >= 1 ) {
+            Assert.fail( "CurrentCompressionRatio >= 1 !" );
         }
     }
 
@@ -189,9 +155,7 @@ public class TestSeqDB6670 extends SdbTestBase {
         Sequoiadb dataDB = null;
         try {
             detail = new BasicBSONObject();
-            List< String > rgNames = LzwTransUtils.getDataRgNames( this.sdb );
-            String url = LzwTransUtils.getGroupIPByGroupName( this.sdb,
-                    this.rgName );
+            String url = LzwTransUtils.getGroupIPByGroupName( sdb, rgName );
             dataDB = new Sequoiadb( url, "", "" );
             // get details of snapshot
             BSONObject nameBSON = new BasicBSONObject();
@@ -200,84 +164,29 @@ public class TestSeqDB6670 extends SdbTestBase {
             BasicBSONList details = ( BasicBSONList ) snapshot.getNext()
                     .get( "Details" );
             detail = ( BSONObject ) details.get( 0 );
-        } catch ( BaseException e ) {
-            Assert.fail( e.getMessage() );
         } finally {
             if ( dataDB != null ) {
-                dataDB.disconnect();
+                dataDB.close();
 
             }
         }
         return detail;
     }
 
-    public void createDataGroup() {
-        try {
-
-            BSONObject configure = new BasicBSONObject();
-            configure.put( "logfilenum", 5 );
-            configure.put( "transactionon", true );
-            configure.put( "diaglevel", 5 );
-            String hostName = sdb.getReplicaGroup( "SYSCatalogGroup" )
-                    .getMaster().getHostName();
-            try {
-                sdb.removeReplicaGroup( this.rgName );
-            } catch ( BaseException e ) {
-                if ( e.getErrorCode() != -154 ) { // SDB_CLS_GRP_NOT_EXIST
-                    throw e;
-                }
-            }
-            ReplicaGroup rg = this.sdb.createReplicaGroup( this.rgName );
-            System.out.println(
-                    this.port1 + ":" + this.port2 + ":" + this.port3 );
-            Node node1 = rg.createNode( hostName, this.port1,
-                    SdbTestBase.workDir + port1 + "/", configure );
-            Node node2 = rg.createNode( hostName, this.port2,
-                    SdbTestBase.workDir + port2 + "/", configure );
-            Node node3 = rg.createNode( hostName, this.port3,
-                    SdbTestBase.workDir + port3 + "/", configure );
-            this.sdb.activateReplicaGroup( this.rgName );
-        } catch ( BaseException e ) {
-            e.printStackTrace();
-            Assert.fail( e.getMessage() );
-        }
-    }
-
-    public void createCL() {
-        try {
-            List< String > rgNames = LzwTransUtils.getDataRgNames( this.sdb );
-            BSONObject option = new BasicBSONObject();
-            // 自己创建组
-            option.put( "Group", this.rgName );
-            option.put( "Compressed", true );
-            option.put( "CompressionType", "lzw" );
-            this.cl = LzwTransUtils.createCL( this.cs, this.clName, option );
-        } catch ( BaseException e ) {
-            Assert.fail( e.getMessage() );
-        }
+    public BSONObject getTransConfig( Sequoiadb db, String role ) {
+        BSONObject transConfig = db.getSnapshot( Sequoiadb.SDB_SNAP_CONFIGS,
+                "{role:'" + role + "'}", "{globtranson:'',mvccon:''}", "" )
+                .getNext();
+        return transConfig;
     }
 
     @AfterClass()
     public void tearDown() {
-        try {
-            if ( this.cs.isCollectionExist( this.clName ) ) {
-                this.cs.dropCollection( this.clName );
-            }
-            ReplicaGroup rg = sdb.getReplicaGroup( this.rgName );
-            String hostName = sdb.getReplicaGroup( "SYSCatalogGroup" )
-                    .getMaster().getHostName();
-            BSONObject configure = new BasicBSONObject();
-            configure.put( "logfilenum", 5 );
-            configure.put( "transactionon", true );
-            if ( rg != null ) {
-                sdb.removeReplicaGroup( this.rgName );
-            }
-
-        } catch ( BaseException e ) {
-            Assert.fail( e.getMessage() );
-        } finally {
-            this.sdb.disconnect();
+        cs.dropCollection( clName );
+        if ( sdb.isRelicaGroupExist( rgName ) ) {
+            sdb.removeReplicaGroup( rgName );
         }
+        sdb.close();
     }
 
 }
