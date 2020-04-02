49c0e255602612a9ea85d63e5d529357a8eb2b4b
xujianhui <xujianhui@sequoiadb.com>
2020-04-03 00:42:01 +0800

    code review: commit message lenght invalid

--- SequoiaDB/engine/client/common.c
+++ SequoiaDB/engine/client/common.c
@@ -3161,8 +3161,7 @@ INT32 clientBuildTransactionCommitMsg( CHAR **ppBuffer, INT32 *bufferSize,
 {
    INT32 rc = SDB_OK ;
    MsgOpTransCommit *transCommitMsg = NULL ;
-   INT32 len = sizeof( MsgOpTransCommit ) +
-               ossRoundUpToMultipleX( 0, sizeof(ossValuePtr) ) ;
+   INT32 len = sizeof( MsgOpTransCommit )  ;
    if ( len < 0 )
    {
       ossPrintf ( "Packet size overflow"OSS_NEWLINE ) ;
@@ -3180,7 +3179,7 @@ INT32 clientBuildTransactionCommitMsg( CHAR **ppBuffer, INT32 *bufferSize,
    transCommitMsg                       = ( MsgOpTransCommit *)( *ppBuffer ) ;
    transCommitMsg->header.requestID     = reqID ;
    transCommitMsg->header.opCode        = MSG_BS_TRANS_COMMIT_REQ ;
-   transCommitMsg->header.messageLength = sizeof( MsgOpTransBegin ) + 0 ;
+   transCommitMsg->header.messageLength = len ;
    transCommitMsg->header.routeID.value = 0 ;
    transCommitMsg->header.TID           = ossGetCurrentThreadID() ;
    if( endianConvert )
