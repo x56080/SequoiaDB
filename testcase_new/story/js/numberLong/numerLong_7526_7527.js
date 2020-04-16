e0eeb8bc0562dce9a0760cbb4e96f560fb0819a4
zhaoxiaoni <zhaoxiaoni@sequoiadb.com>
2020-04-16 11:43:42 +0800

    ci失败用例优化

--- testcase_new/story/js/numberLong/numerLong_7526_7527.js
+++ testcase_new/story/js/numberLong/numerLong_7526_7527.js
@@ -1,73 +1,40 @@
 ﻿/*******************************************************************************
-*@Description : seqDB-7526::shell_输入strict格式，查询显示 
-                seqDB-7527::shell_输入js格式，查询显示                   
-*@Modify List : 2016-3-28  Ting YU  Init
+*@Description : seqDB-7526::shell_输入strict格式，查询显示
+seqDB-7527::shell_输入js格式，查询显示
+*@author : 2020-4-16 Zhao Xiaoni  Init
 *******************************************************************************/
 main();
 
 function main()
-{	  
-	try
-	{			
-      var csName  = COMMCSNAME;
-      var clName  = COMMCLNAME;
-      
-      var clObj = new Collection( csName, clName, {ReplSize:0} );
-      var cl = clObj.create();
-      
-      testStrictFormat( cl );
-      testJSFormat( cl );
-                                            
-   }
-   catch( e )
-   {
-      throw e ;
-   }
-}
-
-function testStrictFormat( cl )
 {
-   println( '---begin to check strict format: {$numberLong:"2147483647"}' );
-   cl.remove();
+   var clName = COMMCLNAME + "_7526";
+   commDropCL( db, COMMCSNAME, clName );
+   var cl = commCreateCL( db, COMMCSNAME, clName );
+
+   var record = { a: NumberLong( "-2147483647" ) };
+   cl.insert( record );
+   var rc = cl.find();
+   var expRecord = [ { a: -2147483647 } ];
+   checkRec( rc, expRecord );
+
+   record = { a: NumberLong( -2147483648 ) };
+   cl.insert( record );
+   rc = cl.find();
+   expRecord.push( { a: -2147483648 } );
+   checkRec( rc, expRecord );
    
-   var val = 2147483647;
-   var rec = {a:{$numberLong:val.toString()}};       
-   cl.insert( rec ); 
+   record = { a: { $numberLong: "2147483647" } };
+   cl.insert( record );
+   rc = cl.find();
+   expRecord.push( { a: 2147483647 } );
+   checkRec( rc, expRecord );
    
-   var rc = cl.find(); 
-   var expRec = {a:2147483647};   
-   checkRec( rc, [expRec] ); 
+   record = { a: { $numberLong: "9007199254740992" } };
+   cl.insert( record );
+   rc = cl.find();
+   expRecord.push( record );
+   checkRec( rc, expRecord );
    
-   println( '---begin to check strict format: {$numberLong:"9007199254740992"}' );
-   cl.remove();
-   
-   var val = 9007199254740992;
-   var rec = {a:{$numberLong:val.toString()}};       
-   cl.insert( rec ); 
-   
-   var rc = cl.find(); 
-   var expRec = rec;   
-   checkRec( rc, [expRec] );        
+   commDropCL( db, COMMCSNAME, clName, false, false );
 }
 
-function testJSFormat( cl )
-{
-   println( '---begin to check JS format: NumberLong("-2147483647")' );
-   cl.remove();
-   
-   var val = -2147483647;
-   var rec = {a:NumberLong(val.toString())};           
-   cl.insert( rec ); 
-
-   var rc = cl.find(); 
-   var expRec = {a:val}; 
-   checkRec( rc, [expRec] );   
-      
-   println( '---begin to check JS format: NumberLong(-2147483647)' );
-   cl.remove();
-   
-   var val = -2147483648;
-   var rec = {a:NumberLong(val)};  
-   var expRec = {a:val};       
-   cl.insert( rec ); 
-}
