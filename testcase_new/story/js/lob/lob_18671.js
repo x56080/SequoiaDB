13aa5e31724bac1d0419d4e526930d4480650bdf
luweikang <luweikang@sequoiadb.com>
2020-04-24 17:05:31 +0800

    创建集合指定replsize参数时写入参数位置错误

--- testcase_new/story/js/lob/lob_18671.js
+++ testcase_new/story/js/lob/lob_18671.js
@@ -42,7 +42,7 @@ function main ()
       println( "\n---Begin to ready cs / cl." );
       commDropCS( db, csName, true, "Failed to drop cs in the pre-condition." );
       var cs = db.createCS( csName );
-      var cl1 = cs.createCL( clName1, { Group: srcGroup }, ReplSize: 0 );
+      var cl1 = cs.createCL( clName1, { Group: srcGroup, ReplSize: 0 } );
       var cl2 = cs.createCL( clName2, { Group: srcGroup, ShardingType: "hash", ShardingKey: { a: 1 }, ReplSize: 0 } );
 
       println( "\n---Begin to cl1.putLob." );
