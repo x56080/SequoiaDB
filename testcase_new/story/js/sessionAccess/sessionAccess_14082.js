f69b9ae3f4ebda2770bd08772e9458d738826b82
zhaoxiaoni <42-zhaoxiaoni@users.noreply.gitlab.example.com>
2020-04-26 09:24:02 +0800

    SEQUOIADBMAINSTREAM-5635添加测试点

--- testcase_new/story/js/sessionAccess/sessionAccess_14082.js
+++ testcase_new/story/js/sessionAccess/sessionAccess_14082.js
@@ -7,6 +7,8 @@ testConf.skipOneDuplicatePerGroup = true;
 
 var group = commGetGroups( db )[0];
 var groupName = group[0].GroupName;
+var primaryPos = group[0].PrimaryPos;
+var primaryNode = group[primaryPos]["HostName"] + ":" + group[primaryPos]["svcname"];
 testConf.clName = CHANGEDPREFIX + "_14082";
 testConf.clOpt = { Group: groupName };
 
@@ -17,26 +19,19 @@ function test( testPara )
    insertData( testPara.testCL );
 
    var options = { PreferedInstance: "M" };
-   var primaryPos = group[0].PrimaryPos;
-   var expAccessNodes = [ group[primaryPos]["HostName"] + ":" + group[primaryPos]["svcname"] ];
+   var expAccessNodes = [ primaryNode ];
    checkAccessNodes( testPara.testCL, expAccessNodes, options );
 
    options = { PreferedInstance: "-M" };
    checkAccessNodes( testPara.testCL, expAccessNodes, options );
 
-   expAccessNodes = [];
-   for( var i = 1; i < group.length; i++ )
-   {
-      if( i !== primaryPos )
-      {
-         expAccessNodes.push( group[i]["HostName"] + ":" + group[i]["svcname"]);
-      }
-   }
    options = { PreferedInstance: "S" };
-   checkAccessNodes( testPara.testCL, expAccessNodes, options );
+   //校验访问备节点负载均衡
+   checkBalance( testPara.testCL, options );
   
    options = { PreferedInstance: "-S" };
-   checkAccessNodes( testPara.testCL, expAccessNodes, options );
+   //校验访问备节点负载均衡
+   checkBalance( testPara.testCL, options );
 
    expAccessNodes = getGroupNodes( groupName );
    options =  { PreferedInstance: "A" };
@@ -44,5 +39,33 @@ function test( testPara )
 
    options =  { PreferedInstance: "-A" };
    checkAccessNodes( testPara.testCL, expAccessNodes, options );
+}
+
+function checkBalance( cl, options )
+{
+   var actAccessNodes = {};
+   for( var i = 0; i < 1000; i++ )
+   {   
+      db.setSessionAttr( options );
+      var cursor = cl.find().explain();
+      while( cursor.next() )
+      {
+         var actAccessNode = cursor.current().toObj().NodeName;
+         if( actAccessNode == primaryNode )
+         {
+            throw new Error( "actAccessNode is primary node: " + primaryNode );
+         }
+         actAccessNodes[ actAccessNode ] == undefined ? actAccessNodes[ actAccessNode ] = 1 :
+                                          actAccessNodes[ actAccessNode ] = actAccessNodes[ actAccessNode ] + 1;        
+      }
+
+   }
+
+   //跟开发确认并没有固定的均衡率，因此暂时认为访问节点差值大于20%为负载不均衡
+   var num =  actAccessNodes[ Object.keys( actAccessNodes )[0] ] - actAccessNodes[ Object.keys( actAccessNodes )[1] ];
+   if( Math.abs( num ) > 100 )
+   {
+      throw new Error( "num:" + num );
+   }
 }
 
