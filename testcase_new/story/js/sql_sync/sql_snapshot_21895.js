0cb1231aada38d35669ca13c046df30767c42b6d
liyuanyue <liyuanyue@sequoiadb.com>
2020-04-29 16:10:04 +0800

    修复ci问题v3.2

--- testcase_new/story/js/sql_sync/sql_snapshot_21895.js
+++ testcase_new/story/js/sql_sync/sql_snapshot_21895.js
@@ -10,29 +10,24 @@ main( test );
 function test ()
 {
    // 使用 snapshot 语句查询快照信息
-   var cur = db.snapshot( SDB_SNAP_SVCTASKS );
+   var cur = db.snapshot( SDB_SNAP_SVCTASKS, { RawData: true } );
    while( cur.next() )
    {
       var tmpObj = cur.current().toObj();
       // 将内置 sql 执行结果累计验证
-      var totalIndexRead = db.exec( "select SUM(TotalIndexRead) as totalIndexRead from $SNAPSHOT_SVCTASKS " ).current().toObj()["totalIndexRead"];
-      var totalDataWrite = db.exec( "select SUM(TotalDataWrite) as totalDataWrite from $SNAPSHOT_SVCTASKS " ).current().toObj()["totalDataWrite"];
-      var totalIndexWrite = db.exec( "select SUM(TotalIndexWrite) as totalIndexWrite from $SNAPSHOT_SVCTASKS " ).current().toObj()["totalIndexWrite"];
-      var totalUpdate = db.exec( "select SUM(TotalUpdate) as totalUpdate from $SNAPSHOT_SVCTASKS " ).current().toObj()["totalUpdate"];
-      var totalDelete = db.exec( "select SUM(TotalDelete) as totalDelete from $SNAPSHOT_SVCTASKS " ).current().toObj()["totalDelete"];
-      var totalInsert = db.exec( "select SUM(TotalInsert) as totalInsert from $SNAPSHOT_SVCTASKS " ).current().toObj()["totalInsert"];
-      var totalWrite = db.exec( "select SUM(TotalWrite) as totalWrite from $SNAPSHOT_SVCTASKS " ).current().toObj()["totalWrite"];
+      var execObj = db.exec( "select * from $SNAPSHOT_SVCTASKS where NodeName='" + tmpObj.NodeName + "'" ).current().toObj();
       var actObj = {
          TotalIndexRead: tmpObj["TotalIndexRead"], TotalDataWrite: tmpObj["TotalDataWrite"], TotalIndexWrite: tmpObj["TotalIndexWrite"],
          TotalUpdate: tmpObj["TotalUpdate"], TotalDelete: tmpObj["TotalDelete"], TotalInsert: tmpObj["TotalInsert"], TotalWrite: tmpObj["TotalWrite"]
       };
       var expObj = {
-         TotalIndexRead: totalIndexRead, TotalDataWrite: totalDataWrite, TotalIndexWrite: totalIndexWrite, TotalUpdate: totalUpdate,
-         TotalDelete: totalDelete, TotalInsert: totalInsert, TotalWrite: totalWrite
+         TotalIndexRead: execObj["TotalIndexRead"], TotalDataWrite: execObj["TotalDataWrite"], TotalIndexWrite: execObj["TotalIndexWrite"],
+         TotalUpdate: execObj["TotalUpdate"], TotalDelete: execObj["TotalDelete"], TotalInsert: execObj["TotalInsert"], TotalWrite: execObj["TotalWrite"]
+
       };
       if( !( commCompareObject( expObj, actObj ) ) )
       {
-         throw new Error( "$SNAPSHOT_SVCTASKS result error\n" + "expObj :" + JSON.stringify( expObj ) + "\nactObj :" + JSON.stringify( actObj ) );
+         throw new Error( "$SNAPSHOT_SVCTASKS result error on " + tmpObj.NodeName + "\n" + "expObj :" + JSON.stringify( expObj ) + "\nactObj :" + JSON.stringify( actObj ) );
       }
    }
 }
