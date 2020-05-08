62c0d3209614ce812e4479bf3be1c30e017da234
heguoming <heguoming@sequoiadb.com>
2020-05-08 23:14:44 +0800

    SEQUOIADBMAINSTREAM-5506: 回放时不设置 transaction ID
    目前备节点中回放事务回滚操作时不回填原事务ID，因此导致切主后记录对应的事务是回滚状态的，可见性判断为不可见跳过记录，因此目前对于备节点回放，均不填事务ID

--- SequoiaDB/engine/cls/clsReplayer.cpp
+++ SequoiaDB/engine/cls/clsReplayer.cpp
@@ -779,13 +779,8 @@ namespace engine
 
       if ( !_dpsCB )
       {
-         // retrieve transID from record and put in eduCB, remember to
-         // reset the transID before exit
+         // set last replay LSN
          eduCB->insertLsn( recordHeader->_lsn ) ;
-         if ( transID.isValid())
-         {
-            eduCB->setTransID( transID ) ;
-         }
       }
 
       // check if a rollback DPS log, if so, mark rollback status
@@ -1538,7 +1533,6 @@ namespace engine
          eduCB->stopTransRollback() ;
       }
       eduCB->resetLsn() ;
-      eduCB->resetTransID() ;
       if ( SDB_OK != rc )
       {
          ftReportErr( rc ) ;
