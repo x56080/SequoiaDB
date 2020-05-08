6904fa25b46daf6c02c028e6a4102a1cfe2486e9
zhaoyu <zhaoyu@sequoiadb.com>
2020-05-08 16:27:33 +0800

    CI上并发执行用例，出现commit操作执行超过2分钟，并发事务等锁超时，导致用例随机失败，修改transreplsize=1避免随机失败，不影响事务正常功能

--- testcase_new/story/java/src/test/java/com/sequoiadb/testcommon/SdbTestBase.java
+++ testcase_new/story/java/src/test/java/com/sequoiadb/testcommon/SdbTestBase.java
@@ -63,10 +63,12 @@ public class SdbTestBase {
     public static final String RS = "rs";
     public static final String RCAUTO = "rcauto";
     public static final String RCUSERBS = "rcuserbs";
+    public static final String TRANSREPLSIZE = "transreplsize";
 
     private static ConfigOptions options = new ConfigOptions();
     public static String testGroup = null;
     private static final int newIndexScanStep = 100;
+    private static final int transReplsize = 1;
     public static final int timeOutLen = 120;
     private static final Map< String, BSONObject > group2Conf = new HashMap< String, BSONObject >();
     private static final Map< String, AtomicInteger > group2Count = new HashMap< String, AtomicInteger >();
@@ -83,6 +85,7 @@ public class SdbTestBase {
         group2Conf.get( RU ).put( TRANSAUTOCOMMIT, false );
         group2Conf.get( RU ).put( TRANSAUTOROLLBACK, true );
         group2Conf.get( RU ).put( TRANSUSERBS, true );
+        group2Conf.get( RU ).put( TRANSREPLSIZE, transReplsize );
 
         group2Conf.put( RC, new BasicBSONObject() );
         group2Conf.get( RC ).put( TRANSISOLATION, 1 );
@@ -92,6 +95,7 @@ public class SdbTestBase {
         group2Conf.get( RC ).put( TRANSAUTOCOMMIT, false );
         group2Conf.get( RC ).put( TRANSAUTOROLLBACK, true );
         group2Conf.get( RC ).put( TRANSUSERBS, true );
+        group2Conf.get( RC ).put( TRANSREPLSIZE, transReplsize );
 
         group2Conf.put( RCWAITLOCK, new BasicBSONObject() );
         group2Conf.get( RCWAITLOCK ).put( TRANSISOLATION, 1 );
@@ -101,6 +105,7 @@ public class SdbTestBase {
         group2Conf.get( RCWAITLOCK ).put( TRANSAUTOCOMMIT, false );
         group2Conf.get( RCWAITLOCK ).put( TRANSAUTOROLLBACK, true );
         group2Conf.get( RCWAITLOCK ).put( TRANSUSERBS, true );
+        group2Conf.get( RCWAITLOCK ).put( TRANSREPLSIZE, transReplsize );
 
         group2Conf.put( RS, new BasicBSONObject() );
         group2Conf.get( RS ).put( TRANSISOLATION, 2 );
@@ -110,6 +115,7 @@ public class SdbTestBase {
         group2Conf.get( RS ).put( TRANSAUTOCOMMIT, false );
         group2Conf.get( RS ).put( TRANSAUTOROLLBACK, true );
         group2Conf.get( RS ).put( TRANSUSERBS, true );
+        group2Conf.get( RS ).put( TRANSREPLSIZE, transReplsize );
 
         group2Conf.put( RCAUTO, new BasicBSONObject() );
         group2Conf.get( RCAUTO ).put( TRANSISOLATION, 1 );
@@ -119,6 +125,7 @@ public class SdbTestBase {
         group2Conf.get( RCAUTO ).put( TRANSAUTOCOMMIT, true );
         group2Conf.get( RCAUTO ).put( TRANSAUTOROLLBACK, false );
         group2Conf.get( RCAUTO ).put( TRANSUSERBS, true );
+        group2Conf.get( RCAUTO ).put( TRANSREPLSIZE, transReplsize );
 
         group2Conf.put( RCUSERBS, new BasicBSONObject() );
         group2Conf.get( RCUSERBS ).put( TRANSISOLATION, 1 );
@@ -128,6 +135,7 @@ public class SdbTestBase {
         group2Conf.get( RCUSERBS ).put( TRANSAUTOCOMMIT, false );
         group2Conf.get( RCUSERBS ).put( TRANSAUTOROLLBACK, true );
         group2Conf.get( RCUSERBS ).put( TRANSUSERBS, false );
+        group2Conf.get( RCUSERBS ).put( TRANSREPLSIZE, transReplsize );
 
         for ( String key : group2Conf.keySet() ) {
             group2Count.put( key, new AtomicInteger( 0 ) );
