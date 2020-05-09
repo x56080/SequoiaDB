f87684e46dca00eac7fd193eb91e3696442e1208
heguoming <heguoming@sequoiadb.com>
2020-05-10 01:27:16 +0800

    性能优化: dpsTransCB 的 LSN map 的锁改成读写锁

--- SequoiaDB/engine/dps/dpsTransCB.cpp
+++ SequoiaDB/engine/dps/dpsTransCB.cpp
@@ -2728,7 +2728,7 @@ namespace engine
       SDB_ASSERT( beginLsn != DPS_INVALID_LSN_OFFSET, "invalid begin-lsn" ) ;
       SDB_ASSERT( transID.isValid(), "invalid transaction-ID" ) ;
       DPS_TRANS_ID origID = getTransID( transID );
-      ossScopedLock _lock( &_lsnMapMutex ) ;
+      ossScopedLock _lock( &_lsnMapMutex, EXCLUSIVE ) ;
       try
       {
          _beginLsnIdMap[ beginLsn ] = origID ;
@@ -2744,7 +2744,7 @@ namespace engine
    void dpsTransCB::delBeginLsn( const DPS_TRANS_ID &transID )
    {
       DPS_TRANS_ID origID = getTransID( transID );
-      ossScopedLock _lock( &_lsnMapMutex );
+      ossScopedLock _lock( &_lsnMapMutex, EXCLUSIVE );
       DPS_LSN_OFFSET beginLsn;
       TRANS_ID_LSN_MAP::iterator iter = _idBeginLsnMap.find( origID ) ;
       if ( iter != _idBeginLsnMap.end() )
@@ -2758,7 +2758,7 @@ namespace engine
    DPS_LSN_OFFSET dpsTransCB::getBeginLsn( const DPS_TRANS_ID &transID )
    {
       DPS_TRANS_ID origID = getTransID( transID ) ;
-      ossScopedLock _lock( &_lsnMapMutex ) ;
+      ossScopedLock _lock( &_lsnMapMutex, SHARED ) ;
       TRANS_ID_LSN_MAP::iterator iter = _idBeginLsnMap.find( origID ) ;
       if ( iter != _idBeginLsnMap.end() )
       {
@@ -2769,7 +2769,7 @@ namespace engine
 
    DPS_LSN_OFFSET dpsTransCB::getOldestBeginLsn()
    {
-      ossScopedLock _lock( &_lsnMapMutex );
+      ossScopedLock _lock( &_lsnMapMutex, SHARED );
       if ( _beginLsnIdMap.size() > 0 )
       {
          return _beginLsnIdMap.begin()->first;
--- SequoiaDB/engine/include/dpsTransCB.hpp
+++ SequoiaDB/engine/include/dpsTransCB.hpp
@@ -1077,7 +1077,7 @@ namespace engine
       BOOLEAN           _doRollback ;
       ossEvent          _rollbackEvent ;
 
-      monSpinXLatch     _lsnMapMutex ;
+      monSpinSLatch     _lsnMapMutex ;
       TRANS_LSN_ID_MAP  _beginLsnIdMap ;
       TRANS_ID_LSN_MAP  _idBeginLsnMap ;
 
