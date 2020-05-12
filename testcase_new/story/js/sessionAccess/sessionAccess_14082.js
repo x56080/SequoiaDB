f46a59f65b58338ad20777112ac855cd2a933495
zhaoxiaoni <zhaoxiaoni@sequoiadb.com>
2020-05-12 14:49:17 +0800

    删除负载均衡测试点

--- testcase_new/story/js/sessionAccess/sessionAccess_14082.js
+++ testcase_new/story/js/sessionAccess/sessionAccess_14082.js
@@ -28,49 +28,27 @@ function main()
    options = { PreferedInstance: "-M" };
    checkAccessNodes( cl, expAccessNodes, options );
 
+   expAccessNodes = [];
+   for( var i = 1; i < group.length; i++ )
+   {
+      if( i !== primaryPos )
+      {
+         expAccessNodes.push( group[i]["HostName"] + ":" + group[i]["svcname"]);
+      }
+   }
    options = { PreferedInstance: "S" };
-   //校验访问备节点负载均衡
-   checkBalance( cl, primaryNode, options );
-  
+   checkAccessNodes( cl, expAccessNodes, options ); 
+ 
    options = { PreferedInstance: "-S" };
-   //校验访问备节点负载均衡
-   checkBalance( cl, primaryNode, options );
-
-   expAccessNodes = getGroupNodes( groupName );
-   options =  { PreferedInstance: "A" };
    checkAccessNodes( cl, expAccessNodes, options );
 
-   options =  { PreferedInstance: "-A" };
+   expAccessNodes = getGroupNodes( groupName );
+   options = { PreferedInstance: "A" };
+   checkAccessNodes( cl, expAccessNodes, options );
+
+   options = { PreferedInstance: "-A" };
    checkAccessNodes( cl, expAccessNodes, options );
 
    commDropCL( db, COMMCSNAME, clName, false, false ) ;
-}
-
-function checkBalance( cl, primaryNode, options )
-{
-   var actAccessNodes = {};
-   for( var i = 0; i < 1000; i++ )
-   {   
-      db.setSessionAttr( options );
-      var cursor = cl.find().explain();
-      while( cursor.next() )
-      {
-         var actAccessNode = cursor.current().toObj().NodeName;
-         if( actAccessNode == primaryNode )
-         {
-            throw new Error( "actAccessNode is primary node: " + primaryNode );
-         }
-         actAccessNodes[ actAccessNode ] == undefined ? actAccessNodes[ actAccessNode ] = 1 :
-                                          actAccessNodes[ actAccessNode ] = actAccessNodes[ actAccessNode ] + 1;        
-      }
-
-   }
-
-   //跟开发确认并没有固定的均衡率，因此暂时认为访问节点差值大于20%为负载不均衡
-   var num =  actAccessNodes[ Object.keys( actAccessNodes )[0] ] - actAccessNodes[ Object.keys( actAccessNodes )[1] ];
-   if( Math.abs( num ) > 100 )
-   {
-      throw new Error( "num:" + num );
-   }
 }
 
