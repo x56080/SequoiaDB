fbb2f8a1aaad47ff315deb0d069a52e11a601ca4
xujianhui <xujianhui@sequoiadb.com>
2020-05-14 23:36:47 +0800

    Update _runCommand default value

--- SequoiaDB/engine/client/clientImpl.hpp
+++ SequoiaDB/engine/client/clientImpl.hpp
@@ -1192,7 +1192,7 @@ namespace sdbclient
                           const BSONObj *arg4 = NULL,
                           SINT32 flag = 0,
                           UINT64 reqID = 0,
-                          SINT64 numToSkip = -1,
+                          SINT64 numToSkip = 0,
                           SINT64 numToReturn = -1,
                           _sdbCursor **ppCursor = NULL ) ;
 
