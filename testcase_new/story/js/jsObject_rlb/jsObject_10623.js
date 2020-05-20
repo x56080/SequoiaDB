5c3819bbb9dcaa544a52de1a32dd7d80533d39a5
zhaoxiaoni <zhaoxiaoni@sequoiadb.com>
2020-05-20 10:56:57 +0800

    放开被屏蔽且未标记单号的用例

---
+++ testcase_new/story/js/jsObject_rlb/jsObject_10623.js
@@ -0,0 +1,59 @@
+/******************************************************************************
+*@Description : seqDB-10623:Oma启动本地cm
+                seqDB-10624:Oma启动本地cm，alivetime设置为0 
+*@author      : Zhao Xiaoni
+******************************************************************************/
+main();
+
+function main()
+{
+   // 获取空闲端口
+   var svcName = toolGetIdleSvcName( COORDHOSTNAME, CMSVCNAME );
+   if( svcName === undefined )
+   {
+      println( "No idle svcname between RSRVPORTBEGIN and RSRVPORTEND" );
+      return;
+   }
+
+   // 先停止当前的cm
+   var InstallPath = commGetInstallPath();
+   var cmd = new Cmd();
+   cmd.run( InstallPath + "/bin/sdbcmtop" );
+
+   try
+   {
+      //测试启动超时时间为10的cm
+      var options = { "port": svcName, "alivetime": 10, "standalone": true };
+      Oma.start( options );
+   
+      // 检查cm是否启动
+      var oma = new Oma( COORDHOSTNAME, svcName );
+      oma.close();
+  
+      sleep( 20 * 1000 );  
+      var nodeArray = Sdbtool.listNodes( { type: "cm", showalone: true } ).toArray();
+      if( nodeArray.length != 0 )
+      {
+         throw new Error( "nodeArray: " + nodeArray );
+      }
+ 
+      // 测试启动超时时间为0的cm
+      options = { "port": svcName, "alivetime": 0, "standalone": false };
+      Oma.start( options );
+   
+      oma = new Oma( COORDHOSTNAME, svcName );
+      oma.close();
+  
+      sleep( 20 * 1000 );
+      nodeArray = Sdbtool.listNodes( { type: "cm" } ).toArray();
+      if( nodeArray.length !== 2 )
+      {  
+         throw new Error( "nodeArray: " + nodeArray );
+      }
+   }
+   finally
+   {
+      cmd.run( InstallPath + "/bin/sdbcmtop --I" );
+      cmd.run( InstallPath + "/bin/sdbcmart" );
+   }
+}
