fc60d10daca9c850f223ccfe9d38a4f985a757e2
luweikang <luweikang@sequoiadb.com>
2020-05-22 22:37:09 +0800

    回归问题单5213优化用例

--- testcase_new/story/java/src/test/java/com/sequoiadb/lob/basicoperation/TestLobSplitAndRead7849.java
+++ testcase_new/story/java/src/test/java/com/sequoiadb/lob/basicoperation/TestLobSplitAndRead7849.java
@@ -1,7 +1,6 @@
 package com.sequoiadb.lob.basicoperation;
 
 import java.util.ArrayList;
-import java.util.Random;
 import java.util.concurrent.LinkedBlockingQueue;
 
 import org.bson.BSONObject;
@@ -31,14 +30,14 @@ import com.sequoiadb.testcommon.SdbThreadBase;
  * @version 1.00
  */
 public class TestLobSplitAndRead7849 extends SdbTestBase {
+    private String csName = "cs_lob7849";
     private String clName = "cl_lob7849";
     private static Sequoiadb sdb = null;
     private CollectionSpace cs = null;
     private DBCollection cl = null;
-    private Random random = new Random();
     private String sourceRGName = "";
     private String targetRGName = "";
-    private LinkedBlockingQueue< SaveOidAndMd5 > id2md5 = new LinkedBlockingQueue< SaveOidAndMd5 >();
+    private LinkedBlockingQueue< SaveOidAndMd5 > id2md5 = new LinkedBlockingQueue<>();
 
     @BeforeClass
     public void setUp() {
@@ -52,21 +51,24 @@ public class TestLobSplitAndRead7849 extends SdbTestBase {
             throw new SkipException( "less two groups skip testcase" );
         }
 
-        cs = sdb.getCollectionSpace( SdbTestBase.csName );
+        if ( sdb.isCollectionSpaceExist( csName ) ) {
+            sdb.dropCollectionSpace( csName );
+        }
+        cs = sdb.createCollectionSpace( csName );
         String clOptions = "{ShardingKey:{no:1},ShardingType:'hash',Partition:4096,"
                 + "ReplSize:0,Compressed:true}";
         cl = cs.createCollection( clName,
                 ( BSONObject ) JSON.parse( clOptions ) );
 
         // write lob
-        int lobtimes = 100;
+        int lobtimes = 10;
         writeLobAndGetMd5( cl, lobtimes );
     }
 
     @Test
     public void testSplitAndWrite() {
         ReadLobsTask readLobsTask = new ReadLobsTask();
-        readLobsTask.start( 100 );
+        readLobsTask.start( 10 );
         SplitCL splitCL = new SplitCL();
         splitCL.start();
         Assert.assertTrue( readLobsTask.isSuccess(),
@@ -75,18 +77,18 @@ public class TestLobSplitAndRead7849 extends SdbTestBase {
 
         // check the split result
         double expErrorValue = 0.5;
-        ArrayList< String > splitRGNames = new ArrayList< String >( 2 );
+        ArrayList< String > splitRGNames = new ArrayList<>( 2 );
         splitRGNames.add( sourceRGName );
         splitRGNames.add( targetRGName );
-        LobOprUtils.checkSplitResult( sdb, SdbTestBase.csName, clName,
-                splitRGNames, expErrorValue );
+        LobOprUtils.checkSplitResult( sdb, csName, clName, splitRGNames,
+                expErrorValue );
 
     }
 
     @AfterClass
     public void tearDown() {
         try {
-            cs.dropCollection( clName );
+            sdb.dropCollectionSpace( csName );
         } finally {
             if ( null != sdb ) {
                 sdb.close();
@@ -97,12 +99,11 @@ public class TestLobSplitAndRead7849 extends SdbTestBase {
     public class SplitCL extends SdbThreadBase {
         @Override
         public void exec() throws BaseException {
-            sourceRGName = LobOprUtils.getSrcGroupName( sdb, SdbTestBase.csName,
-                    clName );
+            sourceRGName = LobOprUtils.getSrcGroupName( sdb, csName, clName );
             targetRGName = LobOprUtils.getSplitGroupName( sourceRGName );
             try ( Sequoiadb db1 = new Sequoiadb( SdbTestBase.coordUrl, "",
                     "" ) ;) {
-                DBCollection cl1 = db1.getCollectionSpace( SdbTestBase.csName )
+                DBCollection cl1 = db1.getCollectionSpace( csName )
                         .getCollection( clName );
                 int percent = 50;
                 cl1.split( sourceRGName, targetRGName, percent );
@@ -117,20 +118,24 @@ public class TestLobSplitAndRead7849 extends SdbTestBase {
                     "" )) {
                 db.setSessionAttr( ( BSONObject ) JSON
                         .parse( "{'PreferedInstance':'M'}" ) );
-                DBCollection dbcl = db.getCollectionSpace( SdbTestBase.csName )
+                DBCollection dbcl = db.getCollectionSpace( csName )
                         .getCollection( clName );
                 SaveOidAndMd5 oidAndMd5 = id2md5.take();
                 ObjectId oid = oidAndMd5.getOid();
 
-                try ( DBLob rLob = dbcl.openLob( oid, DBLob.SDB_LOB_READ )) {
-                    byte[] rbuff = new byte[ ( int ) rLob.getSize() ];
-                    rLob.read( rbuff );
-                    String curMd5 = LobOprUtils.getMd5( rbuff );
-                    String prevMd5 = oidAndMd5.getMd5();
-                    Assert.assertEquals( curMd5, prevMd5 );
-                } catch ( BaseException e ) {
-                    if ( e.getErrorCode() != -4 ) {
-                        Assert.fail( e.getErrorType() + ":" + e.getMessage() );
+                for ( int i = 0; i < 10; i++ ) {
+                    try ( DBLob rLob = dbcl.openLob( oid,
+                            DBLob.SDB_LOB_READ )) {
+                        byte[] rbuff = new byte[ ( int ) rLob.getSize() ];
+                        rLob.read( rbuff );
+                        String curMd5 = LobOprUtils.getMd5( rbuff );
+                        String prevMd5 = oidAndMd5.getMd5();
+                        Assert.assertEquals( curMd5, prevMd5 );
+                    } catch ( BaseException e ) {
+                        e.printStackTrace();
+                        if ( e.getErrorCode() != -4 ) {
+                            throw e;
+                        }
                     }
                 }
             }
@@ -157,8 +162,7 @@ public class TestLobSplitAndRead7849 extends SdbTestBase {
 
     private void writeLobAndGetMd5( DBCollection cl, int lobtimes ) {
         for ( int i = 0; i < lobtimes; i++ ) {
-            int writeLobSize = random.nextInt( 1024 * 1024 );
-            ;
+            int writeLobSize = 1024 * 1024 * 10;
             byte[] wlobBuff = LobOprUtils.getRandomBytes( writeLobSize );
             ObjectId oid = LobOprUtils.createAndWriteLob( cl, wlobBuff );
 
