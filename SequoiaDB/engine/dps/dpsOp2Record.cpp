bebc4fdcfd432eeb5f09fefbee015f1985ea6d6f
chenyuanxi <34-chenyuanxi@users.noreply.gitlab.example.com>
2020-05-28 09:52:46 +0800

    fix transaction begin time check against split time, update debug message

--- SequoiaDB/engine/dps/dpsOp2Record.cpp
+++ SequoiaDB/engine/dps/dpsOp2Record.cpp
@@ -2996,7 +2996,7 @@ namespace engine
       dpsLogRecord::iterator itr = record.find( DPS_LOG_PUBLIC_TRANSID ) ;
       if ( !itr.valid() )
       {
-         PD_LOG( PDDEBUG, "Failed to find tag transaction ID in record" ) ;
+         PD_LOG( PDINFO, "Failed to find tag transaction ID in record" ) ;
          rc = SDB_SYS ;
          goto error ;
       }
