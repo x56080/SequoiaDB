e25fb9e0e4d402928a446c92e09aee1e8b6f8c5f
luweikang <luweikang@sequoiadb.com>
2020-06-05 21:09:54 +0800

    根据问题单5814调整用例

--- testcase_new/story/java/src/test/java/com/sequoiadb/transaction/unlock/Transaction18413.java
+++ testcase_new/story/java/src/test/java/com/sequoiadb/transaction/unlock/Transaction18413.java
@@ -76,8 +76,7 @@ public class Transaction18413 extends SdbTestBase {
         }
     }
 
-    @SuppressWarnings("unchecked")
-    @Test(enabled = false) // SEQUOIADBMAINSTREAM-5814
+    @Test
     public void test() throws InterruptedException {
         DBCollection cl1 = db1.getCollectionSpace( csName )
                 .getCollection( clName );
