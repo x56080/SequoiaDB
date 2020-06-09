bd86755e3c789756decaf06c97c77d21ec851b05
wuyan <wuyan@sequoiadb.com>
2020-06-09 11:01:10 +0800

    ci#1502 updateSymbol_22142用例修改对比结果中的排序字段

--- testcase_new/story/js/updateSymbol/updateSymbol_22142.js
+++ testcase_new/story/js/updateSymbol/updateSymbol_22142.js
@@ -16,23 +16,23 @@ function test(testPara)
    insertData( testPara.testCL, doc1 );   
 
    var updateCondition1 = { $inc: { testa1: { $field: 'fieldb'}} };
-   var findCondition1 = { testa1: -21470 };
+   var findCondition1 = { no:4, testa1: -21470};
    upsertData( testPara.testCL, updateCondition1, findCondition1 );  
-   doc1.push({ testa1: -21470 });   
-   checkResult( testPara.testCL, null, null, doc1, { _id: 1 } );
+   doc1.push({ no:4, testa1: -21470 });   
+   checkResult( testPara.testCL, null, null, doc1, { no: 1 } );
    
    //$field指定字段值为对象类型
    var updateCondition2 = { $inc: { testa2: { $field: 'fieldb.num1'} ,testb2: { $field: 'fieldb.num2.a'}}};
-   var findCondition2 = { fieldb: { num1: 20, num2: {a: 12.01} } };
+   var findCondition2 = { no: 5, fieldb: { num1: 20, num2: {a: 12.01} } };
    upsertData( testPara.testCL, updateCondition2, findCondition2 );   
-   doc1.push({ testa2: 20, testb2:12.01 ,fieldb: { num1: 20, num2: {a: 12.01} }});
-   checkResult( testPara.testCL, null, null, doc1, { _id: 1 } );
+   doc1.push({ no: 5, testa2: 20, testb2:12.01 ,fieldb: { num1: 20, num2: {a: 12.01} }});
+   checkResult( testPara.testCL, null, null, doc1, { no: 1 } );
    
    //$field指定字段值为数组类型
    var updateCondition3 = { $inc: { testa3: { $field: 'field.1'}, testb3: { $field: 'field.2.1'} } };
-   var findCondition3 = { field: ["test0", 10.23 ,[0, -123]] };
+   var findCondition3 = {no: 6, field: ["test0", 10.23 ,[0, -123]] };
    upsertData( testPara.testCL, updateCondition3, findCondition3 );   
-   doc1.push({ testa3: 10.23, testb3:-123 ,field: ["test0", 10.23 ,[0, -123]]});
-   checkResult( testPara.testCL, null, null, doc1, { _id: 1 } );
+   doc1.push({ no: 6, testa3: 10.23, testb3:-123 ,field: ["test0", 10.23 ,[0, -123]]});
+   checkResult( testPara.testCL, null, null, doc1, { no: 1 } );
 }
 
