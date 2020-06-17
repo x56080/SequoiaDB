b5dbb55f77b57df917a6fbc9d32e43971825264f
zhaoyu <43-zhaoyu@users.noreply.gitlab.example.com>
2020-06-17 10:41:45 +0800

    Jira5890 mvcc

--- testcase_new/story/java/src/test/java/com/sequoiadb/transaction/rc/Transaction22294.java
+++ testcase_new/story/java/src/test/java/com/sequoiadb/transaction/rc/Transaction22294.java
@@ -57,6 +57,8 @@ public class Transaction22294 extends SdbTestBase {
                         "{ShardingKey:{'b':1},ShardingType:'range',Group:'"
                                 + srcGroup + "'}" ) );
         cl.createIndex( "a", "{a:1}", false, false );
+        // 创建索引后，休眠0.1s，避免索引未创建完成
+        Thread.sleep( 100 );
         expList = TransUtils.insertRandomDatas( cl, 0, 6 );
     }
 
