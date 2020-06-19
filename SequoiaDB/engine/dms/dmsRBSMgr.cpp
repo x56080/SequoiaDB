2d9cf055167031ebc721f96618a70462351d19b6
heguoming <heguoming@sequoiadb.com>
2020-06-19 11:41:20 +0800

    Revert "SEQUOIADBMAINSTREAM-5948: fix issue when initialize RBS spaces"
    This reverts commit bb68ea84005ccb887c56bca6c70a6ffe2418fbc1

--- SequoiaDB/engine/dms/dmsRBSMgr.cpp
+++ SequoiaDB/engine/dms/dmsRBSMgr.cpp
@@ -76,8 +76,6 @@ namespace engine
 
       PD_TRACE_ENTRY( SDB__DMSRBSMGR_INIT ) ;
 
-      fini() ;
-
       // initialize RBS number
       rc = _initRBSNum( rbsNum ) ;
       PD_RC_CHECK( rc, PDERROR, "Failed to initialize number of RBS "
@@ -145,7 +143,7 @@ namespace engine
 
    INT32 _dmsRBSMgr::fini()
    {
-      INT32 rc = SDB_OK ;
+      INT32 rc = SDB_OK;
 
       if ( NULL != _rbsSUMgrs )
       {
@@ -154,8 +152,6 @@ namespace engine
             _rbsSUMgrs[ i ].fini() ;
          }
          SDB_OSS_DEL [] _rbsSUMgrs ;
-         _rbsSUMgrs = NULL ;
-         _rbsNum = 0 ;
       }
 
       return rc ;
