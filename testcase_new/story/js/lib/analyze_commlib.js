aa0998d026a96fcdb05b8dc9509752e6e80535b0
liyuanyue <liyuanyue@sequoiadb.com>
2020-06-28 18:00:29 +0800

    为getCommonAccessPlans方法增加打印信息，帮助定位ci问题

--- testcase_new/story/js/lib/analyze_commlib.js
+++ testcase_new/story/js/lib/analyze_commlib.js
@@ -829,10 +829,15 @@ function getCommonAccessPlans ( db, findConf, selectorConf, sortConf )
 
       //获取快照信息
       var rc = db.snapshot( 11, findConf, selectorConf, sortConf ).toArray();
+      println( "rc.length:" + rc.length );
       for( var i = 0; i < rc.length; i++ )
       {
          var accessPlan = eval( "( " + rc[i] + " )" );
          var accessPlanObj = {};
+         println( "i:" + i + ", NodeName:" + JSON.stringify( accessPlan.NodeName ) +
+            ", GroupName:" + JSON.stringify( accessPlan.GroupName ) +
+            ", IndexName:" + JSON.stringify( accessPlan.IndexName ) +
+            ", Query:" + JSON.stringify( accessPlan.Query ) );
          for( var f in accessPlan )
          {
             if( ( f == "ScanType" ) || ( f == "IndexName" ) )
