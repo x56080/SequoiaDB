f19f385668517cab19c0488fdcff3132645b6891
wuyan <21-wuyan@users.noreply.gitlab.example.com>
2020-06-30 21:22:55 +0800

    修改ci失败用例transaction22263

--- testcase_new/story/js/transaction_rlb/transaction_22263.js
+++ testcase_new/story/js/transaction_rlb/transaction_22263.js
@@ -10,35 +10,51 @@ main( test );
 
 function test( testPara )
 {
-   var groupName = testPara.srcGroupName;     
+   var groupName = testPara.srcGroupName;
    db.transBegin();
    testPara.testCL.insert( { a: 1 } );
    
-   var newdb = new Sdb( COORDHOSTNAME, COORDSVCNAME );    
-   var rg = newdb.getRG( groupName );
-   var slaveNode = rg.getSlave();
-   slaveHostName = slaveNode.getHostName();
-   slaveServiceName = slaveNode.getServiceName();
-   rg.reelect({Seconds: 300, HostName: slaveHostName, ServiceName: slaveServiceName });
-   
    try
    {
-      
-      db.transCommit();
-      throw new Error( "commit should be failed!");
-   }
-   catch( e )
-   {
-      if( e.message !== "-85" )
+      var newdb = new Sdb( COORDHOSTNAME, COORDSVCNAME );    
+      var rg = newdb.getRG( groupName );
+      var slaveNode = rg.getSlave();
+      slaveHostName = slaveNode.getHostName();
+      slaveServiceName = slaveNode.getServiceName();
+      rg.reelect({Seconds: 300, HostName: slaveHostName, ServiceName: slaveServiceName });
+   
+      try
       {
-         throw e;
+         
+         db.transCommit();
+         throw new Error( "commit should be failed!");
+      }
+      catch( e )
+      {
+         if( e.message !== "-85" )
+         {
+            throw e;
+         }
+      }
+      
+      db.transRollback();
+      checkReelect( groupName, slaveHostName, slaveServiceName );
+       
+      //检查结果开启事务查询，规避非事务查询返回未回滚的记录，导致比较结果不一致
+      db.transBegin();
+      var cl = db.getCS(COMMCSNAME).getCL(testConf.clName);
+      var cursor = cl.find();
+      commCompareResults( cursor, [] );
+      db.transCommit();
+   }
+   finally
+   {
+      db.transRollback(); 
+      if( newdb !== undefined )
+      {
+         newdb.close();
       }
    }
-    
-    newdb.close();
-    checkReelect( groupName, slaveHostName, slaveServiceName );
-    var cursor = testPara.testCL.find();
-    commCompareResults( cursor, [] );
 }
 
 function checkReelect( groupName, hostName, svcName )
