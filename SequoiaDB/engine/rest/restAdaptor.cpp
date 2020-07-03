f871a4a77efc7fa7423a29e0070a111b86fb2c44
hejiawen <hejiawen@sequoiadb.com>
2020-07-03 18:23:50 +0800

    SEQUOIADBMAINSTREAM-6023 修复 rest 接收 body 的长度比 http 消息的 Content-Length 大，导致内存越界的问题

--- SequoiaDB/engine/rest/restAdaptor.cpp
+++ SequoiaDB/engine/rest/restAdaptor.cpp
@@ -46,7 +46,7 @@
 #include "../util/url.h"
 
 /* response body cache max size */
-#define RESPONSE_MAX_CACHE_SIZE  (64*1024*1024)
+#define RESPONSE_MAX_CACHE_SIZE   (64*1024*1024)
 
 /* once recv size */
 #define REST_ONCE_RECV_SIZE       1024
@@ -778,10 +778,16 @@ namespace engine
 
             if ( pRest->_tmpBodyBuf )
             {
-               ossMemcpy( pRest->_bodyBuf, pRest->_tmpBodyBuf,
-                          pRest->_tmpBodySize ) ;
+               INT32 tmpBodySize = pRest->_tmpBodySize ;
 
-               receivedSize = pRest->_tmpBodySize ;
+               if ( tmpBodySize > bodySize )
+               {
+                  tmpBodySize = bodySize ;
+               }
+
+               ossMemcpy( pRest->_bodyBuf, pRest->_tmpBodyBuf, tmpBodySize ) ;
+
+               receivedSize = tmpBodySize ;
 
                pRest->_tmpBodyBuf = NULL ;
                pRest->_tmpBodySize = 0 ;
