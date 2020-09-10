d334db58ab66349082a7220a6cbd4a2b3cae3db5
huangxiaoni <huangxiaoni@sequoiadb.com>
2020-09-10 19:29:23 +0800

    用例用了公共CS/CL,可能会受其他用例影响导致用例执行失败，优化用例

--- testcase_new/story/js/sdbexprt/csclNotExist_13564.js
+++ testcase_new/story/js/sdbexprt/csclNotExist_13564.js
@@ -6,66 +6,67 @@
 * @author      : Liang XueWang 
 *
 *******************************************************************/
-main() ;
+main();
 
-function main()
+function main ()
 {
-   commCreateCL( db, COMMCSNAME, COMMCLNAME, 0 ) ;
-  
-   testExprtNotExistCs() ;
-   testExprtNotExistCl() ;
-   
-   commDropCL( db, COMMCSNAME, COMMCLNAME ) ;
+   var clName = COMMCLNAME + "_13564";
+   commCreateCL( db, COMMCSNAME, clName, {}, true, true );
+
+   testExprtNotExistCs( clName );
+   testExprtNotExistCl( clName );
+
+   commDropCL( db, COMMCSNAME, clName );
 }
 
-function testExprtNotExistCs()
+function testExprtNotExistCs ( clName )
 {
-   var csvfile = tmpFileDir + "sdbexprt13564.csv" ;
-   cmd.run( "rm -rf " + csvfile ) ;
-   
+   var csvfile = tmpFileDir + "sdbexprt13564.csv";
+   cmd.run( "rm -rf " + csvfile );
+
    var command = installPath + "bin/sdbexprt" +
-                 " -s " + COORDHOSTNAME +
-                 " -p " + COORDSVCNAME + 
-                 " --file " + csvfile +
-                 " --type csv" +
-                 " --cscl notExistCs " + 
-                 " --force true" ;
-   testRunCommand( command, 8 ) ;
-   
+      " -s " + COORDHOSTNAME +
+      " -p " + COORDSVCNAME +
+      " --file " + csvfile +
+      " --type csv" +
+      " --cscl notExistCs " +
+      " --force true";
+   testRunCommand( command, 8 );
+
    var command = installPath + "bin/sdbexprt" +
-                 " -s " + COORDHOSTNAME +
-                 " -p " + COORDSVCNAME + 
-                 " -c notExistCs " +
-                 " -l " + COMMCLNAME + 
-                 " --file " + csvfile +
-                 " --type csv" + 
-                 " --force true" ;
-   testRunCommand( command, 8 ) ;
-   
-   cmd.run( "rm -rf " + csvfile ) ;
+      " -s " + COORDHOSTNAME +
+      " -p " + COORDSVCNAME +
+      " -c notExistCs " +
+      " -l " + clName +
+      " --file " + csvfile +
+      " --type csv" +
+      " --force true";
+   testRunCommand( command, 8 );
+
+   cmd.run( "rm -rf " + csvfile );
 }
 
-function testExprtNotExistCl()
+function testExprtNotExistCl ()
 {
-   var jsonfile = tmpFileDir + "sdbexprt13564.json" ;
-   cmd.run( "rm -rf " + jsonfile ) ;
-   
+   var jsonfile = tmpFileDir + "sdbexprt13564.json";
+   cmd.run( "rm -rf " + jsonfile );
+
    var command = installPath + "bin/sdbexprt" +
-                 " -s " + COORDHOSTNAME +
-                 " -p " + COORDSVCNAME + 
-                 " --file " + jsonfile +
-                 " --type json" +
-                 " --cscl " + COMMCSNAME + ".notExistCl" ;
-   testRunCommand( command, 8 ) ;
-   
+      " -s " + COORDHOSTNAME +
+      " -p " + COORDSVCNAME +
+      " --file " + jsonfile +
+      " --type json" +
+      " --cscl " + COMMCSNAME + ".notExistCl";
+   testRunCommand( command, 8 );
+
    var command = installPath + "bin/sdbexprt" +
-                 " -s " + COORDHOSTNAME +
-                 " -p " + COORDSVCNAME +
-                 " -c " + COMMCSNAME +
-                 " -l notExistCl" +
-                 " --file " + jsonfile +
-                 " --type json" ;
-   testRunCommand( command, 8 ) ;
-   
-   cmd.run( "rm -rf " + jsonfile ) ;
+      " -s " + COORDHOSTNAME +
+      " -p " + COORDSVCNAME +
+      " -c " + COMMCSNAME +
+      " -l notExistCl" +
+      " --file " + jsonfile +
+      " --type json";
+   testRunCommand( command, 8 );
+
+   cmd.run( "rm -rf " + jsonfile );
 }
--- testcase_new/story/js/sdbexprt/fieldsBatch_13530.js
+++ testcase_new/story/js/sdbexprt/fieldsBatch_13530.js
@@ -5,54 +5,59 @@
 * @author      : Liang XueWang 
 *
 *******************************************************************/
-var csname = COMMCSNAME ;
-var clnum = 100 ;
-var clnames = [] ;
-var doc = { a: 1 } ;
-var csvContent = "a\n1\n" ;
+var csname = COMMCSNAME + "_13530";
+var clnum = 100;
+var clnames = [];
+var doc = { a: 1 };
+var csvContent = "a\n1\n";
 
-main() ;
+main();
 
-function main()
+function main ()
 {
-   for( var i = 0;i < clnum;i++ )
+   commDropCS( db, csname );
+   commCreateCS( db, csname );
+
+   for( var i = 0; i < clnum; i++ )
    {
-      var clname = COMMCLNAME + "_sdbexprt13530_" + i ;
-      var cl = commCreateCL( db, csname, clname, 0 ) ;
-      cl.insert( doc ) ;
-      clnames.push( clname ) ;
+      var clname = COMMCLNAME + "_sdbexprt13530_" + i;
+      var cl = commCreateCL( db, csname, clname );
+      cl.insert( doc );
+      clnames.push( clname );
    }
-  
-   testExprtCsv() ;
-   
-   for( var i = 0;i < clnames.length;i++ )
+
+   testExprtCsv();
+
+   for( var i = 0; i < clnames.length; i++ )
    {
-      commDropCL( db, csname, clnames[i] ) ;
+      commDropCL( db, csname, clnames[i] );
    }
 }
 
-function testExprtCsv()
+function testExprtCsv ()
 {
-   var csvDir = tmpFileDir + "13530/" ;
-   cmd.run( "mkdir -p " + csvDir ) ;
-   
+   var csvDir = tmpFileDir + "13530/";
+   cmd.run( "mkdir -p " + csvDir );
+
    var command = installPath + "bin/sdbexprt" +
-                 " -s " + COORDHOSTNAME +
-                 " -p " + COORDSVCNAME + 
-                 " --cscl " + csname + 
-                 " --dir " + csvDir +
-                 " --type csv" ;
-   for( var i = 0;i < clnames.length;i++ )
+      " -s " + COORDHOSTNAME +
+      " -p " + COORDSVCNAME +
+      " --cscl " + csname +
+      " --dir " + csvDir +
+      " --type csv";
+   for( var i = 0; i < clnames.length; i++ )
    {
-      command += " --fields " + csname + "." + clnames[i] + ":a" ;
+      command += " --fields " + csname + "." + clnames[i] + ":a";
    }
-   testRunCommand( command ) ;
-   
-   for( var i = 0;i < clnames.length;i++ )
+   testRunCommand( command );
+
+   for( var i = 0; i < clnames.length; i++ )
    {
-      var filename = csvDir + csname + "." + clnames[i] + ".csv" ;
-      checkFileContent( filename, csvContent ) ;
+      var filename = csvDir + csname + "." + clnames[i] + ".csv";
+      checkFileContent( filename, csvContent );
    }
-   
-   cmd.run( "rm -rf " + csvDir ) ;
+
+   cmd.run( "rm -rf " + csvDir );
+
+   commDropCS( db, COMMCSNAME, false );
 }
