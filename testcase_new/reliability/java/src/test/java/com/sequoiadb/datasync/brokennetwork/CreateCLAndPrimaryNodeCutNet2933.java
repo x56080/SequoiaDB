47ceeb95f41acf84f56ec2d0e5a088231ebde3a6
liyuanyue <liyuanyue@sequoiadb.com>
2020-09-28 17:40:43 +0800

    清理残留context_v3.2

--- testcase_new/reliability/java/src/test/java/com/sequoiadb/datasync/brokennetwork/CreateCLAndPrimaryNodeCutNet2933.java
+++ testcase_new/reliability/java/src/test/java/com/sequoiadb/datasync/brokennetwork/CreateCLAndPrimaryNodeCutNet2933.java
@@ -92,6 +92,10 @@ public class CreateCLAndPrimaryNodeCutNet2933 extends SdbTestBase {
             Assert.assertEquals( groupMgr.checkBusinessWithLSN( 600 ), true,
                     "check LSN consistency fail" );
 
+             // clear context
+            String match = preCLName;
+            CommLib.waitContextClose( sdb, match, 300, false );
+
             // check result
             checkCreateCLResult();
 
