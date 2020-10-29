abf487c670bd54e338060839f3dd19071eb8ade6
zhaohailin <zhaohailin@sequoiadb.com>
2020-10-29 14:47:47 +0800

    jsObjectSync目录SystemService_10668.js用例本地跑不通过调整

--- testcase_new/story/js/jsObjectSync/SystemService_10668.js
+++ testcase_new/story/js/jsObjectSync/SystemService_10668.js
@@ -6,7 +6,6 @@
 *                          10671 System对象停止服务，停止已停止的服务
 *@author      : Liang XueWang
 ******************************************************************************/
-
 // 测试启动停止服务，获取服务状态（SSH服务）
 SystemTest.prototype.testRunServiceSSH = function()
 {
@@ -21,28 +20,50 @@ SystemTest.prototype.testRunServiceSSH = function()
    }
    if( !isSSHExist( this.hostname, this.svcname ) )
       return;
-
    // 获取服务状态
-   var info = this.system.runService( "ssh", "status", "" );
-   if( info.indexOf( "start" ) !== -1 )   // 如果服务已启动，则停止再启动服务
+   try
    {
-      this.system.runService( "ssh", "stop", "" );
-      var status = this.system.runService( "ssh", "status" );
-      checkStatus( status, "stop" );
-      this.system.runService( "ssh", "start", "" );
-      status = this.system.runService( "ssh", "status" );
-      checkStatus( status, "start" );
+      var info = this.system.runService( "ssh", "status", "" );
    }
-   else if( info.indexOf( "stop" ) !== -1 )  // 如果服务已停止，则启动服务
+   catch( e )
+   {
+      if( e === 3 )
+      {
+         info = "dead";
+      }
+   }
+   if( info.indexOf( "running" ) !== -1 )   // 如果服务已启动，则停止再启动服务
+   {
+      var status;
+      try
+      {
+         this.system.runService( "ssh", "stop", "" );
+         status = this.system.runService( "ssh", "status" );
+         throw status;
+      }
+      catch( e )
+      {
+         if( e != 3 )
+         {
+            throw new Error( "ssh service stop fail" + e );
+         }
+      }
+      finally
+      {
+         this.system.runService( "ssh", "start", "" );
+         status = this.system.runService( "ssh", "status" );
+         checkStatus( status, "running" );
+      }
+   }
+   else if( info.indexOf( "dead" ) !== -1 )  // 如果服务已停止，则启动服务
    {
       this.system.runService( "ssh", "start", "" );
       var status = this.system.runService( "ssh", "status" );
-      checkStatus( status, "start" );
+      checkStatus( status, "running" );
    }
    else
    {
-      throw buildException( "testRunService", null, "get service status " + this,
-         "start/stop", info );
+      throw new Error( "testRunService get service exp status start/stop. but result:" + info );
    }
    this.release();
 }
@@ -66,7 +87,7 @@ SystemTest.prototype.testRunServiceDuplicate = function()
    // 获取服务状态
    var info = this.system.runService( "ssh", "status" );
    var command;
-   if( info.indexOf( "start" ) !== -1 )
+   if( info.indexOf( "running" ) !== -1 )
       command = "start";
    else
       command = "stop";
@@ -77,23 +98,22 @@ SystemTest.prototype.testRunServiceDuplicate = function()
    }
    catch( e )
    {
-      if( e !== 1 )
+      if( e != 0 )
       {
-         throw buildException( "testRunServiceDuplicate", e,
-            "run service duplicate " + this, 1, e );
+         throw new Error( "testRunServiceDuplicate run service duplicate" + e );
       }
    }
    // 测试完成后，启动服务
    try
    {
       this.system.runService( "ssh", "start" );
+      throw 0;
    }
    catch( e )
    {
-      if( e !== 1 )
+      if( e != 0 )
       {
-         throw buildException( "testRunServiceDuplicate", e,
-            "test start service in the end " + this, 1, e );
+         throw new Error( "testRunServiceDuplicate test start service in the end" + e );
       }
    }
    this.release();
@@ -123,9 +143,9 @@ SystemTest.prototype.testStopSdbcm = function()
    }
    catch( e )
    {
-      if( e !== -16 )
+      if( e != -16 )
       {
-         throw buildException( "testStopSdbcm", e, "stop sdbcm " + this, -16, e );
+         throw new Error( "testStopSdbcm stop sdbcm" + e );
       }
    }
    this.release();
@@ -139,8 +159,7 @@ function checkStatus ( status, msg )
 {
    if( status.indexOf( msg ) === -1 )
    {
-      throw buildException( "checkStatus", null,
-         "check service status " + msg, status, msg );
+      throw new Error( "testStopSdbcm stop sdbcm" );
    }
 }
 
@@ -160,10 +179,7 @@ function isSSHExist ( hostname, svcname )
    }
    catch( e )
    {
-      if( e === 1 )
-         exist = false;
-      else
-         throw buildException( "isSSHExist", e );
+      throw new Error( " service is not exist !" );
    }
    remote.close();
    return exist;
@@ -171,6 +187,7 @@ function isSSHExist ( hostname, svcname )
 
 function main ()
 {
+
    // 获取本地主机和远程主机
    var localhost = toolGetLocalhost();
    var remotehost = toolGetRemotehost();
@@ -178,7 +195,7 @@ function main ()
    var localSystem = new SystemTest( localhost, CMSVCNAME );
    var remoteSystem = new SystemTest( remotehost, CMSVCNAME );
    var systems = [localSystem, remoteSystem];
-
+   println( systems );
    for( var i = 0; i < systems.length; i++ )
    {
       // 测试启动停止ssh服务
@@ -188,8 +205,7 @@ function main ()
       systems[i].testRunServiceDuplicate();
 
       // 测试停止sdbcm服务
-      // systems[i].testStopSdbcm() ;
+      // systems[i].testStopSdbcm();
    }
 }
-
-main()
+main();
