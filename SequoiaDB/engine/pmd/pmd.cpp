59f67153268395b93949de6c3085983378950702
heguoming <heguoming@sequoiadb.com>
2020-12-08 19:25:34 +0800

    SEQUOIADBMAINSTREAM-6496: fix trace breakpoint hangs after sdbstop

--- SequoiaDB/engine/pmd/pmd.cpp
+++ SequoiaDB/engine/pmd/pmd.cpp
@@ -492,6 +492,10 @@ namespace engine
       }
 
       _isActive = FALSE ;
+
+      // destroy trace
+      sdbGetPDTraceCB()->destroy() ;
+
       INT64 shutdownWaitTimeout = _optioncb.shutdownWaitTimeout() ;
       if ( shutdownWaitTimeout < PMD_STOP_DEADCHECK_TIMEOUT )
       {
