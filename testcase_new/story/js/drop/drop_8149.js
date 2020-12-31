d3153047e446a79a87026753c3a13100e5de2f0e
liyuanyue <liyuanyue@sequoiadb.com>
2020-12-31 15:44:21 +0800

    放开drop_8149.js用例

--- testcase_new/story/js/drop/drop_8149.js
+++ testcase_new/story/js/drop/drop_8149.js
@@ -12,7 +12,7 @@
 ******************************************************************************/
 testConf.csName = COMMCLNAME + "_8149";
 // SEQUOIADBMAINSTREAM-6212
-//main( test );
+main( test );
 
 function test ( testPara )
 {
