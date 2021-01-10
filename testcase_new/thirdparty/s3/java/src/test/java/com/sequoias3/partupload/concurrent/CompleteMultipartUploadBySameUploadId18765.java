e8af6034cef1bf28220bbc35ed0f0a13cb7917fe
fanyu <fanyu@sequoiadb.com>
2021-01-10 18:35:10 +0800

    ci-391 并发执行分段上传完成后再并发合并分段

--- testcase_new/thirdparty/s3/java/src/test/java/com/sequoias3/partupload/concurrent/CompleteMultipartUploadBySameUploadId18765.java
+++ testcase_new/thirdparty/s3/java/src/test/java/com/sequoias3/partupload/concurrent/CompleteMultipartUploadBySameUploadId18765.java
@@ -1,8 +1,21 @@
 package com.sequoias3.partupload.concurrent;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.amazonaws.services.s3.model.PartETag;
+import com.amazonaws.services.s3.model.UploadPartRequest;
+import com.amazonaws.services.s3.model.UploadPartResult;
 import com.sequoiadb.threadexecutor.ResultStore;
 import com.sequoiadb.threadexecutor.ThreadExecutor;
 import com.sequoiadb.threadexecutor.annotation.ExecuteOrder;
@@ -11,16 +24,6 @@ import com.sequoias3.testcommon.S3TestBase;
 import com.sequoias3.testcommon.TestTools;
 import com.sequoias3.testcommon.s3utils.ObjectUtils;
 import com.sequoias3.testcommon.s3utils.PartUploadUtils;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
 
 /**
  * @Description seqDB-18765: the key upload multiple parts and
@@ -35,11 +38,12 @@ public class CompleteMultipartUploadBySameUploadId18765 extends S3TestBase {
     private String bucketName = "bucket18765";
     private AmazonS3 s3Client = null;
     private File localPath = null;
-    private String[] filePaths = new String[ 5 ];
-    private int[] fileSizes = { 1024 * 1024 * 50, 1024 * 1024 * 29,
-            1024 * 1024 * 30, 1024 * 1024 * 10, 1024 * 1024 * 40 };
-    private MultiValueMap< Long, String > uploadFileSizeAndMd5 = new LinkedMultiValueMap< Long, String >();
-    private int successNum = 0;
+    private String filePath = null;
+    private int fileSize = 1024 * 1024 * 50;
+    private int partSize = 1024 * 1024 * 5;
+    private String uploadId;
+    private List< PartETag > eTagList = new CopyOnWriteArrayList<>();
+    private AtomicInteger successNum = new AtomicInteger( 0 );
 
     @BeforeClass
     private void setUp() throws IOException {
@@ -47,13 +51,9 @@ public class CompleteMultipartUploadBySameUploadId18765 extends S3TestBase {
                 + TestTools.getClassName() );
         TestTools.LocalFile.removeFile( localPath );
         TestTools.LocalFile.createDir( localPath.toString() );
-        for ( int i = 0; i < fileSizes.length; i++ ) {
-            String filePath = localPath + File.separator + "localFile_"
-                    + fileSizes[ i ] + ".txt";
-            TestTools.LocalFile.createFile( filePath, fileSizes[ i ] );
-            filePaths[ i ] = filePath;
-        }
-
+        filePath = localPath + File.separator + "localFile_" + fileSize
+                + ".txt";
+        TestTools.LocalFile.createFile( filePath, fileSize );
         s3Client = CommLib.buildS3Client();
         CommLib.clearBucket( s3Client, bucketName );
         s3Client.createBucket( bucketName );
@@ -62,18 +62,25 @@ public class CompleteMultipartUploadBySameUploadId18765 extends S3TestBase {
 
     @Test
     public void uploadParts() throws Exception {
-        String uploadId = PartUploadUtils.initPartUpload( s3Client, bucketName,
+        uploadId = PartUploadUtils.initPartUpload( s3Client, bucketName,
                 keyName );
-        ThreadExecutor threadExec = new ThreadExecutor();
-        int[] partSizes = { 1024 * 1024 * 5, 1024 * 1024 * 6, 1024 * 1024 * 6,
-                1024 * 1024 * 5, 1024 * 1024 * 10 };
-        for ( int i = 0; i < filePaths.length; i++ ) {
-            String filePath = filePaths[ i ];
-            int partSize = partSizes[ i ];
-            threadExec.addWorker(
-                    new PartUpload( uploadId, filePath, partSize ) );
+        ThreadExecutor threadExec1 = new ThreadExecutor();
+        // upload part
+        int fileOffset = 0;
+        for ( int i = 1; fileOffset < fileSize; i++ ) {
+            long eachPartSize = Math.min( partSize, fileSize - fileOffset );
+            threadExec1
+                    .addWorker( new PartUpload( i, fileOffset, eachPartSize ) );
+            fileOffset += partSize;
         }
-        threadExec.run();
+        threadExec1.run();
+
+        // complete upload
+        ThreadExecutor threadExec2 = new ThreadExecutor();
+        for ( int i = 0; i < 5; i++ ) {
+            threadExec2.addWorker( new CompletePartUpload() );
+        }
+        threadExec2.run();
 
         // check the upload file
         checkResult();
@@ -95,45 +102,24 @@ public class CompleteMultipartUploadBySameUploadId18765 extends S3TestBase {
     private void checkResult() throws Exception {
         // only one completeMultipartUpload success
         int expSuccessNum = 1;
-        Assert.assertEquals( successNum, expSuccessNum );
+        Assert.assertEquals( successNum.get(), expSuccessNum );
         // get the upload object to check content by md5
         String downfileMd5 = ObjectUtils.getMd5OfObject( s3Client, localPath,
                 bucketName, keyName );
-        long size = ( long ) uploadFileSizeAndMd5.keySet().toArray()[ 0 ];
-        String expFileMd5 = uploadFileSizeAndMd5.get( size ).get( 0 );
+        String expFileMd5 = TestTools.getMD5( filePath );
         Assert.assertEquals( downfileMd5, expFileMd5,
-                "the object size is :" + size );
+                "the filePath is :" + filePath );
     }
 
-    private class PartUpload extends ResultStore {
-        private String filePath;
-        private String uploadId;
-        private int partSize;
-        private List< PartETag > partEtags;
-        private File file = null;
+    private class CompletePartUpload extends ResultStore {
         private AmazonS3 s3Client1 = CommLib.buildS3Client();
 
-        private PartUpload( String uploadId, String filePath, int partSize ) {
-            this.uploadId = uploadId;
-            this.filePath = filePath;
-            this.partSize = partSize;
-        }
-
         @ExecuteOrder(step = 1)
-        private void partUpload() {
-            file = new File( filePath );
-            partEtags = PartUploadUtils.partUpload( s3Client1, bucketName,
-                    keyName, uploadId, file, partSize );
-        }
-
-        @ExecuteOrder(step = 2)
         private void completeMultipartUpload() throws IOException {
             try {
                 PartUploadUtils.completeMultipartUpload( s3Client1, bucketName,
-                        keyName, uploadId, partEtags );
-                successNum++;
-                String fileMd5 = TestTools.getMD5( filePath );
-                uploadFileSizeAndMd5.add( file.length(), fileMd5 );
+                        keyName, uploadId, eTagList );
+                successNum.incrementAndGet();
             } catch ( AmazonS3Exception e ) {
                 int errCode = e.getStatusCode();
                 // 400: InvalidPart,404:NoSuchUpload
@@ -145,6 +131,31 @@ public class CompleteMultipartUploadBySameUploadId18765 extends S3TestBase {
                     s3Client1.shutdown();
                 }
             }
+        }
+    }
+
+    private class PartUpload {
+        private int partNum;
+        private long fileOffset;
+        private long partSize;
+
+        public PartUpload( int partNum, long fileOffset, long partSize ) {
+            this.partNum = partNum;
+            this.fileOffset = fileOffset;
+            this.partSize = partSize;
+        }
+
+        @ExecuteOrder(step = 1)
+        private void partUpload() {
+            File file = new File( filePath );
+            UploadPartRequest partRequest = new UploadPartRequest()
+                    .withFile( file ).withFileOffset( fileOffset )
+                    .withPartNumber( partNum ).withPartSize( partSize )
+                    .withBucketName( bucketName ).withKey( keyName )
+                    .withUploadId( uploadId );
+            UploadPartResult uploadPartResult = s3Client
+                    .uploadPart( partRequest );
+            eTagList.add( uploadPartResult.getPartETag() );
         }
     }
 }
