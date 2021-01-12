2e843396016267651043641fb371a4cb81bc8025
hejiawen <hejiawen@sequoiadb.com>
2021-01-12 22:46:53 +0800

    SEQUOIADBMAINSTREAM-4681 SAC支持double、float指定长度和精度，支持字段注释，支持字段zerofill属性

--- tools/om_plugins/sequoiasql/www/app/controller/Data/MySQL/Database/Index.js
+++ tools/om_plugins/sequoiasql/www/app/controller/Data/MySQL/Database/Index.js
@@ -911,6 +911,12 @@
                            "value": false
                         },
                         {
+                           "name": "zerofill",
+                           "webName": $scope.pAutoLanguage( "零填充" ),
+                           "type": "checkbox",
+                           "value": false
+                        },
+                        {
                            "name": "null",
                            "webName": $scope.pAutoLanguage( "空" ),
                            "type": "checkbox",
@@ -985,6 +991,8 @@
                      case 'timestamp':
                      case 'time':
                      case 'binary':
+                     case 'double':
+                     case 'float':
                         subSql += '(' + fieldInfo['length'] + ') ' ;
                         break ;
                      case 'set':
@@ -1017,7 +1025,26 @@
                   {
                      subSql += ' ' ;
                   }
-                  if( fieldInfo['unsigned'] == true )
+                  if( fieldInfo['zerofill'] == true )
+                  {
+                     switch( fieldInfo['type'] )
+                     {
+                     case 'tinyint':
+                     case 'smallint':
+                     case 'mediumint':
+                     case 'int':
+                     case 'double':
+                     case 'bigint':
+                     case 'decimal':
+                     case 'float':
+                        subSql += 'zerofill ' ;
+                        break ;
+                     default:
+                        subSql += ' ' ;
+                        break ;
+                     }
+                  }
+                  else if( fieldInfo['unsigned'] == true )
                   {
                      switch( fieldInfo['type'] )
                      {
