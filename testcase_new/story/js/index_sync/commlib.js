2005151e8eb5a8285dde37fd704f82a3c32248ba
huangxiaoni <huangxiaoni@sequoiadb.com>
2021-02-24 09:09:00 +0800

    SEQUOIADBMAINSTREAM-6706/5878已修改，补充自动化

---
+++ testcase_new/story/js/index_sync/commlib.js
@@ -0,0 +1,9 @@
+/******************************************************************************
+ * @Description   : 索引配置工程公共方法
+ * @Author        : XiaoNi Huang
+ * @CreateTime    : 2021.02.23
+ * @LastEditTime  : 2021.02.23
+ * @LastEditors   : XiaoNi Huang
+ ******************************************************************************/
+import( "../lib/basic_operation/commlib.js" );
+import( "../lib/main.js" );
