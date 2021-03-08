2eada06f7cd7ad90e1dc1032471f250e45f52216
huangxiaoni <huangxiaoni@sequoiadb.com>
2021-03-08 12:37:32 +0800

    task5745-cl支持设置只读等模式，新增自动化用例

---
+++ testcase_new/story/js/cl/cl_RepairCheck_23602.js
@@ -0,0 +1,15 @@
+﻿/******************************************************************************
+ * @Description   : seqDB-23602:创建集合，配置RepairCheck属性
+ * @Author        : XiaoNi Huang
+ * @CreateTime    : 2021.03.05
+ * @LastEditTime  : 2021.03.08
+ * @LastEditors   : XiaoNi Huang
+ ******************************************************************************/
+
+main( test );
+function test ()
+{
+   var clName = CHANGEDPREFIX + "_cl_23602";
+   var cs = commCreateCS( db, COMMCSNAME, true );
+   assert.tryThrow( -6, function() { cs.createCL( clName, { "RepairCheck": true } ) } );
+}
