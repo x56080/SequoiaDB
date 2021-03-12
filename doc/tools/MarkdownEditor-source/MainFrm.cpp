5c6db33bb38e13b4bf8aaf2fc671836e2e7e70cc
hejiawen <hejiawen@sequoiadb.com>
2021-03-12 19:15:52 +0800

    升级markdown编辑器，保存的内容自动改为linux换行符

--- doc/tools/MarkdownEditor-source/MainFrm.cpp
+++ doc/tools/MarkdownEditor-source/MainFrm.cpp
@@ -164,64 +164,148 @@ IMPLEMENT_DYNCREATE(CMainFrame, CFrameWnd)
 	}
 
    CString STR_EXAMPLE = "\
-# 1. 标题\r\r\n\
-\r\r\n\
-# 这是 H1\r\n\
+# 1. 标题\r\n\
 \r\n\
-### 这是 H3\r\n\
+# 这是 h1\r\n\
 \r\n\
-###### 这是 H6\r\n\
+## 这是 h2\r\n\
+\r\n\
+这也是 h2\r\n\
+---\r\n\
+\r\n\
+### 这是 h3\r\n\
+\r\n\
+###### 这是 h6\r\n\
+\r\n\
+> **Note:**  \r\n\
+> 在官网模式，会根据 H2 生成本页导航\r\n\
 \r\n\
 --\r\n\
 \r\n\
 # 2. 区块引用\r\n\
 \r\n\
-> This is a blockquote with two paragraphs. Lorem ipsum dolor sit amet,\r\n\
-> consectetuer adipiscing elit. Aliquam hendrerit mi posuere lectus.\r\n\
+> This is a blockquote with two paragraphs. Lorem ipsum dolor sit amet,  \r\n\
+> consectetuer adipiscing elit. Aliquam hendrerit mi posuere lectus.  \r\n\
 > Vestibulum enim wisi, viverra nec, fringilla in, laoreet vitae, risus.\r\n\
 > \r\n\
-> Donec sit amet nisl. Aliquam semper ipsum sit amet velit. Suspendisse\r\n\
+> Donec sit amet nisl. Aliquam semper ipsum sit amet velit. Suspendisse  \r\n\
 > id sem consectetuer libero luctus adipiscing.\r\n\
 \r\n\
---\r\n\
-\r\n\
-# 3. 列表\r\n\
-\r\n\
-*   Red\r\n\
-*   Green\r\n\
-*   Blue\r\n\
-\r\n\
-1.  Bird\r\n\
-2.  McHale\r\n\
-3.  Parish\r\n\
-\r\n\
-\r\n\
-* Item 1\r\n\
-* Item 2\r\n\
-* Item 3\r\n\
-   * Item 3a\r\n\
-   * Item 3b\r\n\
+**区块引用内部同样支持双空格强制换行！**\r\n\
 \r\n\
 --\r\n\
 \r\n\
-# 4. 图片\r\n\
+# 3. 换行\r\n\
 \r\n\
-![Pic](http://www.sequoiadb.com/cn/index/Public/Home/images/bbr15_logo.png)\r\n\
+**换行有两种形式，第一种是普通换行，第二种是隔行**\r\n\
+\r\n\
+**换行：在后面加两个空格就可以，如：**\r\n\
+\r\n\
+床前明月光  \r\n\
+疑是地上霜  \r\n\
+举头望明月  \r\n\
+低头思故乡\r\n\
+\r\n\
+**隔行：两个回车，如：**\r\n\
+\r\n\
+床前明月光\r\n\
+\r\n\
+疑是地上霜\r\n\
+\r\n\
+举头望明月\r\n\
+\r\n\
+低头思故乡\r\n\
+\r\n\
+--\r\n\
+\r\n\
+# 4. 列表\r\n\
+\r\n\
+**无序列表**\r\n\
+\r\n\
+- Red\r\n\
+- Green\r\n\
+- Blue\r\n\
+\r\n\
+**有序列表**\r\n\
+\r\n\
+1. Bird\r\n\
+2. McHale\r\n\
+3. Parish\r\n\
+\r\n\
+**嵌入列表**\r\n\
+\r\n\
+- item 1\r\n\
+- item 2\r\n\
+  - item 2_a\r\n\
+  - item 2_b\r\n\
+- item 3\r\n\
+  - item 3_a\r\n\
+  - item 3_b\r\n\
+  - item 3_c\r\n\
+\r\n\
+**分隔比较大的列表**\r\n\
+\r\n\
+1. A\r\n\
+\r\n\
+2. B\r\n\
+\r\n\
+3. C\r\n\
 \r\n\
 \r\n\
-# 5. 文本换行\r\n\
+--\r\n\
 \r\n\
-                                                          两个空格↓↓  \r\n\
-This is a list item with two paragraphs. Lorem ipsum dolorsit amet,  \r\n\
-consectetuer adipiscing elit. Aliquam hendreritmi posuere lectus.\r\n\
 \r\n\
-(在换行的最后加两个空格)\r\n\
+# 5. 注释\r\n\
 \r\n\
-# 6. 链接\r\n\
+[^_^]: 单行注释\r\n\
+\r\n\
+[^_^]: 多行注释\r\n\
+   你看不到我 1\r\n\
+   你看不到我 2\r\n\
+   你看不到我 3\r\n\
+\r\n\
+--\r\n\
+\r\n\
+# 6. 图片\r\n\
+\r\n\
+**图片支持两种语法，两种语法前面都有方括号，里面写的是图片的标注，  \r\n\
+它规定在图像无法显示时的替代文本，同时也有助于搜索引擎的检索。**\r\n\
+\r\n\
+第一种：\r\n\
+\r\n\
+![这是logo](logo.png)\r\n\
+\r\n\
+第二种（推荐）：\r\n\
+\r\n\
+![这是logo][logo_img]\r\n\
+![这是logo][logo_img]\r\n\
+\r\n\
+\r\n\
+[logo_img]: logo.png\r\n\
+\r\n\
+--\r\n\
+\r\n\
+# 7. 链接\r\n\
+\r\n\
+**链接支持两种语法，两种语法前面都有方括号，里面写的是链接的文本。**\r\n\
+\r\n\
+第一种：\r\n\
 \r\n\
 [百度](http://www.baidu.com)\r\n\
 \r\n\
-# 7. 突出显示\r\n\
+第二种（推荐）：\r\n\
+\r\n\
+[百度][baidu_link]\r\n\
+\r\n\
+[baidu][baidu_link]\r\n\
+\r\n\
+\r\n\
+[baidu_link]: http://www.baidu.com\r\n\
+\r\n\
+\r\n\
+--\r\n\
+\r\n\
+# 8. 突出显示\r\n\
 \r\n\
 **加粗1**\r\n\
 __加粗2__\r\n\
@@ -232,27 +316,22 @@ _倾斜2_\r\n\
 _你 **可以** 混合使用_\r\n\
 \r\n\
 \r\n\
-# 8. 代码\r\n\
+# 9. 代码\r\n\
 \r\n\
-```javascript\r\n\
-function fancyAlert(arg) {\r\n\
-  if(arg) {\r\n\
-    $.facebox({div:'#foo'})\r\n\
-  }\r\n\
-}\r\n\
+官网样式，需要设置【菜单】-【模式】-【官网模式】\r\n\
+\r\n\
+```shell\r\n\
+cd /opt\r\n\
+mkdir sequoiadb\r\n\
 ```\r\n\
 \r\n\
-# 9. 代码（官网样式，需要设置【菜单】-【模式】-【官网模式】）\r\n\
+# 10. 高亮\r\n\
 \r\n\
-```lang-javascript\r\n\
-function fancyAlert(arg) {\r\n\
-  if(arg) {\r\n\
-    $.facebox({div:'#foo'})\r\n\
-  }\r\n\
-}\r\n\
-```\r\n\
+事实上这是单行代码，一般用在代码、路径或者比较重要的特殊内容，依旧需要与中文之间前后保持一个空格。如果后接标点符号则不需要空格\r\n\
 \r\n\
-# 10. 表格\r\n\
+你好 `this is demo` 结束\r\n\
+\r\n\
+# 11. 表格\r\n\
 \r\n\
 | First Header | Second Header |\r\n\
 | ------------ | ------------- |\r\n\
@@ -260,12 +339,12 @@ function fancyAlert(arg) {\r\n\
 | Content in the first column | Content in the second column |\r\n\
 | 我要<br/>换行 | 我不换行 |\r\n\
 \r\n\
-# 11. 删除线\r\n\
+# 12. 删除线\r\n\
 \r\n\
 ~~123456~~\r\n\
 \r\n\
 \r\n\
-# 12. 转义字符\r\n\
+# 13. 转义字符\r\n\
 \r\n\
 Markdown 支持以下这些符号前面加上反斜杠来帮助插入普通的符号：\r\n\
 \r\n\
@@ -285,7 +364,50 @@ Markdown 支持以下这些符号前面加上反斜杠来帮助插入普通的符号：\r\n\
 | \\!   | 惊叹号 |\r\n\
 | \\^   | 托字符 |\r\n\
 | \\|   | 竖线 |\r\n\
-| \\<\\> | 尖括号 |" ;
+| \\<\> | 尖括号 |\r\n\
+\r\n\
+# 14. 分页\r\n\
+\r\n\
+**我们利用注释开发的语法，通过模式切换【普通模式】和【官网模式】查看效果  \r\n\
+【普通模式】是列表，【官网模式】是分页**\r\n\
+\r\n\
+**列表支持有序列表和无序列表**\r\n\
+\r\n\
+```\r\n\
+[^_^]:tab\r\n\
+1. [Title1]:\r\n\
+\r\n\
+   Content1\r\n\
+\r\n\
+2. [Title2]:\r\n\
+\r\n\
+   Content2\r\n\
+```\r\n\
+\r\n\
+--\r\n\
+\r\n\
+[^_^]:tab\r\n\
+1. Linux:\r\n\
+\r\n\
+   Linux是一套免费使用和自由传播的类Unix操作系统\r\n\
+\r\n\
+   ```\r\n\
+   还能嵌入代码\r\n\
+   123\r\n\
+   456\r\n\
+   ```\r\n\
+\r\n\
+2. Windows:\r\n\
+\r\n\
+   Microsoft Windows操作系统是美国微软公司研发的一套操作系统\r\n\
+\r\n\
+   ```\r\n\
+   还能嵌入代码\r\n\
+   789\r\n\
+   abc\r\n\
+   ```\r\n\
+\r\n\
+" ;
    void CMainFrame::OnExample()
    {
       static bool s_bShowExample = false;
