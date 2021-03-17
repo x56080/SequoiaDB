9faeb05a3b72dd3a5468c6690a9b7a1f697a4c55
liuli <liuli@sequoiadb.com>
2021-03-17 17:02:22 +0800

    增加数据源用例指定参数

--- runtest.sh
+++ runtest.sh
@@ -12,9 +12,11 @@ sdbRoot="bin"
 
 coordsvcname="50000"
 essvcname="9200"
+dssvcname="11810"
 catasvcname="30000"
 coordhostname="localhost"
 eshostname="localhost"
+dshostname="localhost"
 
 rsrvportbegin="26000"
 rsrvportend="27000"
@@ -74,6 +76,8 @@ function showHelpInfo()
    echo " -c cataport    : 指定测试的CATALOG节点服务名，默认为30000"
    echo " -eh eshost     : 指定es环境主机名或ip，默认是localhost"
    echo " -en essvcname  : 指定es环境节点服务名，默认为9200"
+   echo " -dh dshost     : 指定数据源主机名或ip，默认是localhost"
+   echo " -dn dsscaname  : 指定数据源节点服务名，默认是11810"
    echo " -s1            : 指定预留的RSRVPORTBEGIN端口号，默认为26000"
    echo " -s2            : 指定预留的RSRVPORTEND端口号，默认为27000"
    echo " -sp            : 指定用预留端口创建节点的路径RSRVNODEDIR，默认为 当前路径/database_runtest/"
@@ -125,7 +129,7 @@ function runJSFile()
    local file=$1
    
    result=0
-   lastCmdStr="$sdbRoot/sdb -e \"var CHANGEDPREFIX='${csprefix}'; var COORDSVCNAME='${coordsvcname}'; var COORDHOSTNAME='${coordhostname}';var ESSVCNAME='${essvcname}'; var ESHOSTNAME='${eshostname}';var RSRVPORTBEGIN='${rsrvportbegin}';var RSRVPORTEND='${rsrvportend}'; var CATASVCNAME='$catasvcname'; var RSRVNODEDIR='$rsrvnodedir'; var RUNRESULT=$runresult; \" -f \"${libRoot}/func.js,$file\""
+   lastCmdStr="$sdbRoot/sdb -e \"var CHANGEDPREFIX='${csprefix}'; var COORDSVCNAME='${coordsvcname}'; var COORDHOSTNAME='${coordhostname}';var ESSVCNAME='${essvcname}'; var ESHOSTNAME='${eshostname}';var DSSVCNAME='${dssvcname}'; var DSHOSTNAME='${dshostname}';var RSRVPORTBEGIN='${rsrvportbegin}';var RSRVPORTEND='${rsrvportend}'; var CATASVCNAME='$catasvcname'; var RSRVNODEDIR='$rsrvnodedir'; var RUNRESULT=$runresult; \" -f \"${libRoot}/func.js,$file\""
 #   runresult=0
    if [ $printOut -eq 1 -o $# -gt 1 ] ; then
       echo "CMD: $lastCmdStr"
@@ -330,6 +334,12 @@ function analyPara()
                          ;;
          -en )           shift
                          essvcname="$1"
+                         ;;
+         -dh )           shift
+                         dshostname="$1"
+                         ;;
+         -dn )           shift
+                         dssvcname="$1"
                          ;;
          -print )        printOut=1
                          ;;
@@ -537,6 +547,8 @@ echo "COORDSVCNAME  : $coordsvcname"
 echo "COORDHOSTNAME : $coordhostname"
 echo "ESSVCNAME     : $essvcname"
 echo "ESHOSTNAME    : $eshostname"
+echo "DSSVCNAME     : $dssvcname"
+echo "DSHOSTNAME    : $dshostname"
 echo "RSRVPORTBEGIN : $rsrvportbegin"
 echo "RSRVPORTEND   : $rsrvportend"
 echo "RSRVNODEDIR   : $rsrvnodedir"
