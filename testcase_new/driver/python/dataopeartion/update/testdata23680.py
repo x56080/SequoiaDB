3a76e778f59f9308476b04d3cf2183219e5c0731
liuli <liuli@sequoiadb.com>
2021-03-20 15:27:15 +0800

    SEQUOIADBMAINSTREAM-6667补充python用例

--- testcase_new/driver/python/dataopeartion/update/testdata23680.py
+++ testcase_new/driver/python/dataopeartion/update/testdata23680.py
@@ -4,7 +4,8 @@
 # @createTime: 2021.03.17
 
 from lib import testlib
-from pysequoiadb.error import (SDBBaseError, SDBEndOfCursor,SDBTypeError)
+from pysequoiadb.error import (SDBBaseError, SDBEndOfCursor)
+from pysequoiadb.collection import (DELETE_FLG_DELETE_ONE, UPDATE_FLG_UPDATE_ONE)
 
 class TestUpdateOneAndDeleteOne23680(testlib.SdbTestBase):
    def setUp(self):
@@ -12,44 +13,26 @@ class TestUpdateOneAndDeleteOne23680(testlib.SdbTestBase):
       self.cs = self.db.create_collection_space(self.cs_name)
       self.cl = self.cs.create_collection(self.cl_name)
 
-   def test_DeleteOne_23680(self):
+   def test_23680(self):
       records = [{"a": 1}, {"a": 1}, {"a": 1}, {"a": 1}]
       self.cl.bulk_insert(0, records)
 
       # 匹配多条数据，删除一条
-      DELETE_FLG_DELETE_ONE=2
       self.cl.delete(condition = {"a": 1}, flags = DELETE_FLG_DELETE_ONE)
       try:
          cursor = self.cl.query()
-         actRec = []
          expectRec = [{"a": 1}, {"a": 1}, {"a": 1}]
-         while True:
-            try:
-               rec = cursor.next()
-               del rec["_id"]
-               actRec.append(rec)
-            except SDBEndOfCursor:
-               break
-         self.assertListEqualUnordered(expectRec, actRec)
+         self.checkResult(cursor, expectRec)
       except SDBBaseError as e:
          self.fail("delete one error: " + e.detail)
 
       # 匹配多条记录，更新一条
       update = {'$inc': {"a": 5}}
-      UPDATE_FLG_UPDATE_ONE=2
       self.cl.update(update, flags=UPDATE_FLG_UPDATE_ONE)
       try:
          cursor = self.cl.query(order_by = {"a": 1})
-         actRec = []
          expectRec = [{"a": 1}, {"a": 1}, {"a": 6}]
-         while True:
-            try:
-               rec = cursor.next()
-               del rec["_id"]
-               actRec.append(rec)
-            except SDBEndOfCursor:
-               break
-         self.assertListEqualUnordered(expectRec, actRec)
+         self.checkResult(cursor, expectRec)
       except SDBBaseError as e:
          self.fail("update one error: " + e.detail)
 
@@ -60,19 +43,22 @@ class TestUpdateOneAndDeleteOne23680(testlib.SdbTestBase):
       try:
          sort = {"a": 1}
          cursor = self.cl.query(order_by = sort)
-         actRec = []
          expectRec = [{"a": 6}, {"a": 6}, {"a": 1}]
-         while True:
-            try:
-               rec = cursor.next()
-               del rec["_id"]
-               actRec.append(rec)
-            except SDBEndOfCursor:
-               break
-         self.assertListEqualUnordered(expectRec, actRec)
+         self.checkResult(cursor, expectRec)
       except SDBBaseError as e:
          self.fail("upsert one error: " + e.detail)
 
    def tearDown(self):
       if self.should_clean_env():
          self.db.drop_collection_space(self.cs_name)
+
+   def checkResult(self, cursor, expectRec):
+      actRec = []
+      while True:
+         try:
+            rec = cursor.next()
+            del rec["_id"]
+            actRec.append(rec)
+         except SDBEndOfCursor:
+            break
+      self.assertListEqualUnordered(expectRec, actRec)
