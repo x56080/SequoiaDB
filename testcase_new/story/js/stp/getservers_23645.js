d6ba61b8179e7f58519d03f6f7b68cda2133778d
chensiqin <chensiqin@sequoiadb.com>
2021-03-24 15:25:59 +0800

    提交stp基本用例

---
+++ testcase_new/story/js/stp/getservers_23645.js
@@ -0,0 +1,43 @@
+﻿/******************************************************************************
+ * @Description   : seqDB-23645 stp.getServers()接口验证 
+ * @Author        : Siqin Chen
+ * @CreateTime    : 2021.03.22
+ * @LastEditTime  : 2021.03.22
+ * @LastEditors   : Siqin Chen
+ ******************************************************************************/
+testConf.skipStandAlone = true;
+main( test );
+function test ()
+{
+   testGetServer23645();
+}
+
+function testGetServer23645()
+{
+   var serverGroup = getStpServerNodes(STPHOSTNAME, STPSVCNAME);
+   var primaryNode = getStpPrimaryNode(STPHOSTNAME,STPSVCNAME);
+   var isSuccess = true
+   if (serverGroup.length <= 0)
+   {
+      isSuccess = false;
+   }
+   var primaryInfo = primaryNode["HostName"]+":"+primaryNode["Service"];
+   
+   var i=0;
+   for(i=0; i<serverGroup.length; i++)
+   {
+      if(serverGroup[i] == primaryInfo)
+      {
+         break;
+      }
+   }
+   if(i == serverGroup.length)
+   {
+      isSuccess = false;
+   }
+   
+   if (!isSuccess)
+   {
+      throw new Error( "Error: stp.getServers return servers is not expected!" );
+   }
+}
---
+++ testcase_new/story/js/stp/gettime_timeus_23641.js
@@ -0,0 +1,137 @@
+﻿/******************************************************************************
+ * @Description   : seqDB-23640 new stp()获取stp对象
+                    seqDB-23641 stp.getTime()接口验证
+                    seqDB-23642 stp.getTimeUS()接口验证 
+ * @Author        : Siqin Chen
+ * @CreateTime    : 2021.03.20
+ * @LastEditTime  : 2021.03.20
+ * @LastEditors   : Siqin Chen
+ ******************************************************************************/
+testConf.skipStandAlone = true;
+main( test );
+function test ()
+{
+   testConnect23640();
+   testTime23641();
+   testTimeUs23642();
+}
+
+function testConnect23640()
+{
+   //1.分别在server/client执行进入sdb shell，执行new stp() 
+   var stp1 = new Stp();
+   //2.指定非默认的stp对象，例如：new Stp(hostname/ip,9633) 
+   var stp2 = new Stp(STPHOSTNAME,STPSVCNAME);
+   //3.指定不存在的stp对象
+   try
+   {
+      var stp3 = new Stp("ccc",9622);
+   }
+   catch( e )
+   {
+      if( e != -15 )
+      {
+         throw new Error( "new Stp('ccc',9622)" + e );
+      }
+      
+   }
+}
+
+function testTime23641()
+{
+   //1.分别在server/client主节点上指定stp.getTime()，获取节点的逻辑时间及TimeError 
+   var primaryNode = getStpPrimaryNode(STPHOSTNAME,STPSVCNAME);
+   var spareNode = getStpSpareNode(STPHOSTNAME,STPSVCNAME);
+   var spareNodeHost = spareNode[0]["HostName"];
+   var spareNodePort = spareNode[0]["Service"];
+   
+   var primaryStp = new Stp(primaryNode["HostName"], primaryNode["Service"]);
+   var spareStp = new Stp(spareNodeHost, spareNodePort);
+   var primaryTime1 = primaryStp.getTime();
+   var spareTime = spareStp.getTime();
+   var primaryTime2 = primaryStp.getTime();
+   var rc = checkTime(primaryTime1, spareTime, primaryTime2);
+   if ( !rc )
+   {
+      throw new Error( "Error: stp.getTime in server return time is not expected!" );
+   }
+   //3.分别在server/client备节点上指定stp.getTime()，获取节点的逻辑时间及TimeError 
+   //通过查主再查备再查主，验证逻辑时间正确性 
+   var clientNode = getStpClientNode(STPHOSTNAME,STPSVCNAME);
+   var clientHost = clientNode["HostName"];
+   var clientPort = clientNode["Service"];
+   var clientStp = new Stp(clientHost, clientPort);
+   var clientTime = clientStp.getTime();
+   var primaryTime3 = primaryStp.getTime();
+   rc = checkTime(primaryTime2, clientTime, primaryTime3);
+   if ( !rc )
+   {
+      throw new Error( "Error: stp.getTime in client return time is not expected!" );
+   }
+   
+}
+
+function testTimeUs23642()
+{
+   //1.分别在server/client主节点上指定stp.getTimeUS()，获取节点的逻辑时间及TimeError 
+   var primaryNode = getStpPrimaryNode(STPHOSTNAME,STPSVCNAME);
+   var spareNode = getStpSpareNode(STPHOSTNAME,STPSVCNAME);
+   var spareNodeHost = spareNode[0]["HostName"];
+   var spareNodePort = spareNode[0]["Service"];
+   
+   var primaryStp = new Stp(primaryNode["HostName"], primaryNode["Service"]);
+   var spareStp = new Stp(spareNodeHost, spareNodePort);
+   var primaryTime1 = primaryStp.getTimeUS();
+   var spareTime = spareStp.getTimeUS();
+   var primaryTime2 = primaryStp.getTimeUS();
+
+   var maxTimeError1 = Math.max(primaryTime1["TimeError"], spareTime["TimeError"]);
+   var maxTimeError2 = Math.max(spareTime["TimeError"], primaryTime2["TimeError"]);
+   var time1 = primaryTime1["TimeStamp"] - maxTimeError1;
+   var time2 = spareTime["TimeStamp"];
+   var time3 = primaryTime2 + maxTimeError2;
+
+   if ( time2 < time1 || time2 > time3 )
+   {
+      throw new Error( "Error: stp.getTimeUS in server return time is not expected!" );
+   }
+   //3.分别在server/client备节点上指定stp.getTimeUS()，获取节点的逻辑时间及TimeError 
+   //通过查主再查备再查主，验证逻辑时间正确性 
+   var clientNode = getStpClientNode(STPHOSTNAME,STPSVCNAME);
+   var clientHost = clientNode["HostName"];
+   var clientPort = clientNode["Service"];
+   var clientStp = new Stp(clientHost, clientPort);
+   var clientTime = clientStp.getTimeUS();
+   var primaryTime3 = primaryStp.getTimeUS();
+
+   maxTimeError1 = Math.max(primaryTime2["TimeError"], clientTime["TimeError"]);
+   maxTimeError2 = Math.max(clientTime["TimeError"], primaryTime3["TimeError"]);
+   time1 = primaryTime2["TimeStamp"] - maxTimeError1;
+   time2 = clientTime["TimeStamp"];
+   time3 = primaryTime3 + maxTimeError2;
+
+   if ( time2 < time1 || time2 > time3 )
+   {
+      throw new Error( "Error: stp.getTimeUS in client return time is not expected!" );
+   }
+}
+
+function checkTime(time1, time2, time3)
+{
+   var time1Info = JSON.parse( time1.toString() );
+   var time2Info = JSON.parse( time2.toString() );
+   var time3Info = JSON.parse( time3.toString() );
+   
+   var maxTimeError1 = Math.max(time1Info["TimeError"], time2Info["TimeError"]);
+   var maxTimeError2 = Math.max(time2Info["TimeError"], time3Info["TimeError"]);
+   
+   var time1 = time1Info["TimeStamp"]["Second"]*1000000000 + time1Info["TimeStamp"]["NanoSecond"] - maxTimeError1;
+   var time2 = time2Info["TimeStamp"]["Second"]*1000000000 + time2Info["TimeStamp"]["NanoSecond"];
+   var time3 = time3Info["TimeStamp"]["Second"]*1000000000 + time3Info["TimeStamp"]["NanoSecond"] + maxTimeError2;
+   
+   if(time2 >= time1 && time2 <= time3)
+   {
+      return true;
+   }
+   return false;
+}
