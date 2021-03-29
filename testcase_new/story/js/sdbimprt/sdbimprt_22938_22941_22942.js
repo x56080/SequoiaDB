691927c1c9fc3fe03e8cb759628b17d0b4511031
liuli <liuli@sequoiadb.com>
2021-03-29 14:15:30 +0800

    SEQUOIADBMAINSTREAM-6613根据检视意见修改

--- testcase_new/story/js/sdbimprt/sdbimprt_22938_22941_22942.js
+++ testcase_new/story/js/sdbimprt/sdbimprt_22938_22941_22942.js
@@ -4,7 +4,7 @@
  *                  seqDB-22942:导入字符串类型的数据，指定字符串最小长度>最大长度
  * @Author        : liuli
  * @CreateTime    : 2020.11.02
- * @LastEditTime  : 2021.03.24
+ * @LastEditTime  : 2021.03.29
  * @LastEditors   : liuli
  ******************************************************************************/
 testConf.clName = COMMCLNAME + "_22938";
@@ -22,61 +22,77 @@ function test ( args )
    // min < max, not specify default
    var fields = "a string(4,6)";
    var expectResult = [{ "a": null }, { "a": "abcd" }, { "a": "abcde" }, { "a": "abcdef" }, { "a": "abcdef" }, { "a": null }];
-   testImprt( cl, fields, expectResult );
+   testImprt( fields );
+   commCompareResults( cl.find().sort( { "_id": 1 } ), expectResult );
+   cl.remove();
 
    // default < min < max
    var fields = "a string(4,6) default r*3";
    var expectResult = [];
    assert.tryThrow( 127, function()
    {
-      testImprt( cl, fields, expectResult );
+      testImprt( fields );
    } );
 
    // min < default < max
    var fields = "a string(4,6) default s*345";
    var expectResult = [{ "a": "s*345" }, { "a": "abcd" }, { "a": "abcde" }, { "a": "abcdef" }, { "a": "abcdef" }, { "a": "s*345" }];
-   testImprt( cl, fields, expectResult );
+   testImprt( fields );
+   commCompareResults( cl.find().sort( { "_id": 1 } ), expectResult );
+   cl.remove();
 
    // min < max < default
    var fields = "a string(4,6) default t*34567";
    var expectResult = [{ "a": "t*3456" }, { "a": "abcd" }, { "a": "abcde" }, { "a": "abcdef" }, { "a": "abcdef" }, { "a": "t*3456" }];
-   testImprt( cl, fields, expectResult );
+   testImprt( fields );
+   commCompareResults( cl.find().sort( { "_id": 1 } ), expectResult );
+   cl.remove();
 
    // min = max, not specify default
    var fields = "a string(5,5)";
    var expectResult = [{ "a": null }, { "a": null }, { "a": "abcde" }, { "a": "abcde" }, { "a": "abcde" }, { "a": null }];
-   testImprt( cl, fields, expectResult );
+   testImprt( fields );
+   commCompareResults( cl.find().sort( { "_id": 1 } ), expectResult );
+   cl.remove();
 
    // min = max = default
    var fields = "a string(5,5) default s*345";
    var expectResult = [{ "a": "s*345" }, { "a": "s*345" }, { "a": "abcde" }, { "a": "abcde" }, { "a": "abcde" }, { "a": "s*345" }];
-   testImprt( cl, fields, expectResult );
+   testImprt( fields );
+   commCompareResults( cl.find().sort( { "_id": 1 } ), expectResult );
+   cl.remove();
 
    // min = max < default
    var fields = "a string(5,5) default t*34567";
    var expectResult = [{ "a": "t*345" }, { "a": "t*345" }, { "a": "abcde" }, { "a": "abcde" }, { "a": "abcde" }, { "a": "t*345" }];
-   testImprt( cl, fields, expectResult );
+   testImprt( fields );
+   commCompareResults( cl.find().sort( { "_id": 1 } ), expectResult );
+   cl.remove();
 
    // min > max, max != 0, not specify default
    var fields = "a string(6,4)";
    var expectResult = [];
    assert.tryThrow( 127, function()
    {
-      testImprt( cl, fields, expectResult );
+      testImprt( fields );
    } );
 
    // min > max, max = 0, not specify default
    var fields = "a string(6,0)";
    var expectResult = [{ "a": null }, { "a": null }, { "a": null }, { "a": "abcdef" }, { "a": "abcdefg" }, { "a": null }];
-   testImprt( cl, fields, expectResult );
+   testImprt( fields );
+   commCompareResults( cl.find().sort( { "_id": 1 } ), expectResult );
+   cl.remove();
 
    // min > max, max = 0, default >min
    var fields = "a string(6,0) default t*34567";
    var expectResult = [{ "a": "t*34567" }, { "a": "t*34567" }, { "a": "t*34567" }, { "a": "abcdef" }, { "a": "abcdefg" }, { "a": "t*34567" }];
-   testImprt( cl, fields, expectResult );
+   testImprt( fields );
+   commCompareResults( cl.find().sort( { "_id": 1 } ), expectResult );
+   cl.remove();
 }
 
-function testImprt ( cl, fields, expectResult )
+function testImprt ( fields )
 {
    var command = installDir + "bin/sdbimprt" +
       " -s " + COORDHOSTNAME +
@@ -88,6 +104,4 @@ function testImprt ( cl, fields, expectResult )
       "' --file " + filename;
 
    cmd.run( command );
-   commCompareResults( cl.find().sort( { "_id": 1 } ), expectResult );
-   cl.remove();
 }
