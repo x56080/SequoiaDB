a4fd40f8e6f05e5bfeebaa743634d1229825a58b
linsuqiang <linsuqiang@sequoiadb.com>
2021-04-16 14:39:48 +0800

    SEQUOIADBMAINSTREAM-7019 删除主子表时，对于空的主表不再重新检测集合版本

--- SequoiaDB/engine/cls/clsShardSession.cpp
+++ SequoiaDB/engine/cls/clsShardSession.cpp
@@ -3507,6 +3507,10 @@ namespace engine
                    "main-collection [%s] has no sub-collection!",
                    pCollectionName ) ;
       }
+      else if ( subCLList.empty() )
+      {
+         goto done ;
+      }
 
       // check write status is needed
       if ( isWrite )
