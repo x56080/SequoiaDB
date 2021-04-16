75aee0dc60ee09030a983cff2606d92997261069
heguoming <heguoming@sequoiadb.com>
2021-04-16 16:41:12 +0800

    SEQUOIADBMAINSTREAM-7036: fix performance of _clsBucket::resetUnqIdxLSN

--- SequoiaDB/engine/cls/clsReplBucket.cpp
+++ SequoiaDB/engine/cls/clsReplBucket.cpp
@@ -1692,7 +1692,7 @@ namespace engine
 
    void _clsBucket::resetUnqIdxLSN()
    {
-      if ( _lastUnqIdxSize > 0 )
+      if ( _lastUnqIdxSize > 0 && DPS_INVALID_LSN_OFFSET != _lastExpectLSN )
       {
          for ( UINT32 i = 0 ; i < _lastUnqIdxSize ; ++ i )
          {
@@ -1701,8 +1701,8 @@ namespace engine
             _lastOldUnqIdxLSN[ i ] = DPS_INVALID_LSN_OFFSET ;
             _lastOldUnqIdxBkt[ i ] = -1 ;
          }
+         _lastExpectLSN = DPS_INVALID_LSN_OFFSET ;
       }
-      _lastExpectLSN = DPS_INVALID_LSN_OFFSET ;
    }
 
    DPS_LSN_OFFSET _clsBucket::checkUnqIdxWaitLSN(
