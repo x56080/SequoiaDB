09826da37b69ae2b32410973a1f50647fef30ead
wuyan <wuyan@sequoiadb.com>
2021-04-19 22:02:12 +0800

    更新ci用例

--- testcase_new/story/java/src/test/java/com/sequoiadb/index/IdIndex22105.java
+++ testcase_new/story/java/src/test/java/com/sequoiadb/index/IdIndex22105.java
@@ -15,6 +15,7 @@ import com.sequoiadb.base.CollectionSpace;
 import com.sequoiadb.base.DBCollection;
 import com.sequoiadb.base.Sequoiadb;
 import com.sequoiadb.exception.BaseException;
+import com.sequoiadb.exception.SDBError;
 import com.sequoiadb.testcommon.SdbTestBase;
 import com.sequoiadb.threadexecutor.ThreadExecutor;
 import com.sequoiadb.threadexecutor.annotation.ExecuteOrder;
@@ -29,7 +30,7 @@ public class IdIndex22105 extends SdbTestBase {
     private Sequoiadb sdb = null;
     private CollectionSpace cs;
     private DBCollection cl;
-    private String clName = "cl_idIndex";
+    private String clName = "cl_idIndex22105";
     private int recsNum = 10000;
 
     @BeforeClass
@@ -45,24 +46,29 @@ public class IdIndex22105 extends SdbTestBase {
                         .append( "ShardingKey",
                                 new BasicBSONObject( "a", 1 ) ) );
 
-        ArrayList< BSONObject > insertor = new ArrayList<>();
+        ArrayList< BSONObject > insertor = new ArrayList< >();
         for ( int i = 0; i < recsNum; i++ ) {
             insertor.add( new BasicBSONObject( "a", i ) );
         }
-        cl.bulkInsert( insertor, 0 );
+        cl.insert( insertor );
     }
 
+    @SuppressWarnings("deprecation")
     @Test
     public void test() throws Exception {
         ThreadExecutor es = new ThreadExecutor();
-        int threadNum = 50;
+        int threadNum = 20;
         int threadRecsNum = recsNum / threadNum;
         int startRecsNum = 0;
         for ( int i = 0; i < threadNum; i++ ) {
-            es.addWorker( new RemoveRecs( startRecsNum, threadRecsNum ) );
+            if ( i % 2 == 0 ) {
+                es.addWorker( new RemoveRecs( startRecsNum, threadRecsNum ) );
+            } else {
+                es.addWorker( new UpdateRecs( startRecsNum, threadRecsNum ) );
+            }
+
             startRecsNum += threadRecsNum;
 
-            es.addWorker( new UpdateRecs( i ) );
         }
         es.addWorker( new DropIdIndex() );
         es.run();
@@ -102,42 +108,45 @@ public class IdIndex22105 extends SdbTestBase {
                                 + ( startRecsNum + recsNum ) + "}}]}" );
                 cl.delete( matcher );
             } catch ( BaseException e ) {
-                if ( e.getErrorCode() != -279 ) {
+                if ( e.getErrorCode() != SDBError.SDB_RTN_AUTOINDEXID_IS_FALSE
+                        .getErrorCode() ) {
                     throw e;
                 }
             } finally {
                 if ( db != null )
-                    db.disconnect();
+                    db.close();
             }
         }
     }
 
     private class UpdateRecs {
-        private int updVal;
+        private int startRecsNum;
+        private int recsNum;
 
-        private UpdateRecs( int startRecsNum ) {
-            this.updVal = updVal;
+        private UpdateRecs( int startRecsNum, int recsNum ) {
+            this.startRecsNum = startRecsNum;
+            this.recsNum = recsNum;
         }
 
         @ExecuteOrder(step = 1)
         private void test() {
             System.out.println(
                     new Date() + " " + this.getClass().getName().toString() );
-            Sequoiadb db = null;
-            try {
-                db = new Sequoiadb( SdbTestBase.coordUrl, "", "" );
+            try ( Sequoiadb db = new Sequoiadb( SdbTestBase.coordUrl, "",
+                    "" )) {
                 DBCollection cl = db.getCollectionSpace( SdbTestBase.csName )
                         .getCollection( clName );
+                BSONObject matcher = ( BSONObject ) JSON.parse(
+                        "{$and:[{a:{$gte:" + startRecsNum + "}},{a:{$lt:"
+                                + ( startRecsNum + recsNum ) + "}}]}" );
                 BSONObject modifier = ( BSONObject ) JSON
-                        .parse( "{$inc:{a:" + updVal + "}, $set:{b:'test'}}" );
-                cl.update( new BasicBSONObject(), modifier, null );
+                        .parse( "{$inc:{a:" + recsNum + "}, $set:{b:'test'}}" );
+                cl.update( matcher, modifier, null );
             } catch ( BaseException e ) {
-                if ( e.getErrorCode() != -279 ) {
+                if ( e.getErrorCode() != SDBError.SDB_RTN_AUTOINDEXID_IS_FALSE
+                        .getErrorCode() ) {
                     throw e;
                 }
-            } finally {
-                if ( db != null )
-                    db.disconnect();
             }
         }
     }
@@ -147,15 +156,11 @@ public class IdIndex22105 extends SdbTestBase {
         private void test() {
             System.out.println(
                     new Date() + " " + this.getClass().getName().toString() );
-            Sequoiadb db = null;
-            try {
-                db = new Sequoiadb( SdbTestBase.coordUrl, "", "" );
+            try ( Sequoiadb db = new Sequoiadb( SdbTestBase.coordUrl, "",
+                    "" )) {
                 DBCollection cl = db.getCollectionSpace( SdbTestBase.csName )
                         .getCollection( clName );
                 cl.dropIdIndex();
-            } finally {
-                if ( db != null )
-                    db.disconnect();
             }
         }
     }
@@ -167,7 +172,7 @@ public class IdIndex22105 extends SdbTestBase {
                 cs.dropCollection( clName );
             }
         } finally {
-            sdb.disconnect();
+            sdb.close();
         }
     }
 }
