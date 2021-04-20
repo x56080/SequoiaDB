0dc709613d50333aeacf69004ec9ae0e885ee2ce
liuli <liuli@sequoiadb.com>
2021-04-20 12:39:24 +0800

    问题单6973修改不合理，暂时屏蔽用例

--- testcase_new/story/js/sessionAccess/sessionAccess_14091.js
+++ testcase_new/story/js/sessionAccess/sessionAccess_14091.js
@@ -1,13 +1,10 @@
-/******************************************************************************
- * @Description   : seqDB-14091:设置会话访问属性，多值指定preferedinstance为[M/S/A]，preferedinstancemode覆盖random和ordered
- * @Author        : wuyan
- * @CreateTime    : 2018.01.24
- * @LastEditTime  : 2021.04.20
- * @LastEditors   : liuli
- ******************************************************************************/
+/* *****************************************************************************
+@description: seqDB-14091:设置会话访问属性，多值指定preferedinstance为[M/S/A]，preferedinstancemode覆盖random和ordered
+@author: 2018-1-24 wuyan  Init
+***************************************************************************** */
 testConf.skipStandAlone = true;
 
-main( test );
+// main( test );
 
 function test ( testPara )
 {
@@ -26,17 +23,17 @@ function test ( testPara )
    insertData( cl );
 
    //PreferedInstance为["A","M","S"]，PreferedInstanceMode为random
-   var options = { PreferedInstance: ["A"], PreferedInstanceMode: "random" };
+   var options = { PreferedInstance: ["A", "M", "S"], PreferedInstanceMode: "random" };
    var expAccessNodes = getGroupNodes( groupName );
    checkAccessNodes( cl, expAccessNodes, options );
 
    //PreferedInstance为["M","S","A"]，PreferedInstanceMode为ordered
-   options = { PreferedInstance: ["M"], PreferedInstanceMode: "ordered" };
+   options = { PreferedInstance: ["M", "S", "A"], PreferedInstanceMode: "ordered" };
    var expAccessNodes = [group[primaryPos].HostName + ":" + group[primaryPos].svcname];
    checkAccessNodes( cl, expAccessNodes, options );
 
    //PreferedInstance为["A","M","S"]，不设置PreferedInstanceMode
-   options = { PreferedInstance: ["A"] };
+   options = { PreferedInstance: ["A", "M", "S"] };
    var expAccessNodes = getGroupNodes( groupName );
    checkAccessNodes( cl, expAccessNodes, options );
 
