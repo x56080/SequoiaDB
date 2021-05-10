54b140a29a03f4f4e5c14dd922f5232ce2a1effd
huangxiaoni <huangxiaoni@sequoiadb.com>
2021-05-10 21:52:46 +0800

    jira6970已解决，修改对应测试代码

--- testcase_new/thirdparty/fapmongo/fap3-pymongo3.10.1/test_index_24057.py
+++ testcase_new/thirdparty/fapmongo/fap3-pymongo3.10.1/test_index_24057.py
@@ -2,13 +2,14 @@
 Description   : seqDB-24057:创建/列取索引
 Author        : XiaoNi Huang
 CreateTime    : 2021.03.31
-LastEditTime  : 2021.04.25
+LastEditTime  : 2021.05.08
 LastEditors   : XiaoNi Huang
 '''
 #!/usr/bin/python3.5
 import bson
 import pymongo
 from pymongo import MongoClient
+from pymongo import IndexModel, ASCENDING, DESCENDING
 import utils
 import unittest
 
@@ -25,22 +26,19 @@ class TestIndex24057( utils.TestBase ):
       self.idxName3 = 'idx3'
       # 清理环境
       self.cl.drop()
-      # 准备数据      
-      self.cl.insert_many( [
-         { "_id": 1, "a": 1, "b": 1, "c": 1, "d": 1 },
-         { "_id": 2, "a": 2, "b": 2, "c": 2, "d": 2 },
-         { "_id": 3, "a": 2, "b": 3, "c": 3, "d": 2 }] )
       
    def test_index( self ):
       self.create_drop_list()
-      # self.create_drop_indexes()
+      self.create_drop_indexes()
       self.unique_index()
-      # self.create_index_IndexModel
+      self.create_index_IndexModel
    
    def tearDown( self ):
       self.cl.drop() 
       
-   def create_drop_list( self ):      
+   def create_drop_list( self ):
+      self.cl.drop()
+
       # create normal index, not set name
       self.cl.create_index( self.idxName1 );
       self.cl.create_index( [ ( 'a', 1 ) ], name = self.idxName2 );
@@ -81,38 +79,45 @@ class TestIndex24057( utils.TestBase ):
       self.assertEqual( len( self.indexes ), 3 )
 
    def create_drop_indexes( self ): 
-      ''' jira-6970
+      self.cl.drop()
+
       # create_indexes
-      print('\n[---create_indexes---]')
-      cl.drop_index( 'idx2' )
-      cl.drop_index( 'idx3' )
+      self.index1 = IndexModel( [ ( "name", DESCENDING ) ], name = self.idxName1 ) 
+      self.index2 = IndexModel( [ ( "age", ASCENDING ) ], name = self.idxName2 ) 
+      self.cl.create_indexes( [ self.index1, self.index2 ] ) 
 
-      index1 = IndexModel( [ ( "name", DESCENDING ) ], name = "idx1" ) 
-      index2 = IndexModel( [ ( "age", ASCENDING ) ], name = "idx2" ) 
-      cl.create_indexes( [ index1, index2 ] ) 
-      '''
+      # list_indexes
+      self.indexes = []
+      for self.idx in self.cl.list_indexes():
+         self.indexes.append( self.idx )
+      self.assertEqual( len( self.indexes ), 3 )
       
-      ''' jira-6979
+      ''' SEQUOIADBMAINSTREAM-6979
       # drop_indexes
-      print('\n[---drop_indexes---]')
-      cl.drop_indexes()
+      self.cl.drop_indexes()
 
-      indexes = []
-      for index in cl.list_indexes():
-         indexes.append( index )
-      print( indexes )
-      assert len( indexes ) == 1
+      # list_indexes again
+      self.indexes = []
+      for self.idx in self.cl.list_indexes():
+         self.indexes.append( self.idx )
+      self.assertEqual( len( self.indexes ), 3 )
       '''
 
    def unique_index( self ):
+      # ready data          
+      self.cl.insert_many( [
+         { "_id": 1, "a": 1, "b": 1 },
+         { "_id": 2, "a": 2, "b": 2 },
+         { "_id": 3, "a": 3, "b": 2 }] )
+
       # create_indexes
-      self.cl.drop_index( 'idx2' )
-      self.cl.drop_index( 'idx3' )
+      self.cl.drop_index( self.idxName1 )
+      self.cl.drop_index( self.idxName2 )
 
       # exist unique index, insert duplicate key
-      self.cl.create_index( [( 'b', 1 )], unique = True, backgroud = True )
+      self.cl.create_index( [( 'a', 1 )], unique = True, backgroud = True )
       try:
-         self.cl.insert_one({'b': 1})
+         self.cl.insert_one({'a': 1})
       except pymongo.errors.OperationFailure as e:
          pass
       except:
@@ -120,14 +125,20 @@ class TestIndex24057( utils.TestBase ):
          
       # exist duplicate key, create unique index
       try:
-         self.cl.create_index( [( 'a', 1 )], unique = True, backgroud = True )
+         self.cl.create_index( [( 'b', 1 )], unique = True, backgroud = True )
       except pymongo.errors.OperationFailure as e:
          pass
       except:
          raise
 
    def create_index_IndexModel( self ):
-      '''
-      cl.create_index( [ IndexModel( [ ( "a", ASCENDING ) ], name = "idx2" )  ] );
-      cl.create_index( [ IndexModel( [ ( "a", DESCENDING ) ], name = "idx3" )  ] );
-      '''
+      self.cl.drop()
+
+      self.cl.create_index( [ IndexModel( [ ( "a", ASCENDING ) ], name = self.idxName1 )  ] );
+      self.cl.create_index( [ IndexModel( [ ( "a", DESCENDING ) ], name = self.idxName2 )  ] );
+
+      # list_indexes
+      self.indexes = []
+      for self.idx in self.cl.list_indexes():
+         self.indexes.append( self.idx )
+      self.assertEqual( len( self.indexes ), 3 )
