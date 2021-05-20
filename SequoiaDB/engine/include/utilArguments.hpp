4d987c335caaa94076711dd9ba2d47c567cb2aae
yuweixing <yuweixing@sequoiadb.com>
2021-05-20 19:38:29 +0800

    SEQUOIADBMAINSTREAM-7160 UTIL_CL_AUTOINCREMENT_FIELD 宏值优化

--- SequoiaDB/engine/include/utilArguments.hpp
+++ SequoiaDB/engine/include/utilArguments.hpp
@@ -62,9 +62,7 @@ namespace engine
    #define UTIL_CL_STRICTDATAMODE_FIELD      ( 0x00010000 )
    #define UTIL_CL_AUTOINC_FIELD             ( 0x00020000 )
    #define UTIL_CL_LOBKEYFORMAT_FIELD        ( 0x00040000 )
-
-   // mask for autoincrement option.
-   #define UTIL_CL_AUTOINCREMENT_FIELD       ( 0x00040000 )
+   #define UTIL_CL_AUTOINCREMENT_FIELD       ( 0x00080000 )
 
    // mask for one field of autoincrement filed attr.
    #define UTIL_CL_AUTOINC_INCREMENT_FIELD   ( 0x00000001 )
