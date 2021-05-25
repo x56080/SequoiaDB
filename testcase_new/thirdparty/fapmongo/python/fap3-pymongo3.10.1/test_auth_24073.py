c709a00f60f882aa6ccb3027fd2c7be42f733074
huangxiaoni <huangxiaoni@sequoiadb.com>
2021-05-25 22:13:46 +0800

    【pymongo】jira6980已解决，修改相关用例

--- testcase_new/thirdparty/fapmongo/python/fap3-pymongo3.10.1/test_auth_24073.py
+++ testcase_new/thirdparty/fapmongo/python/fap3-pymongo3.10.1/test_auth_24073.py
@@ -2,7 +2,7 @@
 Description   : seqDB-24073:创建/使用/获取/删除用户
 Author        : XiaoNi Huang
 CreateTime    : 2021.03.31
-LastEditTime  : 2021.04.23
+LastEditTime  : 2021.05.25
 LastEditors   : XiaoNi Huang
 '''
 #!/usr/bin/python3.5
@@ -26,35 +26,82 @@ class TestAuth24073( utils.TestBase ):
       # 前置条件
       self.result = self.db.command( { "usersInfo": 1 } )
       self.assertEqual( self.result, {'users': [], 'ok': 1.0} )
+
+   def test_user_auth_1( self ):
+      # test: db.command("createUser" / "usersInfo" / "dropUser",....)
+      try:
+         # createUser
+         self.result = self.db.command("createUser", self.userName, pwd=self.userName, roles=["dbAdmin"])
+         self.assertEqual( self.result, {'ok': 1} )
+
+         # usersInfo
+         self.result = self.db.command( { "usersInfo": 1 } )
+         if ( {'user': self.userName} in self.result['users'] ):
+            pass
+         else:
+            raise BaseException( 'not exist user ' + str(self.userName) + ', result = ' + str( self.result) )
          
-   ''' jira-6980
-   def test_user_auth( self ):
-      self.result = self.db.command("createUser","admin", pwd="admin",roles=["read"])
-      self.assertEqual( self.result, {'ok': 1} )      
-      
-      self.result = self.db.command( { "usersInfo": 1 } )
-      self.assertEqual( self.result, {'users': [], 'ok': 1.0} )
-   
-
-      # createUser
-      result = db.command("createUser","admin",pwd="admin",roles=["read"])
-      print( result )
-      assert result == []
-
-      # usersInfo
-      result = db.command( { "usersInfo": 1 } )
-      print( result )
-      if userName in result['users']:
-         print( 'ok' )
-      except:
-         raise 'Failed to check usersInfo'
+         # no auth, business operation
+         self.cl.insert_one( {'a': 1} )
+         # connect to mongodb engine and do business operationn without authentication, not bug 
+         self.assertEqual( self.cl.count_documents( {} ), 1 )
          
-      # authenticate
-      result = client.test.authenticate('admin',"admin")
-      assert result == True
+         # authenticate
+         self.result = self.db.authenticate( self.userName, self.userName )
+         self.assertEqual( self.result, True )
+         # business operation
+         self.cl.insert_one( {'a': 1} )
+         self.assertEqual( self.cl.count_documents( {} ), 2 )
+         
+         # dropUser
+         self.result = self.db.command( "dropUser", self.userName )
+         self.assertEqual( self.result, {'ok': 1.0} )
+         # business operation
+         self.cl.insert_one( {'a': 2} )
+         self.assertEqual( self.cl.count_documents( {} ), 3 )
+         # check users
+         self.result = self.db.command( { "usersInfo": 1 } )
+         if ( {'user': self.userName} in self.result['users'] ):
+            raise "failed to drop user"
 
-      # dropUser
-      result = db.command("dropUser","admin")
-      print( result )
-      assert result == {'users': [], 'ok': 1.0}
-   '''
+      finally:
+         try:
+            self.result = self.db.authenticate( self.userName, self.userName )
+            self.db.command( "dropUser", self.userName )
+         except:
+            self.result = self.db.command( { "usersInfo": 1 } )
+            self.assertEqual( self.result, {'users': [], 'ok': 1.0}, self.result )
+         
+   ''' SEQUOIADBMAINSTREAM-7207
+   def test_user_auth_2( self ):
+      # test: db.add_user / db.remove_user
+      try:
+         # createUser
+         self.result = self.db.add_user(self.userName, pwd=self.userName, roles=["dbAdmin"])
+         self.assertEqual( self.result, None )
+
+         # usersInfo
+         self.result = self.db.command( { "usersInfo": 1 } )
+         if ( {'user': self.userName} in self.result['users'] ):
+            pass
+         else:
+            raise BaseException( 'not exist user ' + str(self.userName) + ', result = ' + str( self.result) )
+         
+         # authenticate
+         self.result = self.db.authenticate( self.userName, self.userName )
+         self.assertEqual( self.result, True )
+         # business operation
+         self.cl.insert_one( {'a': 1} )
+         self.assertEqual( self.cl.count_documents( {} ), 1 )
+         
+         # dropUser
+         self.result = self.db.remove_user( self.userName )
+         self.assertEqual( self.result, None )
+      finally:
+         try:
+            self.result = self.db.authenticate( self.userName, self.userName )
+            self.db.remove_user( self.userName )
+         except:
+            self.result = self.db.command( { "usersInfo": 1 } )
+            self.assertEqual( self.result, {'users': [], 'ok': 1.0}, self.result )
+      '''
