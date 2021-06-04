cde0264873bb52b4463ed579b831017add6bb68d
heguoming <heguoming@sequoiadb.com>
2021-06-04 23:17:15 +0800

    SEQUOIADBMAINSTREAM-7230: 减少 dpsTransLockID.toString() 内存分配

--- SequoiaDB/engine/dps/dpsTransLockMgr.cpp
+++ SequoiaDB/engine/dps/dpsTransLockMgr.cpp
@@ -54,7 +54,7 @@
 
 namespace engine
 {
-   #define DPS_LOCKID_STRING_MAX_SIZE      ( 128 )
+
    #define DPS_TRANSLOCK_DUMP_SLICE_SIZE   ( 1000 )
 
    // thredshold of EDU LRB list length when search whether owning a lock
@@ -2273,10 +2273,11 @@ namespace engine
                     PD_PACK_STRING("waitLock rc:"),
                     PD_PACK_INT( rc ) )  ;
 #endif
+         CHAR lockIdStr[ DPS_LOCKID_STRING_MAX_SIZE + 1 ] = { '\0' } ;
          dpsTxExectr->getExecutor()->resetInfo( EDU_INFO_ERROR ) ;
          dpsTxExectr->getExecutor()->printInfo( EDU_INFO_ERROR,
                      "Acquire transaction lock(%s)(%s) failed",
-                     lockId.toString().c_str(),
+                     lockId.toString( lockIdStr, DPS_LOCKID_STRING_MAX_SIZE ),
                      lockModeToString( requestLockMode ) ) ;
          goto postLockWaiting ;
       }
