94f6e639814c194dd7e6bec170b161eba716ce86
heguoming <heguoming@sequoiadb.com>
2021-11-03 10:33:29 +0800

    SEQUOIADBMAINSTREAM-7615: 修改并发backup状态不正确的问题

--- SequoiaDB/engine/cls/clsReplicateSet.cpp
+++ SequoiaDB/engine/cls/clsReplicateSet.cpp
@@ -402,10 +402,10 @@ namespace engine
    {
       if ( pmdGetOptionCB()->maxReplSync() != getBucket()->maxReplSync() )
       {
-         _sync.enableSync( FALSE ) ;
+         _sync.disableSync() ;
          _syncEmptyEvent.wait() ;
          getBucket()->enforceMaxReplSync( pmdGetOptionCB()->maxReplSync() ) ;
-         _sync.enableSync( TRUE ) ;
+         _sync.enableSync() ;
       }
       if ( getStartShiftTime() >= 0 )
       {
--- SequoiaDB/engine/include/barBkupLogger.hpp
+++ SequoiaDB/engine/include/barBkupLogger.hpp
@@ -551,6 +551,7 @@ namespace engine
          UINT64            _curOffset ;
          UINT32            _curSequence ;
          INT32             _replStatus ;
+         BOOLEAN           _blockSync ;
          BOOLEAN           _hasRegBackup ;
 
          CHAR              *_pExtentBuff ;
