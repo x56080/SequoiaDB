52ee07e765ba9836eda2fff11682ce6ac850c001
heguoming <heguoming@sequoiadb.com>
2021-11-04 16:47:16 +0800

    SEQUOIADBMAINSTREAM-7629: optimize SDB_FLG_QUERY_PREPARE_MORE

--- SequoiaDB/engine/include/rtnContext.hpp
+++ SequoiaDB/engine/include/rtnContext.hpp
@@ -541,6 +541,7 @@ namespace engine
 
          BOOLEAN                 _canPrepareMore ;
          INT32                   _prepareMoreDataLimit ;
+         INT32                   _prepareMoreTimeLimit ;
 
          BOOLEAN                 _isTransCtx ;
    } ;
