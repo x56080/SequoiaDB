89a0ce76de65a75cd39b5b7f8670a938232c18b7
liuli <liuli@sequoiadb.com>
2021-11-08 20:32:45 +0800

    SEQUOIADBMAINSTREAM-4576:补充自动化用例

--- testcase_new/story/js/datasource/datasrc_23378.js
+++ testcase_new/story/js/datasource/datasrc_23378.js
@@ -1,8 +1,9 @@
 /******************************************************************************
- * @Description   : seqDB-23378:使用数据源创建cs，关联集合创建索引
+ * @Description   : seqDB-23378:使用数据源创建cl，映射集合创建索引
+ *                : seqDB-24423:创建CL并使用CS.listCollections() (测试集合使用数据源)
  * @Author        : liuli
  * @CreateTime    : 2021.02.04
- * @LastEditTime  : 2021.02.04
+ * @LastEditTime  : 2021.10.14
  * @LastEditors   : liuli
  ******************************************************************************/
 testConf.skipStandAlone = true;
@@ -19,9 +20,20 @@ function test ()
    clearDataSource( csName, dataSrcName );
    commCreateCL( datasrcDB, srcCSName, clName );
 
-   db.createDataSource( dataSrcName, datasrcUrl, userName, passwd, "sequoiadb", {ErrorControlLevel:"high"} );
+   db.createDataSource( dataSrcName, datasrcUrl, userName, passwd, "sequoiadb", { ErrorControlLevel: "high" } );
    var dbcs = commCreateCS( db, csName );
    var dbcl = dbcs.createCL( clName, { DataSource: dataSrcName, Mapping: srcCSName + "." + clName } );
+
+   // 测试seqDB-24423:创建CL并使用CS.listCollections() (测试集合使用数据源)
+   var cursor = dbcs.listCollections();
+   var clNum = 0;
+   while( cursor.next() )
+   {
+      assert.equal( cursor.current().toObj()["Name"], csName + "." + clName );
+      clNum++;
+   }
+   cursor.close();
+   assert.equal( clNum, 1 );
 
    assert.tryThrow( SDB_OPERATION_INCOMPATIBLE, function()
    {
