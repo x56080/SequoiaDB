c41ed88be3275f92d24a034376ec6647d1d37cfb
linyingting <linyingting@sequoiadb.com>
2021-12-21 15:49:44 +0800

    CI上Fulltext15844用例失败，相关问题单SEQUOIADBMAINSTREAM-5121还未解决，该问题为偶现，增加打印信息

--- testcase_new/story/java/src/test/java/com/sequoiadb/fulltext/parallel/Fulltext15844.java
+++ testcase_new/story/java/src/test/java/com/sequoiadb/fulltext/parallel/Fulltext15844.java
@@ -164,7 +164,7 @@ public class Fulltext15844 extends FullTestBase {
                     cursor.getNext();
                     count++;
                 }
-                Assert.assertEquals( count, ( int ) cl.getCount() );
+                Assert.assertEquals( count, ( int ) cl.getCount(), "受SEQUOIADBMAINSTREAM-5121影响，删除索引和全文检索并发时返回的记录数不对");
             } catch ( BaseException e ) {
                 if ( -6 != e.getErrorCode() && -52 != e.getErrorCode() ) {
                     Assert.fail( "actual exception: " + e.getErrorCode() );
