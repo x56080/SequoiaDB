6e8fdec48711246efc0d5c84bc1f066d4f7f9647
liangyongqin <liangyongqin@sequoiadb.com>
2021-12-22 16:29:13 +0800

    SEQUOIADBMAINSTREAM-7649 C驱动增加CS下展示集合信息和域信息接口

--- SequoiaDB/engine/client/clientcpp.cpp
+++ SequoiaDB/engine/client/clientcpp.cpp
@@ -49,6 +49,8 @@
 #ifdef SDB_SSL
 #include "ossSSLWrapper.h"
 #endif
+#include "msg.hpp"
+#include "clientDef_internal.h"
 
 using namespace std ;
 using namespace bson ;
@@ -5341,18 +5343,18 @@ do                                                            \
                                                   INT32 resultLen )
    {
       INT32 rc = SDB_OK ;
-      CHAR sql[ CLIENT_CS_NAMESZ + 50 ] = { '\0' } ;
+      CHAR sql[ CLIENT_SQL_MAX_LEN + CLIENT_CS_NAMESZ + 1 ] = { 0 } ;
       sdbCursor cursor ;
       BSONObj   tempObj ;
 
-      if ( !_connection || '\0' == _collectionSpaceName[0] || resultLen <= 0 )
+      if ( !_connection || '\0' == _collectionSpaceName[0] || 0 >= resultLen || NULL == result )
       {
          rc = SDB_INVALIDARG ;
          goto error ;
       }
-
+      ossMemset( result, 0, resultLen ) ;      
       // build sql
-      ossSnprintf( sql, CLIENT_CS_NAMESZ + 50, 
+      ossSnprintf( sql, CLIENT_SQL_MAX_LEN + CLIENT_CS_NAMESZ, 
                    "select Domain from $LIST_CS where Name = '%s'", 
                    _collectionSpaceName ) ;
 
@@ -5379,7 +5381,7 @@ do                                                            \
       {
          ossStrncpy( result, 
                      tempObj.getStringField( "Domain" ),
-                     resultLen ) ;
+                     resultLen - 1 ) ;
       }
       else
       {
