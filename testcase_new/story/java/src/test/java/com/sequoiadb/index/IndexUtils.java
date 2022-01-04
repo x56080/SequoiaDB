6c90c7043f4b1027c0f90f8d1713a19ea3a0b981
wuyan <wuyan@sequoiadb.com>
2022-01-04 17:34:58 +0800

    更新用例

--- testcase_new/story/java/src/test/java/com/sequoiadb/index/IndexUtils.java
+++ testcase_new/story/java/src/test/java/com/sequoiadb/index/IndexUtils.java
@@ -38,15 +38,38 @@ public class IndexUtils {
     public static ArrayList< BSONObject > insertData( DBCollection dbcl,
             int recordNum, int length ) {
         ArrayList< BSONObject > insertRecord = new ArrayList< BSONObject >();
-        for ( int i = 0; i < recordNum; i++ ) {
-            String keyValue = getRandomString( length );
-            BSONObject obj = new BasicBSONObject();
-            obj.put( "testa", keyValue );
-            obj.put( "no", i );
-            insertRecord.add( obj );
+        int batchNum = 10000;
+        if ( recordNum < batchNum ) {
+            batchNum = recordNum;
         }
-        dbcl.insert( insertRecord );
+        int count = 0;
+        StringBuffer stringValue = getRandomStringBuffer( length );
+        for ( int i = 0; i < recordNum / batchNum; i++ ) {
+            List< BSONObject > batchRecords = new ArrayList< BSONObject >();
+
+            for ( int j = 0; j < batchNum; j++ ) {
+                int value = count++;
+                BSONObject obj = new BasicBSONObject();
+                String writeContent = String.valueOf(value);
+                int offset = length - writeContent.length() -1;
+                StringBuffer keyValue = stringValue.replace(offset,length,writeContent);
+                obj.put( "testa", keyValue.toString() );
+                obj.put( "testb", value );
+                obj.put( "no", value );
+                obj.put( "testno", value );
+                obj.put( "teststr", "teststr" + value );
+                batchRecords.add( obj );
+            }
+            dbcl.insert( batchRecords );
+            insertRecord.addAll( batchRecords );
+            batchRecords.clear();
+        }
         return insertRecord;
+    }
+
+    public static ArrayList< BSONObject > insertData( DBCollection dbcl,
+            int recordNum ) {
+        return insertData( dbcl, recordNum, 1024 );
     }
 
     public static void checkRecords( DBCollection dbcl,
@@ -60,6 +83,31 @@ public class IndexUtils {
         cursor.close();
         Assert.assertEquals( count, expRecord.size() );
     }
+
+	public static StringBuffer getRandomStringBuffer( int length ) {
+			String str = "ABCDEFGHIJKLMNOPQRATUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^asssgggg!@#$";
+			StringBuffer sbBuffer = new StringBuffer();
+			// random generation 80-length string.
+			Random random = new Random();
+			StringBuffer subBuffer = new StringBuffer();
+			int strLen = str.length();
+			for ( int i = 0; i < strLen; i++ ) {
+				int number = random.nextInt( strLen );
+				subBuffer.append( str.charAt( number ) );
+			}
+	
+			// generate a string at a specified length by subBuffer
+			int times = length / str.length();
+			for ( int i = 0; i < times; i++ ) {
+				sbBuffer.append( subBuffer );
+			}
+			int subTimes = length % str.length();
+			if ( subTimes != 0 ) {
+				sbBuffer.append( str.substring( 0, subTimes ) );
+			}
+			return	sbBuffer;
+		}
+
 
     public static String getRandomString( int length ) {
         String str = "ABCDEFGHIJKLMNOPQRATUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^asssgggg!@#$";
