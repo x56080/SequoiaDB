00a39f0e8b2f879a0d6f93520a5f3f685c06c764
zhangyanan <zhangyanan@sequoiadb.com>
2022-01-08 16:45:47 +0800

    上下文限制测试补充自动化用例

---
+++ testcase_new/story/js/context_sync/contextLimit_24896.js
@@ -0,0 +1,69 @@
+/******************************************************************************
+ * @Description   : seqDB-24896:会话在多个节点开启上下文超过限制 
+ * @Author        : Zhang Yanan
+ * @CreateTime    : 2021.12.31
+ * @LastEditTime  : 2022.01.08
+ * @LastEditors   : Zhang Yanan
+ ******************************************************************************/
+testConf.skipStandAlone = true;
+testConf.skipOneGroup = true;
+testConf.clName = COMMCLNAME + "_context_24886";
+testConf.useSrcGroup = true
+testConf.useDstGroup = true
+
+main( test );
+function test ( testPara )
+{
+   commDropCL( db, COMMCSNAME, testConf.clName, true );
+   var options = { "ShardingKey": { "no": 1 }, "ShardingType": "range" };
+   var varCL = commCreateCL( db, COMMCSNAME, testConf.clName, options, true );
+
+   var maxSessioncontextnum = 100;
+   var config = { "maxsessioncontextnum": maxSessioncontextnum };
+   db.updateConf( config );
+   var dataNum = 5000;
+   insertData( varCL, 5000 );
+   varCL.split( testPara.srcGroupName, testPara.dstGroupNames[0], { "no": dataNum / 2 }, { "no": dataNum } );
+
+   // 查询覆盖源组和目标组数据，打开上下文
+   openContext( 1000, 4000, testConf.clName, maxSessioncontextnum );
+
+   commDropCL( db, COMMCSNAME, testConf.clName, false, false );
+   db.deleteConf( { "maxsessioncontextnum": 1 } );
+}
+
+function listContexts ( db )
+{
+   var cursor = db.list( SDB_LIST_CONTEXTS, {}, { "Contexts": { "$include": 0 } } );
+   var actListInof = [];
+   while( cursor.next() )
+   {
+      var obj = cursor.current().toObj();
+      actListInof.push( obj );
+   }
+   cursor.close();
+   println( "---actList=" + JSON.stringify( actListInof ) );
+}
+
+function openContext ( beginNo, endNo, clName, maxSessioncontextnum )
+{
+   try
+   {
+      var db1 = new Sdb( COORDHOSTNAME, COORDSVCNAME );
+      var dbcl = db1.getCS( COMMCSNAME ).getCL( clName );
+      for( var j = 0; j < maxSessioncontextnum; j++ )
+      {
+         var cursor = dbcl.find( { $and: [{ a: { $gte: beginNo } }, { a: { $lt: endNo } }] } );
+         var obj = cursor.next();
+      }
+      assert.tryThrow( SDB_DPS_CONTEXT_NUM_UP_TO_LIMIT, function()
+      {
+         var cursor = dbcl.find( { $and: [{ no: { $gte: beginNo } }, { no: { $lt: endNo } }] } );
+         var obj = cursor.next();
+         listContexts( db1 );
+      } )
+   } finally
+   {
+      db1.close();
+   }
+}
