422d1c5758233a206800013b804182b50cbe843f
root <wangwenjing>
2022-01-08 16:19:55 +0800

    删除多余的提交

--- testcase_new/driver/c/list/listTest.cpp
+++ testcase_new/driver/c/list/listTest.cpp
@@ -150,7 +150,6 @@ TEST_F(listTest, sdbGetList_SDB_LIST_COLLECTIONS_AND_COLLECTIONSPACES_22063)
    bson_init( &matcher ) ;
    bson_append_string( &matcher, "Name", fullCLName ) ;
    bson_finish( &matcher ) ;
-   bson_init( &matcher ) ;
    bson expectObj ;
    bson_init( &expectObj ) ;
    rc = bson_copy( &expectObj, &matcher ) ;
