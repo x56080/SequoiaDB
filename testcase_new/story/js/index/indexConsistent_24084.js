5bfe85d703d27344f99b33780be47ae93c7e840d
wuyan <wuyan@sequoiadb.com>
2022-01-18 15:42:27 +0800

    新增一致性索引js用例

---
+++ testcase_new/story/js/index/indexConsistent_24084.js
@@ -0,0 +1,44 @@
+/******************************************************************************
+ * @Description   : 删除已复制索引，再次复制同名索引（索引定义不同）   
+ * @Author        : wu yan
+ * @CreateTime    : 2021.08.02
+ * @LastEditTime  : 2021.12.17
+ * @LastEditors   : Wu Yan
+ ******************************************************************************/
+testConf.skipStandAlone = true;
+
+main( test );
+function test ()
+{
+   var indexName = "Index_24084";
+   var mainCLName = COMMCLNAME + "_maincl_index24084";
+   var subCLName1 = COMMCLNAME + "_subcl_index24084a";
+   var subCLName2 = COMMCLNAME + "_subcl_index24084b";
+   var recordNum = 10000;
+
+   commDropCL( db, COMMCSNAME, mainCLName, true, true, "Fail to drop CL in the beginning" );
+   var maincl = commCreateCL( db, COMMCSNAME, mainCLName, { ShardingKey: { a: 1 }, ShardingType: "range", IsMainCL: true } );
+   maincl.createIndex( indexName, { no: 1, b: -1 } );
+   var subcl1 = commCreateCL( db, COMMCSNAME, subCLName1, { ShardingKey: { a: 1 }, ShardingType: "hash" } );
+   maincl.attachCL( COMMCSNAME + "." + subCLName1, { LowBound: { a: 0 }, UpBound: { a: 10000 } } );
+
+   //主表上复制索引到指定子表
+   var expRecs = insertBulkData( maincl, recordNum );
+   maincl.copyIndex( COMMCSNAME + "." + subCLName1, indexName );
+
+   //删除索引，构造只有主表上存在索引场景
+   maincl.dropIndex( indexName );
+   maincl.createIndex( indexName, { a: 1, no: 1 } );
+   subcl1.dropIndex( indexName );
+   maincl.copyIndex( COMMCSNAME + "." + subCLName1, indexName );
+
+   var cursor = maincl.find( {}, { "_id": { "$include": 0 } } ).sort( { "a": 1 } );
+   expRecs.sort( sortBy( 'a' ) );
+   commCompareResults( cursor, expRecs );
+   commCheckIndexConsistency( subcl1, indexName, true );
+   //分别指定子表1和子表2范围数据查询
+   checkExplainByMaincl( maincl, { no: 1 }, "tbscan", "" );
+   checkExplainByMaincl( maincl, { a: 1, no: 1 }, "ixscan", indexName );
+
+   commDropCL( db, COMMCSNAME, mainCLName, true, true, "Fail to drop CL in the ending" );
+}
---
+++ testcase_new/story/js/index/indexConsistent_24299.js
@@ -0,0 +1,41 @@
+/******************************************************************************
+ * @Description   : seqDB-24299 :: 等待执行成功的任务   
+ * @Author        : wu yan
+ * @CreateTime    : 2021.08.02
+ * @LastEditTime  : 2021.08.02
+ * @LastEditors   : wuyan
+ ******************************************************************************/
+testConf.clOpt = { ShardingKey: { a: 1 }, ShardingType: "range" };
+testConf.clName = COMMCLNAME + "_index24299";
+
+main( test );
+function test ( testPara )
+{
+   var indexName = "Index_24299";
+   var recordNum = 100000;
+   var expRecs = insertBulkData( testPara.testCL, recordNum );
+   var taskID = testPara.testCL.createIndexAsync( indexName, { testa: 1, b: 1 } );
+
+   db.waitTasks( taskID );
+   checkTask( COMMCSNAME, testConf.clName, indexName );
+   checkIndexConsistent( db, COMMCSNAME, testConf.clName, indexName, true );
+}
+
+function checkTask ( csName, clName, indexName )
+{
+   var cursor = db.listTasks( { "Name": csName + '.' + clName, TaskTypeDesc: "Create index" } );
+   var taskInfo;
+   while( cursor.next() )
+   {
+      taskInfo = cursor.current().toObj();
+   }
+   cursor.close();
+
+   var status = 9;
+   assert.equal( taskInfo.Status, status, "check status error!\ntask=" + JSON.stringify( taskInfo ) );
+
+   var expCode = 0;
+   assert.equal( taskInfo.ResultCode, expCode, "check resultcode error!\ntask=" + JSON.stringify( taskInfo ) );
+
+   assert.equal( taskInfo.IndexName, indexName, "check indexname error!\ntask=" + JSON.stringify( taskInfo ) );
+}
