6f665369fe634f40b01b59e4835971b936236ff2
wangwenjing <23-wangwenjing@users.noreply.gitlab.example.com>
2022-01-21 21:38:24 +0800

    同步检查时长调整为5分钟

--- testcase_new/sdv/js/lib/datasync_commlib.js
+++ testcase_new/sdv/js/lib/datasync_commlib.js
@@ -1013,7 +1013,7 @@ replicaGroup.prototype.checkConsistency =
    function( coll )
    {
 
-      var totalTimeLen = 60000;
+      var totalTimeLen = 300000;
       var sleepTimeLen = 0;
       while( sleepTimeLen < totalTimeLen )
       {
