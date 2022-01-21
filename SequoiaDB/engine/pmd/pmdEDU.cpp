c5998d63d73fed5ca069259473fed609e6789b32
heguoming <heguoming@sequoiadb.com>
2022-01-21 09:52:57 +0800

    SEQUOIADBMAINSTREAM-7974: coord上进行context超时清理失效

--- SequoiaDB/engine/pmd/pmdEDU.cpp
+++ SequoiaDB/engine/pmd/pmdEDU.cpp
@@ -1108,7 +1108,7 @@ namespace engine
    void _pmdEDUCB::enableCheckUrgentEvent()
    {
 #if defined ( SDB_ENGINE )
-      _needCheckUrgentQueue = FALSE ;
+      _needCheckUrgentQueue = TRUE ;
 #endif
    }
 
