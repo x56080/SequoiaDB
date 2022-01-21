54867234ae02c679e788ac1f104414d875e09d18
liuli <liuli@sequoiadb.com>
2022-01-21 13:54:20 +0800

    提交独立索引js用例

--- testcase_new/story/js/index_sync/commlib.js
+++ testcase_new/story/js/index_sync/commlib.js
@@ -2,8 +2,9 @@
  * @Description   : 索引公共方法
  * @Author        : XiaoNi Huang
  * @CreateTime    : 2021.02.23
- * @LastEditTime  : 2021.02.23
- * @LastEditors   : XiaoNi Huang
+ * @LastEditTime  : 2022.01.21
+ * @LastEditors   : liuli
  ******************************************************************************/
 import( "../lib/basic_operation/commlib.js" );
 import( "../lib/main.js" );
+import( "../lib/index_commlib.js" );
