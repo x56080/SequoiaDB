101408393acff6b75305c0a351253658953c315b
zhangyanan <zhangyanan@sequoiadb.com>
2022-01-26 11:05:06 +0800

    补充修改

--- testcase_new/story/js/createCS/unloadCS_24707.js
+++ testcase_new/story/js/createCS/unloadCS_24707.js
@@ -2,13 +2,14 @@
  * @Description   : seqDB-24707:unloadCS，执行find操作
  * @Author        : Zhang Yanan
  * @CreateTime    : 2021.12.13
- * @LastEditTime  : 2021.12.20
+ * @LastEditTime  : 2022.01.26
  * @LastEditors   : Zhang Yanan
  ******************************************************************************/
 testConf.csName = COMMCSNAME + "_cs_24707";
 testConf.clName = COMMCSNAME + "_cl_24707";
 
-main( test );
+//因为问题单SEQUOIADBMAINSTREAM-7899修改，屏蔽了loadCS和unloadCS接口，该用例暂时屏蔽
+//main( test );
 function test ( args )
 {
    var varCL = args.testCL
