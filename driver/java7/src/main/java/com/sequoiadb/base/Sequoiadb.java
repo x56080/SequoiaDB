ea6338ecdaff5598ff7f7a0c0385af1732eb150d
liangyongqin <liangyongqin@sequoiadb.com>
2022-01-27 20:14:52 +0800

    SEQUOIADBMAINSTREAM-7548 驱动部分API描述有误

--- driver/java7/src/main/java/com/sequoiadb/base/Sequoiadb.java
+++ driver/java7/src/main/java/com/sequoiadb/base/Sequoiadb.java
@@ -2547,7 +2547,7 @@ public class Sequoiadb implements Closeable {
     }
 
     /**
-     * Create a sequence with default options.
+     * Create a sequence.
      *
      * @param seqName The name of sequence
      * @return A sequence object of creation
@@ -2557,7 +2557,7 @@ public class Sequoiadb implements Closeable {
     }
 
     /**
-     * Create a sequence with default options.
+     * Create a sequence with the specified options.
      *
      * @param seqName The name of sequence
      * @param options The options specified by user, details as bellow:
