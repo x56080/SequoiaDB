0c37d8ef2e6a4991d144b4cb911d465b96245a8d
yangqincheng <yangqincheng@sequoiadb.com>
2022-01-28 14:39:50 +0800

    SEQUOIADBMAINSTREAM-7452 修改 isRelicaGroupExist() 接口名错误问题

--- driver/java/src/main/java/com/sequoiadb/base/Sequoiadb.java
+++ driver/java/src/main/java/com/sequoiadb/base/Sequoiadb.java
@@ -2173,8 +2173,20 @@ public class Sequoiadb implements Closeable {
      *
      * @param rgName replica group's name
      * @return true or false
+     * @deprecated Use isReplicaGroupExist(String rgName) instead.
      */
+    @Deprecated
     public boolean isRelicaGroupExist(String rgName) {
+        return isReplicaGroupExist(rgName);
+    }
+
+    /**
+     * whether the replica group exists in the database or not
+     *
+     * @param rgName replica group's name
+     * @return true or false
+     */
+    public boolean isReplicaGroupExist(String rgName) {
         BSONObject rg = getDetailByName(rgName);
         if (rg == null) {
             return false;
