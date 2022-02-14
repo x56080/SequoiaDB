b72b4b764eeef04e99cfff954d155dd6c34707dc
heguoming <heguoming@sequoiadb.com>
2022-02-14 21:12:33 +0800

    SEQUOIADBMAINSTREAM-8034: BSONObjBuilder::appendMaxForType 中对于NULL和undefined的处理不正确

--- SequoiaDB/engine/bson/bsonobj.cpp
+++ SequoiaDB/engine/bson/bsonobj.cpp
@@ -1474,8 +1474,15 @@ namespace bson {
         }
         case Undefined:
         case jstNULL:
-            appendMinForType( fieldName , NumberInt );
-        case Bool: appendBool( fieldName , true); break;
+        {
+           appendMinForType( fieldName, NumberInt ) ;
+           break ;
+        }
+        case Bool:
+        {
+           appendBool( fieldName, true ) ;
+           break;
+        }
         case Date: appendDate( fieldName , 0xFFFFFFFFFFFFFFFFLL ); break;
         case Symbol:
         case String: append( fieldName , BSONObj() ); break;
