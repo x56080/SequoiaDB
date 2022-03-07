8a75b66871c16bf5306f08648bcfe3280614e383
heguoming <heguoming@sequoiadb.com>
2022-03-07 10:57:39 +0800

    SEQUOIADBMAINSTREAM-8105: fix array offset issue in utilDeque, utilSet and utilList

--- SequoiaDB/engine/include/utilDeque.hpp
+++ SequoiaDB/engine/include/utilDeque.hpp
@@ -244,7 +244,7 @@ namespace engine
          if ( position._pData )
          {
             --_eleSize ;
-            UINT32 pos = ( position._pData - _staticBuf ) / sizeof( T ) ;
+            UINT32 pos = position._pData - _staticBuf ;
             for ( UINT32 i = pos ; i < _eleSize ; ++i )
             {
                _staticBuf[ i ] = _staticBuf[ i + 1 ] ;
@@ -265,8 +265,8 @@ namespace engine
          }
          else if ( _eleSize > 0 )
          {
-            UINT32 b = ( first._pData - _staticBuf ) / sizeof( T ) ;
-            UINT32 e = ( last._pData - _staticBuf ) / sizeof( T ) ;
+            UINT32 b = first._pData - _staticBuf ;
+            UINT32 e = last._pData - _staticBuf ;
 
             UINT32 i = b ;
             UINT32 j = b ;
@@ -303,7 +303,7 @@ namespace engine
          }
          else
          {
-            UINT32 pos = ( position._pData - _staticBuf ) / sizeof( T ) ;
+            UINT32 pos = position._pData - _staticBuf ;
             if ( pos > _eleSize )
             {
                pos = _eleSize ;
