dff8459b4929876064471071b7518403ff8c49fe
heguoming <heguoming@sequoiadb.com>
2022-03-07 21:48:16 +0800

    SEQUOIADBMAINSTREAM-8108: fix hang issue in heart beat of net event handler

--- SequoiaDB/engine/net/netFrame.cpp
+++ SequoiaDB/engine/net/netFrame.cpp
@@ -1770,10 +1770,15 @@ namespace engine
          reply.startFrom = 0 ;
          reply.flags = pmdDBIsAbnormal() ? SDB_SYS : SDB_OK ;
 
-         eh->mtx().get() ;
-         reply.header.routeID = _local ;
-         eh->syncSend( (const void*)&reply, reply.header.messageLength ) ;
-         eh->mtx().release() ;
+         // try to get lock of event handle
+         // if failed, means someone is using the handle to send data
+         // which can be just instead of heart beat
+         if ( eh->mtx().try_get() )
+         {
+            reply.header.routeID = _local ;
+            eh->syncSend( (const void*)&reply, reply.header.messageLength ) ;
+            eh->mtx().release() ;
+         }
       }
       else if ( MSG_HEARTBEAT_RES == pMsg->opCode )
       {
