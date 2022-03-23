15695db72c20305c0cef747cea09644bffd5f759
liuli <liuli@sequoiadb.com>
2022-03-23 10:08:43 +0800

    CI-319:修改CI失败用例

--- testcase_new/story/js/drop/drop_24127.js
+++ testcase_new/story/js/drop/drop_24127.js
@@ -2,40 +2,29 @@
  * @Description   : seqDB-24127:dropCS可以通过参数注入指定其他CS
  * @Author        : Yi Pan
  * @CreateTime    : 2021.04.26
- * @LastEditTime  : 2021.04.26
- * @LastEditors   : Yi Pan
+ * @LastEditTime  : 2022.03.23
+ * @LastEditors   : liuli
  ******************************************************************************/
 
 main( test );
 
 function test ()
 {
-   var csname1 = "cs_24127a"
-   var csname2 = "cs_24127b"
-   var csname3 = "cs_24127c"
-   var csname4 = "cs_24127d"
+   var csname1 = "cs_24127a";
+   var csname2 = "cs_24127b";
    var clname = "cl_24127";
 
    //直连coord节点创建删除
-   db.createCS( csname1 ).createCL( clname );
-   db.createCS( csname2 ).createCL( clname );
-   assert.tryThrow( SDB_INVALIDARG, function()
+   commDropCS( db, csname1 );
+   commDropCS( db, csname2 );
+   commCreateCL( db, csname1, clname );
+   commCreateCL( db, csname2, clname );
+   db.dropCS( csname1, { "Name": csname2 } );
+   assert.tryThrow( SDB_DMS_CS_NOTEXIST, function()
    {
-      db.dropCS( csname1, { "Name": csname2 } );
+      db.getCS( csname1 );
    } );
-   var nodes = commGetCLNodes( db, csname1 + "." + clname );
+   db.getCS( csname2 );
    commDropCS( db, csname1, true );
    commDropCS( db, csname2, true );
-
-   //直连data节点创建删除
-   var data = new Sdb( nodes[0].HostName + ":" + nodes[0].svcname );
-   data.createCS( csname3 ).createCL( clname );
-   data.createCS( csname4 ).createCL( clname );
-   assert.tryThrow( SDB_INVALIDARG, function()
-   {
-      data.dropCS( csname3, { "Name": csname4 } );
-   } );
-   commDropCS( data, csname3, true );
-   commDropCS( data, csname4, true );
-   data.close();
 }
