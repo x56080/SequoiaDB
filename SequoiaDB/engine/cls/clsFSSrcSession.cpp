72d5a658081ffa7f2c424b48c31e4d1c5ee14586
yuting <yuting@sequoiadb.com>
2022-03-30 16:34:57 +0800

    SEQUOIADBMAINSTREAM-8245 split和drop cs并发，导致drop cs卡住

--- SequoiaDB/engine/cls/clsFSSrcSession.cpp
+++ SequoiaDB/engine/cls/clsFSSrcSession.cpp
@@ -3064,6 +3064,11 @@ namespace engine
             }
             sdbGetShardCB()->unlockCataSet( pCatSet ) ;
          }
+         else if ( SDB_DMS_NOTEXIST == rc || SDB_DMS_CS_NOTEXIST == rc )
+         {
+            // the collection has been dropped
+            sdbGetShardCB()->unlockCataSet( pCatSet ) ;
+         }
          else
          {
             sdbGetShardCB()->unlockCataSet( pCatSet ) ;
