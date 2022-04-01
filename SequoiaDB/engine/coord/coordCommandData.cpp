55fabc5572ed5e1551eb672d5f8e57036200c470
yuting <yuting@sequoiadb.com>
2022-04-01 15:17:07 +0800

    SEQUOIADBMAINSTREAM-8258 取消任务时构造的bson有问题

--- SequoiaDB/engine/coord/coordCommandData.cpp
+++ SequoiaDB/engine/coord/coordCommandData.cpp
@@ -5406,8 +5406,6 @@ namespace engine
             objBD.append( FIELD_NAME_DETAIL, detail ) ;
             objBD.done() ;
          }
-
-         arrayBD.done() ;
       }
       catch( std::exception &e )
       {
@@ -5618,6 +5616,7 @@ namespace engine
          {
             goto error ;
          }
+         arrayBD.done() ;
 
          builder.append( FIELD_NAME_TASKID, (INT64)taskID ) ;
          builder.append( FIELD_NAME_RESULTCODE, resultCode ) ;
