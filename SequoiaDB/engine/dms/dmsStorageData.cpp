41cbaa3f8197b07a996f474b588ce62eb37cd3de
tangtao <tangtao@sequoiadb.com>
2022-04-07 16:14:26 +0800

    Revert "task-7897 把deletelist切分条件换成数据平均值，缓解产生过多不可用磁盘碎片"
    This reverts commit 84e0b57a8e8ba83f324a9d20f79c511f697dae9d.
    
    	modified:   SequoiaDB/engine/dms/dmsStorageData.cpp

--- SequoiaDB/engine/dms/dmsStorageData.cpp
+++ SequoiaDB/engine/dms/dmsStorageData.cpp
@@ -1077,9 +1077,6 @@ namespace engine
       dmsRecord* pRecord               = NULL ;
       dmsOffset  myOffset              = DMS_INVALID_OFFSET ;
 
-      UINT32     recordAvgSize         = 0 ;
-      dmsMBStatInfo* mbStat            = NULL ;
-      
       PD_TRACE_ENTRY ( SDB__DMSSTORAGEDATA__EXTENTINSERTRECORD ) ;
       rc = context->mbLock( EXCLUSIVE ) ;
       PD_RC_CHECK( rc, PDERROR, "dms mb context lock failed, rc: %d", rc ) ;
@@ -1111,21 +1108,8 @@ namespace engine
          _setRecordGlobTransID( context, recordRW, cb, FALSE ) ;
       }
 
-      //Calculate the average size of records in this collection. If the free
-      //spaces in the delete record after inserting the new record is greater
-      //then the average size.Let's split it.
-      mbStat = context->mbStat() ;
-      if ( mbStat->_totalRecords > 0 )
-      {
-         recordAvgSize = mbStat->_totalDataLen / mbStat->_totalRecords ;
-      }
-      else 
-      {
-         recordAvgSize = needRecordSize ;
-      }
-      
       // and then need to check if we need to split deleted record
-      if ( pRecord->getSize() - needRecordSize >= recordAvgSize )
+      if ( pRecord->getSize() - needRecordSize > DMS_MIN_RECORD_SZ )
       {
          // original offset+new size = new delete offset
          dmsOffset newOffset = myOffset + needRecordSize ;
