6ed730399944331778a120ba7ff5e82cb4ad9abf
heguoming <heguoming@sequoiadb.com>
2022-05-06 14:02:05 +0800

    SEQUOIADBMAINSTREAM-8406: fix -252 issue for long write operators

--- SequoiaDB/engine/cls/clsSyncManager.cpp
+++ SequoiaDB/engine/cls/clsSyncManager.cpp
@@ -253,7 +253,8 @@ namespace engine
    // PD_TRACE_DECLARE_FUNCTION ( SDB__CLSSYNCMAG_SYNC, "_clsSyncManager::sync" )
    INT32 _clsSyncManager::sync( _clsSyncSession &session,
                                 const UINT32 &w,
-                                INT64 timeout )
+                                INT64 timeout,
+                                BOOLEAN isFTWhole )
    {
       PD_TRACE_ENTRY ( SDB__CLSSYNCMAG_SYNC ) ;
       SDB_ASSERT( w <= CLS_REPLSET_MAX_NODE_SIZE &&
@@ -291,7 +292,18 @@ namespace engine
       }
       else if ( _aliveCount < _validSync && w > _aliveCount + 1 )
       {
-         rc = SDB_CLS_WAIT_SYNC_FAILED ;
+         // if ReplSize is -1, or ReplSize is valid with FT whole mode,
+         // we can degrade the ReplSize for wait sync, report node is down
+         // to caller, who can adjust ReplSize if needed
+         if ( ( -1 == session.eduCB->getOrgReplSize() ) ||
+              ( 1 != session.eduCB->getOrgReplSize() && isFTWhole ) )
+         {
+            rc = SDB_DATABASE_DOWN ;
+         }
+         else
+         {
+            rc = SDB_CLS_WAIT_SYNC_FAILED ;
+         }
          _info->mtx.release_r() ;
          goto error ;
       }
@@ -547,7 +559,6 @@ namespace engine
       SDB_ASSERT( alives <= _validSync, "impossible" ) ;
       if ( _validSync < alives )
       {
-         
          PD_LOG( PDWARNING, "sync: alives is bigger than valid sync."
                  "[alives:%d][valid:%d]", alives, _validSync ) ;
          goto done ;
