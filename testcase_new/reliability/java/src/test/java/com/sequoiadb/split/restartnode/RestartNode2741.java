54b9d7a12f93987a7d6402d645b26f1ca300f865
liuli <liuli@sequoiadb.com>
2022-05-30 14:10:30 +0800

    CI-607:通过校验group上是否存在cl判断split是否成功

--- testcase_new/reliability/java/src/test/java/com/sequoiadb/split/restartnode/RestartNode2741.java
+++ testcase_new/reliability/java/src/test/java/com/sequoiadb/split/restartnode/RestartNode2741.java
@@ -23,7 +23,6 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -41,42 +40,31 @@ public class RestartNode2741 extends SdbTestBase {
     private int totalCount;
     private Sequoiadb commSdb;
     private boolean clearFlag = false;
-    private boolean isSplitComplete = false;
 
     @BeforeClass()
-    public void setUp() {
-        try {
+    public void setUp() throws ReliabilityException {
+        groupMgr = GroupMgr.getInstance();
 
-            groupMgr = GroupMgr.getInstance();
-
-            // CheckBusiness(true),检测当前集群环境，若存在异常返回false，
-            if ( !groupMgr.checkBusiness( 20 ) ) {
-                throw new SkipException( "checkBusiness return false" );
-            }
-
-            // 确定切分的源和目标组
-            List< GroupWrapper > glist = groupMgr.getAllDataGroup();
-            srcGroupName = glist.get( 0 ).getGroupName();
-            destGroupName = glist.get( 1 ).getGroupName();
-            System.out.println( "split srcRG:" + srcGroupName + " destRG:"
-                    + destGroupName );
-
-            commSdb = new Sequoiadb( SdbTestBase.coordUrl, "", "" );
-            CollectionSpace commCS = commSdb.getCollectionSpace( csName );
-            DBCollection cl = commCS.createCollection( clName,
-                    ( BSONObject ) JSON.parse(
-                            "{ShardingKey:{'sk':1},ShardingType:'range',Group:'"
-                                    + srcGroupName + "'}" ) );
-            // 准备切分的数据
-            insertData( cl, 0, 5000 );
-        } catch ( ReliabilityException e ) {
-            if ( commSdb != null ) {
-                commSdb.close();
-            }
-            Assert.fail( this.getClass().getName()
-                    + " setUp error, error description:" + e.getMessage()
-                    + "\r\n" + Utils.getStackString( e ) );
+        // CheckBusiness(true),检测当前集群环境，若存在异常返回false，
+        if ( !groupMgr.checkBusiness( 20 ) ) {
+            throw new SkipException( "checkBusiness return false" );
         }
+
+        // 确定切分的源和目标组
+        List< GroupWrapper > glist = groupMgr.getAllDataGroup();
+        srcGroupName = glist.get( 0 ).getGroupName();
+        destGroupName = glist.get( 1 ).getGroupName();
+        System.out.println(
+                "split srcRG:" + srcGroupName + " destRG:" + destGroupName );
+
+        commSdb = new Sequoiadb( SdbTestBase.coordUrl, "", "" );
+        CollectionSpace commCS = commSdb.getCollectionSpace( csName );
+        DBCollection cl = commCS.createCollection( clName,
+                ( BSONObject ) JSON.parse(
+                        "{ShardingKey:{'sk':1},ShardingType:'range',Group:'"
+                                + srcGroupName + "'}" ) );
+        // 准备切分的数据
+        insertData( cl, 0, 5000 );
     }
 
     public void insertData( DBCollection cl, int begin, int end ) {
@@ -89,73 +77,73 @@ public class RestartNode2741 extends SdbTestBase {
 
     @Test
     public void test() throws Exception {
-        try {
-            // 获取源和目标组的GroupWrapper对象
-            GroupWrapper srcGroup = groupMgr.getGroupByName( srcGroupName );
-            GroupWrapper destGroup = groupMgr.getGroupByName( destGroupName );
-            GroupWrapper cataGroup = groupMgr
-                    .getGroupByName( Utils.CATA_RG_NAME );
-            NodeWrapper cataMaster = cataGroup.getMaster();
+        // 获取源和目标组的GroupWrapper对象
+        GroupWrapper srcGroup = groupMgr.getGroupByName( srcGroupName );
+        GroupWrapper destGroup = groupMgr.getGroupByName( destGroupName );
+        GroupWrapper cataGroup = groupMgr.getGroupByName( Utils.CATA_RG_NAME );
+        NodeWrapper cataMaster = cataGroup.getMaster();
 
-            System.out.println( "restart Node:" + cataMaster.hostName() + ":"
-                    + cataMaster.svcName() );
+        System.out.println( "restart Node:" + cataMaster.hostName() + ":"
+                + cataMaster.svcName() );
 
-            // 建立并行任务
-            FaultMakeTask faultTask = NodeRestart.getFaultMakeTask( cataMaster,
-                    1, 10, 10 );
-            TaskMgr mgr = new TaskMgr( faultTask );
-            mgr.addTask( new Split() );
-            mgr.execute();
+        // 建立并行任务
+        FaultMakeTask faultTask = NodeRestart.getFaultMakeTask( cataMaster, 1,
+                10, 10 );
+        TaskMgr mgr = new TaskMgr( faultTask );
+        mgr.addTask( new Split() );
+        mgr.execute();
 
-            // TaskMgr检查线程异常
-            Assert.assertEquals( mgr.isAllSuccess(), true, mgr.getErrorMsg() );
+        // TaskMgr检查线程异常
+        Assert.assertTrue( mgr.isAllSuccess(), mgr.getErrorMsg() );
 
-            // 最长等待2分钟的集群环境恢复
-            Assert.assertEquals( groupMgr.checkBusiness( 600 ), true,
-                    "failed to restore business" );
+        // 最长等待2分钟的集群环境恢复
+        Assert.assertTrue( groupMgr.checkBusiness( 600 ),
+                "failed to restore business" );
 
-            // 再次插入数据
-            commSdb.setSessionAttr(
-                    ( BSONObject ) JSON.parse( "{PreferedInstance:'M'}" ) );
-            DBCollection cl = commSdb.getCollectionSpace( csName )
-                    .getCollection( clName );
-            insertData( cl, 5000, 6000 );
+        // 等待切分结束
+        DBCollection cl = commSdb.getCollectionSpace( csName )
+                .getCollection( clName );
+        Utils.waitSplit( commSdb, cl.getFullName() );
 
-            // 比对结果
-            if ( isSplitComplete ) {
-                // 切分任务已执行完后，再执行源和目标数据量比对
-                Assert.assertEquals( destGroup.checkInspect( 60 ), true );
-                Assert.assertEquals( srcGroup.checkInspect( 60 ), true );
-
-                Utils.waitSplit( commSdb, cl.getFullName() );
-                int splitBound = getBound( commSdb );
-
-                long destCount = getGroupData( commSdb, destGroupName );
-                Assert.assertEquals( destCount, 6000 - splitBound );
-
-                long srcCount = getGroupData( commSdb, srcGroupName );
-                Assert.assertEquals( srcCount, splitBound );
-                Assert.assertEquals( srcCount + destCount, totalCount );
-                Assert.assertEquals( cl.getCount( "{sk:{$gte:0,$lt:6000}}" ),
-                        6000 );
-            } else {
-                // 切分任务建立失败，数据全部在源组上
-                Assert.assertEquals( srcGroup.checkInspect( 60 ), true );
-
-                long srcCount = getGroupData( commSdb, srcGroupName );
-                Assert.assertEquals( srcCount, totalCount );
-            }
-            clearFlag = true;
-        } catch ( ReliabilityException e ) {
-            Assert.fail( e.getMessage() + "\r\n" + Utils.getStackString( e ) );
+        boolean isSplitComplete = false;
+        try ( Sequoiadb dataNode = commSdb.getReplicaGroup( destGroupName )
+                .getMaster().connect()) {
+            CollectionSpace cs = dataNode.getCollectionSpace( csName );
+            isSplitComplete = cs.isCollectionExist( clName );
         }
+
+        // 比对结果
+        if ( isSplitComplete ) {
+            // 切分任务已执行完后，再执行源和目标数据量比对
+            Assert.assertTrue( destGroup.checkInspect( 60 ) );
+            Assert.assertTrue( srcGroup.checkInspect( 60 ) );
+        } else {
+            // 切分任务建立失败，数据全部在源组上
+            Assert.assertTrue( srcGroup.checkInspect( 60 ) );
+
+            // 重新执行切分
+            cl.split( srcGroupName, destGroupName, 50 );
+        }
+
+        // 再次插入数据并校验结果
+        insertData( cl, 5000, 6000 );
+
+        int splitBound = getBound( commSdb );
+
+        long destCount = getGroupData( commSdb, destGroupName );
+        Assert.assertEquals( destCount, 6000 - splitBound );
+
+        long srcCount = getGroupData( commSdb, srcGroupName );
+        Assert.assertEquals( srcCount, splitBound );
+        Assert.assertEquals( srcCount + destCount, totalCount );
+        Assert.assertEquals( cl.getCount( "{sk:{$gte:0,$lt:6000}}" ), 6000 );
+        clearFlag = true;
     }
 
     private long getGroupData( Sequoiadb sdb, String groupName ) {
-        Sequoiadb dataNode = null;
-        DBCursor cursor = null;
-        try {
-            dataNode = sdb.getReplicaGroup( groupName ).getMaster().connect();// 获得目标组主节点链接
+        try ( Sequoiadb dataNode = sdb.getReplicaGroup( groupName ).getMaster()
+                .connect()) {
+            // 获得目标组主节点链接
             DBCollection cl = dataNode.getCollectionSpace( csName )
                     .getCollection( clName );
             long count = cl.getCount();
@@ -163,13 +151,6 @@ public class RestartNode2741 extends SdbTestBase {
         } catch ( BaseException e ) {
             e.printStackTrace();
             Assert.fail( e.getMessage() + "\r\n" + Utils.getStackString( e ) );
-        } finally {
-            if ( cursor != null ) {
-                cursor.close();
-            }
-            if ( dataNode != null ) {
-                dataNode.close();
-            }
         }
         return 0;
     }
@@ -236,25 +217,17 @@ public class RestartNode2741 extends SdbTestBase {
     class Split extends OperateTask {
         @Override
         public void exec() throws Exception {
-            Sequoiadb sdb = null;
-            try {
-                sdb = new Sequoiadb( SdbTestBase.coordUrl, "", "" );
+            try ( Sequoiadb sdb = new Sequoiadb( SdbTestBase.coordUrl, "",
+                    "" )) {
                 sdb.setSessionAttr(
                         ( BSONObject ) JSON.parse( "{PreferedInstance:'M'}" ) );
                 DBCollection cl = sdb.getCollectionSpace( csName )
                         .getCollection( clName );
                 try {
                     cl.split( srcGroupName, destGroupName, 50 );
-                    isSplitComplete = true;
                 } catch ( BaseException e ) {
                     System.out.println(
                             "split have exception:" + e.getMessage() );
-                }
-            } catch ( BaseException e ) {
-                throw e;
-            } finally {
-                if ( sdb != null ) {
-                    sdb.close();
                 }
             }
         }
