7b8480c984d99988b28d1ea1865eddd884589849
tanzhaobo <48-tanzhaobo@users.noreply.gitlab.example.com>
2022-06-11 11:48:53 +0800

    SEQUOIADBMAINSTREAM-8457: modify log for blockWrite/unblockWrite

--- SequoiaDB/engine/dms/dmsCB.cpp
+++ SequoiaDB/engine/dms/dmsCB.cpp
@@ -1457,7 +1457,7 @@ namespace engine
          }
          else
          {
-            PD_LOG( PDINFO, "Change _dmsCBState from [%d] to [%d]",
+            PD_LOG( PDINFO, "Change dms state from [%d] to [%d]",
                     _dmsCBState, DMS_STATE_ONLINE_BACKUP ) ;
             _dmsCBState = DMS_STATE_ONLINE_BACKUP ;
          }
@@ -1480,7 +1480,7 @@ namespace engine
       else
       {
          _stateMtx.get() ;
-         PD_LOG( PDINFO, "Change _dmsCBState from [%d] to [%d]",
+         PD_LOG( PDINFO, "Change dms state from [%d] to [%d]",
                  _dmsCBState, DMS_STATE_NORMAL ) ;
          _dmsCBState = DMS_STATE_NORMAL ;
          _stateMtx.release() ;
@@ -1490,19 +1490,12 @@ namespace engine
 
    INT32 _SDB_DMSCB::registerRebuild( _pmdEDUCB *cb )
    {
-      INT32 rc = SDB_OK ;
-      rc = blockWrite( cb, SDB_DB_REBUILDING ) ;
-      if ( SDB_OK == rc )
-      {
-         PD_LOG( PDINFO, "Block write operation succeed" ) ;
-      }
-      return rc ;
+      return blockWrite( cb, SDB_DB_REBUILDING ) ;
    }
 
    void _SDB_DMSCB::rebuildDown( _pmdEDUCB *cb )
    {
       unblockWrite( cb ) ;
-      PD_LOG( PDINFO, "Unblock write operation succeed" ) ;
    }
 
    // PD_TRACE_DECLARE_FUNCTION ( SDB__SDB_DMSCB_REGRESTORE, "_SDB_DMSCB::registerRestore" )
