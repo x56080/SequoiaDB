54a1a630a7b3b81c2bf68ed8b43377f3a89edbe5
heguoming <heguoming@sequoiadb.com>
2022-06-27 18:28:26 +0800

    SEQUOIADBMAINSTREAM-8541: should check cb before remove monQueryCB

--- SequoiaDB/engine/pmd/pmdProcessor.cpp
+++ SequoiaDB/engine/pmd/pmdProcessor.cpp
@@ -1764,6 +1764,8 @@ namespace engine
          messenger->removeSession( eduCB() ) ;
       }
 
+      eduCB()->setMonQueryCB( NULL ) ;
+
       // delete all context
       INT64 contextID = -1 ;
       while ( -1 != ( contextID = eduCB()->contextPeek() ) )
