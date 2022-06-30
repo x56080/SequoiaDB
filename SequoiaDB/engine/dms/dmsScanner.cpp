870b2d94970dd8b87d81b0628796e6241c1e7e90
heguoming <heguoming@sequoiadb.com>
2022-06-30 17:46:14 +0800

    SEQUOIADBMAINSTREAM-8562: fix $Range issue in RC transaction

--- SequoiaDB/engine/dms/dmsScanner.cpp
+++ SequoiaDB/engine/dms/dmsScanner.cpp
@@ -2735,6 +2735,10 @@ namespace engine
                rc = matcher->matches( obj, result, mthContext, parameters ) ;
                if ( rc )
                {
+                  if ( SDB_IXM_ADVANCE_EOC == rc )
+                  {
+                     goto done ;
+                  }
                   PD_LOG( PDERROR, "Failed to match record, rc: %d", rc ) ;
                   goto error ;
                }
