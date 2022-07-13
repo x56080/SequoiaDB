51b526cf97049e5f133519ac16860a11dec33cbd
fangjiabin <fangjiabin@sequoiadb.com>
2022-07-13 09:10:47 +0800

    SEQUOIADBMAINSTREAM-8561，解决preferdinstance=S时，读操作频繁报错-344的问题

--- SequoiaDB/engine/coord/coordGroupHandle.cpp
+++ SequoiaDB/engine/coord/coordGroupHandle.cpp
@@ -55,127 +55,62 @@ namespace engine
                                             _coordGroupSel *pSel,
                                             _coordGroupSessionCtrl *pCtrl )
    {
+      SDB_ASSERT( pSub, "sub session can't be NULL" ) ;
+      SDB_ASSERT( pSel, "group sel can't be NULL" ) ;
+      SDB_ASSERT( pCtrl, "group session ctrl can't be NULL" ) ;
+
+      MsgHeader *pMsg = pSub->getReqMsg() ;
+      BOOLEAN isResend = pCtrl->getRetryTimes() > 0 ? TRUE : FALSE ;
+      SINT32 *pFlags = NULL ;
+      SINT32 primaryFlag = 0 ;
+      SINT32 secondaryFlag = 0 ;
+
+      if ( MSG_BS_QUERY_REQ == pMsg->opCode )
+      {
+         MsgOpQuery *pQuery = (( MsgOpQuery* )pMsg) ;
+         pFlags = &(pQuery->flags) ;
+         primaryFlag = FLG_QUERY_PRIMARY ;
+         secondaryFlag = FLG_QUERY_SECONDARY ;
+      }
+      else if ( pMsg->opCode > ( SINT32 )MSG_LOB_BEGIN &&
+                pMsg->opCode < ( SINT32 )MSG_LOB_END )
+      {
+         MsgOpLob *pLobMsg = ( MsgOpLob* )pMsg ;
+         pFlags = &(pLobMsg->flags) ;
+         primaryFlag = FLG_LOBREAD_PRIMARY ;
+         secondaryFlag = FLG_LOBREAD_SECONDARY ;
+      }
+      else
+      {
+         goto done ;
+      }
+
+      OSS_BIT_CLEAR( *pFlags, primaryFlag ) ;
+      OSS_BIT_CLEAR( *pFlags, secondaryFlag ) ;
+
       if ( !pSel->isPrimary() )
       {
-         MsgHeader *pMsg = pSub->getReqMsg() ;
-         BOOLEAN isResend = pCtrl->getRetryTimes() > 0 ? TRUE : FALSE ;
-
          // NOTE: we need to set query flags for below cases
          // - primary is preferred
          // - secondary is preferred, and not select from the last node
          // NOTE: for secondary case, only set flag for node which is not
          //       selected from the last node, since we need to keep use
          //       the last node during preferred period after a write operator
-         if ( pSel->isPreferredPrimary() )
+         if ( pSel->isPreferredPrimary() && !isResend )
          {
             // if primary is required or preferred, set primary query flag
-            if ( MSG_BS_QUERY_REQ == pMsg->opCode )
-            {
-               MsgOpQuery *pQuery = ( MsgOpQuery* )pMsg ;
-               if ( FALSE == isResend )
-               {
-                  // set primary flag, clear secondary flag
-                  OSS_BIT_SET( pQuery->flags, FLG_QUERY_PRIMARY ) ;
-                  OSS_BIT_CLEAR( pQuery->flags, FLG_QUERY_SECONDARY ) ;
-               }
-               else
-               {
-                  // clear both primary or secondary flag
-                  OSS_BIT_CLEAR( pQuery->flags, FLG_QUERY_PRIMARY ) ;
-                  OSS_BIT_CLEAR( pQuery->flags, FLG_QUERY_SECONDARY ) ;
-               }
-            }
-            else if ( pMsg->opCode > ( SINT32 )MSG_LOB_BEGIN &&
-                      pMsg->opCode < ( SINT32 )MSG_LOB_END )
-            {
-               MsgOpLob *pLobMsg = ( MsgOpLob* )pMsg ;
-               if ( FALSE == isResend )
-               {
-                  // set secondary flag, clear primary flag
-                  OSS_BIT_SET( pLobMsg->flags, FLG_LOBREAD_PRIMARY ) ;
-                  OSS_BIT_CLEAR( pLobMsg->flags, FLG_LOBREAD_SECONDARY ) ;
-               }
-               else
-               {
-                  // clear both primary or secondary flag
-                  OSS_BIT_CLEAR( pLobMsg->flags, FLG_LOBREAD_PRIMARY ) ;
-                  OSS_BIT_CLEAR( pLobMsg->flags, FLG_LOBREAD_SECONDARY ) ;
-               }
-            }
+            OSS_BIT_SET( *pFlags, primaryFlag ) ;
          }
          else if ( pSel->isPreferredSecondary() &&
-                   !pSel->existLastNode( groupID ) )
+                   !pSel->existLastNode( groupID ) && !isResend )
          {
             // if secondary is required or preferred, set secondary query flag
-            if ( MSG_BS_QUERY_REQ == pMsg->opCode )
-            {
-               MsgOpQuery *pQuery = ( MsgOpQuery* )pMsg ;
-               if ( FALSE == isResend )
-               {
-                  // set primary flag, clear secondary flag
-                  OSS_BIT_SET( pQuery->flags, FLG_QUERY_SECONDARY ) ;
-                  OSS_BIT_CLEAR( pQuery->flags, FLG_QUERY_PRIMARY ) ;
-               }
-               else
-               {
-                  // clear both primary or secondary flag
-                  OSS_BIT_CLEAR( pQuery->flags, FLG_QUERY_SECONDARY ) ;
-                  OSS_BIT_CLEAR( pQuery->flags, FLG_QUERY_PRIMARY ) ;
-               }
-            }
-            else if ( pMsg->opCode > ( SINT32 )MSG_LOB_BEGIN &&
-                      pMsg->opCode < ( SINT32 )MSG_LOB_END )
-            {
-               MsgOpLob *pLobMsg = ( MsgOpLob* )pMsg ;
-               if ( FALSE == isResend )
-               {
-                  // set secondary flag, clear primary flag
-                  OSS_BIT_SET( pLobMsg->flags, FLG_LOBREAD_SECONDARY ) ;
-                  OSS_BIT_CLEAR( pLobMsg->flags, FLG_LOBREAD_PRIMARY ) ;
-               }
-               else
-               {
-                  // clear both primary or secondary flag
-                  OSS_BIT_CLEAR( pLobMsg->flags, FLG_LOBREAD_SECONDARY ) ;
-                  OSS_BIT_CLEAR( pLobMsg->flags, FLG_LOBREAD_PRIMARY ) ;
-               }
-            }
-         }
-         else if ( isResend )
-         {
-            // make sure to clear both primary or secondary flag when resend
-            if ( MSG_BS_QUERY_REQ == pMsg->opCode )
-            {
-               MsgOpQuery *pQuery = ( MsgOpQuery* )pMsg ;
-               OSS_BIT_CLEAR( pQuery->flags, FLG_QUERY_SECONDARY ) ;
-               OSS_BIT_CLEAR( pQuery->flags, FLG_QUERY_PRIMARY ) ;
-            }
-            else if ( pMsg->opCode > ( SINT32 )MSG_LOB_BEGIN &&
-                      pMsg->opCode < ( SINT32 )MSG_LOB_END )
-            {
-               MsgOpLob *pLobMsg = ( MsgOpLob* )pMsg ;
-               OSS_BIT_CLEAR( pLobMsg->flags, FLG_LOBREAD_SECONDARY ) ;
-               OSS_BIT_CLEAR( pLobMsg->flags, FLG_LOBREAD_PRIMARY ) ;
-            }
+            OSS_BIT_SET( *pFlags, secondaryFlag ) ;
          }
       }
-      else
-      {
-         MsgHeader *pMsg = pSub->getReqMsg() ;
-         // now it is required primary in resend case
-         // make sure to clear secondary flag
-         if ( MSG_BS_QUERY_REQ == pMsg->opCode )
-         {
-            MsgOpQuery *pQuery = ( MsgOpQuery* )pMsg ;
-            OSS_BIT_CLEAR( pQuery->flags, FLG_QUERY_SECONDARY ) ;
-         }
-         else if ( pMsg->opCode > ( SINT32 )MSG_LOB_BEGIN &&
-                   pMsg->opCode < ( SINT32 )MSG_LOB_END )
-         {
-            MsgOpLob *pLobMsg = ( MsgOpLob* )pMsg ;
-            OSS_BIT_CLEAR( pLobMsg->flags, FLG_LOBREAD_SECONDARY ) ;
-         }
-      }
+
+   done:
+      return ;
    }
 
 }
