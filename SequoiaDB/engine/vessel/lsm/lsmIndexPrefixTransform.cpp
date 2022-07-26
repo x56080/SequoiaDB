c131c81b366abe2d53018f378e95a767be54080e
liuyuchen <liuyuchen@sequoiadb.com>
2022-07-26 11:01:21 +0800

    prefix transform

---
+++ SequoiaDB/engine/vessel/lsm/lsmIndexPrefixTransform.cpp
@@ -0,0 +1,70 @@
+/*******************************************************************************
+
+
+   Copyright (C) 2011-2018 SequoiaDB Ltd.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+   Source File Name = lsmIndexPrefixTransform.h
+
+   Descriptive Name = 
+
+   Dependencies: N/A
+
+   Restrictions: N/A
+
+   Change Activity:
+   defect Date        Who Description
+   ====== =========== === ==============================================
+          07/23/2022  LYC  Initial Draft
+
+   Last Changed =
+
+*******************************************************************************/
+
+#include "rocksdb/slice_transform.h"
+#include "vessel/keyString.h"
+#include "vessel/sliceTransfer.h"
+
+namespace engine
+{
+namespace vessel
+{
+   class lsmIndexPrefixTransform : public rocksdb::SliceTransform
+   {
+      public:
+         virtual rocksdb::Slice Transform(const rocksdb::Slice &key) const override
+         {
+            keyString ks(toSlice(key));
+            SDB_ASSERT(ks.isValid(), "can not be invalid");
+            return rocksdb::Slice(ks.getDataSlice().getData(),
+                                  ks.getSizeBeforeKey() + ks.getKeySize());
+         }
+         virtual bool InDomain(const rocksdb::Slice &key) const override
+         {
+            keyString ks(toSlice(key));
+            return ks.isValid() && ks.hasKeyPart();
+         }
+
+      public:
+         virtual const CHAR *Name() const override {return "sdb.lsmIndexPrefixTransform";}
+   }; // class lsmIndexPrefixTransform
+
+   const rocksdb::SliceTransform *getLsmIndexPrefixTransform()
+   {
+      return new(std::nothrow) lsmIndexPrefixTransform();
+   }
+
+} // namespace vessel
+} // namespace engine
