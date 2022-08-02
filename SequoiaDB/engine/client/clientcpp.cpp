4d1ee605baaebfec5f1ad37955b484f9ba3e248e
yangqincheng <yangqincheng@sequoiadb.com>
2022-08-02 10:12:15 +0800

    SEQUOIADBMAINSTREAM-8415 修正鉴权消息兼容性问题

--- SequoiaDB/engine/client/clientcpp.cpp
+++ SequoiaDB/engine/client/clientcpp.cpp
@@ -9900,6 +9900,12 @@ do                                                            \
       if ( _authVersion >= AUTH_SCRAM_SHA256 )
       {
          rc = _authVer1MsgProcess( pUN, md5 ) ;
+         // During rolling upgrade, the old version of catalog node
+         // does not support the SAH256 algorithm.
+         if ( SDB_UNKNOWN_MESSAGE == rc )
+         {
+            rc = _authVer0MsgProcess( pUN, md5 ) ;
+         }
       }
       else
       {
@@ -9927,6 +9933,12 @@ do                                                            \
          if ( _authVersion >= AUTH_SCRAM_SHA256 )
          {
             rc = _authVer1MsgProcess( pUN, md5 ) ;
+            // During rolling upgrade, the old version of catalog node
+            // does not recognize the SAH256
+            if ( SDB_UNKNOWN_MESSAGE == rc )
+            {
+               rc = _authVer0MsgProcess( pUN, md5 ) ;
+            }
          }
          else
          {
