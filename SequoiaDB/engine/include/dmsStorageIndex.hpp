0cfea83529eb3fb49b6dc15b2e9ec146e892a393
heguoming <heguoming@sequoiadb.com>
2022-08-05 10:19:41 +0800

    SEQUOIADBMAINSTREAM-8650: 索引键检查优化
    - 仅对insert、update、rebuild索引新产生的键值进行检查
    - 优化NotNull和NotArray的键值检查，在生成键值时进行检查

--- SequoiaDB/engine/include/dmsStorageIndex.hpp
+++ SequoiaDB/engine/include/dmsStorageIndex.hpp
@@ -337,6 +337,7 @@ namespace engine
                                         BSONObj &originalObj,
                                         BSONObj &newObj,
                                         _pmdEDUCB *cb,
+                                        BOOLEAN isRollback,
                                         const ixmIdxHashBitmap &idxHashBitmap,
                                         utilWriteResult *pResult = NULL ) ;
 
