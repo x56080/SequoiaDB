cfb678c6b99a6d55a270d912e582817490495365
heguoming <heguoming@sequoiadb.com>
2022-08-15 17:56:09 +0800

    SEQUOIADBMAINSTREAM-8670: 保证handle可以最后被closeHandle处理

--- SequoiaDB/engine/net/netFrame.cpp
+++ SequoiaDB/engine/net/netFrame.cpp
@@ -1134,8 +1134,8 @@ namespace engine
          }
          else
          {
-            // make sure the handle is erased
-            _erase( eh->handle() ) ;
+            // make sure the handle is erased from route
+            _eraseRoute( eh ) ;
 
             rc = SDB_NETWORK ;
          }
