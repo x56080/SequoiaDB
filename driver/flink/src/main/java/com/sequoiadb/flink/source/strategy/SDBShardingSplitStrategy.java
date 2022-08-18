8c11e5c7060dbbbdf560098b5a83cd6f6dbc4031
shenxiaopeng <shenxiaopeng@sequoiadb.com>
2022-08-18 17:13:30 +0800

    SEQUOIADBMAINSTREAM-8685:修改SDBInfoUtil的位置变化，引用失效的问题

--- driver/flink/src/main/java/com/sequoiadb/flink/source/strategy/SDBShardingSplitStrategy.java
+++ driver/flink/src/main/java/com/sequoiadb/flink/source/strategy/SDBShardingSplitStrategy.java
@@ -18,11 +18,11 @@ package com.sequoiadb.flink.source.strategy;
 
 import com.sequoiadb.base.ConfigOptions;
 import com.sequoiadb.base.Sequoiadb;
+import com.sequoiadb.flink.common.util.SDBInfoUtil;
 import com.sequoiadb.flink.config.SDBSourceOptions;
 import com.sequoiadb.flink.config.SplitMode;
 import com.sequoiadb.flink.common.exception.SDBException;
 import com.sequoiadb.flink.source.split.SDBSplit;
-import com.sequoiadb.flink.util.SDBInfoUtil;
 import org.bson.BSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
