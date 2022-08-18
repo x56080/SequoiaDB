31a9fa7ac9f98eb10b866609926358294ebb24d2
wuyun <wuyun@sequoiadb.com>
2022-08-18 11:47:59 +0800

    disable auto compaction

--- SequoiaDB/engine/vessel/lsm/lsmDB.cpp
+++ SequoiaDB/engine/vessel/lsm/lsmDB.cpp
@@ -543,7 +543,7 @@ namespace vessel
          cfOpt.level0_stop_writes_trigger = 1536;
          cfOpt.compression = rocksdb::kNoCompression;
          //cfOpt.compression = rocksdb::kLZ4HCCompression;
-         cfOpt.compaction_style = rocksdb::kCompactionStyleNone;
+         cfOpt.disable_auto_compactions = TRUE;
 
          rocksdb::BlockBasedTableOptions tableOpt;
          tableOpt.filter_policy.reset(rocksdb::NewBloomFilterPolicy(10, false));
