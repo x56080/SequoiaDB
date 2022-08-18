4689e92064d13387396a058f8dc14b42d9199882
yangshangde <yangshangde@sequoiadb.com>
2022-08-18 10:25:13 +0800

    SEQUOIADBMAINSTREAM-8689 Fix context leak on data node when interrupt self

--- SequoiaDB/engine/coord/coordContext.cpp
+++ SequoiaDB/engine/coord/coordContext.cpp
@@ -201,7 +201,7 @@ namespace engine
       _emptyContextMap.clear() ;
 
       // kill sub context
-      if ( cb && !cb->isInterrupted() )
+      if ( cb && ( !cb->isInterrupted() || cb->isOnlySelfWhenInterrupt() ) )
       {
          MsgOpKillContexts killMsg ;
          MsgRouteID routeID ;
@@ -1091,7 +1091,7 @@ namespace engine
          routeID.value = pSubContext->getRouteID().value ;
          pAdvance->header.routeID.value = MSG_INVALID_ROUTEID ;
          pAdvance->contextID = pSubContext->contextID() ;
-   
+
          pSub = _pSession->addSubSession( routeID.value ) ;
          pSub->setReqMsg( (MsgHeader*)pAdvance, PMD_EDU_MEM_NONE ) ;
 
