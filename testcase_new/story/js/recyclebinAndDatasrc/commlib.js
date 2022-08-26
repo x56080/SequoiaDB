6bc439e61b84c0d220f0d8fa3540641577eb75bd
liuli <liuli@sequoiadb.com>
2022-08-26 11:16:15 +0800

    提交回收站测试用例

---
+++ testcase_new/story/js/recyclebinAndDatasrc/commlib.js
@@ -0,0 +1,11 @@
+﻿/******************************************************************************
+ * @Description   : 
+ * @Author        : liuli
+ * @CreateTime    : 2021.02.04
+ * @LastEditTime  : 2022.08.16
+ * @LastEditors   : liuli
+ ******************************************************************************/
+import( "../lib/recyclebin_commlib.js" );
+import( "../lib/datasource_commlib.js" );
+// import( "../lib/basic_operation/commlib.js" );
+testConf.testGroups = ["recycleBin"];
