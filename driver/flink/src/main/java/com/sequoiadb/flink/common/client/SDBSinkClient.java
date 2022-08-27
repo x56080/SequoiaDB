6bff954799404ba32f6d8926ddf43808fe769d9d
shenxiaopeng <shenxiaopeng@sequoiadb.com>
2022-08-27 21:16:31 +0800

    SEQUOIADBMAINSTREAM-8716:当autopartition为false，需要判断shardingkey和shardingtype为空，否则抛出错误

--- driver/flink/src/main/java/com/sequoiadb/flink/common/client/SDBSinkClient.java
+++ driver/flink/src/main/java/com/sequoiadb/flink/common/client/SDBSinkClient.java
@@ -299,26 +299,35 @@ public class SDBSinkClient implements SDBClient {
             }
         }
 
-        if (shardingKey != null){
-            BSONObject skBson = (BSONObject) JSON.parse(shardingKey);
-            //verity pk contain all fields in sharding key
-            if(!pkBson.isEmpty() && !SDBInfoUtil.containValidation(pkBson,skBson)){
-                throw new SDBException(String.format("The primary key must include all fields in sharding key, " +
-                        "primary key:%s, sharding key:%s,please delete flink mapping table and select appropriate " +
-                        "primary key to create a new table",pkBson,skBson));
+        if(sdboptions.getAutoPartition()) {
+            if (shardingKey != null) {
+                BSONObject skBson = (BSONObject) JSON.parse(shardingKey);
+                //verity pk contain all fields in sharding key
+                if (!pkBson.isEmpty() && !SDBInfoUtil.containValidation(pkBson, skBson)) {
+                    throw new SDBException(String.format("The primary key must include all fields in sharding key, " +
+                            "primary key:%s, sharding key:%s,please delete flink mapping table and select appropriate " +
+                            "primary key to create a new table", pkBson, skBson));
+                }
+
+                options.put(SDBConstant.SHARDING_KEY, skBson);
+                options.put(SDBConstant.SHARDING_TYPE, sdboptions.getShardingType());
+                options.put(SDBConstant.AUTO_SPLIT, true);
+            } else if (!pkBson.isEmpty()) {
+                // if user don't specify sharding key, using primary key as sharding key.
+                options.put(SDBConstant.SHARDING_KEY, pkBson);
+                options.put(SDBConstant.SHARDING_TYPE, sdboptions.getShardingType());
+                options.put(SDBConstant.AUTO_SPLIT, true);
             }
-
-            options.put(SDBConstant.SHARDING_KEY, skBson);
-            options.put(SDBConstant.SHARDING_TYPE, sdboptions.getShardingType());
-        } else if (sdboptions.getAutoPartition() && !pkBson.isEmpty()) {
-            // if user don't specify sharding key, using primary key as sharding key.
-            options.put(SDBConstant.SHARDING_KEY, pkBson);
-            options.put(SDBConstant.SHARDING_TYPE, sdboptions.getShardingType());
+        } else {
+            if(shardingKey != null || SDBConstant.SHARDING_TYPE != null){
+                throw new SDBException(String.format("Configuration conflict,autupartition is false,shardingkey and " +
+                        "shardingtype are required to be empty,shardingkey:%s," +
+                        "shardingtype:%s " , shardingKey, SDBConstant.SHARDING_TYPE));
+            }
         }
-
         options.put(SDBConstant.REPL_SIZE, sdboptions.getReplSize());
         options.put(SDBConstant.COMPRESSION_TYPE, sdboptions.getCompressionType());
-        options.put(SDBConstant.AUTO_SPLIT, true);
+
 
         String Group =  sdboptions.getGroup();
         if (Group != null) {
