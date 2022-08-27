d9193f422937893052a55d68b23eba6e99299e79
shenxiaopeng <shenxiaopeng@sequoiadb.com>
2022-08-27 20:20:40 +0800

    SEQUOIADBMAINSTREAM-8724:修改当primary为空时不判断是否包含

--- driver/flink/src/main/java/com/sequoiadb/flink/common/client/SDBSinkClient.java
+++ driver/flink/src/main/java/com/sequoiadb/flink/common/client/SDBSinkClient.java
@@ -302,7 +302,7 @@ public class SDBSinkClient implements SDBClient {
         if (shardingKey != null){
             BSONObject skBson = (BSONObject) JSON.parse(shardingKey);
             //verity pk contain all fields in sharding key
-            if(pkBson.isEmpty() || !SDBInfoUtil.containValidation(pkBson,skBson)){
+            if(!pkBson.isEmpty() && !SDBInfoUtil.containValidation(pkBson,skBson)){
                 throw new SDBException(String.format("The primary key must include all fields in sharding key, " +
                         "primary key:%s, sharding key:%s,please delete flink mapping table and select appropriate " +
                         "primary key to create a new table",pkBson,skBson));
