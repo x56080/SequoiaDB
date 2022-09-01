f7eb990241cd034e54ced8d844b789ef5a2e3321
wuyun <wuyun@sequoiadb.com>
2022-09-01 21:03:00 +0800

    new hit testcase

--- SequoiaDB/engine/test/vessel/hit_index_test.cpp
+++ SequoiaDB/engine/test/vessel/hit_index_test.cpp
@@ -299,6 +299,121 @@ TEST_F(hit_index_test, base_test_2)
    ASSERT_EQ(SDB_OK, rc);
 }
 
+TEST_F(hit_index_test, base_test_3)
+{
+   INT32 rc = SDB_OK;
+   vesselImpl db;
+   outerResource resource = test_outer_resource::getResource();
+   test_executor session;
+   openDBOptions options;
+   options.path.dataPath = DATA_PATH;
+   options.path.lsmPath = LSM_PATH;
+   options.lsmOptions.hitCfMemtableSize = 1 << 20;
+
+   UINT32 count = 100000;
+   UINT32 indexCount = 2;
+   DATA_COLLECTION_PTR handler;
+   
+
+   const string indexNamePrefix = "index";
+  
+   rc = db.open(&session, &resource, options);
+   ASSERT_EQ(SDB_OK, rc);
+
+   rc = db.createCS(&session, "foo", 1, dmsCreateCSOptions(), bson::BSONObj());
+   ASSERT_EQ(SDB_OK, rc);
+
+   rc = db.createCL(&session, "foo.bar", 1, dmsCreateCLOptions(), bson::BSONObj());
+   ASSERT_EQ(SDB_OK, rc);
+
+   rc = db.openCL(&session, "foo.bar", dmsOpenCLOptions(), handler);
+   ASSERT_EQ(SDB_OK, rc);
+
+   for (UINT32 i = 0; i < indexCount; ++i)
+   {
+      string indexName = indexNamePrefix + to_string(i);
+      bson::BSONObj pattern = BSON(indexName << 1);
+      bson::BSONObj indexDef = indexTestUtil::createIndexObj(INDEX_TYPE_HYBRID_TREE, indexName.c_str(), 
+                                                             FALSE, pattern, FALSE);   
+      rc = handler->createIndex(&session, dmsBuildIndexOptions(), indexDef);
+      ASSERT_EQ(SDB_OK, rc);
+   }
+
+   std::default_random_engine generator;
+   std::uniform_int_distribution<INT32> distribution(0, count);
+   std::vector<INT32> bValues;
+   bson::BSONObjBuilder builder;
+
+   for (UINT32 i = 0; i < count; ++i)
+   {
+      utilInsertResult res;
+      builder.reset();
+      INT32 randomInt32 = distribution(generator);
+      bValues.push_back(randomInt32);
+
+      for (UINT32 c = 0; c < indexCount; ++c)
+      {
+         string column = indexNamePrefix + to_string(c);
+         builder.appendIntOrLL(column, randomInt32);
+      }
+      bson::BSONObj obj = builder.done();
+
+      rc = handler->insertRecord(&session, obj, dmsInsertRecordOptions(), &res);
+      ASSERT_EQ(SDB_OK, rc);
+   }
+
+   for (UINT32 i = 0; i < indexCount; ++i)
+   {
+      mthMatchTree mt;
+      dmsIndexScanOptions o;
+      string indexName = indexNamePrefix + to_string(i);
+      const string &column = indexName;
+      bson::BSONObj pattern = BSON(indexName << 1);   
+      builder.reset();
+      bson::BSONObjBuilder subBuilder(builder.subobjStart(column));
+      subBuilder.appendIntOrLL("$gte", 0);
+      subBuilder.done();
+      bson::BSONObj query = builder.done();
+      rc = mt.loadPattern(query, FALSE);
+      ASSERT_EQ(SDB_OK, rc);
+      rtnPredicateSet ps;
+      rc = mt.calcPredicate(ps, NULL);
+      ASSERT_EQ(SDB_OK, rc);
+      rtnPredicateList predicates;
+      UINT32 lvl = 0;
+      rc = predicates.initialize(ps, pattern, 1, lvl);
+      ASSERT_EQ(SDB_OK, rc);
+
+      DATA_CURSOR_PTR cursor;
+      rc = handler->scanIndex(&session, indexName.c_str(),
+                              predicates, o, cursor);
+      ASSERT_EQ(SDB_OK, rc);
+      std::sort(bValues.begin(), bValues.end());
+      for (UINT32 i = 0; i < count; ++i)
+      {
+         rc = cursor->fetchNext(&session);
+         if (SDB_OK != rc)
+         {
+            cout << i << endl;
+         }
+         ASSERT_EQ(SDB_OK, rc);
+         BSONObj obj = cursor->getBsonRecord();
+         INT32 v = obj.getIntField(column.c_str());
+         if (v != bValues[i])
+         {
+            ASSERT_EQ(bValues[i], obj.getIntField(column.c_str()));
+         }
+         //ASSERT_EQ(bValues[i], obj.getIntField("b"));
+      }
+
+      rc = cursor->fetchNext(&session);
+      ASSERT_EQ(SDB_DMS_EOC, rc);
+   }
+
+   rc = db.close(&session, closeDBOptions());
+   ASSERT_EQ(SDB_OK, rc);
+}
+
 TEST_F(hit_index_test, base_compress_test_1)
 {
    INT32 rc = SDB_OK;
