b624826d1d1244849c3f92000416ee858b529863
linyingting <linyingting@sequoiadb.com>
2022-09-01 19:57:24 +0800

    CI-861:测试用例逻辑错误且测试点冗余，修改测试用例

--- testcase_new/story/js/jsObjectSync/SystemService_10668.js
+++ testcase_new/story/js/jsObjectSync/SystemService_10668.js
@@ -1,11 +1,10 @@
 /******************************************************************************
-*@Description : test js object System function: runService 
-*               TestLink : 10668 System对象启动、停止服务如ssh服务
-*                          10669 System对象启动服务，启动已启动的服务
-*                          10670 System对象停止服务，停止sdbcm服务
-*                          10671 System对象停止服务，停止已停止的服务
-*@author      : Liang XueWang
-******************************************************************************/
+ * @Description   : seqDB-10668:System对象调用runService启动、停止服务如ssh服务
+ * @Author        : Liang Xuewang
+ * @CreateTime    : 2016.12.27
+ * @LastEditTime  : 2022.09.02
+ * @LastEditors   : Lin Yingting
+ ******************************************************************************/
 // 测试启动停止服务，获取服务状态（SSH服务）
 SystemTest.prototype.testRunServiceSSH = function()
 {
@@ -15,30 +14,34 @@ SystemTest.prototype.testRunServiceSSH = function()
    var user = this.system.getCurrentUser().toObj().user;
    if( user !== "root" )
    {
+      println( "Skip this test, it requires root user privilege." );
       return;
    }
+   // 检查是否存在ssh服务
    if( !isSSHExist( this.hostname, this.svcname ) )
+   {
+      println( "Skip this test, it requires ssh service." );
       return;
+   }
    // 获取服务状态
    try
    {
-      var info = this.system.runService( "ssh", "status", "" );
+      var info = this.system.runService( "sshd", "status", "" );
    }
    catch( e )
    {
-      if( e === 3 )
+      if( e == 3 )
       {
          info = "dead";
-      }
+      } 
    }
    if( info.indexOf( "running" ) !== -1 )   // 如果服务已启动，则停止再启动服务
    {
       var status;
       try
       {
-         this.system.runService( "ssh", "stop", "" );
-         status = this.system.runService( "ssh", "status" );
-         throw new Error( status );
+         this.system.runService( "sshd", "stop", "" );
+         status = this.system.runService( "sshd", "status" );
       }
       catch( e )
       {
@@ -49,102 +52,23 @@ SystemTest.prototype.testRunServiceSSH = function()
       }
       finally
       {
-         this.system.runService( "ssh", "start", "" );
-         status = this.system.runService( "ssh", "status" );
+         this.system.runService( "sshd", "start", "" );
+         status = this.system.runService( "sshd", "status" );
          checkStatus( status, "running" );
       }
    }
    else if( info.indexOf( "dead" ) !== -1 )  // 如果服务已停止，则启动服务
    {
-      this.system.runService( "ssh", "start", "" );
-      var status = this.system.runService( "ssh", "status" );
+      this.system.runService( "sshd", "start", "" );
+      var status = this.system.runService( "sshd", "status" );
       checkStatus( status, "running" );
    }
    else
    {
-      throw new Error( "testRunService get service exp status start/stop. but result:" + info );
+      throw new Error( "The ssh service status of runService is expected as start/stop, but result is: " + info );
    }
    this.release();
 }
-
-// 测试重复启动或停止服务，（SSH服务）
-SystemTest.prototype.testRunServiceDuplicate = function()
-{
-   this.init();
-
-   // 检查cm用户是否为root
-   var user = this.system.getCurrentUser().toObj().user;
-   if( user !== "root" )
-   {
-      return;
-   }
-   if( !isSSHExist( this.hostname, this.svcname ) )
-      return;
-   // 获取服务状态
-   var info = this.system.runService( "ssh", "status" );
-   var command;
-   if( info.indexOf( "running" ) !== -1 )
-      command = "start";
-   else
-      command = "stop";
-   try
-   {
-      this.system.runService( "ssh", command );
-      throw new Error( "should error" );
-   }
-   catch( e )
-   {
-      if( e.message != 0 )
-      {
-         throw e;
-      }
-   }
-   // 测试完成后，启动服务
-   try
-   {
-      this.system.runService( "ssh", "start" );
-      throw new Error( "should error" );
-   }
-   catch( e )
-   {
-      if( e.message != 0 )
-      {
-         throw e;
-      }
-   }
-   this.release();
-}
-
-// 测试停止sdbcm服务(手工验证)
-SystemTest.prototype.testStopSdbcm = function()
-{
-   this.init();
-
-   // 检查cm用户是否为root
-   var user = this.system.getCurrentUser().toObj().user;
-   if( user === "root" )
-   {
-      return;
-   }
-
-   if( this.system == System )
-   {
-      return;
-   }
-   try
-   {
-      this.system.runService( "sdbcm", "stop" );
-   }
-   catch( e )
-   {
-      if( e.message != SDB_NETWORK_CLOSE )
-      {
-         throw e;
-      }
-   }
-   this.release();
-}
-
 /******************************************************************************
 *@Description : check service status after stop/start service
 *@author      : Liang XueWang            
@@ -153,7 +77,7 @@ function checkStatus ( status, msg )
 {
    if( status.indexOf( msg ) === -1 )
    {
-      throw new Error( "testStopSdbcm stop sdbcm" );
+      throw new Error( "The status of ssh service is not as expected." );
    }
 }
 
@@ -165,20 +89,15 @@ function isSSHExist ( hostname, svcname )
 {
    var remote = new Remote( hostname, svcname );
    var cmd = remote.getCmd();
+   var status = cmd.run( "systemctl is-enabled sshd" ).replace(/\s+/g,"");
    var exist;
-   try
+   if( status == "enabled" )
    {
-      cmd.run( "service ssh status" );
       exist = true;
    }
-   catch( e )
+   else
    {
-      if( e.message == 3 )
-         exist = true;
-      else if( e.message == 1 )
-         exist = false;
-      else
-         throw e;
+      exist = false;
    }
    remote.close();
    return exist;
@@ -188,23 +107,8 @@ main( test );
 
 function test ()
 {
-
-   // 获取本地主机和远程主机
+   // 获取本地主机，测试启动停止ssh服务
    var localhost = toolGetLocalhost();
-   var remotehost = toolGetRemotehost();
-
    var localSystem = new SystemTest( localhost, CMSVCNAME );
-   var remoteSystem = new SystemTest( remotehost, CMSVCNAME );
-   var systems = [localSystem, remoteSystem];
-   for( var i = 0; i < systems.length; i++ )
-   {
-      // 测试启动停止ssh服务
-      systems[i].testRunServiceSSH();
-
-      // 测试重复启动停止ssh服务
-      systems[i].testRunServiceDuplicate();
-
-      // 测试停止sdbcm服务
-      // systems[i].testStopSdbcm();
-   }
+   localSystem.testRunServiceSSH();   
 }
