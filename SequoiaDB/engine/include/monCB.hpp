6a57af6086c2abbca148762ecf8494e8c9bda345
liaochengxi <liaochengxi@sequoiadb.com>
2022-09-14 11:51:56 +0800

    SEQUOIADBMAINSTREAM-8673: 修复SDB_SNAP_QUERIES快照中OpType和LastOpInfo内容展示不完整的问题。

--- SequoiaDB/engine/include/monCB.hpp
+++ SequoiaDB/engine/include/monCB.hpp
@@ -78,7 +78,7 @@ namespace engine
       {                                                              \
          try {                                                       \
             _pMonAppCB_->setLastOpType( opType ) ;                   \
-            _pMonAppCB_->setLastCmdType( CMD_UNKNOW ) ;              \
+            _pMonAppCB_->setUnknownCmdType() ;                        \
             _pMonAppCB_->saveLastOpDetail( format,                   \
                                            ##__VA_ARGS__ ) ;         \
          } catch( ... ) {}                                           \
@@ -91,7 +91,7 @@ namespace engine
       {                                                              \
          try {                                                       \
             _pMonAppCB_->setLastOpType( msg->opCode ) ;              \
-            _pMonAppCB_->setLastCmdType( CMD_UNKNOW ) ;              \
+            _pMonAppCB_->setUnknownCmdType() ;                        \
             _pMonAppCB_->saveLastOpQuery( msg, options ) ;           \
          } catch( ... ) {}                                           \
       }                                                              \
@@ -824,6 +824,7 @@ namespace engine
       void endOperator() ;
       void setLastOpType( INT32 opType ) ;
       void setLastCmdType( INT32 cmdType ) ;
+      void setUnknownCmdType() ;
       void opTimeSpentInc( ossTickDelta delta );
       const CHAR * getLastOpDetail() ;
       void saveLastOpQuery( const MsgHeader *message,
--- SequoiaDB/engine/pmd/pmdProcessor.cpp
+++ SequoiaDB/engine/pmd/pmdProcessor.cpp
@@ -2332,6 +2332,8 @@ namespace engine
       {
          if ( eduCB()->getMonQueryCB() )
          {
+            MONQUERY_SET_QUERY_TEXT( eduCB(),
+                                     eduCB()->getMonAppCB()->getLastOpDetail() ) ;
             monQueryCB = eduCB()->getMonQueryCB() ;
             ossTick endTime ;
             endTime.sample() ;
