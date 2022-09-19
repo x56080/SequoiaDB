b6d0cf6912cffbdb1e04239ce890ed654f7dc3d6
liaochengxi <liaochengxi@sequoiadb.com>
2022-09-19 15:33:45 +0800

    SEQUOIADBMAINSTREAM-8664: 补充调用getNodeInfo接口的异常处理

--- SequoiaDB/engine/cls/clsCatalogAgent.cpp
+++ SequoiaDB/engine/cls/clsCatalogAgent.cpp
@@ -4009,7 +4009,7 @@ namespace engine
          catch( std::exception &e )
          {
             rc = ossException2RC( &e ) ;
-            PD_RC_CHECK( rc, PDERROR, "Assigning value to string occrued "
+            PD_RC_CHECK( rc, PDERROR, "Assigning value to string occured "
                          "exception: %s, rc: %d", e.what(), rc ) ;
          }
       }
@@ -4042,7 +4042,7 @@ namespace engine
          catch( std::exception &e )
          {
             rc = ossException2RC( &e ) ;
-            PD_RC_CHECK( rc, PDERROR, "Assigning value to string occrued "
+            PD_RC_CHECK( rc, PDERROR, "Assigning value to string occured "
                          "exception: %s, rc: %d", e.what(), rc ) ;
          }
       }
