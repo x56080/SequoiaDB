579e5b39ef27dcbef16c14fafdb16d3b23385dc6
heguoming <heguoming@sequoiadb.com>
2022-09-23 23:05:36 +0800

    SEQUOIADBMAINSTREAM-8808: fix issue of reelection after ft confirmed

--- SequoiaDB/engine/cls/clsReplicateSet.cpp
+++ SequoiaDB/engine/cls/clsReplicateSet.cpp
@@ -87,6 +87,8 @@ namespace engine
    #define CLS_FORMART_STR_128                  (128)
    #define CLS_REPL_WAIT_TIME                   (100)
    #define CLS_REPL_RETRY_TIMEOUT               (10000)
+   #define CLS_FT_SW_TIMEOUT                    ( 10000 )
+
    /*
       _clsReplicateSet define
    */
@@ -813,7 +815,9 @@ namespace engine
                   PD_LOG( PDEVENT, "Force to secondary due to %s",
                           ftStatStr ) ;
                   _vote.force( CLS_ELECTION_STATUS_SEC, 5 * OSS_ONE_SEC ) ;
-                  _vote.setShadowWeight( CLS_ELECTION_WEIGHT_MIN ) ;
+                  _vote.setShadowWeight( CLS_ELECTION_WEIGHT_MIN,
+                                         CLS_FT_SW_TIMEOUT,
+                                         FALSE ) ;
                }
             }
          }
