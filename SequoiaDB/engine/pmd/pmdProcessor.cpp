347a8de2b0e98421d1715957b1c4320859616736
chengjingjing <162-chengjingjing@users.noreply.gitlab.example.com>
2022-09-27 23:14:36 +0800

    SEQUOIADBMAINSTREAM-8749 5.0版本对接3.4版本创建数据源失败

--- SequoiaDB/engine/pmd/pmdProcessor.cpp
+++ SequoiaDB/engine/pmd/pmdProcessor.cpp
@@ -276,7 +276,7 @@ namespace engine
                        msg->TID, msg->routeID.columns.groupID,
                        msg->routeID.columns.nodeID,
                        msg->routeID.columns.serviceID, msg->requestID ) ;
-               rc = SDB_INVALIDARG ;
+               rc = SDB_UNKNOWN_MESSAGE ;
                break ;
          }
       }
