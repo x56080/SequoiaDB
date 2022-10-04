22c976f4fb6ba8dbe01619ce04044ad4ed0384e0
chengjingjing <162-chengjingjing@users.noreply.gitlab.example.com>
2022-10-04 21:36:08 +0800

    SEQUOIADBMAINSTREAM-8839 参数名含有大写且有别名，deleteconf无法生效

--- SequoiaDB/engine/rtn/rtnCommand.cpp
+++ SequoiaDB/engine/rtn/rtnCommand.cpp
@@ -2552,13 +2552,13 @@ error:
          {
             BSONElement ele = itr.next() ;
             const CHAR *fieldName = ele.fieldName() ;
-            const CHAR *aliasName = pmdGetConfigAliasName( fieldName ) ;
             INT32 rc = utilStrToLower( fieldName, lowerFieldName ) ;
             if ( rc )
             {
                PD_LOG( PDERROR, "Failed to convert fieldName to lowercase, rc: %d", rc ) ;
                goto error ;
             }
+            const CHAR *aliasName = pmdGetConfigAliasName( fieldName ) ;
             if ( NULL != lowerFieldName)
             {
                newCfgBob.append( lowerFieldName, 1 ) ;
