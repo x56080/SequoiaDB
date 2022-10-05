1a5c93297a9ef3af688b71929401753fb238de21
chengjingjing <162-chengjingjing@users.noreply.gitlab.example.com>
2022-10-05 23:23:23 +0800

    fix issue bug-8839 failed regression test

--- SequoiaDB/engine/rtn/rtnCommand.cpp
+++ SequoiaDB/engine/rtn/rtnCommand.cpp
@@ -2771,7 +2771,7 @@ error:
                        "rc: %d", rc ) ;
                goto error ;
             }
-            const CHAR *aliasName = pmdGetConfigAliasName( fieldName ) ;
+            const CHAR *aliasName = pmdGetConfigAliasName( lowerFieldName ) ;
             if ( NULL != lowerFieldName)
             {
                newCfgBob.append( lowerFieldName, 1 ) ;
