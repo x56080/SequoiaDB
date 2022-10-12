47edf277db0d81dbf0f41f4f07bf2af3be925d27
liuli <liuli@sequoiadb.com>
2022-10-12 15:51:57 +0800

    CI-996:SEQUOIADBMAINSTREAM-8846合入后回收站用例失败调整

--- testcase_new/story/js/recyclebin_sync/recyclebin_23806.js
+++ testcase_new/story/js/recyclebin_sync/recyclebin_23806.js
@@ -2,7 +2,7 @@
  * @Description   : seqDB-23806:分区表且被切分到多个数据组，读写数据并snapshot查看集合统计信息，dropCL后恢复CL 
  * @Author        : liuli
  * @CreateTime    : 2022.03.04
- * @LastEditTime  : 2022.08.22
+ * @LastEditTime  : 2022.10.12
  * @LastEditors   : liuli
  ******************************************************************************/
 testConf.skipStandAlone = true;
@@ -26,7 +26,8 @@ function test ()
    dbcl.insert( docs );
 
    commCheckLSN( db );
-   var expCursor = db.snapshot( SDB_SNAP_COLLECTIONS, { Name: csName + "." + clName }, {}, { "Details.Group.NodeName": 1 } );
+   var expCursor = db.snapshot( SDB_SNAP_COLLECTIONS, { Name: csName + "." + clName },
+      { "Details.Group.UpdateTime": { "$include": 0 } }, { "Details.Group.NodeName": 1 } );
    var expSnapshot = expCursor.current().toObj();
 
    dbcs.dropCL( clName );
@@ -35,7 +36,8 @@ function test ()
    db.getRecycleBin().returnItem( recycleName );
 
    commCheckLSN( db );
-   var actCursor = db.snapshot( SDB_SNAP_COLLECTIONS, { Name: csName + "." + clName }, {}, { "Details.Group.NodeName": 1 } );
+   var actCursor = db.snapshot( SDB_SNAP_COLLECTIONS, { Name: csName + "." + clName },
+      { "Details.Group.UpdateTime": { "$include": 0 } }, { "Details.Group.NodeName": 1 } );
    var actSnapshot = actCursor.current().toObj();
 
    assert.equal( actSnapshot, expSnapshot );
