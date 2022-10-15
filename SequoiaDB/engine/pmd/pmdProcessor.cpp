9cb612629df03c9574cbd69f3f0cd282a3646317
yangshangde <yangshangde@sequoiadb.com>
2022-10-15 11:08:26 +0800

    bug-8559 Fix duplicated LastOpInfo in session snapshot

--- SequoiaDB/engine/pmd/pmdProcessor.cpp
+++ SequoiaDB/engine/pmd/pmdProcessor.cpp
@@ -65,6 +65,7 @@
 #include "utilStr.hpp"
 #include "pdSecure.hpp"
 #include "rtnInsertModifier.hpp"
+#include "monCB.hpp"
 
 using namespace bson ;
 
@@ -2344,6 +2345,7 @@ namespace engine
       if ( SDB_COORD_UNKNOWN_OP_REQ == rc )
       {
          contextBuff.release() ;
+         MON_CLEAR_OP_DETAIL( eduCB()->getMonAppCB() ) ;
          rc = _pmdDataProcessor::processMsg( msg, contextBuff, contextID,
                                              needReply, needRollback,
                                              builder ) ;
