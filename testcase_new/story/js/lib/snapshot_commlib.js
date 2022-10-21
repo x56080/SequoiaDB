65a3c7c450f49a792b21d480e620d9d5d7eef9c7
liuli <liuli@sequoiadb.com>
2022-10-21 10:33:38 +0800

    SEQUOIADBMAINSTREAM-8820：新增快照支持统计事务次数自动化用例

--- testcase_new/story/js/lib/snapshot_commlib.js
+++ testcase_new/story/js/lib/snapshot_commlib.js
@@ -228,3 +228,214 @@ function getCoordUrl ( sdb )
    }
    return coordUrls;
 }
+
+function sortBy ( field )
+{
+   return function( a, b )
+   {
+      return a[field] > b[field];
+   }
+}
+
+function checkSnapshot ( cursor, expResults, range )
+{
+   if( typeof ( range ) == "undefined" ) { range = 0; }
+   // 校验时允许存在误差的指标
+   var checkRangeKeys = ["LobCapacity", "LobMetaCapacity", "MaxLobCapacity", "MaxLobCapSize", "UsedLobSpaceRatio", "LobUsageRate",
+      "TotalLobReadSize", "TotalLobWriteSize", "TotalLobRead", "TotalLobWrite", "TotalLobTruncate", "TotalLobAddressing"];
+   var i = 0;
+   while( cursor.next() )
+   {
+      var obj = cursor.current().toObj();
+      for( var key in expResults[i] )
+      {
+         if( key == "TotalLobAddressing" )
+         {
+            // TotalLobAddressing允许在每次操作中存在2倍误差
+            if( obj[key] < expResults[i][key] || obj[key] > ( expResults[i][key] + range ) )
+            {
+               throw new Error( "actual:" + JSON.stringify( obj[key], "", 1 ) + "\nexpected:" +
+                  JSON.stringify( expResults[i][key], "", 1 ) + "\n" + key + "\n node name:" + expResults[i]["NodeName"] );
+            }
+
+         }
+         else if( checkRangeKeys.indexOf( key ) >= 0 )
+         {
+            var absolute = Math.abs( obj[key] - expResults[i][key] );
+            // 对于部分结果允许存在5%的误差
+            if( absolute > ( obj[key] * 0.05 ) )
+            {
+               throw new Error( "actual:" + JSON.stringify( obj[key], "", 1 ) + "\nexpected:" +
+                  JSON.stringify( expResults[i][key], "", 1 ) + "\n" + key + "\n node name:" + expResults[i]["NodeName"] );
+            }
+         }
+         else
+         {
+            assert.equal( obj[key], expResults[i][key], "different fields:" + key + "\n node name:" + expResults[i]["NodeName"] );
+         }
+      }
+      i++;
+   }
+   cursor.close();
+}
+
+function getSnapshotLobStat ( cursor )
+{
+   var lobStat = ["TotalLobGet", "TotalLobPut", "TotalLobDelete", "TotalLobReadSize", "TotalLobWriteSize", "LobCapacity",
+      "LobMetaCapacity", "MaxLobCapacity", "MaxLobCapSize", "TotalLobs", "TotalLobPages", "TotalUsedLobSpace",
+      "UsedLobSpaceRatio", "TotalLobList", "FreeLobSpace", "FreeLobSize", "TotalLobSize", "TotalValidLobSize", "LobUsageRate",
+      "AvgLobSize", "TotalLobRead", "TotalLobWrite", "TotalLobTruncate", "TotalLobAddressing", "NodeName"];
+   var actLobStats = [];
+   while( cursor.next() )
+   {
+      var actLobStat = {};
+      var obj = cursor.current().toObj();
+      for( var i in lobStat )
+      {
+         actLobStat[lobStat[i]] = obj[lobStat[i]];
+      }
+      actLobStats.push( actLobStat );
+   }
+   cursor.close();
+   actLobStats.sort( sortBy( "NodeName" ) );
+   return actLobStats;
+}
+
+function checkSnapshotToCL ( cursor, expResults, isRawData, range )
+{
+   if( typeof ( range ) == "undefined" ) { range = 0; }
+   // 校验结果时允许存在误差的指标
+   var checkRangeKeys = ["LobCapacity", "LobMetaCapacity", "MaxLobCapacity", "MaxLobCapSize", "UsedLobSpaceRatio", "LobUsageRate",
+      "TotalLobReadSize", "TotalLobWriteSize", "TotalLobRead", "TotalLobWrite", "TotalLobTruncate", "TotalLobAddressing"];
+   var Details = "Details";
+   var Group = "Group";
+
+   var i = 0;
+   while( cursor.next() )
+   {
+      var obj = cursor.current().toObj();
+      for( var key in expResults[i] )
+      {
+         if( isRawData )
+         {
+            if( key == "TotalLobAddressing" )
+            {
+               // TotalLobAddressing允许在每次操作中存在2倍误差
+               if( obj[Details][0][key] < expResults[i][key] || obj[Details][0][key] > ( expResults[i][key] + range ) )
+               {
+                  throw new Error( "actual:" + JSON.stringify( obj[Details][0][key], "", 1 ) + "\nexpected:" +
+                     JSON.stringify( expResults[i][key], "", 1 ) + "\n" + key + "\n node name:" + expResults[i]["NodeName"] );
+               }
+            }
+            else if( checkRangeKeys.indexOf( key ) >= 0 )
+            {
+               var absolute = Math.abs( obj[Details][0][key] - expResults[i][key] );
+               // 对于部分结果允许存在5%的误差
+               if( absolute > ( obj[Details][0][key] * 0.05 ) )
+               {
+                  throw new Error( "actual:" + JSON.stringify( obj[Details][0][key], "", 1 ) + "\nexpected:" +
+                     JSON.stringify( expResults[i][key], "", 1 ) + "\n" + key + "\n node name:" + expResults[i]["NodeName"] );
+               }
+            }
+            else
+            {
+               assert.equal( obj[Details][0][key], expResults[i][key], "different fields:" + key + "\n node name:" + expResults[i]["NodeName"] );
+            }
+         }
+         else
+         {
+            var objInfo = obj[Details][0][Group].sort( sortBy( "NodeName" ) );
+            for( var j in obj[Details][0][Group] )
+            {
+               if( key == "TotalLobAddressing" )
+               {
+                  // TotalLobAddressing允许在每次操作中存在2倍误差
+                  if( objInfo[j][key] < expResults[j][key] || objInfo[j][key] > ( expResults[j][key] + range ) )
+                  {
+                     throw new Error( "actual:" + JSON.stringify( obj[Details][0][Group][j][key], "", 1 ) + "\nexpected:" +
+                        JSON.stringify( expResults[j][key], "", 1 ) + "\n" + key + "\n node name:" + expResults[j]["NodeName"] +
+                        " act node : " + objInfo[j]["NodeName"] );
+                  }
+               }
+               else if( checkRangeKeys.indexOf( key ) >= 0 )
+               {
+                  var absolute = Math.abs( objInfo[j][key] - expResults[j][key] );
+                  // 对于部分结果允许存在5%的误差
+                  if( absolute > ( objInfo[j][key] * 0.05 ) )
+                  {
+                     throw new Error( "actual:" + JSON.stringify( obj[Details][0][Group][j][key], "", 1 ) + "\nexpected:" +
+                        JSON.stringify( expResults[j][key], "", 1 ) + "\n" + key + "\n node name:" + expResults[j]["NodeName"] +
+                        " act node : " + objInfo[j]["NodeName"] );
+                  }
+               }
+               else
+               {
+                  assert.equal( obj[Details][0][Group][j][key], expResults[j][key], "different fields:" + key + "\n node name:" +
+                     expResults[j]["NodeName"] + " act node : " + objInfo[j]["NodeName"] );
+               }
+            }
+         }
+      }
+      i++;
+   }
+   cursor.close();
+}
+
+function getSnapshotLobStatToCL ( cursor, isRawData )
+{
+   var Details = "Details";
+   var Group = "Group";
+   var lobStat = ["TotalLobGet", "TotalLobPut", "TotalLobDelete", "TotalLobReadSize", "TotalLobWriteSize", "LobCapacity",
+      "LobMetaCapacity", "MaxLobCapacity", "MaxLobCapSize", "TotalLobs", "TotalLobPages", "TotalUsedLobSpace",
+      "UsedLobSpaceRatio", "TotalLobList", "FreeLobSpace", "FreeLobSize", "TotalLobSize", "TotalValidLobSize", "LobUsageRate",
+      "AvgLobSize", "TotalLobRead", "TotalLobWrite", "TotalLobTruncate", "TotalLobAddressing", "NodeName"];
+   var actLobStats = [];
+
+   while( cursor.next() )
+   {
+      var obj = cursor.current().toObj();
+      if( isRawData )
+      {
+         var actLobStat = {};
+         for( var i in lobStat )
+         {
+            actLobStat[lobStat[i]] = obj[Details][0][lobStat[i]];
+         }
+         actLobStats.push( actLobStat );
+      }
+      else
+      {
+         for( j in obj[Details][0][Group] )
+         {
+            var actLobStat = {};
+            for( var i in lobStat )
+            {
+               actLobStat[lobStat[i]] = obj[Details][0][Group][j][lobStat[i]];
+            }
+            actLobStats.push( actLobStat );
+         }
+      }
+   }
+   cursor.close()
+   actLobStats.sort( sortBy( "NodeName" ) );
+   return actLobStats;
+}
+
+function getSnapshotResults ( cursor, fields )
+{
+   if( fields == undefined ) { fields = ["TotalTransCommit", "TotalTransRollback", "NodeName"]; }
+   var results = [];
+   while( cursor.next() )
+   {
+      var result = {};
+      var obj = cursor.current().toObj();
+      for( var i in fields )
+      {
+         result[fields[i]] = obj[fields[i]];
+      }
+      results.push( result );
+   }
+   cursor.close();
+   results.sort( sortBy( "NodeName" ) );
+   return results;
+}
