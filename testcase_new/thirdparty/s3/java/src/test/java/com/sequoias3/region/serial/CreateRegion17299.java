e06cd85c99403f361fabd0dbec9a5e19a4d76933
wuyan <wuyan@sequoiadb.com>
2022-10-24 21:25:29 +0800

    SEQUOIADBMAINSTREAM-8363更新ssh连接用户名和密码为指定变量

--- testcase_new/thirdparty/s3/java/src/test/java/com/sequoias3/region/serial/CreateRegion17299.java
+++ testcase_new/thirdparty/s3/java/src/test/java/com/sequoias3/region/serial/CreateRegion17299.java
@@ -26,7 +26,8 @@ import com.sequoias3.testcommon.s3utils.ObjectUtils;
 import com.sequoias3.testcommon.s3utils.RegionUtils;
 
 /**
- * @Description: seqDB-17299 ::创建区域配置DataCSShardingType策略小于CLShardingType策略 此用例时间跳变会影响到db日志，所以不上ci
+ * @Description: seqDB-17299 ::创建区域配置DataCSShardingType策略小于CLShardingType策略
+ *               此用例时间跳变会影响到db日志，所以不上ci
  * @author fanyu
  * @Date:2019年01月22日
  * @version:1.0
@@ -38,8 +39,8 @@ public class CreateRegion17299 extends S3TestBase {
     private AmazonS3 s3Client = null;
     private int fileSize = 1024 * 200;
     private File localPath = null;
-    private List<String> filePathList = new ArrayList<String>();
-    private List<Date> dateList = new ArrayList<Date>();
+    private List< String > filePathList = new ArrayList< String >();
+    private List< Date > dateList = new ArrayList< Date >();
     private int fileNum = 3;
     private boolean runSuccess = false;
     private SequoiaS3 regionClient = null;
@@ -52,21 +53,24 @@ public class CreateRegion17299 extends S3TestBase {
      *            e.g: yyyyMMdd
      * @throws Exception
      */
-    private static void setSystemTime(String host, String dateStr) throws Exception {
+    private static void setSystemTime( String host, String dateStr )
+            throws Exception {
         Ssh ssh = null;
         try {
-            ssh = new Ssh(host, "root", "jenkins");
+            ssh = new Ssh( host, S3TestBase.remoteuser,
+                    S3TestBase.remotepasswd );
 
             // set date
             String cmd = "date -s \"" + dateStr + "\"";
-            ssh.exec(cmd);
+            ssh.exec( cmd );
 
             // print local date after set date
-            ssh.exec("date");
-            String localDate = ssh.getStdout().split("\n")[0];
-            System.out.println("host = " + host + ", localDate = " + localDate + ", after set system time");
+            ssh.exec( "date" );
+            String localDate = ssh.getStdout().split( "\n" )[ 0 ];
+            System.out.println( "host = " + host + ", localDate = " + localDate
+                    + ", after set system time" );
         } finally {
-            if (null != ssh) {
+            if ( null != ssh ) {
                 ssh.disconnect();
             }
         }
@@ -78,11 +82,12 @@ public class CreateRegion17299 extends S3TestBase {
      * @param host
      * @throws Exception
      */
-    private static void restoreSystemTime(String host) throws Exception {
+    private static void restoreSystemTime( String host ) throws Exception {
         Ssh ssh = null;
         try {
             // hostname sshuser sshpasswd
-            ssh = new Ssh(host, "root", "jenkins");
+            ssh = new Ssh( host, S3TestBase.remoteuser,
+                    S3TestBase.remotepasswd );
             String cmd = "ntpdate " + "\"192.168.20.11\"";
 
             // in case of time server not usable, retry in 1 min
@@ -90,27 +95,28 @@ public class CreateRegion17299 extends S3TestBase {
             int intervalSec = 1;
             boolean restoreOk = false;
             Exception lastException = null;
-            for (int i = 0; i < times; ++i) {
+            for ( int i = 0; i < times; ++i ) {
                 try {
-                    ssh.exec(cmd);
+                    ssh.exec( cmd );
                     restoreOk = true;
                     break;
-                } catch (Exception e) {
+                } catch ( Exception e ) {
                     lastException = e;
-                    Thread.sleep(intervalSec);
+                    Thread.sleep( intervalSec );
                 }
             }
 
             // print local date after set date
-            ssh.exec("date");
-            String localDate = ssh.getStdout().split("\n")[0];
-            System.out.println("host = " + host + ", localDate = " + localDate + ", after restore system time");
+            ssh.exec( "date" );
+            String localDate = ssh.getStdout().split( "\n" )[ 0 ];
+            System.out.println( "host = " + host + ", localDate = " + localDate
+                    + ", after restore system time" );
 
-            if (!restoreOk) {
+            if ( !restoreOk ) {
                 throw lastException;
             }
         } finally {
-            if (null != ssh) {
+            if ( null != ssh ) {
                 ssh.disconnect();
             }
         }
@@ -118,52 +124,62 @@ public class CreateRegion17299 extends S3TestBase {
 
     @BeforeClass
     private void setUp() throws Exception {
-        localPath = new File(S3TestBase.workDir + File.separator + TestTools.getClassName());
-        TestTools.LocalFile.removeFile(localPath);
-        TestTools.LocalFile.createDir(localPath.toString());
+        localPath = new File( S3TestBase.workDir + File.separator
+                + TestTools.getClassName() );
+        TestTools.LocalFile.removeFile( localPath );
+        TestTools.LocalFile.createDir( localPath.toString() );
         String filePath = null;
-        for (int i = 0; i < fileNum; i++) {
-            filePath = localPath + File.separator + "localFile_" + (fileSize + i) + ".txt";
-            TestTools.LocalFile.createFile(filePath, fileSize + i);
-            filePathList.add(filePath);
+        for ( int i = 0; i < fileNum; i++ ) {
+            filePath = localPath + File.separator + "localFile_"
+                    + ( fileSize + i ) + ".txt";
+            TestTools.LocalFile.createFile( filePath, fileSize + i );
+            filePathList.add( filePath );
         }
         s3Client = CommLib.buildS3Client();
-        CommLib.clearBucket(s3Client, bucketName);
+        CommLib.clearBucket( s3Client, bucketName );
         regionClient = CommLib.regionClient();
-        RegionUtils.clearRegion(regionClient, regionName);
+        RegionUtils.clearRegion( regionClient, regionName );
     }
 
     @Test
     private void test() throws Exception {
         // create region
-        CreateRegionRequest request = new CreateRegionRequest(regionName);
-        request.withDataCSShardingType(DataShardingType.MONTH).withDataCLShardingType(DataShardingType.YEAR);
-        regionClient.createRegion(request);
+        CreateRegionRequest request = new CreateRegionRequest( regionName );
+        request.withDataCSShardingType( DataShardingType.MONTH )
+                .withDataCLShardingType( DataShardingType.YEAR );
+        regionClient.createRegion( request );
 
         // create bucket
-        s3Client.createBucket(new CreateBucketRequest(bucketName, regionName));
+        s3Client.createBucket(
+                new CreateBucketRequest( bucketName, regionName ) );
         Calendar cal = Calendar.getInstance();
-        cal.set(cal.get(Calendar.YEAR) - 1, 0, 10);
+        cal.set( cal.get( Calendar.YEAR ) - 1, 0, 10 );
 
         // create object with time
-        for (int i = 0; i < fileNum; i++) {
-            setSystemTime(S3TestBase.s3HostName, cal.getTime().toString());
-            dateList.add(cal.getTime());
-            s3Client.putObject(bucketName, objectName + "_" + i, new File(filePathList.get(i)));
-            cal.set(Calendar.MONTH, cal.get(Calendar.MONTH) + 1);
+        for ( int i = 0; i < fileNum; i++ ) {
+            setSystemTime( S3TestBase.s3HostName, cal.getTime().toString() );
+            dateList.add( cal.getTime() );
+            s3Client.putObject( bucketName, objectName + "_" + i,
+                    new File( filePathList.get( i ) ) );
+            cal.set( Calendar.MONTH, cal.get( Calendar.MONTH ) + 1 );
         }
 
         // check cs and cl
-        for (int i = 0; i < dateList.size(); i++) {
-            String csName = RegionUtils.getDataCSName(regionName, DataShardingType.MONTH, dateList.get(i));
-            String clName = RegionUtils.getDataCLName(DataShardingType.YEAR, dateList.get(i));
-            Assert.assertTrue(RegionUtils.clInCS(csName, clName), "csName = " + csName + ",clName = " + clName);
+        for ( int i = 0; i < dateList.size(); i++ ) {
+            String csName = RegionUtils.getDataCSName( regionName,
+                    DataShardingType.MONTH, dateList.get( i ) );
+            String clName = RegionUtils.getDataCLName( DataShardingType.YEAR,
+                    dateList.get( i ) );
+            Assert.assertTrue( RegionUtils.clInCS( csName, clName ),
+                    "csName = " + csName + ",clName = " + clName );
         }
 
         // get object for check
-        for (int i = 0; i < fileNum; i++) {
-            S3Object s3Object = s3Client.getObject(bucketName, objectName + "_" + i);
-            checkObjectMetaAndData(s3Object, filePathList.get(i), dateList.get(i));
+        for ( int i = 0; i < fileNum; i++ ) {
+            S3Object s3Object = s3Client.getObject( bucketName,
+                    objectName + "_" + i );
+            checkObjectMetaAndData( s3Object, filePathList.get( i ),
+                    dateList.get( i ) );
         }
         runSuccess = true;
     }
@@ -171,27 +187,33 @@ public class CreateRegion17299 extends S3TestBase {
     @AfterClass
     private void tearDown() throws Exception {
         try {
-            if (runSuccess) {
-                CommLib.clearBucket(s3Client, bucketName);
-                regionClient.deleteRegion(regionName);
+            if ( runSuccess ) {
+                CommLib.clearBucket( s3Client, bucketName );
+                regionClient.deleteRegion( regionName );
             }
         } finally {
-            restoreSystemTime(S3TestBase.s3HostName);
-            if (s3Client != null) {
+            restoreSystemTime( S3TestBase.s3HostName );
+            if ( s3Client != null ) {
                 s3Client.shutdown();
             }
         }
     }
 
-    private void checkObjectMetaAndData(S3Object object, String filePath, Date date) throws Exception {
+    private void checkObjectMetaAndData( S3Object object, String filePath,
+            Date date ) throws Exception {
         ObjectMetadata metadata = object.getObjectMetadata();
-        Assert.assertEquals(metadata.getVersionId(), "null");
-        Assert.assertEquals(metadata.getETag(), TestTools.getMD5(filePath));
-        Assert.assertTrue(metadata.getLastModified().getTime() / 1000 >= date.getTime() / 1000,
-                "metadata.getLastModified = " + metadata.getLastModified().getTime() + ",expDate = " + date.getTime());
-        String downloadPath = TestTools.LocalFile.initDownloadPath(localPath, TestTools.getMethodName(),
-                Thread.currentThread().getId());
-        ObjectUtils.inputStream2File(object.getObjectContent(), downloadPath);
-        Assert.assertEquals(TestTools.getMD5(downloadPath), TestTools.getMD5(filePath), "filePath = " + filePath);
+        Assert.assertEquals( metadata.getVersionId(), "null" );
+        Assert.assertEquals( metadata.getETag(), TestTools.getMD5( filePath ) );
+        Assert.assertTrue(
+                metadata.getLastModified().getTime() / 1000 >= date.getTime()
+                        / 1000,
+                "metadata.getLastModified = "
+                        + metadata.getLastModified().getTime() + ",expDate = "
+                        + date.getTime() );
+        String downloadPath = TestTools.LocalFile.initDownloadPath( localPath,
+                TestTools.getMethodName(), Thread.currentThread().getId() );
+        ObjectUtils.inputStream2File( object.getObjectContent(), downloadPath );
+        Assert.assertEquals( TestTools.getMD5( downloadPath ),
+                TestTools.getMD5( filePath ), "filePath = " + filePath );
     }
 }
