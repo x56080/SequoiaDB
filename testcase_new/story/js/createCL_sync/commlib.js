87f7a544886ab2cb795671035d8fbed6e4f598ba
huanghaimei <huanghaimei@sequoiadb.com>
2022-10-26 10:54:53 +0800

    CI-943 【SDB】失败用例story_js.createCL.createCLNoTrans_26414

---
+++ testcase_new/story/js/createCL_sync/commlib.js
@@ -0,0 +1,9 @@
+/******************************************************************************
+ * @Description   : 
+ * @Author        : HuangHaimei
+ * @CreateTime    : 2022.10.26
+ * @LastEditTime  : 2022.10.26
+ * @LastEditors   : HuangHaimei
+ ******************************************************************************/
+import( "../lib/basic_operation/commlib.js" );
+import( "../lib/main.js" );
