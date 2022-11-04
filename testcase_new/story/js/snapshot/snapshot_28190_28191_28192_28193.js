0426f4d649e9c042b453730ed1bc9081b5706f9c
xumingxing <xumingxing@sequoiadb.com>
2022-11-04 20:48:17 +0800

    CI-1051 元数据添加创建时间、修改时间

--- testcase_new/story/js/snapshot/snapshot_28190_28191_28192_28193.js
+++ testcase_new/story/js/snapshot/snapshot_28190_28191_28192_28193.js
@@ -5,7 +5,7 @@
  *                  seqDB-28193:编目快照中新增创建/更新时间信息验证 
  * @Author        : Xu Mingxing
  * @CreateTime    : 2022.10.19
- * @LastEditTime  : 2022.10.26
+ * @LastEditTime  : 2022.11.04
  * @LastEditors   : Xu Mingxing
  ******************************************************************************/
 testConf.skipStandAlone = true;
@@ -20,13 +20,15 @@ function test ()
    var newCsName = "newCsName_28190";
    var groupName = commGetDataGroupNames( db )[0];
    var node = db.getRG( groupName ).getMaster();
+   var nodeName = node.getHostName() + ":" + node.getServiceName();
    var cond = { Name: csName + "." + clName, HostName: node.getHostName(), ServiceName: node.getServiceName() };
 
    // 1.创建CS和CL
-   commCreateCS( db, csName, true );
+   commDropCS( db, csName );
+   commCreateCS( db, csName );
    db.getCS( csName ).createCL( clName, { Group: groupName } );
    // 查看集合空间快照信息
-   var cursor1_1 = db.snapshot( SDB_SNAP_COLLECTIONSPACES, { Name: csName } );
+   var cursor1_1 = db.snapshot( SDB_SNAP_COLLECTIONSPACES, { Name: csName, NodeName: nodeName } );
    var createTime1_1 = cursor1_1.current().toObj()["CreateTime"];
    var updateTime1_1 = cursor1_1.current().toObj()["UpdateTime"];
    // 查看集合快照信息
@@ -46,7 +48,7 @@ function test ()
    // 2.更新CL属性信息 
    db.getCS( csName ).getCL( clName ).disableCompression();
    // 查看集合空间快照信息
-   var cursor1_2 = db.snapshot( SDB_SNAP_COLLECTIONSPACES, { Name: csName } );
+   var cursor1_2 = db.snapshot( SDB_SNAP_COLLECTIONSPACES, { Name: csName, NodeName: nodeName } );
    var createTime1_2 = cursor1_2.current().toObj()["CreateTime"];
    var updateTime1_2 = cursor1_2.current().toObj()["UpdateTime"];
    assert.equal( createTime1_2, createTime1_1, "expected createTime equal" );
@@ -76,7 +78,7 @@ function test ()
    // 3.修改CL名
    db.getCS( csName ).renameCL( clName, newClName );
    // 查看集合空间快照信息
-   var cursor1_3 = db.snapshot( SDB_SNAP_COLLECTIONSPACES, { Name: csName } );
+   var cursor1_3 = db.snapshot( SDB_SNAP_COLLECTIONSPACES, { Name: csName, NodeName: nodeName } );
    var createTime1_3 = cursor1_3.current().toObj()["CreateTime"];
    var updateTime1_3 = cursor1_3.current().toObj()["UpdateTime"];
    assert.equal( createTime1_3, createTime1_2, "expected createTime equal" );
@@ -107,7 +109,7 @@ function test ()
    // 4.修改CS名
    db.renameCS( csName, newCsName );
    // 查看集合空间快照信息
-   var cursor1_4 = db.snapshot( SDB_SNAP_COLLECTIONSPACES, { Name: newCsName } );
+   var cursor1_4 = db.snapshot( SDB_SNAP_COLLECTIONSPACES, { Name: newCsName, NodeName: nodeName } );
    var createTime1_4 = cursor1_4.current().toObj()["CreateTime"];
    var updateTime1_4 = cursor1_4.current().toObj()["UpdateTime"];
    assert.equal( createTime1_4, createTime1_3, "expected createTime equal" );
@@ -119,7 +121,7 @@ function test ()
    // 5.CS下新增CL
    db.getCS( newCsName ).createCL( clName1, { Group: groupName } );
    // 查看集合空间快照信息
-   var cursor1_5 = db.snapshot( SDB_SNAP_COLLECTIONSPACES, { Name: newCsName } );
+   var cursor1_5 = db.snapshot( SDB_SNAP_COLLECTIONSPACES, { Name: newCsName, NodeName: nodeName } );
    var createTime1_5 = cursor1_5.current().toObj()["CreateTime"];
    var updateTime1_5 = cursor1_5.current().toObj()["UpdateTime"];
    assert.equal( createTime1_5, createTime1_4, "expected createTime equal" );
@@ -140,7 +142,7 @@ function test ()
    // 6.删除CL
    db.getCS( newCsName ).dropCL( clName1 );
    // 查看集合空间快照信息
-   var cursor1_6 = db.snapshot( SDB_SNAP_COLLECTIONSPACES, { Name: newCsName } );
+   var cursor1_6 = db.snapshot( SDB_SNAP_COLLECTIONSPACES, { Name: newCsName, NodeName: nodeName } );
    var createTime1_6 = cursor1_6.current().toObj()["CreateTime"];
    var updateTime1_6 = cursor1_6.current().toObj()["UpdateTime"];
    assert.equal( createTime1_6, createTime1_5, "expected createTime equal" );
@@ -148,4 +150,7 @@ function test ()
    {
       throw new Error( "expected post-updateTime to be more than pre-updateTime" );
    }
+
+   commDropCS( db, csName );
+   commDropCS( db, newCsName );
 }
