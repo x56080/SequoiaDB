b350494fe7c3afb1cb261d636f2815df339b8ef0
huangyouquan <huangyouquan@sequoiadb.com>
2022-11-09 11:08:42 +0800

    SEQUOIADBMAINSTREAM-8902:rtnSyncDB funtion judgnebt condition modification

--- SequoiaDB/engine/rtn/rtn.cpp
+++ SequoiaDB/engine/rtn/rtn.cpp
@@ -2142,7 +2142,7 @@ namespace engine
       BOOLEAN syncSpecCS = FALSE ;
       UINT32 syncCSNum = 0 ;
 
-      if ( !dpsCB || !dpsCB )
+      if ( !dpsCB || !dmsCB )
       {
          /// do nothing
          goto done ;
