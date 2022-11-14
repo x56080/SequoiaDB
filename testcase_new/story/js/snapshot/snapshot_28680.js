720f88a39337116d8146d9db4383bd060de9303f
xumingxing <xumingxing@sequoiadb.com>
2022-11-14 09:11:16 +0800

    SEQUOIADBMAINSTREAM-8846 元数据添加创建时间、修改时间

---
+++ testcase_new/story/js/snapshot/snapshot_28680.js
@@ -0,0 +1,37 @@
+/******************************************************************************
+ * @Description   : seqDB-28680:检查回收站项目列表信息
+ * @Author        : Xu Mingxing
+ * @CreateTime    : 2022.11.09
+ * @LastEditTime  : 2022.11.14
+ * @LastEditors   : Xu Mingxing
+ ******************************************************************************/
+main( test );
+
+function test ()
+{
+   var csName = "csName_";
+   var clName = "clName_";
+   commDropCS( db, csName );
+   commCreateCS( db, csName );
+   db.getCS( csName ).createCL( clName );
+
+   // 删除CL
+   db.getCS( csName ).dropCL( clName );
+   // 查看回收站项目列表
+   var cursor = db.getRecycleBin().list( { OriginName: csName + "." + clName } );
+   var recycleName = cursor.current().toObj()["RecycleName"];
+   // 恢复集合CL
+   var returnName = db.getRecycleBin().returnItem( recycleName ).toObj()["ReturnName"];
+   assert.equal( returnName, csName + "." + clName );
+
+   // 删除CS 
+   db.dropCS( csName );
+   // 查看回收站项目列表
+   var cursor = db.getRecycleBin().list( { OriginName: csName } );
+   var recycleName = cursor.current().toObj()["RecycleName"];
+   // 恢复集合空间CS
+   var returnName = db.getRecycleBin().returnItem( recycleName ).toObj()["ReturnName"];
+   assert.equal( returnName, csName );
+
+   commDropCS( db, csName );
+}
