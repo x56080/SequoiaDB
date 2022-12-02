69efa8e868328c3659a39da5c386e619715a46dd
liuli <liuli@sequoiadb.com>
2022-12-02 11:37:40 +0800

    CI-1188:修改用例PreferedPeriod大小，修复失败用例

--- testcase_new/story/js/sessionAccess_sync/sessionAccess_22075.js
+++ testcase_new/story/js/sessionAccess_sync/sessionAccess_22075.js
@@ -34,18 +34,18 @@ function test ( testPara )
       throw new Error( "The expAccessNodes do not include the node: " + actAccessNode );
    }
 
-   //preferedPeriod为2
-   db.setSessionAttr( { PreferedPeriod: 2 } )
+   //preferedPeriod为10
+   db.setSessionAttr( { PreferedPeriod: 10 } )
    insertData( testPara.testCL );
 
    actAccessNode = testPara.testCL.find().explain().current().toObj().NodeName;
    if( actAccessNode !== primaryNode )
    {
-      throw new Error( "The expected result is " + expAccessNode + ", but the actual result is " + primaryNode );
+      throw new Error( "The expected result is " + expAccessNodes + ", but the actual result is " + primaryNode );
    }
 
-   //2s后检查会话访问节点为备节点
-   sleep( 2000 );
+   //10s后检查会话访问节点为备节点
+   sleep( 10000 );
 
    actAccessNode = testPara.testCL.find().explain().current().toObj().NodeName;
    if( expAccessNodes.indexOf( actAccessNode ) === -1 )
@@ -60,7 +60,7 @@ function test ( testPara )
    actAccessNode = testPara.testCL.find().explain().current().toObj().NodeName;
    if( actAccessNode !== primaryNode )
    {
-      throw new Error( "The expected result is " + expAccessNode + ", but the actual result is " + primaryNode );
+      throw new Error( "The expected result is " + expAccessNodes + ", but the actual result is " + primaryNode );
    }
 
    //2s后检查会话访问节点还为主节点
@@ -69,6 +69,6 @@ function test ( testPara )
    actAccessNode = testPara.testCL.find().explain().current().toObj().NodeName;
    if( actAccessNode !== primaryNode )
    {
-      throw new Error( "The expected result is " + expAccessNode + ", but the actual result is " + primaryNode );
+      throw new Error( "The expected result is " + expAccessNodes + ", but the actual result is " + primaryNode );
    }
 }
