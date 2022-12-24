63c40a4a955db877f4c761aebdc67600d82b7a52
youjiangfeng <youjiangfeng@sequoiadb.com>
2022-12-24 11:03:41 +0800

    SEQUOIADBMAINSTREAM-8814 get back error info from coord

--- SequoiaDB/engine/rtn/rtnBackup.cpp
+++ SequoiaDB/engine/rtn/rtnBackup.cpp
@@ -148,7 +148,7 @@ namespace engine
       if ( UTIL_COMPRESSOR_INVALID == compType ||
            UTIL_COMPRESSOR_LZW == compType )
       {
-         PD_LOG( PDERROR, "Field[%s]'s value[%s] is invalid, only support: %s",
+         PD_LOG_MSG( PDERROR, "Field[%s]'s value[%s] is invalid, only support: %s",
                  FIELD_NAME_COMPRESSIONTYPE, pCompType,
                  "snappy/lz4/zlib" ) ;
          rc = SDB_INVALIDARG ;
