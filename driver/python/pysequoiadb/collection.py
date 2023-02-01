1c51643e6dc0c01cb5ff7308d0fb7a8c2f2f8f40
yangqincheng <yangqincheng@sequoiadb.com>
2023-02-01 19:20:41 +0800

    SEQUOIADBMAINSTREAM-6241 修正 query 接口的 flag 参数的语法错误

--- driver/python/pysequoiadb/collection.py
+++ driver/python/pysequoiadb/collection.py
@@ -624,17 +624,17 @@ class collection(object):
            pysequoiadb.error.SDBBaseError
         Info:
            query flags:
-           QUERY_FLG_FORCE_HINT      : Force to use specified hint to query, if database have no index assigned by the hint, fail to query
-           QUERY_FLG_PARALLED        : Enable parallel sub query, each sub query will finish scanning different part of the data
-           QUERY_FLG_WITH_RETURNDATA : In general, query won't return data until cursor gets from database, when add this flag, return data in query response, it will be more high-performance
-           QUERY_PREPARE_MORE        : Enable prepare more data when query
+           QUERY_FLG_FORCE_HINT      : Force to use specified hint to query, if database has no index assigned by the hint, fail to query
+           QUERY_FLG_PARALLED        : Enable parallel sub-query, each sub-query will finish scanning different part of data
+           QUERY_FLG_WITH_RETURNDATA : In general, query won't return data until cursor gets the record from database, when adding this flag, return data in query response, it will be more high-performance
+           QUERY_PREPARE_MORE        : Enable prepare more data when querying
            QUERY_FLG_FOR_UPDATE      : Acquire U lock on the records that are read. When the session is in
-                                       transaction and setting this flag, the transaction lock will not released
-                                       until the transaction is committed or rollback. When the session is not
+                                       transaction and setting this flag, the transaction lock will not be released
+                                       will not released. When the session is not
                                        in transaction, the flag does not work.
            QUERY_FLG_FOR_SHARE       : Acquire S lock on the records that are read. When the session is in
-                                       transaction and setting this flag, the transaction lock will not released
-                                       until the transaction is committed or rollback. When the session is not
+                                       transaction and setting this flag, the transaction lock will not be released
+                                       until the transaction is committed or rollbacked. When the session is not
                                        in transaction, the flag does not work.
         """
 
@@ -725,21 +725,21 @@ class collection(object):
         Info:
            query flags:
            QUERY_FLG_FORCE_HINT                 : Force to use specified hint to query, if database have
-                                                        no index assigned by the hint, fail to query
-           QUERY_FLG_PARALLED                   : Enable parallel sub query, each sub query will finish scanning
-                                                        different part of the data
+                                                        no index assigned by the hint, failed to query
+           QUERY_FLG_PARALLED                   : Enable parallel sub-query, each sub-query will finish scanning
+                                                        different part of data
            QUERY_FLG_WITH_RETURNDATA            : In general, query won't return data until cursor gets from
-                                                        database, when add this flag, return data in query response,
+                                                        database, when adding this flag, return data in query response,
                                                         it will be more high-performance
            QUERY_FLG_KEEP_SHARDINGKEY_IN_UPDATE : The sharding key in update rule is not filtered, when executing
                                                         queryAndUpdate.
            QUERY_FLG_FOR_UPDATE      : Acquire U lock on the records that are read. When the session is in
-                                       transaction and setting this flag, the transaction lock will not released
-                                       until the transaction is committed or rollback. When the session is not
+                                       transaction and setting this flag, the transaction lock will not be released
+                                       until the transaction is committed or rollbacked. When the session is not
                                        in transaction, the flag does not work.
            QUERY_FLG_FOR_SHARE       : Acquire S lock on the records that are read. When the session is in
-                                       transaction and setting this flag, the transaction lock will not released
-                                       until the transaction is committed or rollback. When the session is not
+                                       transaction and setting this flag, the transaction lock will not be released
+                                       until the transaction is committed or rollbacked. When the session is not
                                        in transaction, the flag does not work.
         """
 
@@ -847,16 +847,16 @@ class collection(object):
            pysequoiadb.error.SDBBaseError
         Info:
            query flags:
-           QUERY_FLG_FORCE_HINT      : Force to use specified hint to query, if database have no index assigned by the hint, fail to query
-           QUERY_FLG_PARALLED        : Enable parallel sub query, each sub query will finish scanning different part of the data
-           QUERY_FLG_WITH_RETURNDATA : In general, query won't return data until cursor gets from database, when add this flag, return data in query response, it will be more high-performance
+           QUERY_FLG_FORCE_HINT      : Force to use specified hint to query, if database has no index assigned by the hint, fail to query
+           QUERY_FLG_PARALLED        : Enable parallel sub-query, each sub-query will finish scanning different part of data
+           QUERY_FLG_WITH_RETURNDATA : In general, query won't return data until cursor gets the record from database, when adding this flag, return data in query response, it will be more high-performance
            QUERY_FLG_FOR_UPDATE      : Acquire U lock on the records that are read. When the session is in
-                                       transaction and setting this flag, the transaction lock will not released
-                                       until the transaction is committed or rollback. When the session is not
+                                       transaction and setting this flag, the transaction lock will not be released
+                                       until the transaction is committed or rollbacked. When the session is not
                                        in transaction, the flag does not work.
            QUERY_FLG_FOR_SHARE       : Acquire S lock on the records that are read. When the session is in
-                                       transaction and setting this flag, the transaction lock will not released
-                                       until the transaction is committed or rollback. When the session is not
+                                       transaction and setting this flag, the transaction lock will not be released
+                                       until the transaction is committed or rollbacked. When the session is not
                                        in transaction, the flag does not work.
         """
 
@@ -1542,17 +1542,17 @@ class collection(object):
            pysequoiadb.error.SDBBaseError
         Info:
            query flags:
-           QUERY_FLG_FORCE_HINT      : Force to use specified hint to query, if database have no index assigned by the hint, fail to query
-           QUERY_FLG_PARALLED        : Enable parallel sub query, each sub query will finish scanning different part of the data
-           QUERY_FLG_WITH_RETURNDATA : In general, query won't return data until cursor gets from database, when add this flag, return data in query response, it will be more high-performance
-           QUERY_PREPARE_MORE        : Enable prepare more data when query
+           QUERY_FLG_FORCE_HINT      : Force to use specified hint to query, if database has no index assigned by the hint, fail to query
+           QUERY_FLG_PARALLED        : Enable parallel sub-query, each sub-query will finish scanning different part of data
+           QUERY_FLG_WITH_RETURNDATA : In general, query won't return data until cursor gets the record from database, when adding this flag, return data in query response, it will be more high-performance
+           QUERY_PREPARE_MORE        : Enable prepare more data when querying
            QUERY_FLG_FOR_UPDATE      : Acquire U lock on the records that are read. When the session is in
-                                       transaction and setting this flag, the transaction lock will not released
-                                       until the transaction is committed or rollback. When the session is not
+                                       transaction and setting this flag, the transaction lock will not be released
+                                       until the transaction is committed or rollbacked. When the session is not
                                        in transaction, the flag does not work.
            QUERY_FLG_FOR_SHARE       : Acquire S lock on the records that are read. When the session is in
-                                       transaction and setting this flag, the transaction lock will not released
-                                       until the transaction is committed or rollback. When the session is not
+                                       transaction and setting this flag, the transaction lock will not be released
+                                       until the transaction is committed or rollbacked. When the session is not
                                        in transaction, the flag does not work.
         """
         bson_condition = None
@@ -1643,9 +1643,9 @@ class collection(object):
            pysequoiadb.error.SDBBaseError
         Info:
            query flags:
-           QUERY_FLG_FORCE_HINT      : Force to use specified hint to query, if database have no index assigned by the hint, fail to query
-           QUERY_FLG_PARALLED        : Enable parallel sub query, each sub query will finish scanning different part of the data
-           QUERY_FLG_WITH_RETURNDATA : In general, query won't return data until cursor gets from database, when add this flag, return data in query response, it will be more high-performance
+           QUERY_FLG_FORCE_HINT      : Force to use specified hint to query, if database has no index assigned by the hint, fail to query
+           QUERY_FLG_PARALLED        : Enable parallel sub-query, each sub-query will finish scanning different part of data
+           QUERY_FLG_WITH_RETURNDATA : In general, query won't return data until cursor gets the record from database, when adding this flag, return data in query response, it will be more high-performance
         """
 
         bson_condition = None
