0e4d9fdb5c951bdf91889a3c88e4b43157d76de8
yangqincheng <yangqincheng@sequoiadb.com>
2023-02-09 15:26:45 +0800

    SEQUOIADBMAINSTREAM-9118 split 的 cleanup 阶段忽略 -23 -34 错误

--- SequoiaDB/engine/cls/clsCleanupJob.cpp
+++ SequoiaDB/engine/cls/clsCleanupJob.cpp
@@ -301,6 +301,10 @@ namespace engine
       {
          rc = _cleanByTBSCan( w, _cleanupType() ) ;
       }
+      if ( SDB_DMS_CS_NOTEXIST == rc || SDB_DMS_NOTEXIST == rc )
+      {
+         rc = SDB_OK ;
+      }
 
       if ( SDB_OK != rc )
       {
@@ -309,10 +313,15 @@ namespace engine
       }
 
       rc = _cleanLobData( w ) ;
+      if ( SDB_DMS_CS_NOTEXIST == rc || SDB_DMS_NOTEXIST == rc )
+      {
+         rc = SDB_OK ;
+      }
       if ( SDB_OK != rc )
       {
          PD_LOG( PDERROR, "failed to clean lob data:%d", rc ) ;
       }
+
 
    done:
       eduCB()->writingDB( FALSE ) ;
