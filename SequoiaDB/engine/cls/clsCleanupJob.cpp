ba37e19c174b0aa66e2a9e40ed4cf4796fae7983
yangqincheng <yangqincheng@sequoiadb.com>
2023-02-14 21:23:54 +0800

    SEQUOIADBMAINSTREAM-9118 增加注释

--- SequoiaDB/engine/cls/clsCleanupJob.cpp
+++ SequoiaDB/engine/cls/clsCleanupJob.cpp
@@ -303,6 +303,7 @@ namespace engine
       {
          rc = _cleanByTBSCan( w, _cleanupType() ) ;
       }
+      // The cs and cl may be dropped by other jobs, so errors -23 and -34 are ignored
       if ( SDB_DMS_CS_NOTEXIST == rc || SDB_DMS_NOTEXIST == rc )
       {
          rc = SDB_OK ;
@@ -315,6 +316,7 @@ namespace engine
       }
 
       rc = _cleanLobData( w ) ;
+      // The cs and cl may be dropped by other jobs, so errors -23 and -34 are ignored
       if ( SDB_DMS_CS_NOTEXIST == rc || SDB_DMS_NOTEXIST == rc )
       {
          rc = SDB_OK ;
