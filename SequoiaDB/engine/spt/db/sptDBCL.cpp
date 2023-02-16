38044d4ad7502498074aa695c9038e8ad743c9c8
liuyuchen <liuyuchen@sequoiadb.com>
2023-02-16 14:46:35 +0800

    SEQUOIADBMAINSTREAM-9154 fix warnings

--- SequoiaDB/engine/spt/db/sptDBCL.cpp
+++ SequoiaDB/engine/spt/db/sptDBCL.cpp
@@ -493,7 +493,7 @@ namespace engine
          }
 
          // ContOnDupID
-         elem = elem = options.getField( FIELD_NAME_CONTONDUP_ID ) ;
+         elem = options.getField( FIELD_NAME_CONTONDUP_ID ) ;
          value = ( elem.eoo() || Bool != elem.type() ) ? FALSE : elem.Bool() ;
          if ( value )
          {
@@ -501,7 +501,7 @@ namespace engine
          }
 
          // ReplaceOnDupID
-         elem = elem = options.getField( FIELD_NAME_REPLACEONDUP_ID ) ;
+         elem = options.getField( FIELD_NAME_REPLACEONDUP_ID ) ;
          value = ( elem.eoo() || Bool != elem.type() ) ? FALSE : elem.Bool() ;
          if ( value )
          {
