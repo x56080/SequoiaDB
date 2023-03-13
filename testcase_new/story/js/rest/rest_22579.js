72d0463d285d860dfca54fe07e574050fd920eea
chengjingjing <162-chengjingjing@users.noreply.gitlab.example.com>
2023-03-13 15:24:53 +0800

    SEQUOIADBMAINSTREAM-9145 insert新增flag以及option添加自动化用例

--- testcase_new/story/js/rest/rest_22579.js
+++ testcase_new/story/js/rest/rest_22579.js
@@ -29,6 +29,16 @@ function test ()
    checkFlagType( restcl, COMMCSNAME, clName, 44, "user4", "SDB_INSERT_RETURNNUM", [{ 'age': 44, 'name': 'user4' }], -38 );
    checkFlagType( restcl, COMMCSNAME, clName, 44, "user44", "SDB_INSERT_RETURNNUM", [{ 'age': 44, 'name': 'user44' }], 0 );
 
+   restcl.truncate()
+   restcl.insert( { '_id': 1, 'age': 1, 'name': 'user1' }, { '_id': 2, 'age': 2, 'name': 'user2' } );
+   //REST接口flag参数兼容SDB_INSERT_CONTONDUP_ID,当发生_id键冲突时，跳过该记录并插入其他记录
+   checkFlagType( restcl, COMMCSNAME, clName, 11, "user11", "SDB_INSERT_CONTONDUP_ID", [{ '_id': 1, 'age': 1, 'name': 'user1' }], 0, 1 );
+   checkFlagType( restcl, COMMCSNAME, clName, 11, "user11", "SDB_INSERT_CONTONDUP_ID", [{ '_id': 11, 'age': 11, 'name': 'user11' }], 0, 11 );
+
+   //REST接口flag参数兼容SDB_INSERT_REPLACEONDUP_ID,当发生_id冲突时，将已存在的记录更新为待插入的新记录(替换)，并继续插入其他记录
+   checkFlagType( restcl, COMMCSNAME, clName, 22, "user22", "SDB_INSERT_REPLACEONDUP_ID", [{ '_id': 2, 'age': 22, 'name': 'user22' }], 0, 2 );
+   checkFlagType( restcl, COMMCSNAME, clName, 33, "user33", "SDB_INSERT_REPLACEONDUP_ID", [{ '_id': 3, 'age': 33, 'name': 'user33' }], 0, 3 );
+
    commDropCL( db, COMMCSNAME, clName );
 }
 
@@ -42,18 +52,22 @@ function sendRequest ( cmd, content )
    return rcObj;
 }
 
-function checkFlagType ( cl, COMMCSNAME, clName, userage, username, flagType, expData, expError )
+function checkFlagType ( cl, COMMCSNAME, clName, userage, username, flagType, expData, expError, id )
 {
    if( expError === undefined ) { expError = null; }
    if( expData === undefined ) { expData = []; }
    var cmd = new Cmd();
    var data = "{'name':'" + username + "','age': " + userage + "}";
+   if( id != undefined )
+   {
+      data = "{'_id': " + id + ",'name': '" + username + "','age': " + userage + "}";
+   }
    var content = "cmd=insert&name=" + COMMCSNAME + "." + clName + "&insertor=" + data + "&flag=" + flagType;
    var rc = sendRequest( cmd, content );
    var errno = rc[0]["errno"];
    if( expError != errno )
    {
-      throw new Error( " check flag error ！" );
+      throw new Error( " check flag error! actual errno is " + errno );
    }
    else
    {
@@ -72,8 +86,16 @@ function checkFlagType ( cl, COMMCSNAME, clName, userage, username, flagType, ex
             }
             break;
          case 0:
-            var cur = cl.find( { 'name': username } );
-            commCompareResults( cur, expData );
+            if( id == undefined )
+            {
+               var cur = cl.find( { 'name': username } );
+               commCompareResults( cur, expData );
+            }
+            if( id != undefined )
+            {
+               cur = cl.find( { '_id': id } );
+               commCompareResults( cur, expData, false );
+            }
             //校验兼容SDB_INSERT_RETURNNUM带回单条插入成功的返回值{ "InsertedNum": 1, "DuplicatedNum": 0 }
             if( flagType == "SDB_INSERT_RETURNNUM" )
             {
@@ -90,9 +112,3 @@ function checkFlagType ( cl, COMMCSNAME, clName, userage, username, flagType, ex
       }
    }
 }
-
-
-
-
-
-
