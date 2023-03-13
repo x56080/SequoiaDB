80776f529bcc28fe813247793935db266f6a4d83
chengjingjing <162-chengjingjing@users.noreply.gitlab.example.com>
2023-03-13 14:49:32 +0800

    SEQUOIADBMAINSTREAM-9145 insert新增flag以及option添加自动化用例

--- testcase_new/driver/cpp/crud/bulkInsert12540_12666.cpp
+++ testcase_new/driver/cpp/crud/bulkInsert12540_12666.cpp
@@ -23,7 +23,7 @@ protected:
    const CHAR* clName ;
    sdbCollectionSpace cs ;
    sdbCollection cl ;
-   
+
    void SetUp()
    {
       testBase::SetUp() ;
@@ -40,7 +40,7 @@ protected:
       {
          INT32 rc = db.dropCollectionSpace( csName ) ;
          ASSERT_EQ( SDB_OK, rc ) << "fail to drop cs " << csName ;
-      } 
+      }
       testBase::TearDown() ;
    }
 } ;
@@ -66,7 +66,7 @@ TEST_F( bulkInsertTest12540, bulkInsert12540 )
    ASSERT_EQ( SDB_OK, rc ) << "fail to get count" ;
    ASSERT_EQ( 2, count ) << "fail to check count" ;
 
-   // query doc with _id, check _id 
+   // query doc with _id, check _id
    BSONObj cond = BSON( "a" << 1 ) ;
    sdbCursor cursor ;
    rc = cl.query( cursor, cond ) ;
@@ -94,6 +94,67 @@ TEST_F( bulkInsertTest12540, bulkInsert12540 )
    ASSERT_EQ( SDB_OK, rc ) << "fail to drop cl " << clName ;
 }
 
+TEST_F( bulkInsertTest12540, bulkInsert12540_1 )
+{
+   INT32 rc = SDB_OK ;
+
+   vector<BSONObj> docs ;
+   BSONObj doc1 = BSON( "_id" << 1 << "a" << 1 ) ;
+   BSONObj doc2 = BSON( "_id" << 2 << "a" << 2 ) ;
+   BSONObj doc3 = BSON( "_id" << 1 << "a" << 3 ) ;
+   docs.push_back( doc1 ) ;
+   docs.push_back( doc2 ) ;
+   docs.push_back( doc3 ) ;
+
+   // test SDB_INSERT_CONTONDUP_ID
+   rc = cl.bulkInsert( FLG_INSERT_CONTONDUP_ID, docs ) ;
+   ASSERT_EQ( SDB_OK, rc ) << "fail to bulkInsert docs to cl " << clName ;
+
+   // check docs num
+   SINT64 count ;
+   rc = cl.getCount( count ) ;
+   ASSERT_EQ( SDB_OK, rc ) << "fail to get count" ;
+   ASSERT_EQ( 2, count ) << "fail to check count" ;
+
+   // query doc with _id, check _id
+   BSONObj cond = BSON( "_id" << 1 ) ;
+   sdbCursor cursor ;
+   rc = cl.query( cursor, cond ) ;
+   ASSERT_EQ( SDB_OK, rc ) << "fail to query doc" ;
+   BSONObj obj ;
+   rc = cursor.next( obj ) ;
+   ASSERT_EQ( SDB_OK, rc ) << "fail to get next" ;
+   ASSERT_EQ( 1, obj.getField( "a" ).Int() ) << "fail to check _id" ;
+   rc = cursor.close() ;
+   ASSERT_EQ( SDB_OK, rc ) << "fail to close cursor" ;
+
+   // test SDB_INSERT_REPLACEONDUP_ID
+   rc = cl.truncate() ;
+   ASSERT_EQ( SDB_OK, rc ) << "fail to truncate" ;
+   rc = cl.bulkInsert( FLG_INSERT_REPLACEONDUP_ID, docs ) ;
+   ASSERT_EQ( SDB_OK, rc ) << "fail to bulkInsert docs to cl " << clName ;
+
+   // check docs num
+   rc = cl.getCount( count ) ;
+   ASSERT_EQ( SDB_OK, rc ) << "fail to get count" ;
+   ASSERT_EQ( 2, count ) << "fail to check count" ;
+
+   // query doc with _id, check _id
+   sdbCursor cursor1 ;
+   rc = cl.query( cursor, cond ) ;
+   ASSERT_EQ( SDB_OK, rc ) << "fail to query doc" ;
+   rc = cursor.next( obj ) ;
+   ASSERT_EQ( SDB_OK, rc ) << "fail to get next" ;
+   ASSERT_EQ( 3, obj.getField( "a" ).Int() ) << "fail to check _id" ;
+   rc = cursor1.close() ;
+   ASSERT_EQ( SDB_OK, rc ) << "fail to close cursor" ;
+
+   // drop cl
+   rc = cs.dropCollection( clName ) ;
+   ASSERT_EQ( SDB_OK, rc ) << "fail to drop cl " << clName ;
+}
+
+
 TEST_F( bulkInsertTest12540, bulkInsert12666 )
 {
    INT32 rc = SDB_OK ;
@@ -101,11 +162,11 @@ TEST_F( bulkInsertTest12540, bulkInsert12666 )
    vector<BSONObj> docs ;
    rc = cl.bulkInsert( 0, docs ) ;
    ASSERT_EQ( SDB_OK, rc ) << "fail to bulkInsert empty docs" ;
-   
+
    SINT64 count ;
    rc = cl.getCount( count ) ;
    ASSERT_EQ( 0, count ) << "fail to check count" ;
 
    rc = cs.dropCollection( clName ) ;
-   ASSERT_EQ( SDB_OK, rc ) << "fail to drop cl " << clName ; 
+   ASSERT_EQ( SDB_OK, rc ) << "fail to drop cl " << clName ;
 }
