afc2064bf868e19ca4c6ebe30c5daf2ca08fe7f7
chengjingjing <162-chengjingjing@users.noreply.gitlab.example.com>
2023-03-16 22:21:51 +0800

    SEQUOIADBMAINSTREAM-9017 消息添加唯一ID补充自动化用例

---
+++ testcase_new/story/js/snapshot_sync/snapshot_29397.js
@@ -0,0 +1,98 @@
+/******************************************************************************
+* @Description   : seqDB-29397:相同线程QueryID递增，不同线程QueryID不同
+* @Author        : Cheng Jingjing
+* @CreateTime    : 2023.03.15
+* @LastEditTime  : 2023.03.15
+* @LastEditors   : Cheng Jingjing
+******************************************************************************/
+testConf.clName = COMMCLNAME + "_csName_29397"
+testConf.skipStandAlone = true;
+testConf.useSrcGroup = true;
+
+main( test );
+function test ( testPara )
+{
+   var coord = commGetGroups( db, true, "SYSCoord", true, false, true )[0][1];
+   var hostname = coord.HostName;
+   var svcname = coord.svcname;
+   var sdb = new Sdb( hostname, svcname );
+   try
+   {
+      // updateconf
+      sdb.updateConf( { mongroupmask: "all:basic", monslowquerythreshold: 0 } );
+
+      //insert
+      insertRecs( testPara.testCL );
+
+      // SDB_SNAP_QUERY
+      var ret = sdb.snapshot( SDB_SNAP_QUERIES );
+      var queryID = ret.current().toObj()["QueryID"];
+      ret = sdb.snapshot( SDB_SNAP_QUERIES, { QueryID: queryID } );
+      if( !ret.next() )
+      {
+         throw new Error( "Invalid QueryID in query snapshot. It must be " + queryID );
+      }
+      // SDB_SNAP_CONTEXTS
+      queryID = getNewQueryID( queryID );
+      ret = sdb.snapshot( SDB_SNAP_CONTEXTS, { "$and": [{ "Contexts.QueryID": queryID }, { "Contexts.Type": "COORD" }] } );
+      //print( JSON.parse(ret.toString()) );
+      if( !ret.next() )
+      {
+         throw new Error( "Invalid QueryID in contexts snapshot. It must be " + queryID );
+      }
+      // change to a new connection
+      var oldQueryIDPrefixStr = queryID.substr( 0, 18 );
+
+      // catalog
+      var catalog = sdb.getCataRG().getMaster().connect();
+      ret = catalog.snapshot( SDB_SNAP_QUERIES );
+      queryID = ret.current().toObj()["QueryID"];
+      newQueryIDPrefixStr = queryID.substr( 0, 18 );
+      assert.notEqual( newQueryIDPrefixStr, oldQueryIDPrefixStr );
+      catalog.close();
+
+      // data
+      var data = sdb.getRG( testPara.srcGroupName ).getMaster().connect();
+      ret = data.snapshot( SDB_SNAP_QUERIES );
+      queryID = ret.current().toObj()["QueryID"];
+      var newQueryIDPrefixStr = queryID.substr( 0, 18 );
+      assert.notEqual( newQueryIDPrefixStr, oldQueryIDPrefixStr );
+      data.close();
+   }
+   finally
+   {
+      sdb.deleteConf( { mongroupmask: "all:basic", monslowquerythreshold: 0 } );
+      sdb.close();
+   }
+}
+
+function getNewQueryID ( curQueryID )
+{
+   var queryIDPrefixStr = curQueryID.substr( 0, 18 );
+   var sequence = parseInt( curQueryID.substr( 18, 8 ), 16 );
+   var newQueryID = queryIDPrefixStr + numToHexStr( ++sequence, 8 );
+   return newQueryID;
+}
+
+function numToHexStr ( num, totalStrlen )
+{
+   var numHexStr = num.toString( 16 );
+   if( totalStrlen < numHexStr.length )
+   {
+      throw new Error( "Invalid total str len" );
+   }
+   else if( totalStrlen == numHexStr.length )
+   {
+      return numHexStr;
+   }
+   else
+   {
+      var ret = "";
+      for( var i = 0; i < totalStrlen - numHexStr.length; i++ )
+      {
+         ret += "0";
+      }
+      ret += numHexStr;
+      return ret;
+   }
+}
