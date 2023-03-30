e55a5deae989415acd534bd480d511deda0c97a8
liuyuchen <liuyuchen@sequoiadb.com>
2023-03-30 16:50:46 +0800

    SEQUOIADBMAINSTREAM-9304 捕获replay线程抛出的异常

--- SequoiaDB/engine/cls/clsReplBucket.cpp
+++ SequoiaDB/engine/cls/clsReplBucket.cpp
@@ -1239,7 +1239,17 @@ namespace engine
                   ( CLS_PARALLA_REC == info._parallaType ) ? FALSE : TRUE ;
 
    retry:
-      rc = _replayer->replay( header, cb, FALSE, ignoreDupKey, &info._dataExInfo ) ;
+      try
+      {
+         rc = _replayer->replay( header, cb, FALSE, ignoreDupKey, &info._dataExInfo ) ;
+      }
+      catch( std::exception &e )
+      {
+         rc = ossException2RC( &e ) ;
+         PD_LOG( PDERROR, "Failed to replay lsn [%llu] because of exception:%s",
+                 header->_lsn, e.what() ) ;
+      }
+      
       if ( rc )
       {
          PD_LOG( PDERROR, "Replay LSN [%llu] encountered error: %d",
