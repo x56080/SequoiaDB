2a913b91396fe3fbfa8dc0888920954e96295f07
yangqincheng <yangqincheng@sequoiadb.com>
2023-04-11 10:08:03 +0800

    SEQUOIADBMAINSTREAM-9120 同步 java7

--- driver/java7/src/main/java/com/sequoiadb/datasource/SequoiadbDatasource.java
+++ driver/java7/src/main/java/com/sequoiadb/datasource/SequoiadbDatasource.java
@@ -206,9 +206,10 @@ public class SequoiadbDatasource {
                     log.debug(String.format("Clean unnecessary idle connections, minIdleCount: %d, maxIdleCount: %d, " +
                             "unnecessary connections: %d", _dsOpt.getMinIdleCount(), _dsOpt.getMaxIdleCount(), unnecessaryCount));
                 }
-                // when the number of idle connections in the pool is less than the minIdleCount,
-                // we are going to create some connections
-                if (_idleConnPool.count() < _dsOpt.getMinIdleCount()) {
+                // when the number of idle connections in the pool is less than the average of minIdleCount
+                // and maxIdleCount, we are going to create some connections
+                int avgCount = (_dsOpt.getMinIdleCount() + _dsOpt.getMaxIdleCount()) / 2;
+                if (_idleConnPool.count() < avgCount) {
                     synchronized (_createConnSignal) {
                         _createConnSignal.notify();
                     }
@@ -1257,7 +1258,6 @@ public class SequoiadbDatasource {
         if (newOpt == null) {
             throw new BaseException(SDBError.SDB_INVALIDARG, "the offering datasource options can't be null");
         }
-
         int deltaIncCount = newOpt.getDeltaIncCount();
         int maxIdleCount = newOpt.getMaxIdleCount();
         int minIdleCount = newOpt.getMinIdleCount();
@@ -1434,8 +1434,9 @@ public class SequoiadbDatasource {
         log.info("Begin create connections task");
         int createNum = _dsOpt.getDeltaIncCount() ;
         int count = 0;
+        int avgCount = (_dsOpt.getMinIdleCount() + _dsOpt.getMaxIdleCount()) / 2;
 
-        while (createNum > 0 && _idleConnPool.count() < _dsOpt.getMinIdleCount()) {
+        while (createNum > 0 && _idleConnPool.count() < avgCount) {
             // never let "sdb" defined out of current scope
             Sequoiadb sdb = null;
             String addr = null;
