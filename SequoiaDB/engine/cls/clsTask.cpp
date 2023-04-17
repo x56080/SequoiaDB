c2e11edd8dd3271e35a7f2669b098cb5431c4533
tangtao <tangtao@sequoiadb.com>
2023-04-17 19:32:24 +0800

    SEQUOIADBMAINSTREAM-9430 earse groupInfo after accessing

--- SequoiaDB/engine/cls/clsTask.cpp
+++ SequoiaDB/engine/cls/clsTask.cpp
@@ -2933,9 +2933,9 @@ namespace engine
             }
             _decTotalGroups() ;
 
-            _mapGroupInfo.erase( itGroup ) ;
             _changedMask |= CLS_IDX_MASK_PULL_GROUP ;
             _pullGroupName = curGroupInfo.groupName ;
+            _mapGroupInfo.erase( itGroup ) ;
          }
          else
          {
