4484b02a12689c3ee63ab8cba38f03836a9a55f1
liuyuchen <liuyuchen@sequoiadb.com>
2023-04-17 13:24:31 +0800

    SEQUOIADBMAINSTREAM-9279 选择非running状态节点作为全量同步源

--- SequoiaDB/engine/cls/clsSyncManager.cpp
+++ SequoiaDB/engine/cls/clsSyncManager.cpp
@@ -651,8 +651,7 @@ namespace engine
          }
          else if ( 0 == itr->second->beat.endLsn.offset ||
                    0 != itr->second->beat.getFTConfirmStat() ||
-                   SERVICE_NORMAL != itr->second->beat.serviceStatus ||
-                   CLS_NODE_RUNNING != itr->second->beat.nodeRunStat )
+                   SERVICE_NORMAL != itr->second->beat.serviceStatus )
          {
             continue ;
          }
