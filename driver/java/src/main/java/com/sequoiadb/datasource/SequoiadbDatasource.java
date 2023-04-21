ac77f412a4c067b0b145f8917b516b5f6eb2b7af
tanzhaobo <48-tanzhaobo@users.noreply.gitlab.example.com>
2023-04-21 12:09:09 +0800

    SEQUOIADBMAINSTREAM-8919: remove ShutdownHook in java connection pool

--- driver/java/src/main/java/com/sequoiadb/datasource/SequoiadbDatasource.java
+++ driver/java/src/main/java/com/sequoiadb/datasource/SequoiadbDatasource.java
@@ -91,18 +91,6 @@ public class SequoiadbDatasource {
         }
     };
 
-    /// when client program finish running,
-    /// this task will be executed
-    class ExitClearUpTask extends Thread {
-        public void run() {
-            try {
-                close();
-            } catch (Exception e) {
-                // do nothing
-            }
-        }
-    }
-
     class CreateConnectionTask implements Runnable {
         public void run() {
             try {
@@ -999,8 +987,6 @@ public class SequoiadbDatasource {
         }
         log.info(String.format("Sequoiadb datasource initialized successfully, status is %s, %s, %s",
                 _isDatasourceOn? "enable" : "disable", _dsOpt.toString(), _userNwOpt.toString()));
-        // set a hook for closing all the connection, when object is destroyed
-        Runtime.getRuntime().addShutdownHook(new ExitClearUpTask());
     }
 
     private void _startTimer() {
