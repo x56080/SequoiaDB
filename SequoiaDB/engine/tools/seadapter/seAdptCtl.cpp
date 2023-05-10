04d9c777f1fe1617c5c995364b6b5bf9d028933c
jiangrongkang <jiangrongkang@sequoiadb.com>
2023-05-10 16:51:48 +0800

    SEQUOIADBMAINSTREAM-9484:修复 sdbstart 无法获取子进程退出码

--- SequoiaDB/engine/tools/seadapter/seAdptCtl.cpp
+++ SequoiaDB/engine/tools/seadapter/seAdptCtl.cpp
@@ -441,7 +441,7 @@ namespace engine
 
          // run command
          tmpRC = nodeRunner->exec( command.c_str(), exitCode,
-                                   TRUE, -1, TRUE, &nodeHandle ) ;
+                                   TRUE, -1, TRUE, &nodeHandle, FALSE, TRUE, TRUE ) ;
          if ( SDB_OK != tmpRC )
          {
             rc = tmpRC ;
