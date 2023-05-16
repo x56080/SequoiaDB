ab952cd1c8d300a66c8c62eec666329302218c82
huanghaimei <huanghaimei@sequoiadb.com>
2023-05-16 09:47:21 +0800

    CI-1954:【SDB】用例 recyclebin/recyclebin_31131.js 可能会受回收站其他项目影响导致失败

--- testcase_new/story/js/recyclebin/commlib.js
+++ testcase_new/story/js/recyclebin/commlib.js
@@ -2,13 +2,12 @@
  * @Description   : 
  * @Author        : liuli
  * @CreateTime    : 2021.02.04
- * @LastEditTime  : 2023.04.15
+ * @LastEditTime  : 2023.05.09
  * @LastEditors   : HuangHaimei
  ******************************************************************************/
 import( "../lib/recyclebin_commlib.js" );
 import( "../lib/lobSubCL_commlib.js" );
 import( "../lib/index_commlib.js" );
-import( "../lib/rest_commlib.js" );
 import( "../lib/basic_operation/commlib.js" );
 testConf.testGroups = ["recycleBin"];
 testConf.skipStandAlone = true;
