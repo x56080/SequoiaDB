a1bce403f5c19188437b7fe77e701bec77f11ef1
yangqincheng <yangqincheng@sequoiadb.com>
2023-05-17 14:33:40 +0800

    SEQUOIADBMAINSTREAM-9517 调整 java 连接池日志

--- driver/java/src/main/java/com/sequoiadb/datasource/SequoiadbDatasource.java
+++ driver/java/src/main/java/com/sequoiadb/datasource/SequoiadbDatasource.java
@@ -285,7 +285,7 @@ public class SequoiadbDatasource {
                     addrInfoObj = queryAddressInfo(db);
                     version = parseVersionInfo(addrInfoObj);
                     if (version == -1) {
-                        log.info("Failed to get version of SYSCoord group");
+                        log.debug("Failed to get version of SYSCoord group");
                     }
                 } catch (Exception e) {
                     return;
@@ -1281,7 +1281,6 @@ public class SequoiadbDatasource {
     }
 
     private void _createConnections() {
-        log.info("Begin create connections task");
         int createNum = _dsOpt.getDeltaIncCount() ;
         int count = 0;
         int avgCount = (_dsOpt.getMinIdleCount() + _dsOpt.getMaxIdleCount()) / 2;
@@ -1348,7 +1347,7 @@ public class SequoiadbDatasource {
             createNum--;
             count++;
         }
-        log.info(String.format("Finish create connection task, increase idle connections: %d", count));
+        log.debug(String.format("Finish create connection task, increase idle connections: %d", count));
     }
 
     private boolean _connIsValid(ConnItem item, Sequoiadb sdb) {
