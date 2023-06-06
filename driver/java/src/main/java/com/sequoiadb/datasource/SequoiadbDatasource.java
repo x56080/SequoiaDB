267c693f2e00612ca8ee623a5054f2561ec84791
guosongxin <guosongxin@sequoiadb.com>
2023-06-06 15:47:11 +0800

    SEQUOIADBMAINSTREAM-9517 调整 java 连接池日志
    (cherry picked from commit a1bce403f5c19188437b7fe77e701bec77f11ef1)

--- driver/java/src/main/java/com/sequoiadb/datasource/SequoiadbDatasource.java
+++ driver/java/src/main/java/com/sequoiadb/datasource/SequoiadbDatasource.java
@@ -1387,7 +1387,6 @@ public class SequoiadbDatasource {
     }
 
     private void _createConnections() {
-        log.info("Begin create connections task");
         int createNum = _dsOpt.getDeltaIncCount() ;
         int count = 0;
         int avgCount = (_dsOpt.getMinIdleCount() + _dsOpt.getMaxIdleCount()) / 2;
@@ -1454,7 +1453,7 @@ public class SequoiadbDatasource {
             createNum--;
             count++;
         }
-        log.info(String.format("Finish create connection task, increase idle connections: %d", count));
+        log.debug(String.format("Finish create connection task, increase idle connections: %d", count));
     }
 
     private boolean _connIsValid(ConnItem item, Sequoiadb sdb) {
