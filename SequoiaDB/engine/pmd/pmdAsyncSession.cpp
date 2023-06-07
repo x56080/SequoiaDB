dba9aa1377ec73ca25d302b9f0c6d8043142ccb0
liuyuchen <liuyuchen@sequoiadb.com>
2023-06-07 16:12:17 +0800

    SEQUOIADBMAINSTREAM-9579 set interrupt self param

--- SequoiaDB/engine/pmd/pmdAsyncSession.cpp
+++ SequoiaDB/engine/pmd/pmdAsyncSession.cpp
@@ -1001,7 +1001,7 @@ namespace engine
       {
          PD_LOG( PDINFO, "Session[%s] recieved interrupt self message",
                  pSession->sessionName() ) ;
-         pSession->eduCB()->interrupt() ;
+         pSession->eduCB()->interrupt( TRUE ) ;
          goto done ;
       }
 
