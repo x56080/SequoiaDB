219e4c8846c0cc4f076fe73c5715e26ffc0b1974
huanghaimei <huanghaimei@sequoiadb.com>
2023-06-09 14:15:47 +0800

    SEQUOIADBMAINSTREAM-8744:【精细化容灾】Location选主机制

--- testcase_new/story/js/lib/location_commlib.js
+++ testcase_new/story/js/lib/location_commlib.js
@@ -229,3 +229,279 @@ function attachNode ( rg, hostName, port, option )
       }
    }
 }
+
+/******************************************************************************
+ * @description: 设置节点的location
+ * @param {array} nodeList
+ * @param {string} location
+ ******************************************************************************/
+function setLocationForNodes ( rg, nodeList, location )
+{
+   for( var i in nodeList )
+   {
+      var nodeInfo = nodeList[i];
+      var node = rg.getNode( nodeInfo.HostName, nodeInfo.svcname );
+      node.setLocation( location );
+   }
+}
+
+/******************************************************************************
+ * @description: 检查该位置集是否有主节点
+ * @param {string} groupName
+ * @param {string} location
+ * @param {int} seconds 
+ * @return {string} primary //如果位置集内有主节点，则返回该节点的NodeName
+ ******************************************************************************/
+function checkAndGetLocationHasPrimary ( db, groupName, location, seconds )
+{
+   while( seconds-- > 0 )
+   {
+      sleep( 1000 );
+      var primary = getLocationPrimary( db, groupName, location );
+      if( primary !== "" )
+      {
+         break;
+      }
+   }
+   assert.notEqual( "", primary, "Location[" + location + "] doesn't have primary" );
+   return primary;
+}
+
+/******************************************************************************
+ * @description: Get location's primary node name
+ * @param {string} groupName
+ * @param {string} location
+ * @return {string} locationPrimary  // if location has primary, return primary; else return ""
+ ******************************************************************************/
+function getLocationPrimary ( db, groupName, location )
+{
+   var groupObj = db.getRG( groupName ).getDetailObj().toObj();
+
+   var locations = groupObj.Locations;
+   var groupInfo = groupObj.Group;
+
+   // Get location's primary in SYSCAT.SYSNODES
+   var primaryNodeID = 0;
+   for( var i in locations )
+   {
+      var locationItem = locations[i];
+      if( location === locationItem["Location"] )
+      {
+         if( "PrimaryNode" in locationItem )
+         {
+            primaryNodeID = locationItem["PrimaryNode"];
+         } else
+         {
+            break;
+         }
+      }
+   }
+   if( primaryNodeID === 0 )
+   {
+      return "";
+   }
+
+   var hostName, serviceName;
+   for( var i in groupInfo )
+   {
+      var nodeItem = groupInfo[i];
+      if( primaryNodeID === nodeItem["NodeID"] )
+      {
+         hostName = nodeItem["HostName"];
+         var services = nodeItem["Service"];
+         for( var j in services )
+         {
+            var service = services[j];
+            if( 0 === service["Type"] )
+            {
+               serviceName = service["Name"];
+            }
+         }
+      }
+   }
+   var nodeName1 = hostName + ":" + serviceName;
+
+   // Get location's primary in SDB_SNAP_SYSTEM
+   var nodeName2 = "";
+   var cursor = db.snapshot( SDB_SNAP_SYSTEM, { RawData: true, GroupName: groupName } );
+   while( cursor.next() )
+   {
+      var snapshotObj = cursor.current().toObj();
+      if( "ErrNodes" in snapshotObj )
+      {
+         continue;
+      }
+      if( location === snapshotObj.Location )
+      {
+         if( true === snapshotObj.IsLocationPrimary )
+         {
+            nodeName2 = snapshotObj.NodeName;
+            break;
+         }
+      }
+   }
+   if( nodeName2 === "" )
+   {
+      return "";
+   }
+
+   // Get location's primary in SDB_SNAP_DATABASE
+   var nodeName3 = "";
+   var cursor = db.snapshot( SDB_SNAP_DATABASE, { RawData: true, GroupName: groupName } );
+   while( cursor.next() )
+   {
+      var snapshotObj = cursor.current().toObj();
+      if( "ErrNodes" in snapshotObj )
+      {
+         continue;
+      }
+      if( location === snapshotObj.Location )
+      {
+         if( true === snapshotObj.IsLocationPrimary )
+         {
+            nodeName3 = snapshotObj.NodeName;
+            break;
+         }
+      }
+   }
+   if( nodeName3 === "" )
+   {
+      return "";
+   }
+
+   assert.equal(
+      nodeName1,
+      nodeName2,
+      "Location[" +
+      location +
+      "] primary in SYSCAT.SYSNODES[" +
+      nodeName1 +
+      "] is not equal to in SDB_SNAP_SYSTEM[" +
+      nodeName2 +
+      "]"
+   );
+   assert.equal(
+      nodeName1,
+      nodeName3,
+      "Location[" +
+      location +
+      "] primary in SYSCAT.SYSNODES[" +
+      nodeName1 +
+      "] is not equal to in SDB_SNAP_DATABASE[" +
+      nodeName3 +
+      "]"
+   );
+
+   return nodeName1;
+}
+
+/******************************************************************************
+ * @description: 获取数据库快照中位置集主节点的部分信息
+ * @param {string} NodeName
+ * @return {Array} values 包含设置的location、节点中IsLocationPrimary的值、节点的NodeID
+ ******************************************************************************/
+function getSnapshotDatabase ( db, NodeName )
+{
+   var cursor = db.snapshot( SDB_SNAP_DATABASE, { RawData: true, NodeName: NodeName } );
+   var values = [];
+   while( cursor.next() )
+   {
+      var bson = {};
+      bson["Location"] = cursor.current().toObj().Location;
+      bson["IsLocationPrimary"] = cursor.current().toObj().IsLocationPrimary;
+      bson["NodeID"] = cursor.current().toObj().NodeID[1];
+      values.push( bson );
+   }
+   cursor.close();
+   return values;
+}
+
+/******************************************************************************
+ * @description: 获取分区列表中位置集的主节点 ID
+ * @param {string} groupName
+ * @return {int} PrimaryNode
+ ******************************************************************************/
+function getPrimaryNode ( db, groupName )
+{
+   var cursor = db.list( SDB_LIST_GROUPS, { GroupName: groupName } );
+   var obj = cursor.current().toObj();
+   var primaryNode = obj["Locations"][0]["PrimaryNode"];
+   cursor.close();
+   return primaryNode;
+}
+
+/******************************************************************************
+ * @description: 获取系统快照中位置集主节点的部分信息
+ * @param {string} NodeName
+ * @return {Array} values 包含设置的location、节点中IsLocationPrimary的值、节点的NodeID
+ ******************************************************************************/
+function getSnapshotSystem ( db, NodeName )
+{
+   var cursor = db.snapshot( SDB_SNAP_SYSTEM, { RawData: true, NodeName: NodeName } );
+   var values = [];
+   while( cursor.next() )
+   {
+      var bson = {};
+      bson["Location"] = cursor.current().toObj().Location;
+      bson["IsLocationPrimary"] = cursor.current().toObj().IsLocationPrimary;
+      bson["NodeID"] = cursor.current().toObj().NodeID[1];
+      values.push( bson );
+   }
+   cursor.close();
+   return values;
+}
+
+/************************************************************************
+*@Description: 返回指定group中的备节点名
+*@input: db            
+         groups     指定的group
+**************************************************************************/
+function getGroupSlaveNodeName ( db, groups )
+{
+   if( typeof ( groups ) == "string" ) { groups = [groups]; }
+   var slaveNodeName = [];
+   for( var i in groups )
+   {
+      var cursor = db.list( SDB_LIST_GROUPS, { "GroupName": groups[i] } );
+      while( cursor.next() )
+      {
+         var groupInfo = cursor.current().toObj();
+         var primaryNode = groupInfo.PrimaryNode;
+         var nodeInfos = groupInfo.Group;
+         for( var i in nodeInfos )
+         {
+            if( nodeInfos[i].NodeID != primaryNode )
+            {
+               var service = nodeInfos[i].Service;
+               for( var j in service )
+               {
+                  if( service[j].Type == 0 )
+                  {
+                     svcname = service[j].Name;
+                  }
+                  break;
+               }
+               slaveNodeName.push( nodeInfos[i].HostName + ":" + svcname );
+            }
+         }
+      }
+      cursor.close();
+   }
+   return slaveNodeName;
+}
+
+/******************************************************************************
+ * @description: 获取节点的NodeID
+ *  @param {string} group  // 节点所在的group
+ * @param {string} nodeName  // 节点的NodeName
+ ******************************************************************************/
+function getNodeId ( db, nodeName )
+{
+   var cursor = db.snapshot( SDB_SNAP_DATABASE, { RawData: true, NodeName: nodeName } );
+   while( cursor.next() )
+   {
+      var nodeID = cursor.current().toObj().NodeID[1];
+   }
+   cursor.close();
+   return nodeID;
+}
