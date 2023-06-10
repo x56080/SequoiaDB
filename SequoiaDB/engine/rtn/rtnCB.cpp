64a6ad9393830a472b0eef9edff6c068b3f2aa12
heguoming <heguoming@sequoiadb.com>
2023-06-10 09:08:52 +0800

    SEQUOIADBMAINSTREAM-8852: improve performance of shard lob context with repl size

--- SequoiaDB/engine/rtn/rtnCB.cpp
+++ SequoiaDB/engine/rtn/rtnCB.cpp
@@ -309,7 +309,7 @@ namespace engine
    {
       INT32 rc = SDB_OK ;
 
-      std::pair<rtnContextPtr, bool> ret = _contextMap.find( contextID ) ;
+      std::pair<rtnContextInternalPtr, bool> ret = _contextMap.find( contextID ) ;
       if ( ret.second )
       {
          if ( cb && !cb->contextFind( contextID ) )
@@ -320,7 +320,7 @@ namespace engine
          }
          else
          {
-            context = ret.first ;
+            context.init( ret.first, cb ) ;
             _setGlobalID( cb, context ) ;
          }
       }
@@ -376,7 +376,7 @@ namespace engine
    {
       PD_TRACE_ENTRY ( SDB__SDB_RTNCB_CONTEXTDEL ) ;
 
-      rtnContextPtr pContext ;
+      rtnContextInternalPtr pContext ;
       pmdEDUCB *cb = ( pmdEDUCB* )pExe ;
 
       if ( cb )
@@ -385,7 +385,7 @@ namespace engine
       }
 
       {
-         pair<rtnContextPtr, bool> ret = _contextMap.find( contextID ) ;
+         pair<rtnContextInternalPtr, bool> ret = _contextMap.find( contextID ) ;
          if ( ret.second )
          {
             pContext = ret.first ;
@@ -407,9 +407,15 @@ namespace engine
          {
             if ( NULL != cb )
             {
+               if ( DPS_INVALID_LSN_OFFSET == cb->getEndLsn() &&
+                    DPS_INVALID_LSN_OFFSET != pContext->getEndLSN() )
+               {
+                  cb->insertLsn( (UINT64)( pContext->getEndLSN() ) ) ;
+               }
                cb->setOrgReplSize( pContext->getW() ) ;
             }
             pContext->getDPSCB()->completeOpr( cb, pContext->getW() ) ;
+            pContext->resetEndLSN() ;
          }
 
          monClassQuery *monQueryCB = pContext->getMonQueryCB() ;
@@ -631,6 +637,7 @@ namespace engine
                                  INT64 &contextID,
                                  _pmdEDUCB * pEDUCB )
    {
+      rtnContextInternalPtr newContext ;
       monSvcTaskInfo *pTaskInfo = NULL ;
 
       if ( pEDUCB->isFromLocal() )
@@ -667,27 +674,27 @@ namespace engine
          return SDB_SYS ;
       }
 
-      context = sdbGetRTNContextBuilder()->create(
-                     type, _contextId, pEDUCB->getID() ) ;
+      newContext = sdbGetRTNContextBuilder()->create( type, _contextId, pEDUCB->getID() ) ;
 
-      if ( !context )
+      if ( !newContext )
       {
          return SDB_OOM ;
       }
 
-      if ( !( _contextMap.insert( _contextId, context ).second ) )
+      if ( !( _contextMap.insert( _contextId, newContext ).second ) )
       {
-         context.release() ;
+         newContext.release() ;
          return SDB_OOM ;
       }
 
       if ( !pEDUCB->contextInsert( _contextId ) )
       {
          _contextMap.erase( _contextId ) ;
-         context.release() ;
+         newContext.release() ;
          return SDB_OOM ;
       }
 
+      context.init( newContext, pEDUCB ) ;
       contextID = _contextId ;
 
       pTaskInfo = pEDUCB->getMonAppCB()->getSvcTaskInfo() ;
