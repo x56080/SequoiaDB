6d1cf520c9371e6b98b84b090f42aee40c24b1fc
liuli <liuli@sequoiadb.com>
2023-06-12 13:48:24 +0800

    CI-2107:调整用例使用ssh执行oma.start

--- testcase_new/story/js/jsObject_rlb/jsObject_10623.js
+++ testcase_new/story/js/jsObject_rlb/jsObject_10623.js
@@ -7,29 +7,9 @@ main( test );
 
 function test ()
 {
-   // 获取集群所有机器的主机名
-   var hostNames = [];
-   var cursor = db.snapshot( SDB_SNAP_DATABASE, { RawData: true } );
-   while( cursor.next() )
-   {
-      var obj = cursor.current().toObj();
-      var hostName1 = obj["HostName"];
-      if( hostNames.indexOf( hostName1 ) == -1 )
-      {
-         hostNames.push( hostName1 );
-      }
-   }
-   cursor.close();
-   // 获取当前机器的主机名
-   var hostName2 = System.getHostName();
+   var ssh = new Ssh( COORDHOSTNAME, REMOTEUSER, REMOTEPASSWD );
 
-   // 集群中不包含当前机器的主机名跳过测试
-   if( hostNames.indexOf( hostName2 ) == -1 )
-   {
-      return;
-   }
-
-   var svcName = toolGetIdleSvcName( hostName2, CMSVCNAME );
+   var svcName = toolGetIdleSvcName( COORDHOSTNAME, CMSVCNAME );
    if( svcName === undefined )
    {
       return;
@@ -37,43 +17,47 @@ function test ()
 
    // 先停止当前的cm
    var InstallPath = commGetInstallPath();
-   var cmd = new Cmd();
-   cmd.run( InstallPath + "/bin/sdbcmtop" );
+   ssh.exec( InstallPath + "/bin/sdbcmtop" );
 
    try
    {
       //测试启动超时时间为10的cm
-      var options = { "port": svcName, "alivetime": 10, "standalone": true };
-      Oma.start( options );
+      var command = InstallPath + "/bin/sdb \"Oma.start( { port: " + svcName + ", alivetime: 10, standalone: true } );\"";
+      ssh.exec( command );
 
       // 检查cm是否启动
-      var oma = new Oma( hostName2, svcName );
+      var oma = new Oma( COORDHOSTNAME, svcName );
       oma.close();
 
       sleep( 20 * 1000 );
-      var nodeArray = Sdbtool.listNodes( { type: "cm", showalone: true } ).toArray();
+      command = InstallPath + "/bin/sdb \"Sdbtool.listNodes( { type: 'cm', showalone: true } ).toArray();\"";
+      var nodeArray = ssh.exec( command );
+
       if( nodeArray.length != 0 )
       {
-         throw new Error( "nodeArray: " + nodeArray );
+         throw new Error( "nodeArray: " + JSON.stringify( nodeArray ) );
       }
 
       // 测试启动超时时间为0的cm
-      options = { "port": svcName, "alivetime": 0, "standalone": false };
-      Oma.start( options );
+      command = InstallPath + "/bin/sdb \"Oma.start( { port: " + svcName + ", alivetime: 0, standalone: false } );\"";
+      ssh.exec( command );
 
-      oma = new Oma( hostName2, svcName );
+      oma = new Oma( COORDHOSTNAME, svcName );
       oma.close();
 
       sleep( 20 * 1000 );
-      nodeArray = Sdbtool.listNodes( { type: "cm" } ).toArray();
+      command = InstallPath + "/bin/sdb \"Sdbtool.listNodes( { type: 'cm' } ).toArray();\"";
+      var nodeString = ssh.exec( command );
+      var nodeArray = JSON.parse( "[" + nodeString + "]" );
       if( nodeArray.length !== 2 )
       {
-         throw new Error( "nodeArray: " + nodeArray );
+         throw new Error( "nodeArray: " + JSON.stringify( nodeArray ) );
       }
    }
    finally
    {
-      cmd.run( InstallPath + "/bin/sdbcmtop --I" );
-      cmd.run( InstallPath + "/bin/sdbcmart" );
+      ssh.exec( InstallPath + "/bin/sdbcmtop --I" );
+      ssh.exec( InstallPath + "/bin/sdbcmart" );
+      ssh.close();
    }
 }
