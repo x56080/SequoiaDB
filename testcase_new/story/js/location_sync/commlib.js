df69b8847d5a758523bdea8f43d18b788db19ef1
liuli <liuli@sequoiadb.com>
2023-06-19 16:17:50 +0800

    CI-2130:将Location选举机制用例移动到sync工程

---
+++ testcase_new/story/js/location_sync/commlib.js
@@ -0,0 +1,9 @@
+/******************************************************************************
+ * @Description   : 
+ * @Author        : liuli
+ * @CreateTime    : 2021.09.08
+ * @LastEditTime  : 2022.11.18
+ * @LastEditors   : HuangHaimei
+ ******************************************************************************/
+import( "../lib/location_commlib.js" );
+import( "../lib/basic_operation/commlib.js" );
