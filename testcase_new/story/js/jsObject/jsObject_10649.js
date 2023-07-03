0458b9870d7c0ab983994066d8e711a8bddd2ded
liuli <liuli@sequoiadb.com>
2023-07-03 15:02:13 +0800

    CI-2166:env获取环境变量存在换行符时进行合并

--- testcase_new/story/js/jsObject/jsObject_10649.js
+++ testcase_new/story/js/jsObject/jsObject_10649.js
@@ -18,7 +18,22 @@ SystemTest.prototype.getUserEnv = function()
    var keys = [];
    var values = [];
    var environ = this.system.getUserEnv().toObj();
-   var tmpInfo = this.cmd.run( "env" ).split( "\n" );
+   var cmdEnvInfo = this.cmd.run( "env" ).split( "\n" );
+
+   // 将cmdEnvInfo数组中不包含"="的元素与前一个元素合并，加在前一个元素的末尾，输出为tmpInfo数组
+   var tmpInfo = [];
+   for( var i = 0; i < cmdEnvInfo.length; i++ )
+   {
+      if( cmdEnvInfo[i].indexOf( "=" ) != -1 )
+      {
+         tmpInfo.push( cmdEnvInfo[i] );
+      }
+      else
+      {
+         tmpInfo[tmpInfo.length - 1] = tmpInfo[tmpInfo.length - 1] + cmdEnvInfo[i];
+      }
+   }
+
    for( var i = 0; i < tmpInfo.length - 1; i++ )
    {
       var index = tmpInfo[i].indexOf( "=" );
