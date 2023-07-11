26d69278275f9fac2cd568b55ff0a765d3943c8a
heguoming <heguoming@sequoiadb.com>
2023-07-11 18:53:56 +0800

    SEQUOIADBMAINSTREAM-9738: fix selectivity of compound indexes

--- testcase_new/story_at/js/index/commlib.js
+++ testcase_new/story_at/js/index/commlib.js
@@ -1,1 +1,19 @@
 import( "../lib/index_commlib.js" );
+
+// select primary node with a collection
+function selectPrimaryNode( db, csName, clName )
+{
+   var groups = commGetCLGroups( db, csName + "." + clName ) ;
+   var index = parseInt( Math.random() * groups.length ) ;
+   println( "selected group " + groups[ index ] ) ;
+   var node = db.getRG( groups[ index ] ).getMaster() ;
+   println( "selected node " + node ) ;
+   return node.connect() ;
+}
+
+function getOneSample( db, csName, clName, ixName )
+{
+   var cl = db.getCS( "SYSSTAT" ).getCL( "SYSINDEXSTAT" ) ;
+   var stat = cl.find( { CollectionSpace: csName, Collection: clName, Index: ixName } ).current().toObj() ;
+   return stat.MCV.Values[ 1 ] ;
+}
