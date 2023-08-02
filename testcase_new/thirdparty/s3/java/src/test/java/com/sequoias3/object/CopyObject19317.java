d00fe812039fbda3070bd02780278cfdd7dd6c2a
wuyan <wuyan@sequoiadb.com>
2023-08-02 21:43:58 +0800

    CI--2311更新s3用例

--- testcase_new/thirdparty/s3/java/src/test/java/com/sequoias3/object/CopyObject19317.java
+++ testcase_new/thirdparty/s3/java/src/test/java/com/sequoias3/object/CopyObject19317.java
@@ -34,7 +34,7 @@ public class CopyObject19317 extends S3TestBase {
     private String bucketName;
     private String keyNameA = "srcObj19317A";
     private String keyNameB = "srcObj19317B";
-    private Map< String, String > xMeta = new HashMap<>();
+    private Map<String, String> xMeta = new HashMap<>();
     private ObjectMetadata metadata = new ObjectMetadata();
     private int fileSize = 6 * 1024 * 1024;
     private File localPath = null;
@@ -42,128 +42,136 @@ public class CopyObject19317 extends S3TestBase {
 
     @BeforeClass
     private void setUp() throws IOException {
-        localPath = new File( S3TestBase.workDir + File.separator
-                + TestTools.getClassName() );
+        localPath = new File(S3TestBase.workDir + File.separator
+                + TestTools.getClassName());
         filePath = localPath + File.separator + "localFile_" + fileSize
                 + ".txt";
-        TestTools.LocalFile.removeFile( localPath );
-        TestTools.LocalFile.createDir( localPath.toString() );
-        TestTools.LocalFile.createFile( filePath, fileSize );
+        TestTools.LocalFile.removeFile(localPath);
+        TestTools.LocalFile.createDir(localPath.toString());
+        TestTools.LocalFile.createFile(filePath, fileSize);
 
         bucketName = S3TestBase.bucketName;
         s3Client = CommLib.buildS3Client();
 
         // put object A, and set user-defined metadata
-        PutObjectResult objResultA = s3Client.putObject( bucketName, keyNameA,
-                new File( filePath ) );
-        xMeta.put( "def", "test" );
-        metadata.setUserMetadata( xMeta );
-        objResultA.setMetadata( metadata );
-        PutObjectRequest request = new PutObjectRequest( bucketName, keyNameA,
-                new File( filePath ) );
-        request.withMetadata( metadata );
-        s3Client.putObject( request );
+        PutObjectResult objResultA = s3Client.putObject(bucketName, keyNameA,
+                new File(filePath));
+        xMeta.put("def", "test");
+        metadata.setUserMetadata(xMeta);
+        objResultA.setMetadata(metadata);
+        PutObjectRequest request = new PutObjectRequest(bucketName, keyNameA,
+                new File(filePath));
+        request.withMetadata(metadata);
+        s3Client.putObject(request);
 
         // put object B
-        s3Client.putObject( bucketName, keyNameB, new File( filePath ) );
+        s3Client.putObject(bucketName, keyNameB, new File(filePath));
     }
 
-    // init keyNameB after copy
-    @AfterMethod
-    private void afterMethod() {
-        s3Client.deleteObject( bucketName, keyNameB );
-        s3Client.putObject( bucketName, keyNameB, new File( filePath ) );
-    }
 
     // a.setMetadataDirective is "COPY", object A copy to object B
     @Test
+    private void test() throws Exception {
+        testCopyObject_A();
+        testCopyObject_B();
+        testCopyObject_C();
+        testCopyObject_D();
+
+    }
+
     private void testCopyObject_A() throws Exception {
-        CopyObjectRequest request = new CopyObjectRequest( bucketName, keyNameA,
-                bucketName, keyNameB );
-        request.setMetadataDirective( "COPY" );
-        s3Client.copyObject( request );
-        checkObjectAttribute( keyNameB, xMeta );
-        checkObjectContent( keyNameB );
+        CopyObjectRequest request = new CopyObjectRequest(bucketName, keyNameA,
+                bucketName, keyNameB);
+        request.setMetadataDirective("COPY");
+        s3Client.copyObject(request);
+        checkObjectAttribute(keyNameB, xMeta);
+        checkObjectContent(keyNameB);
+        s3Client.deleteObject(bucketName, keyNameB);
+        s3Client.putObject(bucketName, keyNameB, new File(filePath));
         runSuccessNum++;
     }
 
     // b.setMetadataDirective is "REPLACE", object A copy to object B
-    @Test
     private void testCopyObject_B() throws Exception {
-        CopyObjectRequest request = new CopyObjectRequest( bucketName, keyNameA,
-                bucketName, keyNameB );
-        request.setMetadataDirective( "REPLACE" );
-        s3Client.copyObject( request );
-        checkObjectAttribute( keyNameB, new HashMap< String, String >() );
-        checkObjectContent( keyNameB );
+        CopyObjectRequest request = new CopyObjectRequest(bucketName, keyNameA,
+                bucketName, keyNameB);
+        request.setMetadataDirective("REPLACE");
+        s3Client.copyObject(request);
+        checkObjectAttribute(keyNameB, new HashMap<String, String>());
+        checkObjectContent(keyNameB);
+        // init keyNameB after copy
+        s3Client.deleteObject(bucketName, keyNameB);
+        s3Client.putObject(bucketName, keyNameB, new File(filePath));
         runSuccessNum++;
     }
 
     // c.setMetadataDirective is "COPY", srcObj and dstObj are objB
-    @Test
     private void testCopyObject_C() throws Exception {
-        CopyObjectRequest request = new CopyObjectRequest( bucketName, keyNameB,
-                bucketName, keyNameB );
-        request.setMetadataDirective( "COPY" );
+        CopyObjectRequest request = new CopyObjectRequest(bucketName, keyNameB,
+                bucketName, keyNameB);
+        request.setMetadataDirective("COPY");
         try {
-            s3Client.copyObject( request );
-            Assert.fail( "expect fail, but actual success." );
-        } catch ( AmazonS3Exception e ) {
-            Assert.assertEquals( e.getErrorCode(), "InvalidRequest" );
+            s3Client.copyObject(request);
+            Assert.fail("expect fail, but actual success.");
+        } catch (AmazonS3Exception e) {
+            Assert.assertEquals(e.getErrorCode(), "InvalidRequest");
         }
-        checkObjectAttribute( keyNameB, new HashMap< String, String >() );
-        checkObjectContent( keyNameB );
+        checkObjectAttribute(keyNameB, new HashMap<String, String>());
+        checkObjectContent(keyNameB);
+        s3Client.deleteObject(bucketName, keyNameB);
+        s3Client.putObject(bucketName, keyNameB, new File(filePath));
         runSuccessNum++;
     }
 
     // d.setMetadataDirective is "REPLACE", srcObj and dstObj are objB, and not
     // set metadata
-    @Test
     private void testCopyObject_D() throws Exception {
-        CopyObjectRequest request = new CopyObjectRequest( bucketName, keyNameB,
-                bucketName, keyNameB );
-        request.setMetadataDirective( "REPLACE" );
-        s3Client.copyObject( request );
-        checkObjectAttribute( keyNameB, new HashMap< String, String >() );
-        checkObjectContent( keyNameB );
+        CopyObjectRequest request = new CopyObjectRequest(bucketName, keyNameB,
+                bucketName, keyNameB);
+        request.setMetadataDirective("REPLACE");
+        s3Client.copyObject(request);
+        checkObjectAttribute(keyNameB, new HashMap<String, String>());
+        checkObjectContent(keyNameB);
+        s3Client.deleteObject(bucketName, keyNameB);
+        s3Client.putObject(bucketName, keyNameB, new File(filePath));
         runSuccessNum++;
     }
 
     @AfterClass
     private void tearDown() {
         try {
-            if ( runSuccessNum == expRunSuccessNum ) {
-                s3Client.deleteObject( bucketName, keyNameA );
-                s3Client.deleteObject( bucketName, keyNameB );
-                TestTools.LocalFile.removeFile( localPath );
+            if (runSuccessNum == expRunSuccessNum) {
+                s3Client.deleteObject(bucketName, keyNameA);
+                s3Client.deleteObject(bucketName, keyNameB);
+                TestTools.LocalFile.removeFile(localPath);
             }
         } finally {
             s3Client.shutdown();
         }
     }
 
-    private void checkObjectContent( String keyName ) throws Exception {
-        String downfileMd5 = ObjectUtils.getMd5OfObject( s3Client, localPath,
-                bucketName, keyName );
-        Assert.assertEquals( downfileMd5, TestTools.getMD5( filePath ) );
+    private void checkObjectContent(String keyName) throws Exception {
+        String downfileMd5 = ObjectUtils.getMd5OfObject(s3Client, localPath,
+                bucketName, keyName);
+        Assert.assertEquals(downfileMd5, TestTools.getMD5(filePath));
     }
 
-    private void checkObjectAttribute( String keyName,
-            Map< String, String > expMeta ) throws IOException {
+    private void checkObjectAttribute(String keyName,
+                                      Map<String, String> expMeta) throws IOException {
         GetObjectMetadataRequest request = new GetObjectMetadataRequest(
-                bucketName, keyName );
-        ObjectMetadata result = s3Client.getObjectMetadata( request );
-        Assert.assertEquals( result.getETag(), TestTools.getMD5( filePath ) );
-        Assert.assertEquals( result.getContentLength(), fileSize );
+                bucketName, keyName);
+        ObjectMetadata result = s3Client.getObjectMetadata(request);
+        Assert.assertEquals(result.getETag(), TestTools.getMD5(filePath));
+        Assert.assertEquals(result.getContentLength(), fileSize);
 
-        Map< String, String > actMeta = result.getUserMetadata();
-        Assert.assertEquals( actMeta.size(), expMeta.size(), "expMeta is : "
-                + expMeta.toString() + "actMeta is : " + actMeta.toString() );
-        for ( Map.Entry< String, String > entry : expMeta.entrySet() ) {
+        Map<String, String> actMeta = result.getUserMetadata();
+        Assert.assertEquals(actMeta.size(), expMeta.size(), "expMeta is : "
+                + expMeta.toString() + "actMeta is : " + actMeta.toString());
+        for (Map.Entry<String, String> entry : expMeta.entrySet()) {
             Object key = entry.getKey();
-            Assert.assertEquals( actMeta.get( key ), expMeta.get( key ),
+            Assert.assertEquals(actMeta.get(key), expMeta.get(key),
                     "actMeta = " + actMeta.toString() + ",expMeta = "
-                            + expMeta.toString() );
+                            + expMeta.toString());
         }
     }
 }
--- testcase_new/thirdparty/s3/java/src/test/java/com/sequoias3/object/concurrent/CopyObject19349.java
+++ testcase_new/thirdparty/s3/java/src/test/java/com/sequoias3/object/concurrent/CopyObject19349.java
@@ -5,6 +5,9 @@ import com.amazonaws.services.s3.model.CopyObjectRequest;
 import com.amazonaws.services.s3.model.GetObjectMetadataRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.sequoiadb.threadexecutor.ThreadExecutor;
+import com.sequoiadb.threadexecutor.annotation.ExecuteOrder;
+import com.sequoiadb.threadexecutor.exception.SchException;
 import com.sequoias3.testcommon.CommLib;
 import com.sequoias3.testcommon.S3TestBase;
 import com.sequoias3.testcommon.TestTools;
@@ -20,10 +23,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * @Description seqDB-19349:携带相同元数据并发复制对象，指定源和目标对象相同
  * @author wuyan
- * @Date 2019.09.20
  * @version 1.00
+ * @Description seqDB-19349:携带相同元数据并发复制对象，指定源和目标对象相同
+ * @Date 2019.09.20
  */
 public class CopyObject19349 extends S3TestBase {
 
@@ -34,100 +37,108 @@ public class CopyObject19349 extends S3TestBase {
     private int fileSize = 1024 * 1024 * 2;
     private File localPath = null;
     private String filePath = null;
-    private Map< String, String > userMeta = new HashMap<>();
+    private Map<String, String> userMeta = new HashMap<>();
     private ObjectMetadata metaData = new ObjectMetadata();
     private String contentDisposition = "this is copy object!";
 
     @BeforeClass
     private void setUp() throws IOException {
-        localPath = new File( S3TestBase.workDir + File.separator
-                + TestTools.getClassName() );
+        localPath = new File(S3TestBase.workDir + File.separator
+                + TestTools.getClassName());
         filePath = localPath + File.separator + "localFile_" + fileSize
                 + ".txt";
 
-        TestTools.LocalFile.removeFile( localPath );
-        TestTools.LocalFile.createDir( localPath.toString() );
-        TestTools.LocalFile.createFile( filePath, fileSize );
+        TestTools.LocalFile.removeFile(localPath);
+        TestTools.LocalFile.createDir(localPath.toString());
+        TestTools.LocalFile.createFile(filePath, fileSize);
         s3Client = CommLib.buildS3Client();
-        CommLib.clearBucket( s3Client, bucketName );
+        CommLib.clearBucket(s3Client, bucketName);
 
-        s3Client.createBucket( bucketName );
-        Map< String, String > srcMeta = new HashMap<>();
-        srcMeta.put( "oldtag", "test125" );
+        s3Client.createBucket(bucketName);
+        Map<String, String> srcMeta = new HashMap<>();
+        srcMeta.put("oldtag", "test125");
         ObjectMetadata srcMetaData = new ObjectMetadata();
-        srcMetaData.setUserMetadata( srcMeta );
-        PutObjectRequest request = new PutObjectRequest( bucketName, keyName,
-                new File( filePath ) );
-        request.withMetadata( srcMetaData );
-        s3Client.putObject( request );
+        srcMetaData.setUserMetadata(srcMeta);
+        PutObjectRequest request = new PutObjectRequest(bucketName, keyName,
+                new File(filePath));
+        request.withMetadata(srcMetaData);
+        s3Client.putObject(request);
 
         // meta set the userMeta and contentDisposition
-        userMeta.put( "tag1", "testa" );
-        userMeta.put( "tag2", "testa2" );
+        userMeta.put("tag1", "testa");
+        userMeta.put("tag2", "testa2");
         metaData = new ObjectMetadata();
-        metaData.setUserMetadata( userMeta );
-        metaData.setContentDisposition( contentDisposition );
+        metaData.setUserMetadata(userMeta);
+        metaData.setContentDisposition(contentDisposition);
     }
 
     @Test(invocationCount = 3, threadPoolSize = 3)
-    private void testCopyObject() {
-        AmazonS3 s3Client1 = CommLib.buildS3Client();
-        try {
-            CopyObjectRequest request = new CopyObjectRequest( bucketName,
-                    keyName, bucketName, keyName );
-            request.setMetadataDirective( "REPLACE" );
-            request.withNewObjectMetadata( metaData );
-            s3Client1.copyObject( request );
-        } finally {
-            if ( s3Client1 != null ) {
-                s3Client1.shutdown();
-            }
-        }
-    }
-
-    @Test(dependsOnMethods = "testCopyObject")
-    private void checkResult() throws Exception {
-        checkObjectMetaData( userMeta, contentDisposition );
-        checkObjectContent( bucketName, keyName );
+    private void testCopyObject() throws Exception {
+        ThreadExecutor threadExec = new ThreadExecutor();
+        ThreadCopyObject copyObject1 = new ThreadCopyObject();
+        ThreadCopyObject copyObject2 = new ThreadCopyObject();
+        threadExec.addWorker(copyObject1);
+        threadExec.addWorker(copyObject2);
+        threadExec.run();
+        checkObjectMetaData(userMeta, contentDisposition);
+        checkObjectContent(bucketName, keyName);
         runSuccess = true;
     }
+
 
     @AfterClass
     private void tearDown() {
         try {
-            if ( runSuccess ) {
-                CommLib.clearBucket( s3Client, bucketName );
-                TestTools.LocalFile.removeFile( localPath );
+            if (runSuccess) {
+                CommLib.clearBucket(s3Client, bucketName);
+                TestTools.LocalFile.removeFile(localPath);
             }
         } finally {
             s3Client.shutdown();
         }
     }
 
-    private void checkObjectContent( String bucketName, String keyName )
+    private void checkObjectContent(String bucketName, String keyName)
             throws Exception {
-        String downfileMd5 = ObjectUtils.getMd5OfObject( s3Client, localPath,
-                bucketName, keyName );
-        Assert.assertEquals( downfileMd5, TestTools.getMD5( filePath ) );
+        String downfileMd5 = ObjectUtils.getMd5OfObject(s3Client, localPath,
+                bucketName, keyName);
+        Assert.assertEquals(downfileMd5, TestTools.getMD5(filePath));
     }
 
-    private void checkObjectMetaData( Map< String, String > expMeta,
-            String contentDisposition ) {
+    private void checkObjectMetaData(Map<String, String> expMeta,
+                                     String contentDisposition) {
         GetObjectMetadataRequest request = new GetObjectMetadataRequest(
-                bucketName, keyName );
-        ObjectMetadata result = s3Client.getObjectMetadata( request );
+                bucketName, keyName);
+        ObjectMetadata result = s3Client.getObjectMetadata(request);
 
-        Map< String, String > actMeta = result.getUserMetadata();
-        Assert.assertEquals( actMeta.size(), expMeta.size(), "expMetaB is : "
-                + expMeta.toString() + "actMetaB is : " + actMeta.toString() );
-        for ( Map.Entry< String, String > entry : expMeta.entrySet() ) {
+        Map<String, String> actMeta = result.getUserMetadata();
+        Assert.assertEquals(actMeta.size(), expMeta.size(), "expMetaB is : "
+                + expMeta.toString() + "actMetaB is : " + actMeta.toString());
+        for (Map.Entry<String, String> entry : expMeta.entrySet()) {
             Object key = entry.getKey();
-            Assert.assertEquals( actMeta.get( key ), expMeta.get( key ),
+            Assert.assertEquals(actMeta.get(key), expMeta.get(key),
                     "actMetaB = " + actMeta.toString() + ",expMeta = "
-                            + expMeta.toString() );
+                            + expMeta.toString());
         }
-        Assert.assertEquals( result.getContentDisposition(),
-                contentDisposition );
+        Assert.assertEquals(result.getContentDisposition(),
+                contentDisposition);
+    }
 
+    private class ThreadCopyObject {
+        @ExecuteOrder(step = 1)
+        private void copyObject() {
+            AmazonS3 s3Client1 = CommLib.buildS3Client();
+            try {
+                CopyObjectRequest request = new CopyObjectRequest(bucketName,
+                        keyName, bucketName, keyName);
+                request.setMetadataDirective("REPLACE");
+                request.withNewObjectMetadata(metaData);
+                s3Client1.copyObject(request);
+            } finally {
+                if (s3Client1 != null) {
+                    s3Client1.shutdown();
+                }
+            }
+        }
     }
 }
