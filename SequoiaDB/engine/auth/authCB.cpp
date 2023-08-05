6c94eed00e072a20a1a4918c9762e224d70eb40f
zhouhongye <zhouhongye@sequoiadb.com>
2023-08-05 11:15:40 +0800

    SEQUOIADBMAINSTREAM-9803 fix authRoleManager on role OM

--- SequoiaDB/engine/auth/authCB.cpp
+++ SequoiaDB/engine/auth/authCB.cpp
@@ -81,7 +81,12 @@ namespace engine
 
    INT32 _authCB::active ()
    {
-      return _roleMgr.active() ;
+      INT32 rc = SDB_OK ;
+      if ( SDB_ROLE_OM != pmdGetDBRole() )
+      {
+         rc = _roleMgr.active() ;
+      }
+      return rc ;
    }
 
    INT32 _authCB::deactive ()
