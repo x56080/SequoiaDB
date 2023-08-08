9de9f432ba3ac6e5588dc43e1ac18994145caabe
liuli <liuli@sequoiadb.com>
2023-08-08 09:33:11 +0800

    Revert "CI-2325:校验索引一致后删除集合"
    This reverts commit f8f580b2de76fa1d75d10f65517f28410a0b7905.

--- testcase_new/story/js/recyclebin/recyclebin_25383.js
+++ testcase_new/story/js/recyclebin/recyclebin_25383.js
@@ -2,7 +2,7 @@
  * @Description   : seqDB-25383:回收站恢复一致性索引 
  * @Author        : liuli
  * @CreateTime    : 2022.02.24
- * @LastEditTime  : 2023.08.07
+ * @LastEditTime  : 2022.02.24
  * @LastEditors   : liuli
  ******************************************************************************/
 testConf.skipOneGroup = true;
@@ -29,7 +29,6 @@ function test ()
    // 创建一致性索引
    dbcl.createIndex( indexName, { a: 1 } );
    checkIndexTask( "Create index", csName, clName, indexName, 0 );
-   commCheckIndexConsistent( db, csName, clName, indexName, true );
 
    // 删除CL
    dbcs.dropCL( clName );
