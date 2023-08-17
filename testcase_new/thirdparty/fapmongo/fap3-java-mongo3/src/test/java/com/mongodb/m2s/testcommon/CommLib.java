b05cc30082ca2aae8c39b24ee731878445dc62ee
chenzejia <chenzejia@sequoiadb.com>
2023-08-17 14:45:29 +0800

    补充ssh类，提供判断集群模式公共方法

--- testcase_new/thirdparty/fapmongo/fap3-java-mongo3/src/test/java/com/mongodb/m2s/testcommon/CommLib.java
+++ testcase_new/thirdparty/fapmongo/fap3-java-mongo3/src/test/java/com/mongodb/m2s/testcommon/CommLib.java
@@ -1,7 +1,9 @@
 package com.mongodb.m2s.testcommon;
 
+import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.utils.Ssh;
+import org.bson.Document;
 
 /**
  * @Descreption
@@ -14,7 +16,7 @@ import com.mongodb.utils.Ssh;
  */
 public class CommLib {
 
-    public static void initDir(Ssh ssh, String path ) throws Exception {
+    public static void initDir( Ssh ssh, String path ) throws Exception {
         try {
             ssh.exec( "ls " + path );
         } catch ( Exception e ) {
@@ -26,7 +28,7 @@ public class CommLib {
         }
     }
 
-    public static void rmDir(Ssh ssh, String path ) throws Exception {
+    public static void rmDir( Ssh ssh, String path ) throws Exception {
         try {
             ssh.exec( "ls " + path );
             ssh.exec( "rm -rf " + path );
@@ -49,6 +51,40 @@ public class CommLib {
         return false;
     }
 
+    public static boolean isSharded( MongoClient client ) {
+        MongoDatabase db = client.getDatabase( "admin" );
+        Document document = new Document( "isMaster", 1 );
+        Document result = db.runCommand( document );
+        if ( result.containsKey( "msg" )
+                && result.getString( "msg" ).equals( "isdbgrid" ) ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isReplicaSet( MongoClient client ) {
+        MongoDatabase db = client.getDatabase( "admin" );
+        Document document = new Document( "isMaster", 1 );
+        Document result = db.runCommand( document );
+        if ( result.containsKey( "setName" ) ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isStandalone( MongoClient client ) {
+        MongoDatabase db = client.getDatabase( "admin" );
+        Document document = new Document( "isMaster", 1 );
+        Document result = db.runCommand( document );
+        if ( result.getBoolean( "ismaster" ) ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public static void execCmd( String cmd ) {
         try {
             Process process = Runtime.getRuntime().exec( cmd );
@@ -58,22 +94,22 @@ public class CommLib {
         }
     }
 
-    public static void collectCluster(Ssh ssh, String collectorUri,
-                                      String collectorOutputPath ) throws Exception {
+    public static void collectCluster( Ssh ssh, String collectorUri,
+            String collectorOutputPath ) throws Exception {
         String collectCommand = collectorUri + " -t cluster -o "
                 + collectorOutputPath;
         ssh.exec( collectCommand );
     }
 
-    public static void collectCollection(Ssh ssh, String CollectorUri,
-                                         String collectorOutputPath ) throws Exception {
+    public static void collectCollection( Ssh ssh, String CollectorUri,
+            String collectorOutputPath ) throws Exception {
         String collectCommand = CollectorUri + " -t collection -o "
                 + collectorOutputPath;
         ssh.exec( collectCommand );
     }
 
-    public static void analyzeCluster(Ssh ssh, String clusterJsonPath,
-                                      String analyzerUri, String analyzerOutputPath, String sdbversion )
+    public static void analyzeCluster( Ssh ssh, String clusterJsonPath,
+            String analyzerUri, String analyzerOutputPath, String sdbversion )
             throws Exception {
         String analyzeCommand = analyzerUri + " --clusterjson "
                 + clusterJsonPath + " -t json --sdbversion " + sdbversion
@@ -81,8 +117,8 @@ public class CommLib {
         ssh.exec( analyzeCommand );
     }
 
-    public static void analyzeCollection(Ssh ssh, String collectionJsonPath,
-                                         String analyzerUri, String analyzerOutputPath, String sdbversion )
+    public static void analyzeCollection( Ssh ssh, String collectionJsonPath,
+            String analyzerUri, String analyzerOutputPath, String sdbversion )
             throws Exception {
         String analyzeCommand = analyzerUri + " --collectionjson "
                 + collectionJsonPath + " -t json --sdbversion " + sdbversion
