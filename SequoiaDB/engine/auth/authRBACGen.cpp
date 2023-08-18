7b287e30572078aab0033724592bdf590d2f7f22
zhouhongye <zhouhongye@sequoiadb.com>
2023-08-18 11:59:10 +0800

    SEQUOIADBMAINSTREAM-9855 修复exec中获取监控视图无视权限

--- SequoiaDB/engine/auth/authRBACGen.cpp
+++ SequoiaDB/engine/auth/authRBACGen.cpp
@@ -2573,10 +2573,14 @@ namespace engine
    static const authActionSet AUTH_CMD_NAME_LIST_SPACE_INTR_default_ACTION_SETS_ARRAY[] = {
       authActionSet( ACTION_SET_NUMBER_ARRAY(
       0ULL,
+      16ULL
+      )),
+      authActionSet( ACTION_SET_NUMBER_ARRAY(
+      0ULL,
       8ULL
       )),
    };
-   static const authRequiredActionSets AUTH_CMD_NAME_LIST_SPACE_INTR_default_SETS( RESOURCE_TYPE_CLUSTER , AUTH_CMD_NAME_LIST_SPACE_INTR_default_ACTION_SETS_ARRAY, 1, 
+   static const authRequiredActionSets AUTH_CMD_NAME_LIST_SPACE_INTR_default_SETS( RESOURCE_TYPE_CLUSTER , AUTH_CMD_NAME_LIST_SPACE_INTR_default_ACTION_SETS_ARRAY, 2, 
    authRequiredActionSets::SOURCE_OBJ_NONE, NULL
    );
 
