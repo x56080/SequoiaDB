853df24d3c3800caa04be2ca770a94ca4955f4e7
fangjiabin <fangjiabin@sequoiadb.com>
2023-09-04 14:38:28 +0800

    SEQUOIADBMAINSTREAM-9904，修复 getMore 丢数据的问题

--- SequoiaDB/engine/coord/coordContext.cpp
+++ SequoiaDB/engine/coord/coordContext.cpp
@@ -1090,7 +1090,7 @@ namespace engine
       }
 
       if ( !_hitEnd && _emptyContextMap.empty() &&
-           _orderedContexts.empty() && _prepareContextMap.empty() )
+           _orderedContexts.empty() && _prepareContextMap.empty() && isEmpty() )
       {
          _hitEnd = TRUE ;
       }
