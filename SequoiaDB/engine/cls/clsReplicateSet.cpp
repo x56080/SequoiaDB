699c97901e878bacf32f5b7a0d0f124504eef380
wuyun <wuyun@sequoiadb.com>
2023-09-25 10:22:31 +0000

    Revert "SEQUOIADBMAINSTREAM-9954，修复备节点重放日志 coredump 问题"
    This reverts commit 51e7ddd80d05495f340cda5b21bcdbcae1c41d2b

--- SequoiaDB/engine/cls/clsReplicateSet.cpp
+++ SequoiaDB/engine/cls/clsReplicateSet.cpp
@@ -1455,20 +1455,6 @@ namespace engine
       _lastConsultTick = tick ;
    }
 
-   BOOLEAN _clsReplicateSet::isReadyToReplay()
-   {
-      if ( ( _vote.isStatus( CLS_ELECTION_STATUS_SILENCE ) ||
-             _vote.isStatus( CLS_ELECTION_STATUS_SEC ) ) &&
-           _sync.isReadyToReplay() )
-      {
-         return TRUE ;
-      }
-      else
-      {
-         return FALSE ;
-      }
-   }
-
    UINT64 _clsReplicateSet::getLastConsultTick() const
    {
       return _lastConsultTick ;
