5c671a283e057d6a5257d336bcd58633d2881bf7
liuli <liuli@sequoiadb.com>
2023-09-26 14:30:42 +0800

    CI-2487:集合设置ReplSize为0

--- testcase_new/story/js/recyclebin_sync/recyclebin_31023.js
+++ testcase_new/story/js/recyclebin_sync/recyclebin_31023.js
@@ -2,8 +2,8 @@
  * @Description   : seqDB-31023:获取回收站项目快照，指定Role为all/data/coord/catalog
  * @Author        : Bi Qin
  * @CreateTime    : 2023.04.04
- * @LastEditTime  : 2023.04.11
- * @LastEditors   : Bi Qin
+ * @LastEditTime  : 2023.09.26
+ * @LastEditors   : liuli
  ******************************************************************************/
 testConf.skipStandAlone = true;
 main( test );
@@ -16,7 +16,7 @@ function test ()
    commDropCS( db, csName );
    cleanRecycleBin( db, csName );
    var dbcs = commCreateCS( db, csName );
-   commCreateCL( db, csName, clName );
+   commCreateCL( db, csName, clName, { ReplSize: 0 } );
    dbcs.dropCL( clName );
 
    var rawdata = [true, false];
