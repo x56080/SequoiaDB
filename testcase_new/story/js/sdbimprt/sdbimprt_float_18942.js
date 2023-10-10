9d0ec1b8479a9816a01e35ab416050f14f165273
wuyan <wuyan@sequoiadb.com>
2023-10-10 17:50:03 +0800

    ci-2526修改用例中获取文件类型方式

--- testcase_new/story/js/sdbimprt/sdbimprt_float_18942.js
+++ testcase_new/story/js/sdbimprt/sdbimprt_float_18942.js
@@ -68,7 +68,8 @@ function prepareDate ( typeFile )
       {
          right = right.substring( 0, right.length - 1 );
       }
-      if( typeFile.substring( typeFile.indexOf( "." ) + 1, typeFile.length ) == "csv" )
+      var type = typeFile.split( "." ).pop();
+      if( type == "csv" )
       {
          file.write( id + "," + left + "\n" );
          ++id;
