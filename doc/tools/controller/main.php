d49cf2d3caedd7d70490b07656c436836c38679a
yangqincheng <yangqincheng@sequoiadb.com>
2024-01-10 08:14:25 +0000

    SEQUOIADBMAINSTREAM-10066 修正 doc 编译问题

--- doc/tools/controller/main.php
+++ doc/tools/controller/main.php
@@ -3,7 +3,7 @@
 include_once "parseConf.php" ;
 include_once "function.php" ;
 
-//doxygenÉú³ÉµÄÎÄ¼şºÍtoc.jsonµÄÓ³Éä¹ØÏµ
+//doxygenç”Ÿæˆçš„æ–‡ä»¶å’Œtoc.jsonçš„æ˜ å°„å…³ç³»
 $apiList = array(
    "c"         => 1432190718,
    "bson"      => 1432190718,
@@ -40,7 +40,7 @@ if( $edition == FALSE )
 $major = $edition['major'] ;
 $minor = $edition['minor'] ;
 
-//Êä³öµÄÎÄ¼şÃû
+//è¾“å‡ºçš„æ–‡ä»¶å
 $outputFileName = "SequoiaDB_usermanuals_v$major.$minor" ;
 
 $config = getConfig( $path ) ;
@@ -79,7 +79,7 @@ if( $param['h'] == "1" )
    exit( 0 ) ;
 }
 
-//=== ³õÊ¼»¯ ===
+//=== åˆå§‹åŒ– ===
 printLog( "Init...", "Event" ) ;
 
 $os = getOSInfo() ;
@@ -87,7 +87,7 @@ $mdConvert = $os == 'windows' ? 'mdConverter.exe' : 'linux_mdConverter' ;
 $html2mysql = $os == 'windows' ? 'exec.bat' : 'exec.sh' ;
 $wkhtmltopdf = $os == 'windows' ? 'wkhtmltopdf.exe' : 'wkhtmltopdf' ;
 
-//2.ÇåÀí¾ÉÎÄ¼ş
+//2.æ¸…ç†æ—§æ–‡ä»¶
 printLog( "Clear file...", "Event" ) ;
 if( file_exists( $os == 'windows' ? "$root\build\mid" : "$root/build/mid" ) && removeDir( $os == 'windows' ? "$root\build\mid" : "$root/build/mid" ) == false )
 {
@@ -101,7 +101,7 @@ if( file_exists( $os == 'windows' ? "$root\build\output" : "$root/build/output" 
    exit( 1 ) ;
 }
 
-//2.´´½¨Ä¿Â¼
+//2.åˆ›å»ºç›®å½•
 mkdir( "$root/build/mid", 0777, true ) ;
 if( file_exists( "$root/build/output/api" ) == false )
 {
@@ -109,13 +109,13 @@ if( file_exists( "$root/build/output/api" ) == false )
 }
 chmod( "$root/tools/$mdConvert", 0777 ) ;
 
-//=== Ô¤´¦Àí ===
+//=== é¢„å¤„ç† ===
 if( $param['m'] == "doc" || $param['m'] == "chm" || $param['m'] == "offline" || $param['m'] == "website" || $param['m'] == "doxygen" )
 {
    printLog( "Generate doxygen file", "Event" ) ;
    $doxygen_files = iterDoxygenConfig( "$root/config/doxygen" ) ;
 
-   //1. Éú³Édoxygen
+   //1. ç”Ÿæˆdoxygen
    foreach( $doxygen_files as $index => $file )
    {
       if ( $file == "config/doxygen/doxygen-java.conf" )
@@ -172,7 +172,7 @@ if( $param['m'] == "doc" || $param['m'] == "chm" || $param['m'] == "offline" || 
 
 }
 
-//=== ×ª»» + Éú³É ===
+//=== è½¬æ¢ + ç”Ÿæˆ ===
 //1. pdf
 if( $param['m'] == "doc" || $param['m'] == "pdf" )
 {
@@ -192,7 +192,7 @@ if( $param['m'] == "doc" || $param['m'] == "pdf" )
    $platform = $os == 'windows' ? 'win32' : 'linux64' ;
    chmod( "$root/tools/pdfConvertor/tools/$platform/wkhtmltox/bin/$wkhtmltopdf", 0777 ) ;
 
-   //ĞŞ¸ÄÅäÖÃ
+   //ä¿®æ”¹é…ç½®
    $headerContents = file_get_contents( "$root/tools/pdfConvertor/src/header.html" ) ;
    $headerContents = str_replace( '{{version}}', "$major.$minor", $headerContents ) ;
    file_put_contents( "$root/tools/pdfConvertor/src/header_tmp.html", $headerContents ) ;
@@ -204,7 +204,7 @@ if( $param['m'] == "doc" || $param['m'] == "pdf" )
    $pdf = "$root/tools/pdfConvertor/tools/$platform/wkhtmltox/bin/$wkhtmltopdf --page-size A4 --dpi 150 --margin-top 20 --enable-local-file-access --enable-smart-shrinking --load-error-handling ignore --encoding utf-8 --user-style-sheet $root/tools/pdfConvertor/src/pdf_global.css --footer-html $root/tools/pdfConvertor/src/footer.html --header-html $root/tools/pdfConvertor/src/header_tmp.html --page-offset -1 cover $root/tools/pdfConvertor/src/cover_tmp.html toc --xsl-style-sheet $root/tools/pdfConvertor/src/toc.xsl $root/build/mid/build.html $root/build/output/$outputFileName.pdf" ;
    if( execCmd( $pdf ) != 0 )
    {
-      //ºöÂÔpdfÔÚlinuxµÄ´íÎó
+      //å¿½ç•¥pdfåœ¨linuxçš„é”™è¯¯
       if( $os == 'windows' )
       {
          printLog( 'Failed to convert pdf file' ) ;
@@ -292,6 +292,8 @@ if( $param['m'] == "chm" && $os == 'windows' )
    execCmd( $chm ) ;
 
    $log = file_get_contents( "$root/tools/anjian/anjian.log" ) ;
+   $log = iconv( 'gb2312', 'utf-8//IGNORE', $log ) ;
+
    echo "\n".$log."\n" ;
    if( strpos( $log, "Error" ) !== false )
    {
@@ -354,6 +356,8 @@ if( $param['m'] == "offline" && $os == 'windows' )
    execCmd( $chm ) ;
 
    $log = file_get_contents( "$root/tools/anjian/anjian.log" ) ;
+   $log = iconv( 'gb2312', 'utf-8//IGNORE', $log ) ;
+
    echo "\n".$log."\n" ;
    if( strpos( $log, "Error" ) !== false )
    {
@@ -364,7 +368,7 @@ if( $param['m'] == "offline" && $os == 'windows' )
    printLog( "Finish build chm document, path: doc/build/output/$outputFileName.chm", "Event" ) ;
 }
 
-//5. ¹ÙÍø
+//5. å®˜ç½‘
 if( $param['m'] == "website" )
 {
    printLog( "Generate website", "Event" ) ;
