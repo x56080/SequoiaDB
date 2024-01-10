2fe871f982ef71699678f47992028e6fdcc0b7bc
heguoming <heguoming@sequoiadb.com>
2024-01-10 16:57:15 +0800

    Fix explain testcase

--- testcase_new/story/js/explain/explain_11373.js
+++ testcase_new/story/js/explain/explain_11373.js
@@ -24,7 +24,7 @@ function test ( args )
 
    // 生成随机数
    var rd = new commDataGenerator();
-   var value = rd.getRecords( 20000, "int", ["a", "b", "c"] );
+   var value = rd.getRecords( 11000, "int", ["a", "b", "c"] );
    cl.insert( value );
 
    db.analyze( { Collection: fullclName } );
@@ -51,7 +51,7 @@ function test ( args )
    var sortCond = { "a": -1 };
    checkExplain( cl, cond, expIndexName, expScanType, sortCond );
 
-   var value = rd.getRecords( 15000, "int", ["a", "b", "c"] );
+   var value = rd.getRecords( 20000, "int", ["a", "b", "c"] );
    cl.insert( value );
 
    db.analyze( { Collection: fullclName } );
