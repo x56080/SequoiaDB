fee8262df337e39c14172acb9694d29e5a959cc4
heguoming <heguoming@sequoiadb.com>
2024-01-16 05:57:23 +0800

    Fix RU transaction in table scan

--- SequoiaDB/engine/include/rtnTBScanner.hpp
+++ SequoiaDB/engine/include/rtnTBScanner.hpp
@@ -86,6 +86,11 @@ namespace engine
          return DMS_INVALID_EXTENT ;
       }
 
+      virtual INT32 init()
+      {
+         return SDB_OK ;
+      }
+
    public:
       BOOLEAN isEOF() const
       {
