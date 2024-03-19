77d38cf9a458547d44530d3427c60f8c3d7877f5
linsuqiang <linsuqiang@sequoiadb.com>
2024-03-19 16:09:09 +0800

    SEQUOIADBMAINSTREAM-10121 修复错误的警告日志

--- SequoiaDB/engine/dms/dmsStorageBase.cpp
+++ SequoiaDB/engine/dms/dmsStorageBase.cpp
@@ -754,7 +754,7 @@ namespace engine
       return rs ;
    }
 
-   // PD_TRACE_DECLARE_FUNCTION ( SDB__DMSMMAPFILE_INVALIDATECACHE, "_dmsMmapFile::invalidateCache" )
+   // PD_TRACE_DECLARE_FUNCTION ( SDB__DMSSTORAGEBASE_INVALIDATEFSCACHE, "_dmsStorageBase::invalidateFsCache" )
    INT32 _dmsStorageBase::invalidateFsCache( const UINT64 *pExpiredMs )
    {
       INT32 rc = SDB_OK ;
@@ -762,7 +762,7 @@ namespace engine
       UINT32 i = 0 ;
       UINT64 expiredMs = ( pExpiredMs ) ? *pExpiredMs : _fsCacheExpiredMs ;
 
-      PD_TRACE_ENTRY ( SDB__DMSMMAPFILE_INVALIDATECACHE ) ;
+      PD_TRACE_ENTRY ( SDB__DMSSTORAGEBASE_INVALIDATEFSCACHE ) ;
 
       if ( (UINT64)~0 == expiredMs )
       {
@@ -800,7 +800,7 @@ namespace engine
       }
 
    done:
-      PD_TRACE_EXITRC( SDB__DMSMMAPFILE_INVALIDATECACHE, rc ) ;
+      PD_TRACE_EXITRC( SDB__DMSSTORAGEBASE_INVALIDATEFSCACHE, rc ) ;
       return rc ;
    }
 
