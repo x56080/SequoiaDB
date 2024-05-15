bdb1ef6ce5d7ffe5d91131bdfdce14430e92618b
wangwenjing <wangwenjing@sequoiadb.com>
2024-05-15 17:17:03 +0800

    CI-2920 用例story_js.advance.range_27391_27392失败

--- testcase_new/story/js/advance/range_27391_27392.js
+++ testcase_new/story/js/advance/range_27391_27392.js
@@ -4,28 +4,30 @@
  * @Author        : liuli
  * @CreateTime    : 2022.09.03
  * @LastEditTime  : 2022.09.05
+                    2024.05.15
  * @LastEditors   : liuli
+                    wenjingwang
  ******************************************************************************/
 testConf.skipStandAlone = true;
+testConf.csName = "cs_27391_27392";
+testConf.clName = "mainCL_27391_27392";
+testConf.clOpt = { ShardingKey: { a: 1 }, ShardingType: "range", IsMainCL: true } 
 
-main( test );
+main( setUp, test );
 
-function test ()
+function setUp(testpara)
 {
-   var csName = "cs_27391_27392";
-   var mainCLName = "mainCL_27391_27392";
+   var csName = testConf.csName;
    var subCLName1 = "subCL_27391_27392_1";
    var subCLName2 = "subCL_27391_27392_2";
    var idxName = "idx_27391_27392";
-   var recsNum = 10;
 
-   commDropCS( db, csName );
-   var maincl = commCreateCL( db, csName, mainCLName, { ShardingKey: { a: 1 }, ShardingType: "range", IsMainCL: true } );
    commCreateCL( db, csName, subCLName1, { ShardingKey: { a: 1 }, ShardingType: "hash", AutoSplit: true } );
    commCreateCL( db, csName, subCLName2, { ShardingKey: { a: 1 }, ShardingType: "hash", AutoSplit: true } );
-   maincl.attachCL( csName + "." + subCLName1, { LowBound: { a: 0 }, UpBound: { a: 5 } } );
-   maincl.attachCL( csName + "." + subCLName2, { LowBound: { a: 5 }, UpBound: { a: 10 } } );
-
+   testpara.testCL.attachCL( csName + "." + subCLName1, { LowBound: { a: 0 }, UpBound: { a: 5 } } );
+   testpara.testCL.attachCL( csName + "." + subCLName2, { LowBound: { a: 5 }, UpBound: { a: 10 } } );
+   
+   var recsNum = 10;
    var docs = [];
    for( var i = 0; i < recsNum; i++ )
    {
@@ -34,31 +36,45 @@ function test ()
          docs.push( { a: i, b: j } );
       }
    }
-   maincl.insert( docs );
-   maincl.createIndex( idxName, { a: 1, b: 1 } );
+ 
+   testpara.testCL.insert( docs );
+   testpara.testCL.createIndex( idxName, { a: 1, b: 1 } );
+   testpara.idxName = idxName ;
+}
 
-   // 指定hint不指定sort批量点查
-   var cursor = maincl.find().hint( {
-      "": idxName,
+function test (testpara)
+{
+   var hints = [ {
+      "": testpara.idxName,
       "$Range": {
          "IsAllEqual": true,
          "PrefixNum": 2,
          "IndexValue": [{ "1": 1, "2": 1 }, { "1": 3, "2": 3 }]
       }
-   } );
-   var expResult = [{ a: 1, b: 1 }, { a: 3, b: 3 }];
-   commCompareResults( cursor.sort( sortBy( 'a' )), expResult.sort( sortBy( 'a' ) ) );
-
-   // 指定hint不指定sort批量范围查
-   var cursor = maincl.find().hint( {
-      "": idxName,
+   } ,
+   {
+      "": testpara.idxName,
       "$Range": {
          "IsAllEqual": false,
          "PrefixNum": [2, 2, 2, 2],
          "IndexValueIncluded": [[true, false], [true, false], [true, false], [true, false]],
          "IndexValue": [[{ "1": 4, "2": 2 }, { "1": 4, "2": 6 }], [{ "1": 5, "2": 2 }, { "1": 5, "2": 6 }], [{ "1": 5, "2": 2 }, { "1": 5, "2": 6 }], [{ "1": 6, "2": 2 }, { "1": 6, "2": 6 }]]
       }
-   } );
+   }
+   ]
+
+   var expResults = [[{ a: 1, b: 1 }, { a: 3, b: 3 }]];
+   expResults.push(getExpectResult());
+   for ( var i = 0; i < hints.length; ++i)
+   {
+       var cursor = testpara.testCL.find().hint( hints[i] );
+       var actResult = getRealResult(cursor);
+       assert.equal( actResult.sort( sortBy( 'a' ) ), expResults[i].sort( sortBy( 'a' ) ) );
+   }
+}
+
+function getExpectResult()
+{
    var expResult = [];
    for( var i = 4; i <= 6; i++ )
    {
@@ -67,7 +83,11 @@ function test ()
          expResult.push( { a: i, b: j } );
       }
    }
+   return expResult;
+}
 
+function getRealResult( cursor )
+{
    var actResult = [];
    while( cursor.next() )
    {
@@ -76,9 +96,7 @@ function test ()
       actResult.push( actObj );
    }
    cursor.close();
-   assert.equal( actResult.sort( sortBy( 'a' ) ), expResult.sort( sortBy( 'a' ) ) );
-
-   commDropCS( db, csName );
+   return actResult;
 }
 
 function sortBy ( field )
