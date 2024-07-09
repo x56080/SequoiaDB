4fe9ebb4313ddb3dd106ff903d40d754930ad66c
xujianhui <xujianhui@sequoiadb.com>
2024-07-09 11:32:12 +0000

    SEQUOIADBMAINSTREAM-10176:日志打印和慢查询内存异常保护优化

--- SequoiaDB/engine/rtn/rtnCB.cpp
+++ SequoiaDB/engine/rtn/rtnCB.cpp
@@ -317,7 +317,6 @@ namespace engine
             {
                cb->setMonQueryCB( monQueryCB ) ;
                hasSetCBMon = TRUE ;
-               pmdGetKRCB()->getMonMgr()->removeMonitorObject( monQueryCB ) ;
             }
             else if ( ( NULL == cb ) ||
                       ( cb->getMonQueryCB() != monQueryCB ) )
@@ -336,6 +335,7 @@ namespace engine
          {
             cb->setMonQueryCB( NULL ) ;
             hasSetCBMon = FALSE ;
+            pmdGetKRCB()->getMonMgr()->removeMonitorObject( monQueryCB ) ;
          }
       }
 
