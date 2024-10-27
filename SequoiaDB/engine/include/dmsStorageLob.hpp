000cecbda07b1b97aff429d0822cca7d921f0eb1
xujianhui <xujianhui@sequoiadb.com>
2024-10-27 16:19:09 +0000

    fix compile error

--- SequoiaDB/engine/include/dmsStorageLob.hpp
+++ SequoiaDB/engine/include/dmsStorageLob.hpp
@@ -172,10 +172,6 @@ namespace engine
 
       INT32 rebuildBME() ;
 
-      virtual void incWritePtrCount( INT32 collectionID ) ;
-
-      virtual void decWritePtrCount( INT32 collectionID ) ;
-
    public:
       /// get the segment pages of lobd
       OSS_INLINE UINT32 dataSegmentPages() const ;
