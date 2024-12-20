5db08f46cb96ed94ee1cc35ee65f8a583bbd3dcb
xujianhui <xujianhui@sequoiadb.com>
2024-12-20 06:48:32 +0000

    code review: 类型转换错误

--- SequoiaDB/engine/opt/optAPM.cpp
+++ SequoiaDB/engine/opt/optAPM.cpp
@@ -2677,7 +2677,7 @@ namespace engine
       SDB_ASSERT( pCacheHolder, "pCacheHolder is invalid" ) ;
 
       dmsCachedPlanMgr *pCachedPlanMgr =
-                           dynamic_cast<dmsCachedPlanMgr *>(pCacheHolder) ;
+            (dmsCachedPlanMgr *)pCacheHolder->getSUCache( DMS_CACHE_TYPE_PLAN ) ;
       if ( pCachedPlanMgr )
       {
          pCachedPlanMgr->resizeBitmaps( _planCache.getBucketNum() ) ;
