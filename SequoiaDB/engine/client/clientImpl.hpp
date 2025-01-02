20da3e4d74f3bf273c1a459fae1f508b274385e1
林苏强 <41-linsuqiang@users.noreply.gitlab.example.com>
2025-01-02 06:15:59 +0000

    SEQUOIADBMAINSTREAM-9679 改进 SdbCL::interruptOperation() 避免多线程操作 socket

--- SequoiaDB/engine/client/clientImpl.hpp
+++ SequoiaDB/engine/client/clientImpl.hpp
@@ -1572,6 +1572,7 @@ namespace sdbclient
       ossTimestamp             _lastAliveTime ;
 
       BOOLEAN                  _isOldVersionLobServer ;
+      BOOLEAN                  _isOperationInterrupted ;
 
       void _disconnect () ;
       void _removeObjects() ;
@@ -1650,6 +1651,8 @@ namespace sdbclient
                     const string &saltBase64,
                     const string &combineNonceBase64,
                     const string &clientProofBase64 ) ;
+
+      INT32 _sendInterruptOpMsg() ;
 
       friend class _sdbBase ;
       friend class _sdbCollectionSpaceImpl ;
