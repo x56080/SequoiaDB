c280c728520d1ff7314cf280e20772511cd666df
linsuqiang <linsuqiang@sequoiadb.com>
2025-01-21 14:51:54 +0800

    SEQUOIADBMAINSTREAM-8027 优化sort buffer，循环加锁放锁时，每秒睡眠 1 毫秒

--- SequoiaDB/engine/dms/dmsIndexBuilderImpl.cpp
+++ SequoiaDB/engine/dms/dmsIndexBuilderImpl.cpp
@@ -186,6 +186,8 @@ namespace engine
       INT32 rc = SDB_OK ;
       dmsExtentID startExtID = DMS_INVALID_EXTENT ;
       dmsExtentID endExtID = DMS_INVALID_EXTENT ;
+      UINT64 lastSleepTick = pmdGetDBTick() ;
+      UINT64 currentTick = 0 ;
 
       for(;;)
       {
@@ -292,6 +294,16 @@ namespace engine
                      "rc: %d", rc ) ;
 
          _mbContext->mbUnlock() ;
+
+         // When lock immediately after unlock, this thread has a great advantage
+         // to regain the lock. So sleep to allow other threads compete the lock
+         currentTick = pmdGetDBTick() ;
+         if ( pmdDBTickSpan2Time( currentTick - lastSleepTick ) >=
+              LOCK_SLEEP_INTERVAL_MS )
+         {
+            ossSleep( 1 ) ;
+            lastSleepTick = currentTick ;
+         }
       }
 
    done:
@@ -306,6 +318,8 @@ namespace engine
       #define _KEYS_PER_BATCH 10000
       INT32 rc = SDB_OK ;
       dmsIndexChangeWatcher* pWatcher = _suIndex->getIndexChangeWatcher() ;
+      UINT64 lastSleepTick = pmdGetDBTick() ;
+      UINT64 currentTick = 0 ;
 
       for (;;)
       {
@@ -359,6 +373,16 @@ namespace engine
          }
 
          _mbContext->mbUnlock() ;
+
+         // When lock immediately after unlock, this thread has a great advantage
+         // to regain the lock. So sleep to allow other threads compete the lock
+         currentTick = pmdGetDBTick() ;
+         if ( pmdDBTickSpan2Time( currentTick - lastSleepTick ) >=
+              LOCK_SLEEP_INTERVAL_MS )
+         {
+            ossSleep( 1 ) ;
+            lastSleepTick = currentTick ;
+         }
       }
 
    done:
