fbe81ce7b191bf00f9cdfd2804730bbb0bf46125
xujianhui <xujianhui@sequoiadb.com>
2025-02-12 23:19:37 +0000

    Update Transaction33400.java select timeout to 60

--- testcase_new/story/java/src/test/java/com/sequoiadb/transaction/sessionserial/Transaction33400.java
+++ testcase_new/story/java/src/test/java/com/sequoiadb/transaction/sessionserial/Transaction33400.java
@@ -137,7 +137,7 @@ public class Transaction33400 extends SdbTestBase {
                 ReplicaGroup group = sdb.getReplicaGroup( groupNames.get( 0 ) );
 
                 BasicBSONObject options = new BasicBSONObject();
-                options.put( "Seconds", 120 );
+                options.put( "Seconds", 60 );
 
                 group.reelect( options );
             }
