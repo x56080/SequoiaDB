c4a243de7dff59a7fc4f4a72ac31129c620e5ecb
xujianhui <xujianhui@sequoiadb.com>
2025-02-12 23:50:42 +0800

    SEQUOIADBMAINSTREAM-10258:增加meta文件刷盘机制

--- SequoiaDB/engine/dms/dmsStorageBase.cpp
+++ SequoiaDB/engine/dms/dmsStorageBase.cpp
@@ -2215,9 +2215,6 @@ namespace engine
 
       PD_TRACE_ENTRY ( SDB__DMSSTORAGEBASE__FINDFREESPACE ) ;
 
-      /// first invalidate the meta file
-      _pStorageInfo->_pMetaFile->invalidate() ;
-
       while ( TRUE )
       {
          totalDataPageNum = _pageNum ;
@@ -2312,6 +2309,9 @@ namespace engine
       }
 
    done:
+      /// invalidate the meta file
+      _pStorageInfo->_pMetaFile->invalidate() ;
+
       PD_TRACE_EXITRC( SDB__DMSSTORAGEBASE__FINDFREESPACE, rc ) ;
       return rc ;
    error:
@@ -2321,9 +2321,6 @@ namespace engine
    INT32 _dmsStorageBase::_releaseSpace( SINT32 pageStart, UINT16 numPages )
    {
       INT32 rc = SDB_OK ;
-
-      /// first invalidate the meta file
-      _pStorageInfo->_pMetaFile->invalidate() ;
 
       rc = _smeMgr.releasePages( pageStart, numPages ) ;
       if ( SDB_OK == rc )
@@ -2335,6 +2332,10 @@ namespace engine
             _pStatMgr->incPageRelease( numPages ) ;
          }
       }
+
+      /// invalidate the meta file
+      _pStorageInfo->_pMetaFile->invalidate() ;
+
       return rc ;
    }
 
