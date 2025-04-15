98fbbba1db080badbcb9b6851b821824ea2e88ba
fangjiabin <fangjiabin@sequoiadb.com>
2025-04-15 09:47:12 +0800

    SEQUOIADBMAINSTREAM-9385: Java 驱动添加获取引擎版本信息接口 cherry-pick v7.2 acbac59494f0ca8962485ebcc2fa47189d74b908

--- driver/java/src/main/java/com/sequoiadb/base/Sequoiadb.java
+++ driver/java/src/main/java/com/sequoiadb/base/Sequoiadb.java
@@ -61,6 +61,8 @@ public class Sequoiadb implements Closeable {
     private int currentCacheSize = 0;
     private boolean isOldVersionLobServer = false;
 
+    private DBVersion dbVersion;
+
     // cache cs/cl name
     private Map<String, Long> nameCache = new HashMap<String, Long>();
     private static ClientOptions globalClientConf = new ClientOptions();
@@ -634,6 +636,9 @@ public class Sequoiadb implements Closeable {
         byteOrder = sysInfoResponse.byteOrder();
         protocolVersion = sysInfoResponse.getPeerProtocolVersion();
         authVersion = sysInfoResponse.getAuthVersion();
+        dbVersion = new DBVersion(sysInfoResponse.getVersion(),
+                                  sysInfoResponse.getSubVersion(),
+                                  sysInfoResponse.getFixVersion());
 
         authenticate(username, password, authVersion);
         this.userName = username;
@@ -3823,5 +3828,17 @@ public class Sequoiadb implements Closeable {
             }
             return new Sequoiadb( this );
         }
+    }
+
+    /**
+     * Get the SequoiaDB version information.
+     *
+     * <p> The v3.2.7 / v3.4.1 / v5.0.1 and below SequoiaDB does not support get version information,
+     * all the version number in <code> DBVersion </code> object will be set to <code> 0 </code>.
+     *
+     * @return An object of {@link DBVersion}.
+     */
+    public DBVersion getDBVersion() {
+        return dbVersion;
     }
 }
--- driver/java/src/main/java/com/sequoiadb/message/response/SysInfoResponse.java
+++ driver/java/src/main/java/com/sequoiadb/message/response/SysInfoResponse.java
@@ -77,6 +77,18 @@ public class SysInfoResponse extends SysInfoHeader implements Response {
         }
     }
 
+    public byte getVersion() {
+        return version;
+    }
+
+    public byte getSubVersion() {
+        return subVersion;
+    }
+
+    public byte getFixVersion() {
+        return fixVersion;
+    }
+
     @Override
     public void decode(ByteBuffer in, SdbProtocolVersion protocolVersion) {
         // Java platform is BIG_ENDIAN
