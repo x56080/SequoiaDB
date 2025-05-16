4747dacd950cdd77cb8e4ca3255f6497c3ffa06d
xujianhui <xujianhui@sequoiadb.com>
2025-05-16 09:42:10 +0000

    Revert "SEQUOIADBMAINSTREAM-10288:索引平衡性能优化"

--- SequoiaDB/engine/include/ixmExtent.hpp
+++ SequoiaDB/engine/include/ixmExtent.hpp
@@ -221,7 +221,6 @@ namespace engine
       INT32 _doMergeChildren ( UINT16 pos,
                                const Ordering &order,
                                ixmIndexCB *indexCB,
-                               INT32 direction,       /// 1 for forward, -1 for backword
                                BOOLEAN &result ) ;
 
       INT32 _delExtent ( ixmIndexCB *indexCB ) ;
