e0a88575a21378bd01fb05ec58241f7873296897
xujianhui <xujianhui@sequoiadb.com>
2025-06-03 15:25:53 +0000

    Task license

--- misc/sdblicense.py
+++ misc/sdblicense.py
@@ -1,3 +1,35 @@
+/*******************************************************************************
+
+   Copyright (C) 2011-Present SequoiaDB Ltd.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+   Source File Name = sdblicense.py
+
+   Descriptive Name = N/A
+
+   Dependencies: N/A
+
+   Restrictions: N/A
+
+   Change Activity:
+   defect Date        Who Description
+   ====== =========== === ==============================================
+          11/05/2019  Xu Jianhui  Initial Draft
+
+   Last Changed =
+
+*******************************************************************************/
 #!/usr/bin/python
 import getopt
 import os
@@ -6,20 +38,19 @@ from datetime import datetime
 
 LICENSE_COMMENT_START = "/*******************************************************************************"
 LICENSE_COMMENT_END = "*******************************************************************************/"
-LICENSE = '''   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU Affero General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
+LICENSE = '''   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU Affero General Public License for more details.
+      http://www.apache.org/licenses/LICENSE-2.0
 
-   You should have received a copy of the GNU Affero General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
 '''
-COPYRIGHT = "   Copyright (C) 2011-2018 SequoiaDB Ltd.\n"
+COPYRIGHT = "   Copyright (C) 2011-Present SequoiaDB Ltd.\n"
 BLANK_LINE = "\n"
 LICENSE_AGPL = "GNU Affero General Public License"
 LICENSE_APACHE = "Apache License"
@@ -193,7 +224,7 @@ def process_file(file_path):
         return
     if suffix.lower() in CXX_FILE_SUFFIX:
         license = find_cxx_license(file_path)
-        if license in [LICENSE_AGPL, LICENSE_APACHE]:
+        if license in [LICENSE_AGPL, LICENSE_UNKNOWN]:
             replace_cxx_license(file_path)
             log("%s: %s" % (file_path, license))
         else:
