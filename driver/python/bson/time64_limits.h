72e8d222a8ff1186beb021b6718c249f4400f0dc
fangjiabin <fangjiabin@sequoiadb.com>
2025-07-23 05:32:17 -0400

    change license

--- driver/python/bson/time64_limits.h
+++ driver/python/bson/time64_limits.h
@@ -1,10 +1,21 @@
-/* 
-   Maximum and minimum inputs your system's respective time functions
-   can correctly handle.  time64.h will use your system functions if
-   the input falls inside these ranges and corresponding USE_SYSTEM_*
-   constant is defined.
-*/
+/*******************************************************************************
 
+   Copyright (C) 2011-Present SequoiaDB Ltd.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+   
+*******************************************************************************/
 #ifndef TIME64_LIMITS_H
 #define TIME64_LIMITS_H
 
