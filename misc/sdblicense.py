8313233bf2593716f11ccc98f8147f83f54db4f2
fangjiabin <fangjiabin@sequoiadb.com>
2025-07-25 10:38:11 +0800

    change license

--- misc/sdblicense.py
+++ misc/sdblicense.py
@@ -1,5 +1,4 @@
-/*******************************************************************************
-
+"""
    Copyright (C) 2011-Present SequoiaDB Ltd.
 
    Licensed under the Apache License, Version 2.0 (the "License");
@@ -29,7 +28,7 @@
 
    Last Changed =
 
-*******************************************************************************/
+"""
 #!/usr/bin/python
 import getopt
 import os
