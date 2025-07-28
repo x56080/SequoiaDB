44d6c09dea3bae3814653b08255f0b36783d3265
fangjiabin <fangjiabin@sequoiadb.com>
2025-07-28 04:24:41 -0400

    change license

--- driver/test/python/sequoiadb/query_sort.py
+++ driver/test/python/sequoiadb/query_sort.py
@@ -1,4 +1,4 @@
-/*******************************************************************************
+"""
 
    Copyright (C) 2011-Present SequoiaDB Ltd.
 
@@ -29,7 +29,7 @@
 
    Last Changed =
 
-*******************************************************************************/
+"""
 #modify: Ting YU 2015-12-11
 import pysequoiadb
 from pysequoiadb import client
