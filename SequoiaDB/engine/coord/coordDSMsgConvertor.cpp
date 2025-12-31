742ba92ba3b258c5497e430bcf70c6e23513c6b5
linsuqiang <linsuqiang@sequoiadb.com>
2025-12-31 10:24:09 +0800

    SEQUOIADBMAINSTREAM-10336 优化代码

--- SequoiaDB/engine/coord/coordDSMsgConvertor.cpp
+++ SequoiaDB/engine/coord/coordDSMsgConvertor.cpp
@@ -1150,7 +1150,7 @@ namespace engine
             ++itr ;
          }
          insertMsg->header.messageLength = offset ;
-         insertMsg->version = 1 ;
+         insertMsg->version = 0 ;
 
          pSub->setReqMsg( (MsgHeader *)buff,  PMD_EDU_MEM_THREAD ) ;
          buff = NULL ;
@@ -1304,7 +1304,7 @@ namespace engine
             ossMemcpy( (CHAR *) buff + offset, newHint.objdata(), newHint.objsize() ) ;
             offset = ossRoundUpToMultipleX( offset + newHint.objsize(), 4 ) ;
             insertMsg->header.messageLength = offset ;
-            insertMsg->version = 1 ;
+            insertMsg->version = 0 ;
          }
 
          pSub->setReqMsg( (MsgHeader *)buff,  PMD_EDU_MEM_THREAD ) ;
@@ -1383,7 +1383,7 @@ namespace engine
                   4 ) ;
             ossMemcpy( (CHAR *) buff + newBodyOffset, body, bodyLen ) ;
             newInsertMsg->header.messageLength = newBodyOffset + bodyLen ;
-            newInsertMsg->version = 1 ;
+            newInsertMsg->version = 0 ;
 
             pSub->setReqMsg( (MsgHeader *)newInsertMsg, PMD_EDU_MEM_THREAD ) ;
             buff = NULL ;
@@ -1542,6 +1542,7 @@ namespace engine
             UINT32 newMsgLen = ( (MsgHeader *)newMsg)->messageLength ;
             *(MsgHeader *)newMsg = *msg ;
             ((MsgHeader *)newMsg)->messageLength = newMsgLen ;
+            ((MsgOpUpdate *)newMsg)->version = 0 ;
             pSub->setReqMsg( (MsgHeader *)newMsg, PMD_EDU_MEM_THREAD ) ;
             newMsg = NULL ;
          }
@@ -1701,6 +1702,7 @@ namespace engine
             UINT32 newMsgLen = ( (MsgHeader *)newMsg)->messageLength ;
             *(MsgHeader *)newMsg = *msg ;
             ((MsgHeader *)newMsg)->messageLength = newMsgLen ;
+            ((MsgOpDelete *)newMsg)->version = 0 ;
             pSub->setReqMsg( (MsgHeader *)newMsg, PMD_EDU_MEM_THREAD ) ;
             newMsg = NULL ;
          }
@@ -1906,6 +1908,7 @@ namespace engine
                UINT32 newMsgLen = ( (MsgHeader *)newMsg )->messageLength ;
                *(MsgHeader *)newMsg = *msg ;
                ((MsgHeader *)newMsg)->messageLength = newMsgLen ;
+               ((MsgOpQuery *)newMsg)->version = 0 ;
                pSub->setReqMsg( (MsgHeader *)newMsg, PMD_EDU_MEM_THREAD ) ;
                newMsg = NULL ;
             }
@@ -2286,6 +2289,7 @@ namespace engine
                ((MsgOpQuery *)newMsg)->flags = pQueryMsg->flags ;
                ((MsgOpQuery *)newMsg)->numToSkip = pQueryMsg->numToSkip ;
                ((MsgOpQuery *)newMsg)->numToReturn = pQueryMsg->numToReturn ;
+               ((MsgOpQuery *)newMsg)->version = 0 ;
             }
 
             PD_LOG( PDDEBUG, "After conversion, the message for data source "
