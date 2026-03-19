fba849dd77d58ed9918e03c3181298c58a9a26f9
xujianhui <xujianhui@sequoiadb.com>
2026-03-19 06:03:57 +0000

    fix compile error

--- SequoiaDB/engine/cat/catContextAlterTask.cpp
+++ SequoiaDB/engine/cat/catContextAlterTask.cpp
@@ -4334,7 +4334,7 @@ namespace engine
          ossPoolString newActLoc = task->getActiveLocation() ;
          BOOLEAN locationExist = FALSE ;
 
-         CAT_DOMAIN_GROUP_MAP::const_iterator itr = _groupMap.begin() ;
+         CAT_DOMAIN_GROUP_MAP::iterator itr = _groupMap.begin() ;
          while ( _groupMap.end() != itr )
          {
             BSONObj groupObj ;
@@ -4437,7 +4437,7 @@ namespace engine
          // Get Location
          ossPoolString newLoc = task->getLocation() ;
 
-         CAT_DOMAIN_GROUP_MAP::const_iterator itr = _groupMap.begin() ;
+         CAT_DOMAIN_GROUP_MAP::iterator itr = _groupMap.begin() ;
          while ( _groupMap.end() != itr )
          {
             UINT32 groupID = itr->second ;
